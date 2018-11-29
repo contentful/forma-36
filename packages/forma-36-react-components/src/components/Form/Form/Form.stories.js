bb47e84442027f9790a764e6904891ec8690ac08
Mike Mitchell <mike.mitchell@contentful.com>
2018-11-29 14:34:06 +0100

    fix(Storybook): Swap Storybook state plugins

--- packages/forma-36-react-components/src/components/Form/Form/Form.stories.js
+++ packages/forma-36-react-components/src/components/Form/Form/Form.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { host } from 'storybook-host';
 import { withInfo } from '@storybook/addon-info';
-import { withState } from '@dump247/storybook-state';
+import { StateDecorator, Store } from '@sambego/storybook-state';
 import { action } from '@storybook/addon-actions';
 import { boolean, select } from '@storybook/addon-knobs';
 
@@ -12,6 +12,10 @@ import Button from '../../Button';
 import TextField from '../../TextField';
 import CheckboxField from '../../CheckboxField';
 
+const store = new Store({
+  agreeTerms: 'yes',
+});
+
 storiesOf('Components|Form', module)
   .addDecorator(
     host({
@@ -20,57 +24,56 @@ storiesOf('Components|Form', module)
       width: 400,
     }),
   )
+  .addDecorator(StateDecorator(store))
   .add(
     'default',
-    withState({ agreeTerms: 'yes' }, store =>
-      withInfo()(() => (
-        <Form
-          onSubmit={action('onSubmit')}
-          spacing={select(
-            'Spacing',
-            {
-              Default: 'default',
-              Condensed: 'condensed',
-            },
-            'default',
-          )}
-        >
-          <TextField
-            required
-            name="nameInput"
-            id="nameInput"
-            labelText="Name"
-            value="Johannes Bugiel"
-            helpText="Please enter your name"
+    withInfo()(() => (
+      <Form
+        onSubmit={action('onSubmit')}
+        spacing={select(
+          'Spacing',
+          {
+            Default: 'default',
+            Condensed: 'condensed',
+          },
+          'default',
+        )}
+      >
+        <TextField
+          required
+          name="nameInput"
+          id="nameInput"
+          labelText="Name"
+          value="Johannes Bugiel"
+          helpText="Please enter your name"
+        />
+        <TextField
+          required
+          name="emailInput"
+          id="emailInput"
+          labelText="E-Mail"
+          value="johannes.bugiel@contentful.com"
+          helpText="Please enter your mail"
+        />
+        <FieldGroup row={boolean('Field Group Row', false)}>
+          <CheckboxField
+            labelText="I agree"
+            value="yes"
+            helpText="Click if you agree"
+            onChange={e => store.set({ agreeTerms: e.target.value })}
+            checked={store.state.agreeTerms === 'yes'}
+            id="termsCheckboxYes"
           />
-          <TextField
-            required
-            name="emailInput"
-            id="emailInput"
-            labelText="E-Mail"
-            value="johannes.bugiel@contentful.com"
-            helpText="Please enter your mail"
+          <CheckboxField
+            labelText="I don't agree"
+            value="no"
+            onChange={e => store.set({ agreeTerms: e.target.value })}
+            checked={store.state.agreeTerms === 'no'}
+            helpText="Click if you don't agree"
+            id="termsCheckboxNo"
           />
-          <FieldGroup row={boolean('Field Group Row', false)}>
-            <CheckboxField
-              labelText="I agree"
-              value="yes"
-              helpText="Click if you agree"
-              onChange={e => store.set({ agreeTerms: e.target.value })}
-              checked={store.state.agreeTerms === 'yes'}
-              id="termsCheckboxYes"
-            />
-            <CheckboxField
-              labelText="I don't agree"
-              value="no"
-              onChange={e => store.set({ agreeTerms: e.target.value })}
-              checked={store.state.agreeTerms === 'no'}
-              helpText="Click if you don't agree"
-              id="termsCheckboxNo"
-            />
-          </FieldGroup>
-          <Button>Submit</Button>
-        </Form>
-      )),
-    ),
+        </FieldGroup>
+        <Button>Submit</Button>
+      </Form>
+    )),
   );
--- packages/forma-36-react-components/src/components/Modal/ModalConfirm/ModalConfirm.stories.js
+++ packages/forma-36-react-components/src/components/Modal/ModalConfirm/ModalConfirm.stories.js
@@ -4,12 +4,19 @@ import { action } from '@storybook/addon-actions';
 import { text, boolean, select } from '@storybook/addon-knobs';
 import { host } from 'storybook-host';
 import { withInfo } from '@storybook/addon-info';
-import { withState } from '@dump247/storybook-state';
+import { StateDecorator, Store } from '@sambego/storybook-state';
 
 import ModalConfirm from './ModalConfirm';
 import Modal from '../Modal';
 import Button from '../../Button';
 import TextInput from '../../TextInput';
+
+const store = new Store({
+  isShown: false,
+  isLoading: false,
+  isDisabled: true,
+  repeat: '',
+});
 
 storiesOf('Components|Modal/ModalConfirm', module)
   .addDecorator(
@@ -18,123 +25,118 @@ storiesOf('Components|Modal/ModalConfirm', module)
       cropMarks: false,
     }),
   )
+  .addDecorator(StateDecorator(store))
   .add(
     'default',
-    withState({ isShown: false }, store =>
-      withInfo()(() => (
-        <div>
-          <Button
-            buttonType="negative"
-            onClick={() => store.set({ isShown: true })}
-          >
-            Delete something
-          </Button>
-          <ModalConfirm
-            isShown={store.state.isShown}
-            title={text('title', ModalConfirm.defaultProps.title)}
-            intent={select(
-              'intent',
-              ['negative', 'positive', 'primary'],
-              ModalConfirm.defaultProps.intent,
-            )}
-            size={select(
-              'size',
-              [
-                Modal.Sizes.SMALL,
-                Modal.Sizes.MEDIUM,
-                Modal.Sizes.LARGE,
-                '300px',
-                '1500px',
-              ],
-              ModalConfirm.defaultProps.size,
-            )}
-            shouldCloseOnEscapePress={boolean(
-              'shouldCloseOnEscapePress',
-              ModalConfirm.defaultProps.shouldCloseOnEscapePress,
-            )}
-            shouldCloseOnOverlayClick={boolean(
-              'shouldCloseOnOverlayClick',
-              ModalConfirm.defaultProps.shouldCloseOnOverlayClick,
-            )}
-            confirmLabel={text(
-              'confirmLabel',
-              ModalConfirm.defaultProps.confirmLabel,
-            )}
-            cancelLabel={text(
-              'cancelLabel',
-              ModalConfirm.defaultProps.cancelLabel,
-            )}
-            isConfirmDisabled={boolean(
-              'isConfirmDisabled',
-              ModalConfirm.defaultProps.isConfirmDisabled,
-            )}
-            isConfirmLoading={boolean(
-              'isConfirmLoading',
-              ModalConfirm.defaultProps.isConfirmLoading,
-            )}
-            testId={text('testId', ModalConfirm.defaultProps.testId)}
-            confirmTestId={text(
-              'confirmTestId',
-              ModalConfirm.defaultProps.confirmTestId,
-            )}
-            cancelTextId={text(
-              'cancelTextId',
-              ModalConfirm.defaultProps.cancelTestId,
-            )}
-            onCancel={() => {
-              store.set({ isShown: false });
-              action('onCancel')();
-            }}
-            onConfirm={() => {
-              store.set({ isShown: false });
-              action('onConfirm')();
-            }}
-          >
-            You are about to delete SOMETHING. Think twice!
-          </ModalConfirm>
-        </div>
-      )),
-    ),
+    withInfo()(() => (
+      <div>
+        <Button
+          buttonType="negative"
+          onClick={() => store.set({ isShown: true })}
+        >
+          Delete something
+        </Button>
+        <ModalConfirm
+          isShown={store.state.isShown}
+          title={text('title', ModalConfirm.defaultProps.title)}
+          intent={select(
+            'intent',
+            ['negative', 'positive', 'primary'],
+            ModalConfirm.defaultProps.intent,
+          )}
+          size={select(
+            'size',
+            [
+              Modal.Sizes.SMALL,
+              Modal.Sizes.MEDIUM,
+              Modal.Sizes.LARGE,
+              '300px',
+              '1500px',
+            ],
+            ModalConfirm.defaultProps.size,
+          )}
+          shouldCloseOnEscapePress={boolean(
+            'shouldCloseOnEscapePress',
+            ModalConfirm.defaultProps.shouldCloseOnEscapePress,
+          )}
+          shouldCloseOnOverlayClick={boolean(
+            'shouldCloseOnOverlayClick',
+            ModalConfirm.defaultProps.shouldCloseOnOverlayClick,
+          )}
+          confirmLabel={text(
+            'confirmLabel',
+            ModalConfirm.defaultProps.confirmLabel,
+          )}
+          cancelLabel={text(
+            'cancelLabel',
+            ModalConfirm.defaultProps.cancelLabel,
+          )}
+          isConfirmDisabled={boolean(
+            'isConfirmDisabled',
+            ModalConfirm.defaultProps.isConfirmDisabled,
+          )}
+          isConfirmLoading={boolean(
+            'isConfirmLoading',
+            ModalConfirm.defaultProps.isConfirmLoading,
+          )}
+          testId={text('testId', ModalConfirm.defaultProps.testId)}
+          confirmTestId={text(
+            'confirmTestId',
+            ModalConfirm.defaultProps.confirmTestId,
+          )}
+          cancelTextId={text(
+            'cancelTextId',
+            ModalConfirm.defaultProps.cancelTestId,
+          )}
+          onCancel={() => {
+            store.set({ isShown: false });
+            action('onCancel')();
+          }}
+          onConfirm={() => {
+            store.set({ isShown: false });
+            action('onConfirm')();
+          }}
+        >
+          You are about to delete SOMETHING. Think twice!
+        </ModalConfirm>
+      </div>
+    )),
   )
   .add(
     'complex example',
-    withState(
-      { isShown: false, isLoading: false, isDisabled: true, repeat: '' },
-      store =>
-        withInfo()(() => (
-          <div>
-            <Button
-              buttonType="negative"
-              onClick={() => store.set({ isShown: true })}
-            >
-              Delete something
-            </Button>
-            <ModalConfirm
-              isShown={store.state.isShown}
-              intent="negative"
-              isConfirmDisabled={store.state.repeat !== 'unlock'}
-              isConfirmLoading={store.state.isLoading}
-              onCancel={() => {
-                store.set({ isShown: false });
-                action('onCancel')();
-              }}
-              onConfirm={() => {
-                store.set({ isLoading: true });
-                setTimeout(() => {
-                  store.set({ isLoading: false, isShown: false, repeat: '' });
-                }, 1500);
-                action('onConfirm')();
-              }}
-            >
-              <p>
-                Type <strong>unlock</strong> to allow confirming this modal
-              </p>
-              <TextInput
-                value={store.state.repeat}
-                onChange={e => store.set({ repeat: e.target.value })}
-              />
-            </ModalConfirm>
-          </div>
-        )),
-    ),
+    withInfo()(() => (
+      <div>
+        <Button
+          buttonType="negative"
+          onClick={() => store.set({ isShown: true })}
+        >
+          Delete something
+        </Button>
+        <ModalConfirm
+          isShown={store.state.isShown}
+          intent="negative"
+          isConfirmDisabled={store.state.repeat !== 'unlock'}
+          isConfirmLoading={store.state.isLoading}
+          onCancel={() => {
+            store.set({ isShown: false });
+            action('onCancel')();
+          }}
+          onConfirm={() => {
+            store.set({ isLoading: true });
+            setTimeout(() => {
+              store.set({ isLoading: false, isShown: false, repeat: '' });
+            }, 1500);
+            action('onConfirm')();
+          }}
+        >
+          <p>
+            Type <strong>unlock</strong> to allow confirming this modal
+          </p>
+          <TextInput
+            value={store.state.repeat}
+            onChange={e => store.set({ repeat: e.target.value })}
+          />
+        </ModalConfirm>
+      </div>
+    )),
   );
