36fb1b2cd2f25cd4ea6e7a2d5e1f59df3bf40cdf
Johannes Bugiel <jo.bugiel@gmail.com>
2018-11-29 16:28:21 +0100

    feat(List): Added List and List Item Component

---
+++ packages/forma-36-react-components/src/components/List/List/List.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { axe } from 'jest-axe';
+import List from './List';
+import ListItem from '../ListItem';
+
+it('renders the component', () => {
+  const output = shallow(
+    <List>
+      <ListItem>Item</ListItem>
+    </List>,
+  );
+
+  expect(output).toMatchSnapshot();
+});
+
+it('renders the component with an additional class name', () => {
+  const output = shallow(
+    <List extraClassNames="my-extra-class">
+      <ListItem>Item</ListItem>
+    </List>,
+  );
+
+  expect(output).toMatchSnapshot();
+});
+
+it('has no a11y issues', async () => {
+  const output = mount(
+    <List>
+      <ListItem>Item</ListItem>
+    </List>,
+  ).html();
+  const results = await axe(output);
+
+  expect(results).toHaveNoViolations();
+});
---
+++ packages/forma-36-react-components/src/components/List/ListItem/ListItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+import styles from './ListItem.css';
+import List from '../List';
+
+class ListItem extends React.Component {
+  static propTypes = {
+    extraClassNames: PropTypes.string,
+    children: PropTypes.node.isRequired,
+    testId: PropTypes.string,
+  };
+
+  static defaultProps = {
+    extraClassNames: undefined,
+    testId: 'cf-ui-list-item',
+  };
+
+  render() {
+    const { extraClassNames, children, testId, ...otherProps } = this.props;
+
+    const classNames = cn(styles.ListItem, extraClassNames, {
+      [styles['ListItem--nested-list']]: this.props.children.type === List,
+    });
+
+    return (
+      <li className={classNames} data-test-id={testId} {...otherProps}>
+        {children}
+      </li>
+    );
+  }
+}
+
+export default ListItem;
