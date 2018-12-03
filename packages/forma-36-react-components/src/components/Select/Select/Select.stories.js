f691eebff768c216abfe7f86579298bad280a0df
Mike Mitchell <mike.mitchell@contentful.com>
2018-12-03 13:53:34 +0100

    fix(Storybook): Use correct action add-on

--- packages/forma-36-react-components/src/components/Select/Select/Select.stories.js
+++ packages/forma-36-react-components/src/components/Select/Select/Select.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { storiesOf, action } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { text, boolean, select } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
 import { host } from 'storybook-host';
