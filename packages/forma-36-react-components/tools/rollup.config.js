a0eb187b86f073920efe1ad340befce3f0785d81
Alexander Suevalov <suevalov.work@gmail.com>
2018-12-04 14:27:42 +0100

    New UMD configuration. Required correct peer React version. (#26)

--- packages/forma-36-react-components/tools/rollup.config.js
+++ packages/forma-36-react-components/tools/rollup.config.js
@@ -2,31 +2,51 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import localResolve from 'rollup-plugin-local-resolve';
 import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import babel from 'rollup-plugin-babel';
 import path from 'path';
 
-const config = {
-  input: path.resolve(__dirname, '../dist/index.js'),
-  output: [
-    {
-      file: path.resolve(__dirname, '../dist', 'index.umd.js'),
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
+function getConfig(environment) {
+  return {
+    input: path.resolve(__dirname, '../dist/index.js'),
+    output: [
+      {
+        file: path.resolve(
+          __dirname,
+          '../dist',
+          'umd',
+          `forma-36-react-components.${environment}.min.js`,
+        ),
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        format: 'umd',
+        name: 'Forma36',
+        exports: 'named',
       },
-      format: 'umd',
-      name: 'UIComponentLibrary',
-      exports: 'named',
-    },
-  ],
-  external: ['react', 'react-dom'],
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    localResolve(),
-    resolve(),
-    commonjs(),
-  ],
-};
+    ],
+    external: ['react', 'react-dom'],
+    plugins: [
+      environment === 'production'
+        ? babel({
+            babelrc: false,
+            plugins: [
+              require.resolve('babel-plugin-transform-react-remove-prop-types'),
+            ],
+          })
+        : null,
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(environment),
+      }),
+      localResolve(),
+      resolve(),
+      commonjs(),
+      terser(),
+    ].filter(_ => _),
+  };
+}
+
+const config = [getConfig('development'), getConfig('production')];
 
 export default config;
