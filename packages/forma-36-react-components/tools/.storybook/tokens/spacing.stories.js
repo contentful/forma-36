09c1ac398dfd5cf6d2c2ef60512fb80a0bae517b
Mike Mitchell <mike.mitchell@contentful.com>
2019-01-19 14:29:29 +0100

    feature(Tokens): Use F36 tokens for spacing

--- packages/forma-36-react-components/tools/.storybook/tokens/spacing.stories.js
+++ packages/forma-36-react-components/tools/.storybook/tokens/spacing.stories.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { host } from 'storybook-host';
 import { withInfo } from '@storybook/addon-info';
-import { spacingUnits } from './tokens';
-
+import tokens from '@contentful/forma-36-tokens/dist/json/spacing';
 import DocPage from '../components/DocPage/DocPage';
 import Table from '../../../src/components/Table/Table';
 import TableHead from '../../../src/components/Table/TableHead';
 import TableBody from '../../../src/components/Table/TableBody';
 import TableCell from '../../../src/components/Table/TableCell';
 import TableRow from '../../../src/components/Table/TableRow';
+
+console.log(tokens);
 
 storiesOf('Tokens|Spacing', module)
   .addDecorator(
@@ -27,36 +28,33 @@ storiesOf('Tokens|Spacing', module)
         <Table style={{ width: '100%' }}>
           <TableHead>
             <TableRow>
-              <TableCell>Name</TableCell>
               <TableCell>Token</TableCell>
-              <TableCell>px</TableCell>
-              <TableCell>rem</TableCell>
+              <TableCell>Value (px)</TableCell>
+              <TableCell>Value (rem)</TableCell>
               <TableCell>Example</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {spacingUnits.map(unit => {
-              const sizeInRem = (1 * parseInt(unit.px, 10)) / 16;
+            {Object.keys(tokens).map(token => {
+              const value = tokens[token];
+              const valuePx = 1 * parseFloat(value) * 16;
 
               return (
-                <TableRow key={unit.cssVar}>
+                <TableRow key={token}>
                   <TableCell style={{ verticalAlign: 'middle' }}>
-                    <strong>{unit.name}</strong>
+                    <strong>{token}</strong>
                   </TableCell>
                   <TableCell style={{ verticalAlign: 'middle' }}>
-                    <code>{unit.cssVar}</code>
+                    <code>{valuePx}px</code>
                   </TableCell>
                   <TableCell style={{ verticalAlign: 'middle' }}>
-                    <code>{unit.px}</code>
-                  </TableCell>
-                  <TableCell style={{ verticalAlign: 'middle' }}>
-                    <code>{`${sizeInRem}rem`}</code>
+                    <code>{value}</code>
                   </TableCell>
                   <TableCell style={{ verticalAlign: 'middle' }}>
                     <div
                       style={{
-                        width: `${sizeInRem}rem`,
-                        height: `${sizeInRem}rem`,
+                        width: value,
+                        height: value,
                         background: '#3c80cf',
                       }}
                     />
