86ae125a1fedeb643a1f6ff7d8661492f25d9663
Mike Mitchell <mike.mitchell@contentful.com>
2019-02-11 15:52:09 +0100

    docs(TextLink): Add documentation

--- packages/forma-36-react-components/src/components/TextLink/TextLink.stories.js
+++ packages/forma-36-react-components/src/components/TextLink/TextLink.stories.js
@@ -4,39 +4,42 @@ import { text, select, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { host } from 'storybook-host';
 import { withInfo } from '@storybook/addon-info';
+import { withNotes } from '@storybook/addon-notes';
 
 import TextLink from './TextLink';
+import notes from './TextLink.md';
 import { iconName } from '../Icon/constants';
 
 storiesOf('Components|TextLink', module)
+  .addDecorator((story, context) => withInfo()(story)(context))
+  .addDecorator((story, context) =>
+    withNotes({ markdown: notes })(story)(context),
+  )
   .addDecorator(
     host({
       align: 'center middle',
       cropMarks: false,
     }),
   )
-  .add(
-    'default',
-    withInfo()(() => (
-      <TextLink
-        href={text('Href', '')}
-        onClick={action('onClick')}
-        linkType={select(
-          'Link Type',
-          {
-            'Primary (default)': 'primary',
-            Positive: 'positive',
-            Negative: 'negative',
-            Secondary: 'secondary',
-            Muted: 'muted',
-          },
-          'primary',
-        )}
-        disabled={boolean('Disabled', false)}
-        extraClassNames={text('Extra Class Names', '')}
-        icon={select('Icon', ['', ...Object.keys(iconName)], undefined)}
-      >
-        {text('Label (children)', 'Text Link Label')}
-      </TextLink>
-    )),
-  );
+  .add('default', () => (
+    <TextLink
+      href={text('Href', '')}
+      onClick={action('onClick')}
+      linkType={select(
+        'Link Type',
+        {
+          'Primary (default)': 'primary',
+          Positive: 'positive',
+          Negative: 'negative',
+          Secondary: 'secondary',
+          Muted: 'muted',
+        },
+        'primary',
+      )}
+      disabled={boolean('Disabled', false)}
+      extraClassNames={text('Extra Class Names', '')}
+      icon={select('Icon', ['', ...Object.keys(iconName)], undefined)}
+    >
+      {text('Label (children)', 'Text Link Label')}
+    </TextLink>
+  ));
