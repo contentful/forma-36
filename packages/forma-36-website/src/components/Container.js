6d2093dffb9c9487f4eef607530ab1effe00d72a
Johannes Bugiel <jo.bugiel@gmail.com>
2019-02-12 17:24:23 +0100

    docs(Website): Added basic structure for doc app

--- packages/forma-36-website/src/components/Container.js
+++ packages/forma-36-website/src/components/Container.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import './Container.css';
+import PropTypes from 'prop-types';
+import tokens from '@contentful/forma-36-tokens';
+import { css } from 'emotion';
 
-const Container = ({ children }) => <div className="container">{children}</div>;
+const styles = {
+  container: css`
+    min-height: 100vh;
+    flex: 1;
+    padding: ${tokens.spacing2Xl} ${tokens.spacingXl};
+  `,
+};
+
+const Container = ({ children }) => (
+  <div className={styles.container}>{children}</div>
+);
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 export default Container;
