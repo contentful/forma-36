bd5bce87386650650645400b6bc070f05f51144e
Alexander Suevalov <suevalov.work@gmail.com>
2019-03-08 11:19:50 +0100

    chore(ValidationMessage): convert ValidationMessage to TypeScript

--- packages/forma-36-react-components/src/components/ValidationMessage/ValidationMessage.stories.tsx
+++ packages/forma-36-react-components/src/components/ValidationMessage/ValidationMessage.stories.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
-import { withInfo } from '@storybook/addon-info';
 
 import ValidationMessage from './ValidationMessage';
 
 storiesOf('Components|ValidationMessage', module)
-  .addDecorator(withInfo)
+  .addParameters({
+    propTypes: ValidationMessage['__docgenInfo'],
+  })
   .add('default', () => (
     <ValidationMessage extraClassNames={text('Extra Class Names', '')}>
       {text('Message', 'This field is required')}
