c5010ece91ab61f2de5d1c35f3629c27aa77caf2
Alexander Suevalov <suevalov.work@gmail.com>
2019-03-10 10:23:43 +0100

    chore(CopyButton): Convert CopyButton to TypeScript

--- packages/forma-36-react-components/src/components/CopyButton/CopyButton.stories.tsx
+++ packages/forma-36-react-components/src/components/CopyButton/CopyButton.stories.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
-import { withInfo } from '@storybook/addon-info';
 
 import CopyButton from './CopyButton';
 
 storiesOf('Components|CopyButton', module)
-  .addDecorator(withInfo)
+  .addParameters({
+    propTypes: CopyButton['__docgenInfo'],
+  })
   .add('default', () => (
     <div>
       <CopyButton
