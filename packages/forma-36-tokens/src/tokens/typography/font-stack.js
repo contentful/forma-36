b4e95601d968b3f9228faba5e6d7344961b1c145
Alex Suevalov <alex.suevalov@contentful.com>
2019-03-14 11:58:36 +0100

    fix: More concise fonts fallback list

--- packages/forma-36-tokens/src/tokens/typography/font-stack.js
+++ packages/forma-36-tokens/src/tokens/typography/font-stack.js
@@ -1,5 +1,6 @@
 const fontStack = {
-  'font-stack-primary': "'Avenir Next W01', '-apple-system', 'BlinkMacSystemTypography', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif'",
+  'font-stack-primary':
+    "'Avenir Next W01', -apple-system, BlinkMacSystemTypography, 'Segoe UI', Cantarell, Helvetica, Arial, sans-serif",
   'font-stack-monospace': "'Menlo', 'Andale mono', monospace",
 };
 
