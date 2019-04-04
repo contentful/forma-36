d8d776d633dcc637713664df6ea772d4eaaab06b
Johannes Bugiel <jo.bugiel@gmail.com>
2019-04-04 16:04:47 +0200

    fix(CardDragHandle): fixed lint issues

--- packages/forma-36-react-components/src/components/Card/CardDragHandle/CardActions.stories.tsx
+++ packages/forma-36-react-components/src/components/Card/CardDragHandle/CardActions.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, boolean } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
 
 import CardDragHandle from './CardDragHandle';
 
--- packages/forma-36-react-components/src/components/Card/CardDragHandle/CardDragHandle.test.tsx
+++ packages/forma-36-react-components/src/components/Card/CardDragHandle/CardDragHandle.test.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { axe } from 'jest-axe';
 import CardDragHandle from './CardDragHandle';
-import DropdownListItem from '../../Dropdown/DropdownListItem';
-import DropdownList from '../../Dropdown/DropdownList';
 
 it('renders the component', () => {
   const output = shallow(<CardDragHandle>CardDragHandle</CardDragHandle>);
