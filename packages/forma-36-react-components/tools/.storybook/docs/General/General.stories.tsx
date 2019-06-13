39f9fad338e0b1649ac741183874748dce6f109b
Mike Mitchell <mike.mitchell@contentful.com>
2019-06-13 15:40:38 +0200

    chore(Storybook): Bump Storybook to v5.1.4

--- packages/forma-36-react-components/tools/.storybook/docs/General/General.stories.tsx
+++ packages/forma-36-react-components/tools/.storybook/docs/General/General.stories.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MarkdownIt from 'markdown-it';
-
+import Notes from './General.md';
 import DocPage from '../../components/DocPage/DocPage';
 
-const Notes = require('./General.md');
 const md = new MarkdownIt();
 
 storiesOf('Documentation|General', module).add('General', () => (
