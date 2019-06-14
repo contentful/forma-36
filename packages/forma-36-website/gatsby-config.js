3244b5ab9c5e0e7b49e45d7e4544046c8f99316c
Mike Mitchell <mike.mitchell@contentful.com>
2019-06-14 15:59:21 +0200

    chore(Website): Add GA

--- packages/forma-36-website/gatsby-config.js
+++ packages/forma-36-website/gatsby-config.js
@@ -173,6 +173,14 @@ module.exports = {
         icon: 'src/images/forma-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-40725207-15',
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
