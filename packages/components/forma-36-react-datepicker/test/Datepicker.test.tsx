2588843500fe21774ae4f3ddd6f0933b92fb29eb
Johannes Bugiel <jo.bugiel@gmail.com>
2019-11-05 09:28:35 +0100

    test(Datepicker): Add tests for date selection

--- packages/components/forma-36-react-datepicker/test/Datepicker.test.tsx
+++ packages/components/forma-36-react-datepicker/test/Datepicker.test.tsx
@@ -1,11 +1,56 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+import 'jest-extended';
 import Datepicker from '../src/Datepicker';
 
 describe('Datepicker', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Datepicker labelText="Date" />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  afterEach(cleanup);
+
+  const build = () => {
+    const props = {
+      labelText: 'label text',
+      value: new Date('2017-01-01'),
+      onChange: jest.fn(),
+    };
+    return [render(<Datepicker {...props} />), props];
+  };
+
+  it('renders datepicker on focus', () => {
+    const [renderResult] = build();
+    console.log(renderResult);
+    // @ts-ignore
+    const dateInput = renderResult.container.querySelector(
+      '[data-test-id="date-input"]'
+    );
+    expect(
+      getDatepicker(renderResult).classList.contains('is-hidden')
+    ).toBeTruthy();
+
+    fireEvent.focus(dateInput);
+
+    expect(
+      getDatepicker(renderResult).classList.contains('is-hidden')
+    ).toBeFalsy();
+
+    const yearSelect = getDatepicker(renderResult).querySelector(
+      '.pika-select-year'
+    );
+    fireEvent.focus(yearSelect);
+
+    expect(
+      getDatepicker(renderResult).classList.contains('is-hidden')
+    ).toBeFalsy();
+
+    fireEvent.blur(dateInput);
+
+    expect(
+      getDatepicker(renderResult).classList.contains('is-hidden')
+    ).toBeTruthy();
   });
 });
+// @ts-ignore
+function getDatepicker(renderResult) {
+  return renderResult.baseElement.getRootNode().querySelector('.pika-single');
+}
