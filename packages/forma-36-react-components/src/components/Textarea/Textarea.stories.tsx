6916cee5827bab239bcc6e54abe5ffddb7a49e05
Mike Mitchell <mike.mitchell@contentful.com>
2019-12-13 14:18:18 +0100

    fix(Textarea): Expose ref on textarea

--- packages/forma-36-react-components/src/components/Textarea/Textarea.stories.tsx
+++ packages/forma-36-react-components/src/components/Textarea/Textarea.stories.tsx
@@ -4,6 +4,7 @@ import { text, boolean, select, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
 import Textarea from './Textarea';
+import Button from './../Button';
 
 storiesOf('Components|TextArea', module)
   .addParameters({
@@ -34,4 +35,42 @@ storiesOf('Components|TextArea', module)
       rows={number('rows', 2)}
       willBlurOnEsc={boolean('willBlurOnEsc', true)}
     />
-  ));
+  ))
+  .add('Controlling focus via ref', () => {
+    const textareaRef = React.createRef<HTMLTextAreaElement>();
+
+    return (
+      <React.Fragment>
+        <Textarea
+          className={text('className', '')}
+          name="someInput"
+          id="someInput"
+          error={boolean('error', false)}
+          maxLength={number('maxLength', 50)}
+          required={boolean('required', false)}
+          width={select(
+            'width',
+            {
+              'Full (default)': 'full',
+              large: 'large',
+              medium: 'medium',
+              small: 'small',
+            },
+            'full',
+          )}
+          onChange={action('onChange')}
+          onBlur={action('onBlur')}
+          disabled={boolean('disabled', false)}
+          value={text('value', '123456')}
+          rows={number('rows', 2)}
+          willBlurOnEsc={boolean('willBlurOnEsc', true)}
+          textareaRef={textareaRef}
+        />
+        <Button
+          onClick={() => (textareaRef.current as HTMLTextAreaElement).focus()}
+        >
+          Focus Textarea with ref
+        </Button>
+      </React.Fragment>
+    );
+  });
