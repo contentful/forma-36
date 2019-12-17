3df2cfa1092319880648763b445fca177d0a866f
Tanya Bessonova <tatiana.bessonova@contentful.com>
2019-12-17 09:51:11 +0100

    docs(Icon): Added documentation for Icon (#336)

--- packages/forma-36-website/gatsby-config.js
+++ packages/forma-36-website/gatsby-config.js
@@ -116,6 +116,10 @@ module.exports = {
             link: '/components/help-text/',
           },
           {
+            name: 'Icon',
+            link: '/components/icon/',
+          },
+          {
             name: 'Note',
             link: '/components/note/',
           },
