dc5d7e9d70ea15c3864372bb4ec1607f69d3aea2
Aris Plakias <aris.plakias@gmail.com>
2020-01-03 16:18:48 +0100

    feat(autocomplete): port Autocomplete component from user_interface
    Port Autocomplete component from user_interface into a standalone package similar to date and time picker

---
+++ packages/forma-36-react-components/src/components/Autocomplete/Autocomplete.test.tsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import {
+  render,
+  cleanup,
+  fireEvent,
+  within,
+  configure,
+} from '@testing-library/react';
+
+import Autocomplete from '../Autocomplete';
+
+type Item = { label: string; id: string };
+
+const items: Item[] = [
+  { label: 'Jalapeño', id: 'jalapeno' },
+  { label: 'Lime', id: 'lime' },
+  { label: 'Avocado', id: 'avocado' },
+  { label: 'Coriander', id: 'coriander' },
+];
+
+configure({ testIdAttribute: 'data-test-id' });
+
+describe('Autocomplete', () => {
+  let onChangeFn: any;
+  let onQueryChangeFn: any;
+
+  afterEach(cleanup);
+
+  const build = ({ placeholder = '', width = 'large' }: any) => {
+    onChangeFn = jest.fn();
+    onQueryChangeFn = jest.fn();
+
+    return render(
+      <Autocomplete
+        items={items}
+        onChange={onChangeFn}
+        onQueryChange={onQueryChangeFn}
+        placeholder={placeholder}
+        width={width}
+      >
+        {(options: Item[]) =>
+          options.map(option => (
+            <span key={option.id} data-testid="option-content">
+              {option.label}
+            </span>
+          ))
+        }
+      </Autocomplete>,
+    );
+  };
+
+  describe('search input', () => {
+    it('displays the input field', () => {
+      const { getByTestId } = build({});
+      const input = getByTestId('autocomplete.input');
+      expect(input).toBeInTheDocument();
+    });
+
+    it('calls the onQueryChange callback', () => {
+      const { getByTestId } = build({});
+      const input = getByTestId('autocomplete.input');
+      fireEvent.change(input, { target: { value: 'foo' } });
+      expect(onQueryChangeFn).toHaveBeenCalledWith('foo');
+    });
+
+    it('displays the placeholder text', () => {
+      const { getByTestId } = build({ placeholder: 'Search' });
+      const input = getByTestId('autocomplete.input');
+      expect(input).toHaveAttribute('placeholder', 'Search');
+    });
+
+    it('shows the dropdown', () => {
+      const { getByTestId } = build({});
+      const input = getByTestId('autocomplete.input');
+      fireEvent.keyDown(input, { keyCode: 40 });
+      const dropdown = getByTestId('autocomplete.dropdown-list');
+      expect(dropdown).toBeVisible();
+    });
+  });
+
+  describe('dropdown', () => {
+    let input: any, dropdown, options: any;
+
+    beforeEach(() => {
+      const { getByTestId } = build({});
+      input = getByTestId('autocomplete.input');
+      fireEvent.keyDown(input, { keyCode: 40 });
+      dropdown = getByTestId('autocomplete.dropdown-list');
+      options = within(dropdown).getAllByTestId(
+        'autocomplete.dropdown-list-item',
+      );
+    });
+
+    it('displays the list of items', () => {
+      expect(options).toHaveLength(items.length);
+    });
+
+    it('calls the onChange and onQueryChange callbacks when the selecting an item with the enter key', () => {
+      fireEvent.keyDown(input, { keyCode: 13 }); // select the first item
+      expect(onChangeFn).toHaveBeenCalledWith(items[0]);
+      expect(onQueryChangeFn).toHaveBeenCalledWith('');
+    });
+
+    it('calls the onChange and onQueryChange callbacks when selecting an item with a mouse click', () => {
+      const button = within(options[1]).getByTestId(
+        'cf-ui-dropdown-list-item-button',
+      );
+      fireEvent.click(button); // select the second item
+      expect(onChangeFn).toHaveBeenCalledWith(items[1]);
+      expect(onQueryChangeFn).toHaveBeenCalledWith('');
+    });
+
+    it('dismisses the dropdown when selecting with the enter key', () => {
+      fireEvent.keyDown(input, { keyCode: 13 });
+      const dropdown = within(document as any).queryByTestId(
+        'autocomplete.dropdown-list',
+      );
+      expect(dropdown).toBeNull();
+    });
+  });
+});
