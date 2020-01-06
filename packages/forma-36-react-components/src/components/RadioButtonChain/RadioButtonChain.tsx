c79e264f3cbd98d478faf310df1c90c3c152bbae
Daniel Vasylenko <danielspr3@gmail.com>
2020-01-06 16:35:04 +0100

    feat(RadioButtonChain): Styling for  a chain of multiple/single item(s)

--- packages/forma-36-react-components/src/components/RadioButtonChain/RadioButtonChain.tsx
+++ packages/forma-36-react-components/src/components/RadioButtonChain/RadioButtonChain.tsx
@@ -6,7 +6,7 @@ const styles = require('./RadioButtonChain.css');
 
 export type RadioButtonChainProps = {
   className?: string;
-  children: React.ReactElement<RadioButtonFieldProps>;
+  children: React.ReactElement<RadioButtonFieldProps>[];
   testId?: string;
 } & typeof defaultProps;
 
@@ -20,19 +20,32 @@ export class RadioButtonChain extends Component<RadioButtonChainProps> {
   render() {
     const { className, children, testId, ...otherProps } = this.props;
 
+    const chainClassName = cn(
+      styles.RadioButtonChain,
+      children.length <= 2 ? styles.RadioButtonChain__relative : null,
+      className,
+    );
+
     return (
-      <div
-        className={cn(styles.RadioButtonChain, className)}
-        data-test-id={testId}
-        {...otherProps}
-      >
+      <div className={chainClassName} data-test-id={testId} {...otherProps}>
         {Children.map(
           children,
           (child: React.ReactElement<RadioButtonFieldProps>) =>
             React.cloneElement(child, {
               className: cn(
                 child.props.className,
+                // make radiobutton vertically aligned
                 styles.RadioButtonChain__item,
+                // if more than 2 items, we set flex-grow on 1st and last item to 0, while all in the middle are flex-grow: 1
+                // to ensure that the chain is fluid and it starts and ends on the opposite edges of the container block
+                children.length > 2
+                  ? styles.RadioButtonChain__item__relative
+                  : null,
+                // if 1 child, it will be just rendered as a standard radiobutton field
+                // otherwise - it will spread it's wings to it's neighbour to the right
+                children.length > 1
+                  ? styles.RadioButtonChain__item__link
+                  : null,
               ),
             }),
         )}
