76b723ef7d3a11b118f21d6702fc46fb35052364
Moe Shaaban <info@mshaaban.com>
2020-02-04 17:51:09 +0100

    fix: update gatsby image processor, and use full width images

--- packages/forma-36-website/gatsby-config.js
+++ packages/forma-36-website/gatsby-config.js
@@ -201,8 +201,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 800,
-              sizeByPixelDensity: true,
+              maxWidth: 912
             },
           },
         ],
