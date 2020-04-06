93d5f1e561937b8f804981be24c226161305736b
Moe Shaaban <info@mshaaban.com>
2020-04-06 18:29:50 +0200

    fix(grid tests): use axe helper

--- packages/forma-36-react-components/src/components/Grid/Grid.test.tsx
+++ packages/forma-36-react-components/src/components/Grid/Grid.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { axe } from 'jest-axe';
+import axe from '../../utils/axeHelper';
 import Grid from './Grid';
 
 it('renders the component', () => {
@@ -26,28 +26,28 @@ describe('should have correct styles', () => {
   it('rowGap should be 0 by default', () => {
     const output = shallow(<Grid>Grid</Grid>);
     const outputStyles = output.get(0).props.style;
-    expect(outputStyles).toMatchObject({gridRowGap: 0});
-  })
-  
+    expect(outputStyles).toMatchObject({ gridRowGap: 0 });
+  });
+
   it('columnGap should be 1rem by default', () => {
     const output = shallow(<Grid>Grid</Grid>);
     const outputStyles = output.get(0).props.style;
-    expect(outputStyles).toMatchObject({gridColumnGap: '1rem'});
-  })
-  
+    expect(outputStyles).toMatchObject({ gridColumnGap: '1rem' });
+  });
+
   it('should has correct numbers of columns', () => {
     const output = shallow(<Grid columns={12}>Grid</Grid>);
     const outputStyles = output.get(0).props.style;
     expect(outputStyles).toMatchObject({
-      gridTemplateColumns: 'repeat(12, minmax(0, 1fr)'
-    })
-  })
+      gridTemplateColumns: 'repeat(12, minmax(0, 1fr)',
+    });
+  });
 
   it('should has correct numbers of rows', () => {
     const output = shallow(<Grid rows={12}>Grid</Grid>);
     const outputStyles = output.get(0).props.style;
     expect(outputStyles).toMatchObject({
-      gridTemplateRows: 'repeat(12, minmax(0, 1fr)'
-    })
-  })
-})
+      gridTemplateRows: 'repeat(12, minmax(0, 1fr)',
+    });
+  });
+});
--- packages/forma-36-react-components/src/components/Grid/GridItem/GridItem.test.tsx
+++ packages/forma-36-react-components/src/components/Grid/GridItem/GridItem.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { axe } from 'jest-axe';
+import axe from '../../../utils/axeHelper';
 import GridItem from './GridItem';
 
 it('renders the component', () => {
@@ -10,7 +10,9 @@ it('renders the component', () => {
 });
 
 it('renders the component with an additional class name', () => {
-  const output = shallow(<GridItem className="my-extra-class">Grid Item</GridItem>);
+  const output = shallow(
+    <GridItem className="my-extra-class">Grid Item</GridItem>,
+  );
 
   expect(output).toMatchSnapshot();
 });
@@ -23,34 +25,33 @@ it('has no a11y issues', async () => {
 });
 
 describe('should have correct styles', () => {
-    
   it('should result to gridColumn', () => {
     const output = shallow(<GridItem columnStart={3} columnEnd={6} />);
     const outputStyles = output.get(0).props.style;
-    expect(outputStyles).toMatchObject({gridColumn: '3 / 6'});
-  })
+    expect(outputStyles).toMatchObject({ gridColumn: '3 / 6' });
+  });
 
   it('columnStart should have the correct value', () => {
     const output = shallow(<GridItem columnStart={3} />);
     const outputStyles = output.get(0).props.style;
-    expect(outputStyles).toMatchObject({gridColumnStart: 3});
-  })
+    expect(outputStyles).toMatchObject({ gridColumnStart: 3 });
+  });
 
   it('columnEnd should have the correct value', () => {
     const output = shallow(<GridItem columnEnd={4} />);
     const outputStyles = output.get(0).props.style;
-    expect(outputStyles).toMatchObject({gridColumnEnd: 4});
-  })
+    expect(outputStyles).toMatchObject({ gridColumnEnd: 4 });
+  });
 
   it('rowStart should have the correct value', () => {
     const output = shallow(<GridItem rowStart={3} />);
     const outputStyles = output.get(0).props.style;
-    expect(outputStyles).toMatchObject({gridRowStart: 3});
-  })
+    expect(outputStyles).toMatchObject({ gridRowStart: 3 });
+  });
 
   it('rowEnd should have the correct value', () => {
     const output = shallow(<GridItem rowEnd={4} />);
     const outputStyles = output.get(0).props.style;
-    expect(outputStyles).toMatchObject({gridRowEnd: 4});
-  })
-})
+    expect(outputStyles).toMatchObject({ gridRowEnd: 4 });
+  });
+});
