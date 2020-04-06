c307082bb3933980962288adf07fa632da883213
Moe Shaaban <info@mshaaban.com>
2020-04-06 18:18:39 +0200

    fix(grid stories): fix the examples counter bug

--- packages/forma-36-react-components/src/components/Grid/Grid.stories.tsx
+++ packages/forma-36-react-components/src/components/Grid/Grid.stories.tsx
@@ -10,61 +10,66 @@ import notes from './Grid.md';
 const gridKnobsId = 'Grid Props';
 const spacingValues = [
   'spacing2Xs',
-  'spacingXs', 
-  'spacingS', 
-  'spacingM', 
-  'spacingL', 
-  'spacingXl', 
-  'spacing2Xl', 
-  'spacing3Xl', 
-  'spacing4Xl'
+  'spacingXs',
+  'spacingS',
+  'spacingM',
+  'spacingL',
+  'spacingXl',
+  'spacing2Xl',
+  'spacing3Xl',
+  'spacing4Xl',
 ];
 
 const styles = {
   demoBox: {
-    backgroundColor: tokens.colorContrastLight
+    backgroundColor: tokens.colorContrastLight,
   },
   demoBoxDark: {
-    backgroundColor: tokens.colorContrastDark
+    backgroundColor: tokens.colorContrastDark,
   },
-}
+};
 
-const DemoBox = ({times, id}: {times?: number; id?: string}) => {
-  if(times) {
+const DemoBox = ({ times, id }: { times?: number; id?: string }) => {
+  if (times) {
     let result = [];
-    for (let i=0; i<=times; i++) {
-      result.push(<GridItem key={`${id}-${i}`} style={styles.demoBox} />)
+    for (let i = 0; i < times; i++) {
+      result.push(<GridItem key={`${id}-${i}`} style={styles.demoBox} />);
     }
-    return <>{result}</>
+    return <>{result}</>;
   }
-  return <GridItem style={styles.demoBox}></GridItem>
-}
+  return <GridItem style={styles.demoBox}></GridItem>;
+};
 
 storiesOf('Components|Grid', module)
   .addParameters({
     propTypes: Grid['__docgenInfo'],
   })
-  .add('default', () => {
-    const knobs = {
-      columns: number('columns', 6, '', gridKnobsId),
-      rows: number('rows', 4, '', gridKnobsId),
-      columnGap: select('columnGap', spacingValues, 'spacingXs', gridKnobsId),
-      rowGap: select('rowGap', spacingValues, 'spacingXs', gridKnobsId),
-      className: text('className', '', gridKnobsId),
-      exampleBoxes: number('Example Items', 23),
-      exampleGridHeight: text('Example Grid Height', '90vh')
-    };
+  .add(
+    'default',
+    () => {
+      const knobs = {
+        columns: number('columns', 6, '', gridKnobsId),
+        rows: number('rows', 4, '', gridKnobsId),
+        columnGap: select('columnGap', spacingValues, 'spacingXs', gridKnobsId),
+        rowGap: select('rowGap', spacingValues, 'spacingXs', gridKnobsId),
+        className: text('className', '', gridKnobsId),
+        exampleBoxes: number('Example Items', 24),
+        exampleGridHeight: text('Example Grid Height', '90vh'),
+      };
 
-    return (
-      <Grid 
-        columns={knobs.columns}
-        rows={knobs.rows}
-        columnGap={knobs.columnGap}
-        rowGap={knobs.rowGap}
-        flow='row'
-        className={knobs.className}
-        style={{height: knobs.exampleGridHeight}}>
+      return (
+        <Grid
+          columns={knobs.columns}
+          rows={knobs.rows}
+          columnGap={knobs.columnGap}
+          rowGap={knobs.rowGap}
+          flow="row"
+          className={knobs.className}
+          style={{ height: knobs.exampleGridHeight }}
+        >
           <DemoBox id="g" times={knobs.exampleBoxes} />
-      </Grid>
-    )
-  }, {notes})
+        </Grid>
+      );
+    },
+    { notes },
+  );
--- packages/forma-36-react-components/src/components/Grid/GridItem/GridItem.stories.tsx
+++ packages/forma-36-react-components/src/components/Grid/GridItem/GridItem.stories.tsx
@@ -11,79 +11,89 @@ const gridKnobsId = 'Grid Props';
 const gridItemKnobsId = 'GridItem Props';
 const spacingValues = [
   'spacing2Xs',
-  'spacingXs', 
-  'spacingS', 
-  'spacingM', 
-  'spacingL', 
-  'spacingXl', 
-  'spacing2Xl', 
-  'spacing3Xl', 
-  'spacing4Xl'
+  'spacingXs',
+  'spacingS',
+  'spacingM',
+  'spacingL',
+  'spacingXl',
+  'spacing2Xl',
+  'spacing3Xl',
+  'spacing4Xl',
 ];
 
 const styles = {
   demoBox: {
-    backgroundColor: tokens.colorContrastLight
+    backgroundColor: tokens.colorContrastLight,
   },
   demoBoxDark: {
-    backgroundColor: tokens.colorContrastDark
+    backgroundColor: tokens.colorContrastDark,
   },
-}
+};
 
-const DemoBox = ({times, id}: {times?: number; id?: string}) => {
-  if(times) {
+const DemoBox = ({ times, id }: { times?: number; id?: string }) => {
+  if (times) {
     let result = [];
-    for (let i=0; i<=times; i++) {
-      result.push(<GridItem key={`${id}-${i}`} style={styles.demoBox} />)
+    for (let i = 0; i < times; i++) {
+      result.push(<GridItem key={`${id}-${i}`} style={styles.demoBox} />);
     }
-    return <>{result}</>
+    return <>{result}</>;
   }
-  return <GridItem style={styles.demoBox}></GridItem>
-}
+  return <GridItem style={styles.demoBox}></GridItem>;
+};
 
 storiesOf('Components|Grid/GridItem', module)
   .addParameters({
     propTypes: Grid['__docgenInfo'],
   })
-  .add('default', () => {
-    const knobs = {
-      exampleBoxes: number('Example Items', 22),
-      exampleGridHeight: text('Example Grid Height', '90vh'),
-      grid: {
-        columns: number('columns', 6, '', gridKnobsId),
-        rows: number('rows', 4, '', gridKnobsId),
-        columnGap: select('columnGap', spacingValues, 'spacingXs', gridKnobsId),
-        rowGap: select('rowGap', spacingValues, 'spacingXs', gridKnobsId),
-        className: text('className', '', gridKnobsId)
-      },
-      gridItem: {
-        columnStart: number('columnStart', 1, '', gridItemKnobsId),
-        columnEnd: number('columnEnd', 3, '', gridItemKnobsId),
-        rowStart: number('rowStart', 1, '', gridItemKnobsId),
-        rowEnd: number('rowEnd', 4, '', gridItemKnobsId),
-        area: text('className', '', gridItemKnobsId),
-        order: number('order', 0, '', gridItemKnobsId)
-      }
-    }
+  .add(
+    'default',
+    () => {
+      const knobs = {
+        exampleBoxes: number('Example Items', 23),
+        exampleGridHeight: text('Example Grid Height', '90vh'),
+        grid: {
+          columns: number('columns', 6, '', gridKnobsId),
+          rows: number('rows', 4, '', gridKnobsId),
+          columnGap: select(
+            'columnGap',
+            spacingValues,
+            'spacingXs',
+            gridKnobsId,
+          ),
+          rowGap: select('rowGap', spacingValues, 'spacingXs', gridKnobsId),
+          className: text('className', '', gridKnobsId),
+        },
+        gridItem: {
+          columnStart: number('columnStart', 1, '', gridItemKnobsId),
+          columnEnd: number('columnEnd', 3, '', gridItemKnobsId),
+          rowStart: number('rowStart', 1, '', gridItemKnobsId),
+          rowEnd: number('rowEnd', 4, '', gridItemKnobsId),
+          area: text('className', '', gridItemKnobsId),
+          order: number('order', 0, '', gridItemKnobsId),
+        },
+      };
 
-    return (
-      <Grid
-        columns={knobs.grid.columns}
-        rows={knobs.grid.rows}
-        columnGap={knobs.grid.columnGap}
-        rowGap={knobs.grid.rowGap}
-        className={knobs.grid.className}
-        style={{height: knobs.exampleGridHeight}}>
-        <GridItem
-          style={styles.demoBoxDark}
-          columnStart={knobs.gridItem.columnStart}
-          columnEnd={knobs.gridItem.columnEnd}
-          rowStart={knobs.gridItem.rowStart}
-          rowEnd={knobs.gridItem.rowEnd}
-          area={knobs.gridItem.area}
-          order={knobs.gridItem.order}
-         />
-        <DemoBox id="gi" times={knobs.exampleBoxes} />
-      </Grid>
-    )
-  }, {notes})
+      return (
+        <Grid
+          columns={knobs.grid.columns}
+          rows={knobs.grid.rows}
+          columnGap={knobs.grid.columnGap}
+          rowGap={knobs.grid.rowGap}
+          className={knobs.grid.className}
+          style={{ height: knobs.exampleGridHeight }}
+        >
+          <GridItem
+            style={styles.demoBoxDark}
+            columnStart={knobs.gridItem.columnStart}
+            columnEnd={knobs.gridItem.columnEnd}
+            rowStart={knobs.gridItem.rowStart}
+            rowEnd={knobs.gridItem.rowEnd}
+            area={knobs.gridItem.area}
+            order={knobs.gridItem.order}
+          />
+          <DemoBox id="gi" times={knobs.exampleBoxes} />
+        </Grid>
+      );
+    },
+    { notes },
+  );
