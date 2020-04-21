ce2022ba62369331a72dd08e8170c7b7f15adf9f
Gui Santos <guilherme.goncalvessantos@gmail.com>
2020-04-21 18:01:51 +0200

    test(table): upload table related snapshots

--- packages/forma-36-react-components/src/components/Skeleton/SkeletonRow/SkeletonRow.test.tsx
+++ packages/forma-36-react-components/src/components/Skeleton/SkeletonRow/SkeletonRow.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { axe } from 'jest-axe';
+import axe from '../../../utils/axeHelper';
 
 import SkeletonRow from './SkeletonRow';
 import Table from '../../Table';
