ce66c199cdf745fc37d4993fe835107d32bf67c3
Alexander Suevalov <alex.suevalov@contentful.com>
2020-06-09 14:19:28 +0200

    fix(Tab): expose event in onSelect callback (#492)

--- packages/forma-36-react-components/src/components/Tabs/Tab.tsx
+++ packages/forma-36-react-components/src/components/Tabs/Tab.tsx
@@ -10,7 +10,7 @@ import styles from './Tabs.css';
 
 export type TabProps = {
   id: string;
-  onSelect?: (id: string) => void;
+  onSelect?: (id: string, e: React.SyntheticEvent) => void;
   selected?: boolean;
   href?: string;
   target?: string;
@@ -32,15 +32,15 @@ const defaultProps = {
 export class Tab extends Component<TabProps> {
   static defaultProps = defaultProps;
 
-  onClick: MouseEventHandler = () => {
+  onClick: MouseEventHandler = e => {
     if (this.props.onSelect && !this.props.disabled) {
-      this.props.onSelect(this.props.id);
+      this.props.onSelect(this.props.id, e);
     }
   };
 
   onKeyPress = (e: KeyboardEvent<HTMLElement>) => {
     if (this.props.onSelect && e.key === 'Enter') {
-      this.props.onSelect(this.props.id);
+      this.props.onSelect(this.props.id, e);
       e.preventDefault();
     }
   };
