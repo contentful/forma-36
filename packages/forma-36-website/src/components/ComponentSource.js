220b3ae2027cd4c2298eb7b290c971e59db3feb7
burakukula <trycjapa@gmail.com>
2020-06-11 16:40:26 +0200

    Fix import

--- packages/forma-36-website/src/components/ComponentSource.js
+++ packages/forma-36-website/src/components/ComponentSource.js
@@ -3,7 +3,7 @@ import { css } from '@emotion/core';
 import tokens from '@contentful/forma-36-tokens';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import * as f36Components from '@contentful/forma-36-react-components';
-import { Grid, GridItem, Autocomplete } from '@contentful/forma-36-react-components/dist/alpha';
+import * as f36AlphaComponents from '@contentful/forma-36-react-components';
 import { Card, Button } from '@contentful/forma-36-react-components';
 
 const styles = {
@@ -37,7 +37,7 @@ class ComponentSource extends React.Component {
   render() {
     return (
       <div className="f36-margin-bottom--m">
-        <LiveProvider code={this.props.children} scope={{ ...f36Components, Grid, GridItem, Autocomplete }}>
+        <LiveProvider code={this.props.children} scope={{ ...f36Components, f36AlphaComponents }}>
           <Card padding="none">
             <div css={styles.preview}>
               <LivePreview />
