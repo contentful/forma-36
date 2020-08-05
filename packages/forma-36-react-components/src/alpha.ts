2529c89ab6fb69cce988c0e013048c23e9d4b422
Patrycja Radaczyńska <trycjapa@gmail.com>
2020-08-05 08:37:50 +0200

    feat(flex): introducing Flex component (#524)

--- packages/forma-36-react-components/src/alpha.ts
+++ packages/forma-36-react-components/src/alpha.ts
@@ -1,4 +1,5 @@
 export { Autocomplete } from './components/Autocomplete/Autocomplete';
 export { Grid } from './components/Grid';
 export { GridItem } from './components/Grid/GridItem';
+export { Flex } from './components/Flex';
 export { NavigationIcon } from './components/NavigationIcon/NavigationIcon';
