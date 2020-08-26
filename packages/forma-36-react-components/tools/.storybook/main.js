bfc41cff6603ff37da0c50ce615fc06777ba31cb
Gui Santos <guilherme.goncalvessantos@gmail.com>
2020-08-26 18:32:49 +0200

    chore(storybook): migrate addon-notes/info content to addon-docs

--- packages/forma-36-react-components/tools/.storybook/main.js
+++ packages/forma-36-react-components/tools/.storybook/main.js
@@ -3,10 +3,11 @@ const path = require('path');
 module.exports = {
   stories: ['./docs/**/*.stories.tsx', '../../src/components/**/*.stories.tsx'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-knobs',
-    'storybook-prop-types-addon',
     '@storybook/addon-a11y',
     '@storybook/addon-actions',
+    'storybook-prop-types-addon',
     'storybook-addon-jsx',
     {
       name: '@storybook/addon-storysource',
--- packages/forma-36-react-components/tools/.storybook/preview.js
+++ packages/forma-36-react-components/tools/.storybook/preview.js
@@ -10,3 +10,16 @@ addDecorator(jsxDecorator);
 
 // Setup Storybook options
 addParameters({ info: { header: false, source: false } });
+
+// Creating DocPage from our old notes
+addParameters({
+  docs: {
+    disable: true,
+    extractComponentDescription: (component, { notes }) => {
+      if (notes) {
+        return typeof notes === 'string' ? notes : notes.markdown || notes.text;
+      }
+      return null;
+    },
+  },
+});
