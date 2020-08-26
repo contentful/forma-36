31731130de47b3962631e82250d86975332732c4
Gui Santos <guilherme.goncalvessantos@gmail.com>
2020-08-26 18:02:59 +0200

    chore(storybook): remove deprecated addons and unecessary configs

--- packages/forma-36-react-components/tools/.storybook/manager.js
+++ packages/forma-36-react-components/tools/.storybook/manager.js
@@ -1,5 +1,4 @@
 import { addons } from '@storybook/addons';
-import { create } from '@storybook/theming/create';
 import contentfulTheme from './contentful-theme';
 
 addons.setConfig({
--- packages/forma-36-react-components/tools/.storybook/preview.js
+++ packages/forma-36-react-components/tools/.storybook/preview.js
@@ -1,16 +1,11 @@
-import { configure, addParameters, addDecorator } from '@storybook/react';
-// import './storybook.global.css';
+import { addParameters, addDecorator } from '@storybook/react';
 
 // Storybook Addon Dependencies
-import { withKnobs } from '@storybook/addon-knobs';
 import { withPropTypes } from 'storybook-prop-types-addon';
-import { withA11y } from '@storybook/addon-a11y';
 import { jsxDecorator } from 'storybook-addon-jsx';
 
 // Setup Addons
-addDecorator(withKnobs);
 addDecorator(withPropTypes);
-addDecorator(withA11y);
 addDecorator(jsxDecorator);
 
 // Setup Storybook options
