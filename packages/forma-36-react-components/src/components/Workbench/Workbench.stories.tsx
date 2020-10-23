5e60d58f0b8ae196e7fc912bff71db117360194d
burakukula <trycjapa@gmail.com>
2020-10-23 13:21:51 +0200

    feat: update Workbench component

--- packages/forma-36-react-components/src/components/Workbench/Workbench.stories.tsx
+++ packages/forma-36-react-components/src/components/Workbench/Workbench.stories.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text, select } from '@storybook/addon-knobs';
+import { select } from '@storybook/addon-knobs';
 import tokens from '@contentful/forma-36-tokens';
 
-import Workbench from './Workbench';
+import Workbench, {WorkbenchProps} from './Workbench';
 import Icon from '../Icon';
 import Button from '../Button';
+import notes from './README.md'
 
-storiesOf('(alpha)/Workbench', module)
-  .addParameters({
+export default {
+  title: 'components/Workbench',
+  component: Workbench,
+  parameters: {
     propTypes: Workbench['__docgenInfo'],
-    component: Workbench,
-  })
-  .add('default', () => (
-    <Workbench className={text('className', '')} testId={text('testId')}>
+    notes,
+  },
+  argTypes: {
+    children: { control: { disable: true } },
+    className: { control: { disable: true } },
+    testId: { control: { disable: true } },
+  },
+};
+
+export const basic = (args: WorkbenchProps) => (
+  <div>
+    <Workbench>
       <Workbench.Header
-        title={'Page title'}
-        description="Lorem Ipsum dolor sit amet."
+        title={args['header title']}
+        description={args['header description']}
         icon={<Icon icon="ArrowDown" />}
         actions={<Button buttonType="muted">Click</Button>}
       />
@@ -30,117 +40,146 @@ storiesOf('(alpha)/Workbench', module)
         <div
           style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
         >
-          Workbench
+          {args['workbench content']}
         </div>
       </Workbench.Content>
     </Workbench>
-  ))
-  .add('with left sidebar', () => (
-    <Workbench className={text('className', '')} testId={text('testId')}>
-      <Workbench.Header
-        title={'Page title'}
-        icon={<Icon icon="ArrowDown" />}
-        actions={<Button buttonType="muted">Click</Button>}
-      />
-      <Workbench.Sidebar position="left">
-        <div style={{ height: '2000px', backgroundColor: tokens.colorRedBase }}>
-          left sidebar
-        </div>
-      </Workbench.Sidebar>
-      <Workbench.Content
-        type={select(
-          'Workbench.Content -> type',
-          ['default', 'text', 'full'],
-          'default',
-        )}
+  </div>
+)
+
+export const withLeftSidebar = (args: WorkbenchProps) => (
+  <Workbench>
+    <Workbench.Header
+      title={args['header title']}
+      icon={<Icon icon="ArrowDown" />}
+      actions={<Button buttonType="muted">Click</Button>}
+    />
+    <Workbench.Sidebar position="left">
+      <div style={{ height: '2000px', backgroundColor: tokens.colorRedBase }}>
+        {args['left sidebar']}
+      </div>
+    </Workbench.Sidebar>
+    <Workbench.Content
+      type={select(
+        'Workbench.Content -> type',
+        ['default', 'text', 'full'],
+        'default',
+      )}
+    >
+      <div
+        style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
       >
-        <div
-          style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
-        >
-          Workbench
-        </div>
-      </Workbench.Content>
-    </Workbench>
-  ))
-  .add('with right sidebar', () => (
-    <Workbench className={text('className', '')} testId={text('testId')}>
-      <Workbench.Header
-        title={'Page title'}
-        icon={<Icon icon="ArrowDown" />}
-        actions={<Button buttonType="muted">Click</Button>}
-      />
-      <Workbench.Content
-        type={select(
-          'Workbench.Content -> type',
-          ['default', 'text', 'full'],
-          'default',
-        )}
+        {args['workbench content']}
+      </div>
+    </Workbench.Content>
+  </Workbench>
+)
+
+export const withRightSidebar = (args: WorkbenchProps) => (
+  <Workbench>
+    <Workbench.Header
+      title={'Page title'}
+      icon={<Icon icon="ArrowDown" />}
+      actions={<Button buttonType="muted">Click</Button>}
+    />
+    <Workbench.Content
+      type={select(
+        'Workbench.Content -> type',
+        ['default', 'text', 'full'],
+        'default',
+      )}
+    >
+      <div
+        style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
       >
-        <div
-          style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
-        >
-          Workbench
-        </div>
-      </Workbench.Content>
-      <Workbench.Sidebar position="right">
-        <div style={{ height: '2000px', backgroundColor: tokens.colorRedBase }}>
-          right sidebar
-        </div>
-      </Workbench.Sidebar>
-    </Workbench>
-  ))
-  .add('with left and right sidebar', () => (
-    <Workbench className={text('className', '')} testId={text('testId')}>
-      <Workbench.Header
-        title={'Page title'}
-        icon={<Icon icon="ArrowDown" />}
-        actions={<Button buttonType="muted">Click</Button>}
-      />
-      <Workbench.Sidebar position="left">
-        <div style={{ height: '2000px', backgroundColor: tokens.colorRedBase }}>
-          left sidebar
-        </div>
-      </Workbench.Sidebar>
-      <Workbench.Content
-        type={select(
-          'Workbench.Content -> type',
-          ['default', 'text', 'full'],
-          'default',
-        )}
+        {args['workbench content']}
+      </div>
+    </Workbench.Content>
+    <Workbench.Sidebar position="right">
+      <div style={{ height: '2000px', backgroundColor: tokens.colorRedBase }}>
+        {args['right sidebar']}
+      </div>
+    </Workbench.Sidebar>
+  </Workbench>
+);
+
+export const withLeftAndRightSidebar = (args: WorkbenchProps) => (
+  <Workbench>
+    <Workbench.Header
+      title={'Page title'}
+      icon={<Icon icon="ArrowDown" />}
+      actions={<Button buttonType="muted">Click</Button>}
+    />
+    <Workbench.Sidebar position="left">
+      <div style={{ height: '2000px', backgroundColor: tokens.colorRedBase }}>
+        {args['left sidebar']}
+      </div>
+    </Workbench.Sidebar>
+    <Workbench.Content
+      type={select(
+        'Workbench.Content -> type',
+        ['default', 'text', 'full'],
+        'default',
+      )}
+    >
+      <div
+        style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
       >
-        <div
-          style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
-        >
-          Workbench
-        </div>
-      </Workbench.Content>
-      <Workbench.Sidebar position="right">
-        <div style={{ height: '2000px', backgroundColor: tokens.colorRedBase }}>
-          right sidebar
-        </div>
-      </Workbench.Sidebar>
-    </Workbench>
-  ))
-  .add('with back button', () => (
-    <Workbench className={text('className', '')} testId={text('testId')}>
-      <Workbench.Header
-        onBack={() => {}}
-        title={'Page title'}
-        icon={<Icon icon="ArrowDown" />}
-        actions={<Button buttonType="muted">Click</Button>}
-      />
-      <Workbench.Content
-        type={select(
-          'Workbench.Content -> type',
-          ['default', 'text', 'full'],
-          'default',
-        )}
+        {args['workbench content']}
+      </div>
+    </Workbench.Content>
+    <Workbench.Sidebar position="right">
+      <div style={{ height: '2000px', backgroundColor: tokens.colorRedBase }}>
+        {args['right sidebar']}
+      </div>
+    </Workbench.Sidebar>
+  </Workbench>
+)
+
+export const withBackButton = (args: WorkbenchProps) => (
+  <Workbench>
+    <Workbench.Header
+      onBack={() => {}}
+      title={'Page title'}
+      icon={<Icon icon="ArrowDown" />}
+      actions={<Button buttonType="muted">Click</Button>}
+    />
+    <Workbench.Content
+      type={select(
+        'Workbench.Content -> type',
+        ['default', 'text', 'full'],
+        'default',
+      )}
+    >
+      <div
+        style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
       >
-        <div
-          style={{ height: '2000px', backgroundColor: tokens.colorBlueBase }}
-        >
-          Workbench
-        </div>
-      </Workbench.Content>
-    </Workbench>
-  ));
+        {args['workbench content']}
+      </div>
+    </Workbench.Content>
+  </Workbench>
+);
+
+basic.args = {
+  ['header title']: 'Page title',
+  ['header description']: 'Lorem Ipsum dolor sit amet.',
+  ['workbench content']: 'Workbench',
+  ['left sidebar']: 'left sidebar',
+  ['right sidebar']: 'right sidebar'
+}
+
+withLeftSidebar.args = {
+  ...basic.args,
+}
+
+withRightSidebar.args = {
+  ...basic.args,
+}
+
+withLeftAndRightSidebar.args = {
+  ...basic.args,
+}
+
+withBackButton.args = {
+  ...basic.args,
+}
