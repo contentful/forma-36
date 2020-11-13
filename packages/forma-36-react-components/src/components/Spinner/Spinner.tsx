965afc8365e094d44238105da4d35df96d25466f
Gui Santos <guilherme.goncalvessantos@gmail.com>
2020-11-13 18:02:30 +0100

    refactor(spinner): make it a functional component

--- packages/forma-36-react-components/src/components/Spinner/Spinner.tsx
+++ packages/forma-36-react-components/src/components/Spinner/Spinner.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import cn from 'classnames';
 
 import styles from './Spinner.css';
@@ -21,46 +21,41 @@ const defaultProps: Partial<SpinnerProps> = {
   color: 'default',
 };
 
-export class Spinner extends Component<SpinnerProps> {
-  static defaultProps = defaultProps;
+export const Spinner: React.FC<SpinnerProps> = ({
+  className,
+  size,
+  testId,
+  color,
+  customSize,
+  ...otherProps
+}) => {
+  const classNames = cn(styles.Spinner, className, {
+    [styles[`Spinner--${size}`]]: size,
+    [styles[`Spinner--${color}`]]: color,
+  });
 
-  render() {
-    const {
-      className,
-      size,
-      testId,
-      color,
-      customSize,
-      ...otherProps
-    } = this.props;
-
-    const classNames = cn(styles.Spinner, className, {
-      [styles[`Spinner--${size}`]]: size,
-      [styles[`Spinner--${color}`]]: color,
-    });
-
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        data-test-id={testId}
-        className={classNames}
-        style={
-          customSize
-            ? { height: `${customSize}px`, width: `${customSize}px` }
-            : {}
-        }
-        {...otherProps}
-      >
-        <title>Loading…</title>
-        <path d="M2,10a8,8,0,0,1,8-8V0a10,10,0,0,0,0,20V18A8,8,0,0,1,2,10Z" />
-        <path
-          d="M10,0V2a8,8,0,0,1,0,16v2A10,10,0,0,0,10,0Z"
-          style={{ opacity: 0.4 }}
-        />
-      </svg>
-    );
-  }
-}
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      data-test-id={testId}
+      className={classNames}
+      style={
+        customSize
+          ? { height: `${customSize}px`, width: `${customSize}px` }
+          : {}
+      }
+      {...otherProps}
+    >
+      <title>Loading…</title>
+      <path d="M2,10a8,8,0,0,1,8-8V0a10,10,0,0,0,0,20V18A8,8,0,0,1,2,10Z" />
+      <path
+        d="M10,0V2a8,8,0,0,1,0,16v2A10,10,0,0,0,10,0Z"
+        style={{ opacity: 0.4 }}
+      />
+    </svg>
+  );
+};
+Spinner.defaultProps = defaultProps;
 
 export default Spinner;
