7a08c874c7d39bae9738325f84e75c9c1a168b30
Gui Santos <guilherme.goncalvessantos@gmail.com>
2020-11-13 16:41:02 +0100

    fix: lint issues

--- packages/forma-36-react-components/src/components/TextLink/TextLink.stories.tsx
+++ packages/forma-36-react-components/src/components/TextLink/TextLink.stories.tsx
@@ -24,30 +24,27 @@ export const UsedWithText = () => {
   return (
     <Paragraph>
       Berlin (/bɜːrˈlɪn/; German: [bɛʁˈliːn] (About this soundlisten)) is the{' '}
-      <TextLink
-        href="https://www.wikiwand.com/en/Capital_city"
-        target="_blanck"
-      >
+      <TextLink href="https://www.wikiwand.com/en/Capital_city" target="_blank">
         capital
       </TextLink>{' '}
       and largest city of{' '}
-      <TextLink href="https://www.wikiwand.com/en/Germany" target="_blanck">
+      <TextLink href="https://www.wikiwand.com/en/Germany" target="_blank">
         Germany
       </TextLink>{' '}
       by both area and population. Its 3,769,495 inhabitants as of 31 December
       2019 make it the most populous city of the European Union, according to
-      population within city limits.[8] The city is also one of Germany's 16
+      population within city limits.[8] The city is also one of Germany’s 16
       federal states. It is surrounded by the state of{' '}
-      <TextLink href="https://www.wikiwand.com/en/Brandenburg" target="_blanck">
+      <TextLink href="https://www.wikiwand.com/en/Brandenburg" target="_blank">
         Brandenburg
       </TextLink>
       , and contiguous with{' '}
       <TextLink href="https://www.wikiwand.com/en/Potsdam" target="_blanck">
         Potsdam
       </TextLink>
-      , Brandenburg's capital. The two cities are at the center of the
+      , Brandenburg’s capital. The two cities are at the center of the
       Berlin-Brandenburg capital region, which is, with about six million
-      inhabitants and an area of more than 30,000 km2,[9] Germany's
+      inhabitants and an area of more than 30,000 km2,[9] Germany’s
       third-largest metropolitan region after the Rhine-Ruhr and Rhine-Main
       regions.
     </Paragraph>
