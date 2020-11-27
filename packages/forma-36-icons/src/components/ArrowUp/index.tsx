c63cbbc2129bdaa103c45e588ac5f373113fb131
burakukula <trycjapa@gmail.com>
2020-11-27 14:24:02 +0100

    Helper in the right place

---
+++ packages/forma-36-icons/src/components/ArrowUp/index.tsx
@@ -0,0 +1,1 @@
+export { default as ArrowDown } from './ArrowDown';
--- packages/forma-36-icons/src/components/Icon/Icon.test.tsx
+++ packages/forma-36-icons/src/components/Icon/Icon.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import axe from '../../../tools/axeHelper';
+import axe from '../../tests/axeHelper';
 import Icon from './Icon';
 import { iconName } from './constants';
 
