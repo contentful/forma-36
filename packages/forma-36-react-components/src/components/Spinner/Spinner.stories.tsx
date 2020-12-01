f509f0b4c54932f8065144999488c333cd5cfbb7
Gui Santos <guilherme.santos@contentful.com>
2020-12-01 11:23:44 +0100

    Feat: change Spinner animation (#654)
    * docs: show components in the center of the canvas
    
    * refactor(spinner): make it a functional component
    
    * feat(spinner): implement ellipsis animation
    
    * test: fix snapshots and customSizes
    
    * docs(spinner): add readme and props descriptions
    
    * chore(size-limit): increase css limit
    
    * test: update snapshots
    
    * feat: make animation faster

--- packages/forma-36-react-components/src/components/Spinner/Spinner.stories.tsx
+++ packages/forma-36-react-components/src/components/Spinner/Spinner.stories.tsx
@@ -1,44 +1,27 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text, select, number } from '@storybook/addon-knobs';
 
-import Spinner from './Spinner';
+import Spinner, { SpinnerProps } from './Spinner';
 
-storiesOf('Components/Spinner', module)
-  .addParameters({
+export default {
+  title: 'Components/Spinner',
+  component: Spinner,
+  parameters: {
     propTypes: Spinner['__docgenInfo'],
-    component: Spinner,
-  })
-  .add('default', () => (
-    <Spinner
-      className={text('className', '')}
-      size={select(
-        'Size',
-        {
-          Default: 'default',
-          Small: 'small',
-          Large: 'large',
-        },
-        'default',
-      )}
-      customSize={number('Custom Size', undefined)}
-    />
-  ))
-  .add('with inline text', () => (
-    <div>
-      Loading{' '}
-      <Spinner
-        className={text('className', '')}
-        size={select(
-          'Size',
-          {
-            Default: 'default',
-            Small: 'small',
-            Large: 'large',
-          },
-          'default',
-        )}
-        customSize={number('Custom Size', undefined)}
-      />
-    </div>
-  ));
+  },
+};
+
+export const Default = (args: SpinnerProps) => {
+  return <Spinner {...args} />;
+};
+Default.args = {
+  color: 'primary',
+  size: 'large',
+};
+
+export const WithText = (args: SpinnerProps) => {
+  return (
+    <>
+      Loading <Spinner {...args} />
+    </>
+  );
+};
