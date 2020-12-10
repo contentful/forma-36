dac339e09f3fb8487e45b9c87f92ba4dcc61a8ea
Alex Suevalov <alex.suevalov@contentful.com>
2020-12-10 15:51:19 +0100

    chore: change color

--- packages/forma-36-tokens/src/tokens/colors/colors-blue.js
+++ packages/forma-36-tokens/src/tokens/colors/colors-blue.js
@@ -1,5 +1,5 @@
 const colorsBlue = {
-  'color-blue-dark': '#174E8C',
+  'color-blue-dark': '#074E8C',
   'color-blue-base': '#2D64B3',
   'color-blue-mid': '#2E75D4',
   'color-blue-light': '#84B9F5',
