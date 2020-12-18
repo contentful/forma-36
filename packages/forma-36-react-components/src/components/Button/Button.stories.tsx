63b805b56bc9fb75cf31889fd0e96c0cef816e7e
burakukula <trycjapa@gmail.com>
2020-12-18 12:57:33 +0100

    fix: refactor Button story with the example overview of components variants

--- packages/forma-36-react-components/src/components/Button/Button.stories.tsx
+++ packages/forma-36-react-components/src/components/Button/Button.stories.tsx
@@ -1,47 +1,79 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { text, boolean, select } from '@storybook/addon-knobs';
+
+import Button, { ButtonProps, buttonTypes } from './Button';
 import { iconName } from '../Icon/constants';
 
-import Button from './Button';
 import notes from './README.mdx';
 
-storiesOf('Components/Button', module)
-  .addParameters({
-    propTypes: Button['__docgenInfo'],
-    component: Button,
-  })
-  .add(
-    'Button',
-    () => (
-      <Button
-        className={text('className', '')}
-        icon={select('icon', [undefined, ...Object.keys(iconName)], undefined)}
-        buttonType={select(
-          'buttonType',
-          {
-            muted: 'muted',
-            primary: 'primary',
-            positive: 'positive',
-            negative: 'negative',
-            naked: 'naked',
-            warning: 'warning',
-          },
-          'muted',
-        )}
-        size={select('size', [undefined, 'small', 'large'], undefined)}
-        loading={boolean('loading', false)}
-        indicateDropdown={boolean('indicateDropdown', false)}
-        disabled={boolean('disabled', false)}
-        isFullWidth={boolean('isFullWidth', false)}
-        onClick={action('onClick')}
-        onBlur={action('onBlur')}
-        href={text('href', '')}
-        isActive={boolean('isActive', false)}
-      >
-        {text('Text', 'Embed entry')}
-      </Button>
-    ),
-    { notes },
-  );
+export default {
+  title: 'Components/Button',
+  component: Button,
+  parameters: {
+    propTypes: [Button['__docgenInfo']],
+    notes,
+  },
+  argTypes: {
+    icon: { control: { type: 'select', options: iconName } },
+    buttonType: { control: { type: 'select', options: buttonTypes } },
+    size: {
+      control: { type: 'select', options: [undefined, 'small', 'large'] },
+    },
+    className: { control: { disable: true } },
+    testId: { control: { disable: true } },
+    onClick: { control: { disable: true } },
+    onBlur: { control: { disable: true } },
+    loading: { control: { disable: true } },
+  },
+};
+
+export const basic = (args: ButtonProps) => (
+  <Button buttonType={args.buttonType} size={args.size}>
+    {args['Button Text']}
+  </Button>
+);
+
+export const overviewTypes = (args: ButtonProps) => (
+  <>
+    <Button buttonType="primary">primary</Button>
+    <Button buttonType="positive">posiitve</Button>
+
+    <Button buttonType="warning">warning</Button>
+    <Button buttonType="negative">negative</Button>
+    <Button buttonType="muted">muted</Button>
+    <Button buttonType="naked">naked</Button>
+  </>
+);
+
+export const sizesOverview = (args: ButtonProps) => (
+  <>
+    <Button buttonType="primary" size="small">
+      small
+    </Button>
+    <Button buttonType="primary">default size</Button>
+    <Button buttonType="primary" size="large">
+      large
+    </Button>
+  </>
+);
+
+export const statesOverview = (args: ButtonProps) => (
+  <>
+    <Button buttonType="primary" isActive>
+      isActive
+    </Button>
+    <Button buttonType="primary" disabled>
+      disabled
+    </Button>
+    <Button buttonType="primary" indicateDropdown>
+      indicateDropdown
+    </Button>
+    <Button buttonType="primary" loading>
+      loading
+    </Button>
+  </>
+);
+
+basic.args = {
+  buttonType: 'positive',
+  'Button Text': 'Embed entry',
+};
