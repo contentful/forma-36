d5c11998ae075d683471e6cc034ca6a5f14ee638
denkristoffer <denkristoffer@users.noreply.github.com>
2020-12-22 16:55:12 +0100

    refactor(card-actions): function component

--- packages/forma-36-react-components/src/components/Card/CardActions/CardActions.tsx
+++ packages/forma-36-react-components/src/components/Card/CardActions/CardActions.tsx
@@ -1,4 +1,6 @@
-import React, { Component, MouseEvent as ReactMouseEvent } from 'react';
+import React, { useCallback, useState } from 'react';
+import type { MouseEvent as ReactMouseEvent } from 'react';
+
 import Dropdown from '../../Dropdown';
 import DropdownList from '../../Dropdown/DropdownList';
 import IconButton, { IconButtonProps } from '../../IconButton';
@@ -32,91 +34,78 @@ export interface CardActionsState {
   isDropdownOpen: boolean;
 }
 
-const defaultProps: Partial<CardActionsPropTypes> = {
+export function CardActions({
+  className,
+  children,
+  testId,
+  iconButtonProps,
+  isDisabled,
+  ...otherProps
+}: CardActionsPropTypes): React.ReactElement {
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+  const handleClick = useCallback((event: ReactMouseEvent) => {
+    setIsDropdownOpen((prevState) => !prevState);
+
+    if (iconButtonProps && iconButtonProps.onClick) {
+      event.stopPropagation();
+    }
+  }, []);
+
+  return (
+    <Dropdown
+      onClose={() => {
+        setIsDropdownOpen(false);
+      }}
+      position="bottom-right"
+      className={className}
+      isOpen={isDropdownOpen}
+      testId={testId}
+      toggleElement={
+        <IconButton
+          iconProps={{ icon: 'MoreHorizontal' }}
+          buttonType="secondary"
+          disabled={isDisabled}
+          label="Actions"
+          {...iconButtonProps}
+          onClick={(event) => {
+            event.preventDefault();
+            handleClick(event);
+          }}
+        />
+      }
+      {...otherProps}
+    >
+      {React.Children.map(children, (listItems: React.ReactElement) => {
+        return React.Children.map(listItems, (item: React.ReactElement) => {
+          // React.Children behaves differently if the object is a Fragment.
+          const resolvedChildren =
+            item.type === React.Fragment ? item.props.children : item;
+
+          const enhancedChildren = React.Children.map(
+            resolvedChildren,
+            (child: React.ReactElement) =>
+              React.cloneElement(child, {
+                onClick: (e: ReactMouseEvent) => {
+                  if (child.props.onClick) {
+                    child.props.onClick(e);
+                  }
+                  setIsDropdownOpen(false);
+                  e.stopPropagation();
+                },
+              }),
+          );
+
+          return enhancedChildren;
+        });
+      })}
+    </Dropdown>
+  );
+}
+
+CardActions.defaultProps = {
   testId: 'cf-ui-card-actions',
   isDisabled: false,
 };
-
-export class CardActions extends Component<
-  CardActionsPropTypes,
-  CardActionsState
-> {
-  static defaultProps = defaultProps;
-
-  state = { isDropdownOpen: false };
-
-  handleClick = (event: ReactMouseEvent) => {
-    this.setState((prevState) => ({
-      isDropdownOpen: !prevState.isDropdownOpen,
-    }));
-
-    if (this.props.iconButtonProps && this.props.iconButtonProps.onClick) {
-      event.stopPropagation();
-    }
-  };
-
-  render() {
-    const {
-      className,
-      children,
-      testId,
-      iconButtonProps,
-      isDisabled,
-      ...otherProps
-    } = this.props;
-
-    return (
-      <Dropdown
-        onClose={() => {
-          this.setState({
-            isDropdownOpen: false,
-          });
-        }}
-        position="bottom-right"
-        className={className}
-        isOpen={this.state.isDropdownOpen}
-        testId={testId}
-        toggleElement={
-          <IconButton
-            iconProps={{ icon: 'MoreHorizontal' }}
-            buttonType="secondary"
-            disabled={isDisabled}
-            label="Actions"
-            {...iconButtonProps}
-            onClick={(event) => {
-              event.preventDefault();
-              this.handleClick(event);
-            }}
-          />
-        }
-        {...otherProps}
-      >
-        {React.Children.map(children, (listItems: React.ReactElement) => {
-          return React.Children.map(listItems, (item: React.ReactElement) => {
-            // React.Children behaves differently if the object is a Fragment.
-            const resolvedChildren =
-              item.type === React.Fragment ? item.props.children : item;
-
-            const enhancedChildren = React.Children.map(
-              resolvedChildren,
-              (child: React.ReactElement) =>
-                React.cloneElement(child, {
-                  onClick: (e: ReactMouseEvent) => {
-                    if (child.props.onClick) {
-                      child.props.onClick(e);
-                    }
-                    this.setState({ isDropdownOpen: false });
-                    e.stopPropagation();
-                  },
-                }),
-            );
-
-            return enhancedChildren;
-          });
-        })}
-      </Dropdown>
-    );
-  }
-}
 
 export default CardActions;
