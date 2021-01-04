3146a153b967dbb924a3ef1d6ea368a307e3560c
burakukula <trycjapa@gmail.com>
2021-01-04 15:13:43 +0100

    fix: add muted glow for muted buttons

--- packages/forma-36-tokens/src/tokens/box-shadows/glows.js
+++ packages/forma-36-tokens/src/tokens/box-shadows/glows.js
@@ -2,12 +2,14 @@ const colorsBlue = require('./../colors/colors-blue.js');
 const colorsRed = require('./../colors/colors-red.js');
 const colorsGreen = require('./../colors/colors-green.js');
 const colorsOrange = require('./../colors/colors-orange.js');
+const colorsMuted = require('./../colors/colors-elements.js');
 
 const glows = {
   'glow-primary': `0px 0px 0px 3px ${colorsBlue['color-blue-light']}`,
-  'glow-negative': `0px 0px 0px 3px ${colorsRed['color-red-lightest']}`,
-  'glow-positive': `0px 0px 0px 3px ${colorsGreen['color-green-lightest']}`,
-  'glow-warning': `0px 0px 0px 3px ${colorsOrange['color-orange-lightest']}`,
+  'glow-negative': `0px 0px 0px 3px ${colorsRed['color-red-light']}`,
+  'glow-positive': `0px 0px 0px 3px ${colorsGreen['color-green-light']}`,
+  'glow-warning': `0px 0px 0px 3px ${colorsOrange['color-orange-light']}`,
+  'glow-muted': `0px 0px 0px 3px ${colorsMuted['color-element-light']}`,
 };
 
 module.exports = glows;
