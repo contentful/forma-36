3a3dc815766964753366dca96aebd3142c7970a5
burakukula <trycjapa@gmail.com>
2021-01-04 13:53:40 +0100

    feat: update tokens radius, glow and shadow

--- packages/forma-36-tokens/src/tokens/box-shadows/box-shadows.js
+++ packages/forma-36-tokens/src/tokens/box-shadows/box-shadows.js
@@ -1,6 +1,8 @@
 const boxShadows = {
-  'box-shadow-default': '0 1px 3px rgba(0, 0, 0, 0.08)',
-  'box-shadow-heavy': '0 2px 3px rgba(0, 0, 0, 0.35)',
+  'box-shadow-default':
+    '0px 0px 0px 1px rgba(25, 37, 50, 0.1), 0px 3px 7px -3px rgba(25, 37, 50, 0.1), 0px 6px 12px -2px rgba(25, 37, 50, 0.1);',
+  'box-shadow-heavy':
+    '0px 0px 0px 1px rgba(25, 37, 50, 0.1), 0px -6px 16px -6px rgba(25, 37, 50, 0.03), 0px 8px 16px -8px rgba(25, 37, 50, 0.2), 0px 13px 27px -5px rgba(25, 37, 50, 0.15);',
 };
 
 module.exports = boxShadows;
--- packages/forma-36-tokens/src/tokens/box-shadows/glows.js
+++ packages/forma-36-tokens/src/tokens/box-shadows/glows.js
@@ -1,10 +1,10 @@
 const colors = require('./../colors/colors-semantic.js');
 
 const glows = {
-  'glow-primary': `0px 0px 7px ${colors['color-primary']}`,
-  'glow-negative': `0px 0px 7px ${colors['color-negative']}`,
-  'glow-positive': `0px 0px 7px ${colors['color-positive']}`,
-  'glow-warning': `0px 0px 7px ${colors['color-warning']}`,
+  'glow-primary': `0px 0px 0px 3px ${colors['color-primary']}`,
+  'glow-negative': `0px 0px 0px 3px ${colors['color-negative']}`,
+  'glow-positive': `0px 0px 0px 3px ${colors['color-positive']}`,
+  'glow-warning': `0px 0px 0px 3px ${colors['color-warning']}`,
 };
 
 module.exports = glows;
