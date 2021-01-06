f0257a4e79e4cd831c6687ac2c2b0bc543f1251f
burakukula <trycjapa@gmail.com>
2021-01-06 13:06:21 +0100

    fix: refactor stories into new format

--- packages/forma-36-react-components/src/components/TextInput/TextInput.stories.tsx
+++ packages/forma-36-react-components/src/components/TextInput/TextInput.stories.tsx
@@ -3,173 +3,164 @@ import { storiesOf } from '@storybook/react';
 import { text, boolean, select, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
-import TextInput from './TextInput';
+import TextInput, { TextInputProps } from './TextInput';
 import Button from './../Button';
 import SectionHeading from '../Typography/SectionHeading';
 import Flex from '../Flex/Flex';
 
-storiesOf('Components/TextInput', module)
-  .addParameters({
-    propTypes: TextInput['__docgenInfo'],
-    component: TextInput,
-  })
-  .add('default', () => (
-    <TextInput
-      error={boolean('error', false)}
-      className={text('className', '')}
-      disabled={boolean('disabled', false)}
-      isReadOnly={boolean('isReadOnly', false)}
-      withCopyButton={boolean('withCopyButton', false)}
-      value={text('value', '123456')}
-      maxLength={number('maxLength', 50)}
-      width={select(
-        'width',
-        {
-          'Full (default)': 'full',
-          large: 'large',
-          medium: 'medium',
-          small: 'small',
-        },
-        'full',
-      )}
-      onChange={action('onChange')}
-      onBlur={action('onBlur')}
-      onCopy={action('onCopy')}
-      onKeyPress={action('onKeyPress')}
-      name="emailInput"
-      id="emailInput"
-      willBlurOnEsc={boolean('willBlurOnEsc', true)}
-    />
-  ))
-  .add('Controlling focus via ref', () => {
-    const textInputRef = React.createRef<HTMLInputElement>();
+export default {
+  title: 'Components/TextInput',
+  component: TextInput,
+  parameters: {
+    propTypes: [TextInput['__docgenInfo']],
+  },
+  argTypes: {
+    className: { control: { disable: true } },
+    testId: { control: { disable: true } },
+    isReadOnly: { control: { disable: true } },
+    onChange: { control: { disable: true } },
+    onBlur: { control: { disable: true } },
+    onCopy: { control: { disable: true } },
+    onKeyPress: { control: { disable: true } },
+  },
+};
 
-    return (
-      <React.Fragment>
-        <TextInput
-          error={boolean('error', false)}
-          className={text('className', '')}
-          disabled={boolean('disabled', false)}
-          isReadOnly={boolean('isReadOnly', false)}
-          withCopyButton={boolean('withCopyButton', false)}
-          value={text('value', '123456')}
-          maxLength={number('maxLength', 50)}
-          width={select(
-            'width',
-            {
-              'Full (default)': 'full',
-              large: 'large',
-              medium: 'medium',
-              small: 'small',
-            },
-            'full',
-          )}
-          onChange={action('onChange')}
-          onBlur={action('onBlur')}
-          onCopy={action('onCopy')}
-          onKeyPress={action('onKeyPress')}
-          name="emailInput"
-          id="emailInput"
-          inputRef={textInputRef}
-          willBlurOnEsc={boolean('willBlurOnEsc', true)}
-        />
-        <Button
-          onClick={() => (textInputRef.current as HTMLInputElement).focus()}
-        >
-          Focus TextInput with ref
-        </Button>
-      </React.Fragment>
-    );
-  })
-  .add('overview', () => (
+const ControlledTextInput = (args: TextInputProps) => {
+  const textInputRef = React.createRef<HTMLInputElement>();
+  return (
     <>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Text input default</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <TextInput
-          value="123456"
-          maxLength={50}
-          name="emailInput"
-          id="emailInput"
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Text input width small</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <TextInput
-          value="123456"
-          maxLength={50}
-          width="small"
-          name="emailInput"
-          id="emailInput"
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Text input width medium</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <TextInput
-          value="123456"
-          maxLength={50}
-          width="medium"
-          name="emailInput"
-          id="emailInput"
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Text input width large</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <TextInput
-          value="123456"
-          maxLength={50}
-          width="large"
-          name="emailInput"
-          id="emailInput"
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Text input with error</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <TextInput
-          error
-          value="123456"
-          maxLength={50}
-          width="large"
-          name="emailInput"
-          id="emailInput"
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Text input disabled</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <TextInput
-          disabled
-          value="123456"
-          maxLength={50}
-          width="large"
-          name="emailInput"
-          id="emailInput"
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">
-          Text input with copy button
-        </SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <TextInput
-          withCopyButton
-          value="123456"
-          maxLength={50}
-          width="large"
-          name="emailInput"
-          id="emailInput"
-        />
-      </Flex>
+      <TextInput
+        value={args.value}
+        width={args.width}
+        name="emailInput"
+        id="emailInput"
+        required={args.required}
+        error={args.error}
+        disabled={args.disabled}
+        maxLength={args.maxLength}
+        inputRef={textInputRef}
+      />
+      <Button
+        onClick={() => (textInputRef.current as HTMLInputElement).focus()}
+      >
+        Focus TextInput with ref
+      </Button>
     </>
-  ));
+  );
+};
+
+export const basic = (args: TextInputProps) => (
+  <TextInput
+    value={args.value}
+    width={args.width}
+    name="emailInput"
+    id="emailInput"
+    required={args.required}
+    error={args.error}
+    disabled={args.disabled}
+    maxLength={args.maxLength}
+  />
+);
+
+export const controlledTextInput = (args: TextInputProps) => (
+  <ControlledTextInput {...args} />
+);
+
+export const overview = () => (
+  <>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Text input default</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <TextInput
+        value="123456"
+        maxLength={50}
+        name="emailInput"
+        id="emailInput"
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Text input width small</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <TextInput
+        value="123456"
+        maxLength={50}
+        width="small"
+        name="emailInput"
+        id="emailInput"
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Text input width medium</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <TextInput
+        value="123456"
+        maxLength={50}
+        width="medium"
+        name="emailInput"
+        id="emailInput"
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Text input width large</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <TextInput
+        value="123456"
+        maxLength={50}
+        width="large"
+        name="emailInput"
+        id="emailInput"
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Text input with error</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <TextInput
+        error
+        value="123456"
+        maxLength={50}
+        width="large"
+        name="emailInput"
+        id="emailInput"
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Text input disabled</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <TextInput
+        disabled
+        value="123456"
+        maxLength={50}
+        width="large"
+        name="emailInput"
+        id="emailInput"
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Text input with copy button</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <TextInput
+        withCopyButton
+        value="123456"
+        maxLength={50}
+        width="large"
+        name="emailInput"
+        id="emailInput"
+      />
+    </Flex>
+  </>
+);
+basic.args = {
+  width: 'full',
+  value: '123456',
+  required: false,
+  error: false,
+  disabled: false,
+  maxLength: 50,
+};
--- packages/forma-36-react-components/src/components/Textarea/Textarea.stories.tsx
+++ packages/forma-36-react-components/src/components/Textarea/Textarea.stories.tsx
@@ -1,150 +1,131 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text, boolean, select, number } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
 
-import Textarea from './Textarea';
+import Textarea, { TextareaProps } from './Textarea';
 import Button from './../Button';
 import SectionHeading from '../Typography/SectionHeading';
 import Flex from '../Flex/Flex';
 
-storiesOf('Components/TextArea', module)
-  .addParameters({
-    propTypes: Textarea['__docgenInfo'],
-    component: Textarea,
-  })
-  .add('default', () => (
-    <Textarea
-      className={text('className', '')}
-      name="someInput"
-      id="someInput"
-      error={boolean('error', false)}
-      maxLength={number('maxLength', 50)}
-      required={boolean('required', false)}
-      width={select(
-        'width',
-        {
-          'Full (default)': 'full',
-          large: 'large',
-          medium: 'medium',
-          small: 'small',
-        },
-        'full',
-      )}
-      onChange={action('onChange')}
-      onBlur={action('onBlur')}
-      disabled={boolean('disabled', false)}
-      value={text('value', '123456')}
-      rows={number('rows', 2)}
-      willBlurOnEsc={boolean('willBlurOnEsc', true)}
-    />
-  ))
-  .add('Controlling focus via ref', () => {
-    const textareaRef = React.createRef<HTMLTextAreaElement>();
+export default {
+  title: 'Components/TextArea',
+  component: Textarea,
+  parameters: {
+    propTypes: [Textarea['__docgenInfo']],
+  },
+  argTypes: {
+    className: { control: { disable: true } },
+    testId: { control: { disable: true } },
+    onBlur: { control: { disable: true } },
+    onChange: { control: { disable: true } },
+    required: { control: { disable: true } },
+    error: { control: { disable: true } },
+    disabled: { control: { disable: true } },
+    maxLength: { control: { number: 50 } },
+    rows: { control: { number: 2 } },
+  },
+};
 
-    return (
-      <React.Fragment>
-        <Textarea
-          className={text('className', '')}
-          name="someInput"
-          id="someInput"
-          error={boolean('error', false)}
-          maxLength={number('maxLength', 50)}
-          required={boolean('required', false)}
-          width={select(
-            'width',
-            {
-              'Full (default)': 'full',
-              large: 'large',
-              medium: 'medium',
-              small: 'small',
-            },
-            'full',
-          )}
-          onChange={action('onChange')}
-          onBlur={action('onBlur')}
-          disabled={boolean('disabled', false)}
-          value={text('value', '123456')}
-          rows={number('rows', 2)}
-          willBlurOnEsc={boolean('willBlurOnEsc', true)}
-          textareaRef={textareaRef}
-        />
-        <Button
-          onClick={() => (textareaRef.current as HTMLTextAreaElement).focus()}
-        >
-          Focus Textarea with ref
-        </Button>
-      </React.Fragment>
-    );
-  })
-  .add('overview', () => (
-    <>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Textarea default</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <Textarea name="someInput" id="someInput" value="123456" rows={2} />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Textarea small width</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <Textarea
-          name="someInput"
-          id="someInput"
-          error={boolean('error', false)}
-          width="small"
-          value="123456"
-          rows={2}
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Textarea medium width</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <Textarea
-          name="someInput"
-          id="someInput"
-          width="medium"
-          value="123456"
-          rows={2}
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Textarea large width</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <Textarea
-          name="someInput"
-          id="someInput"
-          width="large"
-          value="123456"
-          rows={2}
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Textarea with error</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <Textarea
-          name="someInput"
-          id="someInput"
-          error
-          value="123456"
-          rows={2}
-        />
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Textarea disabled</SectionHeading>
-      </Flex>
-      <Flex marginBottom="spacingS">
-        <Textarea
-          name="someInput"
-          id="someInput"
-          disabled
-          value="123456"
-          rows={2}
-        />
-      </Flex>
-    </>
-  ));
+const ControlledTextarea = () => {
+  const textareaRef = React.createRef<HTMLTextAreaElement>();
+
+  return (
+    <React.Fragment>
+      <Textarea name="someInput" id="someInput" textareaRef={textareaRef} />
+      <Button
+        onClick={() => (textareaRef.current as HTMLTextAreaElement).focus()}
+      >
+        Focus Textarea with ref
+      </Button>
+    </React.Fragment>
+  );
+};
+
+export const basic = (args: TextareaProps) => (
+  <Textarea
+    name="someInput"
+    id="someInput"
+    width={args.width}
+    value={args.value}
+    required={args.required}
+    error={args.error}
+    disabled={args.disabled}
+    maxLength={args.maxLength}
+    rows={args.rows}
+  />
+);
+
+export const controlFocusViaRef = () => <ControlledTextarea />;
+
+export const overview = () => (
+  <>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Textarea default</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <Textarea name="someInput" id="someInput" value="123456" rows={2} />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Textarea small width</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <Textarea
+        name="someInput"
+        id="someInput"
+        width="small"
+        value="123456"
+        rows={2}
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Textarea medium width</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <Textarea
+        name="someInput"
+        id="someInput"
+        width="medium"
+        value="123456"
+        rows={2}
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Textarea large width</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <Textarea
+        name="someInput"
+        id="someInput"
+        width="large"
+        value="123456"
+        rows={2}
+      />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Textarea with error</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <Textarea name="someInput" id="someInput" error value="123456" rows={2} />
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Textarea disabled</SectionHeading>
+    </Flex>
+    <Flex marginBottom="spacingS">
+      <Textarea
+        name="someInput"
+        id="someInput"
+        disabled
+        value="123456"
+        rows={2}
+      />
+    </Flex>
+  </>
+);
+
+basic.args = {
+  width: 'full',
+  value: '123456',
+  required: false,
+  error: false,
+  disabled: false,
+  maxLength: 50,
+  rows: 2,
+};
