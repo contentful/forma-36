054ac16ef0a527f3e3dd095c798da30d0a282996
burakukula <trycjapa@gmail.com>
2021-01-06 10:40:26 +0100

    fix: rewrite story to new format

--- packages/forma-36-react-components/src/components/Tag/Tag.stories.tsx
+++ packages/forma-36-react-components/src/components/Tag/Tag.stories.tsx
@@ -1,58 +1,41 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text, select } from '@storybook/addon-knobs';
 
-import Tag, { tagType } from './Tag';
+import Tag, { tagType, TagProps } from './Tag';
 import SectionHeading from '../Typography/SectionHeading';
 import Flex from '../Flex/Flex';
 
-storiesOf('Components/Tag', module)
-  .addParameters({
-    propTypes: Tag['__docgenInfo'],
-    component: Tag,
-  })
-  .add('default', () => (
-    <Tag
-      tagType={select(
-        'tagType',
-        {
-          'Primary (default)': 'primary',
-          Positive: 'positive',
-          Negative: 'negative',
-          Warning: 'warning',
-          Secondary: 'secondary',
-          Muted: 'muted',
-        },
-        'primary',
-      )}
-      entityStatusType={select(
-        'Entity Status Type',
-        {
-          Published: 'published',
-          Archived: 'archived',
-          draft: 'draft',
-          changed: 'changed',
-          deleted: 'deleted',
-          'None (default)': undefined,
-        },
-        undefined,
-      )}
-      className={text('className', '')}
-    >
-      {text('Children', 'Published')}
-    </Tag>
-  ))
-  .add('overview', () => (
-    <>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Tag types overview</SectionHeading>
-      </Flex>
-      {tagType.map((type, idx) => (
-        <Flex marginBottom="spacingM" alignItems="center" key={idx}>
-          <Flex marginRight="spacingS">
-            <Tag tagType={type as any}>{type}</Tag>
-          </Flex>
+export default {
+  title: 'Components/Tag',
+  component: Tag,
+  parameters: {
+    propTypes: [Tag['__docgenInfo']],
+  },
+  argTypes: {
+    className: { control: { disable: true } },
+    testId: { control: { disable: true } },
+  },
+};
+
+export const basic = (args: TagProps) => (
+  <Tag tagType={args.tagType}>{args['Tag Text']}</Tag>
+);
+
+basic.args = {
+  tagType: 'primary',
+  'Tag Text': 'Published',
+};
+
+export const overview = () => (
+  <>
+    <Flex marginBottom="spacingS">
+      <SectionHeading element="h3">Tag types overview</SectionHeading>
+    </Flex>
+    {tagType.map((type, idx) => (
+      <Flex marginBottom="spacingM" alignItems="center" key={idx}>
+        <Flex marginRight="spacingS">
+          <Tag tagType={type}>{type}</Tag>
         </Flex>
-      ))}
-    </>
-  ));
+      </Flex>
+    ))}
+  </>
+);
