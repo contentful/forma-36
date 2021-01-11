1e4fd90b12ebec5b28530b90bcd2e73fef93c12b
Gui Santos <guilherme.santos@contentful.com>
2021-01-11 10:34:17 +0100

    IconButton documentation updates (#739)
    * fix: input visual adjustments
    
    * fix: update snapshots
    
    * refactor: stories to new sb API
    
    Co-authored-by: burakukula <trycjapa@gmail.com>

--- packages/forma-36-react-components/src/components/IconButton/IconButton.stories.tsx
+++ packages/forma-36-react-components/src/components/IconButton/IconButton.stories.tsx
@@ -1,90 +1,139 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text, select, boolean } from '@storybook/addon-knobs';
 
-import IconButton from './IconButton';
+import IconButton, { IconButtonProps } from './IconButton';
 import { iconName } from '../Icon/constants';
 import Flex from '../Flex/Flex';
 import SectionHeading from '../Typography/SectionHeading';
 import Paragraph from '../Typography/Paragraph';
 
 // TODO align colors between Icon and IconButton? (warning is missing in IconButton)
-export const iconButtonColors = [
-  'primary',
-  'positive',
-  'negative',
-  'muted',
-  'secondary',
-  'white',
-];
+enum IconButtonTypes {
+  'primary' = 'primary',
+  'positive' = 'positive',
+  'negative' = 'negative',
+  'muted' = 'muted',
+  'secondary' = 'secondary',
+  'white' = 'white',
+}
 
-storiesOf('Components/IconButton', module)
-  .addParameters({
-    propTypes: IconButton['__docgenInfo'],
-    component: IconButton,
-  })
-  .add('default', () => (
+export default {
+  title: 'Components/IconButton',
+  component: IconButton,
+  parameters: {
+    propTypes: [IconButton['__docgenInfo']],
+  },
+  argTypes: {
+    className: { control: { disable: true } },
+    testId: { control: { disable: true } },
+    iconProps: { control: { disable: true } },
+    icon: { control: { type: 'select', options: iconName } },
+    iconSize: {
+      control: {
+        type: 'select',
+        options: ['tiny', 'small', 'medium', 'large'],
+      },
+    },
+  },
+};
+
+interface Args extends IconButtonProps {
+  icon: IconButtonProps['iconProps']['icon'];
+  iconSize?: IconButtonProps['iconProps']['size'];
+}
+
+export const Basic = ({ icon, iconSize, ...args }: Args) => (
+  <IconButton iconProps={{ icon, size: iconSize }} {...args} />
+);
+Basic.args = {
+  icon: iconName.Star,
+  iconSize: 'medium',
+};
+
+export const Overview = () => (
+  <>
+    <Flex marginBottom="spacingXs">
+      <SectionHeading element="h3">IconButton colors</SectionHeading>
+    </Flex>
+    {Object.keys(IconButtonTypes).map((color, idx) => (
+      <Flex
+        padding="spacingXs"
+        alignItems="center"
+        style={{ backgroundColor: color === 'white' ? 'black' : 'transparent' }}
+        key={idx}
+      >
+        <Flex marginRight="spacingS">
+          <IconButton
+            iconProps={{
+              icon: 'Star',
+            }}
+            buttonType={IconButtonTypes[color]}
+            label="Add New Element"
+          />
+        </Flex>
+        <Paragraph style={{ color: color === 'white' ? 'white' : 'initial' }}>
+          {color}
+        </Paragraph>
+      </Flex>
+    ))}
+    <Flex marginTop="spacingL" marginBottom="spacingS">
+      <SectionHeading element="h3">IconButton sizes</SectionHeading>
+    </Flex>
+    <Flex flexDirection="row">
+      <Flex marginRight="spacingS">
+        <IconButton
+          iconProps={{
+            icon: 'Star',
+            size: 'tiny',
+          }}
+        />
+      </Flex>
+      <Flex marginRight="spacingS">
+        <IconButton
+          iconProps={{
+            icon: 'Star',
+            size: 'small',
+          }}
+        />
+      </Flex>
+      <Flex marginRight="spacingS">
+        <IconButton
+          iconProps={{
+            icon: 'Star',
+            size: 'medium',
+          }}
+        />
+      </Flex>
+      <Flex marginRight="spacingS">
+        <IconButton
+          iconProps={{
+            icon: 'Star',
+            size: 'large',
+          }}
+        />
+      </Flex>
+    </Flex>
+    <Flex marginTop="spacingL" marginBottom="spacingS">
+      <SectionHeading element="h3">IconButton disabled</SectionHeading>
+    </Flex>
     <IconButton
       iconProps={{
-        icon: select('icon', Object.keys(iconName), Object.keys(iconName)[0]),
+        icon: 'Star',
       }}
-      buttonType={select('buttonType', [
-        'primary',
-        'positive',
-        'negative',
-        'secondary',
-        'muted',
-        'white',
-      ])}
-      label={text('label (screenreader only)', 'Add New Element')}
-      disabled={boolean('disabled', false)}
-      withDropdown={boolean('withDropdown', false)}
-      className={text('className', '')}
+      buttonType="primary"
+      label="Add New Element"
+      disabled
     />
-  ))
-  .add('overview', () => (
-    <>
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">
-          Icon button colors overview
-        </SectionHeading>
-      </Flex>
-      {iconButtonColors.map((color, idx) => (
-        <Flex marginBottom="spacingM" alignItems="center" key={idx}>
-          <Flex marginRight="spacingS">
-            <IconButton
-              iconProps={{
-                icon: 'Calendar',
-              }}
-              buttonType={color as any}
-              label="Add New Element"
-            />
-          </Flex>
-          <Paragraph>{color}</Paragraph>
-        </Flex>
-      ))}
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Icon button disabled</SectionHeading>
-      </Flex>
-      <IconButton
-        iconProps={{
-          icon: 'Calendar',
-        }}
-        buttonType="primary"
-        label="Add New Element"
-        disabled
-      />
-      {/* Not sure what how this option should look like and where it is used */}
-      <Flex marginBottom="spacingS">
-        <SectionHeading element="h3">Icon button with dropdown</SectionHeading>
-      </Flex>
-      <IconButton
-        iconProps={{
-          icon: 'Calendar',
-        }}
-        buttonType="primary"
-        label="Add New Element"
-        withDropdown
-      />
-    </>
-  ));
+    {/* Not sure what how this option should look like and where it is used */}
+    <Flex marginTop="spacingL" marginBottom="spacingS">
+      <SectionHeading element="h3">IconButton with dropdown</SectionHeading>
+    </Flex>
+    <IconButton
+      iconProps={{
+        icon: 'Star',
+      }}
+      buttonType="primary"
+      label="Add New Element"
+      withDropdown
+    />
+  </>
+);
--- packages/forma-36-react-components/src/components/IconButton/IconButton.tsx
+++ packages/forma-36-react-components/src/components/IconButton/IconButton.tsx
@@ -6,10 +6,15 @@ import TabFocusTrap from '../TabFocusTrap';
 import styles from './IconButton.css';
 
 export interface IconButtonProps extends React.HTMLAttributes<HTMLElement> {
-  label: string;
-  href?: string;
+  /**
+   * It used to controls which icon to render and it accepts all props that you could pass in the Icon component.
+   *
+   * But the color prop won't work, since its value gets overwritten by the buttonType prop of the IconButton
+   */
   iconProps: IconProps;
-  disabled?: boolean;
+  /**
+   * It controls the color of the button
+   */
   buttonType?:
     | 'primary'
     | 'positive'
@@ -17,8 +22,29 @@ export interface IconButtonProps extends React.HTMLAttributes<HTMLElement> {
     | 'secondary'
     | 'muted'
     | 'white';
+  /**
+   * It renders a label with the icon, making it look like the naked button
+   */
+  label?: string;
+  /**
+   * It should be used when the button works as a link
+   */
+  href?: string;
+  /**
+   * If true, it will render the Chevron icon to indicate that the button opens a dropdown
+   */
   withDropdown?: boolean;
+  /**
+   * It puts the button in disabled state
+   */
+  disabled?: boolean;
+  /**
+   * Class names to be appended to the className prop of the Accordion wrapper
+   */
   className?: string;
+  /**
+   * An ID used for testing purposes applied as a data attribute (data-test-id)
+   */
   testId?: string;
 }
 
@@ -52,7 +78,7 @@ export const IconButton = ({
         {...iconProps}
         className={cn(styles.IconButton__icon, iconProps.className)}
       />
-      <span className={styles.IconButton__label}>{label}</span>
+      {label && <span className={styles.IconButton__label}>{label}</span>}
       {withDropdown && (
         <Icon
           icon="ChevronDown"
