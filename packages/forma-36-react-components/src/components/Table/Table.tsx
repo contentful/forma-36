75e20dc24bda95e40076c34feb65d13f176f8e1b
Kristoffer <denkristoffer@users.noreply.github.com>
2021-01-12 17:01:45 +0100

    feat(table): visual changes (#773)

--- packages/forma-36-react-components/src/components/Table/Table.tsx
+++ packages/forma-36-react-components/src/components/Table/Table.tsx
@@ -7,18 +7,22 @@ import styles from './Table.css';
 export interface TableProps extends HTMLProps<HTMLTableElement> {
   testId?: string;
   className?: string;
-  children: React.ReactNode;
+  children: React.ReactNode | React.ReactElement | React.ReactElement[];
+  layout?: 'inline' | 'embedded';
 }
 
 export const Table = ({
   className,
   children,
+  layout = 'inline',
   testId = 'cf-ui-table',
   ...otherProps
 }: TableProps) => {
   return (
     <table
-      className={cn(className, styles['Table'])}
+      className={cn(className, styles['Table'], {
+        [styles['Table--inline']]: layout === 'inline',
+      })}
       cellPadding="0"
       cellSpacing="0"
       data-test-id={testId}
