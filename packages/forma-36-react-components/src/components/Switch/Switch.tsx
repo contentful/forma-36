9ea620e4c96d907d5f081955192711df5132fc0c
burakukula <trycjapa@gmail.com>
2021-01-12 14:58:09 +0100

    fix: switch component visual update

--- packages/forma-36-react-components/src/components/Switch/Switch.tsx
+++ packages/forma-36-react-components/src/components/Switch/Switch.tsx
@@ -2,19 +2,21 @@ import React, { FunctionComponent, KeyboardEvent } from 'react';
 import FormLabel from '../FormLabel';
 import classNames from 'classnames';
 import styles from './Switch.css';
+import Icon from '../Icon';
 
 export interface SwitchProps {
   onToggle?: (isChecked: boolean) => void;
   isChecked?: boolean;
   isDisabled?: boolean;
   labelText: string;
+  size?: 'default' | 'small';
   className?: string;
   id: string;
 }
 
 export const Switch: FunctionComponent<SwitchProps> = (props: SwitchProps) => {
   return (
-    <div className={classNames(styles['Switch__wrapper'], props.className)}>
+    <div className={classNames(styles['Switch__wrapper'], {}, props.className)}>
       <input
         type="checkbox"
         onClick={onToggle}
@@ -24,17 +26,36 @@ export const Switch: FunctionComponent<SwitchProps> = (props: SwitchProps) => {
         className={classNames(styles['Switch'], {
           [styles['Switch--checked']]: props.isChecked,
           [styles['Switch--disabled']]: props.isDisabled,
+          [styles['Switch--small']]: props.size === 'small',
         })}
         id={props.id}
       />
-      <FormLabel
+      <label
         className={classNames(styles['Switch__label'], {
           [styles['Switch__label--disabled']]: props.isDisabled,
         })}
         htmlFor={props.id}
       >
+        <Icon
+          icon="Done"
+          color="white"
+          size="tiny"
+          className={classNames(
+            styles['Switch__icon'],
+            styles['Switch__icon--done'],
+          )}
+        />
         {props.labelText}
-      </FormLabel>
+        <Icon
+          icon="Close"
+          color="white"
+          size="tiny"
+          className={classNames(
+            styles['Switch__icon'],
+            styles['Switch__icon--close'],
+          )}
+        />
+      </label>
     </div>
   );
 
