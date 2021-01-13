c9653cf1dd05c687b0ebda671495d160949d49e6
Patrycja Radaczyńska <trycjapa@gmail.com>
2021-01-13 08:44:11 +0100

    fix: replace Chevron, Close and Done icons (#777)

--- packages/forma-36-react-components/src/components/Icon/svg/ChevronLeft.tsx
+++ packages/forma-36-react-components/src/components/Icon/svg/ChevronLeft.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 
 function SvgChevronLeft(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg height="1em" viewBox="0 0 24 24" width="1em" {...props}>
-      <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
-      <path d="M0 0h24v24H0z" fill="none" />
+    <svg viewBox="0 0 24 24" width="1em" height="1em" {...props}>
+      <path d="M15.3738 5.29289C15.7643 5.68342 15.7643 6.31658 15.3738 6.70711L10.0809 12L15.3738 17.2929C15.7643 17.6834 15.7643 18.3166 15.3738 18.7071C14.9833 19.0976 14.3501 19.0976 13.9596 18.7071L7.95956 12.7071C7.56904 12.3166 7.56904 11.6834 7.95956 11.2929L13.9596 5.29289C14.3501 4.90237 14.9833 4.90237 15.3738 5.29289Z" />
     </svg>
   );
 }
