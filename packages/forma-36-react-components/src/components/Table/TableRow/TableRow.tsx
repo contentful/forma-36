9997184f68a2c0c711494deb5afea7adc55976ba
denkristoffer <denkristoffer@users.noreply.github.com>
2021-01-22 11:25:33 +0100

    fix(table): move row styling to table cells
    Fixes #801

--- packages/forma-36-react-components/src/components/Table/TableRow/TableRow.tsx
+++ packages/forma-36-react-components/src/components/Table/TableRow/TableRow.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 import type { HTMLProps } from 'react';
 
-import styles from './TableRow.css';
+import styles from '../TableCell/TableCell/TableCell.css';
 
 export interface TableRowProps extends HTMLProps<HTMLTableRowElement> {
   className?: string;
@@ -20,7 +20,7 @@ export const TableRow = ({
 }: TableRowProps) => {
   return (
     <tr
-      className={cn(styles['TableRow'], className, {
+      className={cn(className, {
         [styles['TableRow--selected']]: selected,
       })}
       data-test-id={testId}
