4233f244ff748c69590dd35f6ada65252280343c
Nika Zawila <nika.zawila@contentful.com>
2021-01-26 08:56:43 +0100

    feat: handle tag change in the List component

--- packages/forma-36-react-components/src/components/List/List.stories.tsx
+++ packages/forma-36-react-components/src/components/List/List.stories.tsx
@@ -1,30 +1,92 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text } from '@storybook/addon-knobs';
 
-import List from './List';
+import List, { ListProps } from './List';
 import ListItem from './ListItem';
 import TextLink from '../TextLink';
+import Flex from '../Flex/Flex';
+import SectionHeading from '../Typography/SectionHeading';
 
-storiesOf('Components/List', module)
-  .addParameters({
+import notes from './README.mdx';
+
+export default {
+  title: 'Components/List',
+  component: List,
+  subcomponents: { ListItem },
+  parameters: {
     propTypes: [List['__docgenInfo'], ListItem['__docgenInfo']],
-    component: List,
-    subcomponents: { ListItem },
-  })
-  .add('default', () => (
-    <List className={text('className', '')}>
-      <ListItem>List Item 1</ListItem>
-      <ListItem>List Item 2</ListItem>
-      <ListItem>
-        List Item with a&nbsp;
-        <TextLink>text link</TextLink>&nbsp;
-      </ListItem>
-      <ListItem>
-        <List>
-          <ListItem>Sublist Item 1</ListItem>
-          <ListItem>Sublist Item 2</ListItem>
+    notes,
+  },
+  argTypes: {
+    element: {
+      table: { defaultValue: { summary: 'ul' } },
+      control: { type: 'select', options: ['ul', 'ol'] },
+    },
+    className: { control: { disable: true } },
+    testId: { control: { disable: true } },
+    children: { control: { disable: true } },
+  },
+};
+
+export const basic = ({ ...args }: ListProps) => (
+  <List {...args}>
+    <ListItem>List Item 1</ListItem>
+    <ListItem>List Item 2</ListItem>
+    <ListItem>
+      List Item with a&nbsp;
+      <TextLink>text link</TextLink>&nbsp;
+    </ListItem>
+    <ListItem>
+      <List>
+        <ListItem>Sublist Item 1</ListItem>
+        <ListItem>Sublist Item 2</ListItem>
+      </List>
+    </ListItem>
+  </List>
+);
+
+export const overview = ({ ...args }: ListProps) => (
+  <>
+    <Flex flexDirection="column" marginBottom="spacingM" fullWidth>
+      <Flex marginBottom="spacingS">
+        <SectionHeading>Unordered List</SectionHeading>
+      </Flex>
+      <Flex>
+        <List {...args}>
+          <ListItem>List Item 1</ListItem>
+          <ListItem>List Item 2</ListItem>
+          <ListItem>
+            List Item with a&nbsp;
+            <TextLink>text link</TextLink>&nbsp;
+          </ListItem>
+          <ListItem>
+            <List>
+              <ListItem>Sublist Item 1</ListItem>
+              <ListItem>Sublist Item 2</ListItem>
+            </List>
+          </ListItem>
         </List>
-      </ListItem>
-    </List>
-  ));
+      </Flex>
+    </Flex>
+    <Flex flexDirection="column">
+      <Flex marginBottom="spacingS">
+        <SectionHeading>Ordered List</SectionHeading>
+      </Flex>
+      <Flex>
+        <List element="ol" {...args}>
+          <ListItem>List Item 1</ListItem>
+          <ListItem>List Item 2</ListItem>
+          <ListItem>
+            List Item with a&nbsp;
+            <TextLink>text link</TextLink>&nbsp;
+          </ListItem>
+          <ListItem>
+            <List element="ol">
+              <ListItem>Sublist Item 1</ListItem>
+              <ListItem>Sublist Item 2</ListItem>
+            </List>
+          </ListItem>
+        </List>
+      </Flex>
+    </Flex>
+  </>
+);
