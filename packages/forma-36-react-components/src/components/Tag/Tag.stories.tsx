6f0eba59eb32bc2ce19407bc3e1a83822f2e9c96
Patrycja Radaczyńska <trycjapa@gmail.com>
2021-02-24 08:47:50 +0100

    feat: introducing size property in Tag component (#838)

--- packages/forma-36-react-components/src/components/Tag/Tag.stories.tsx
+++ packages/forma-36-react-components/src/components/Tag/Tag.stories.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Tag, { TagProps } from './Tag';
 import SectionHeading from '../Typography/SectionHeading';
 import Flex from '../Flex/Flex';
+import tokens from '@contentful/forma-36-tokens';
+import { Paragraph } from '../Typography/Paragraph/Paragraph';
 
 export default {
   title: 'Components/Tag',
@@ -31,42 +33,67 @@ basic.args = {
 
 export const overview = () => (
   <>
+    <Flex marginBottom="spacingM" flexDirection="column">
+      <Flex marginBottom="spacingS">
+        <SectionHeading element="h3">Tag types overview</SectionHeading>
+      </Flex>
+      <Flex marginBottom="spacingM" alignItems="center">
+        <Flex marginRight="spacingS">
+          <Tag tagType="primary">primary</Tag>
+        </Flex>
+      </Flex>
+      <Flex marginBottom="spacingM" alignItems="center">
+        <Flex marginRight="spacingS">
+          <Tag tagType="primary-filled">primary filled</Tag>
+        </Flex>
+      </Flex>
+      <Flex marginBottom="spacingM" alignItems="center">
+        <Flex marginRight="spacingS">
+          <Tag tagType="positive">positive</Tag>
+        </Flex>
+      </Flex>
+      <Flex marginBottom="spacingM" alignItems="center">
+        <Flex marginRight="spacingS">
+          <Tag tagType="negative">negative</Tag>
+        </Flex>
+      </Flex>
+      <Flex marginBottom="spacingM" alignItems="center">
+        <Flex marginRight="spacingS">
+          <Tag tagType="warning">warning</Tag>
+        </Flex>
+      </Flex>
+      <Flex marginBottom="spacingM" alignItems="center">
+        <Flex marginRight="spacingS">
+          <Tag tagType="secondary">secondary</Tag>
+        </Flex>
+      </Flex>
+      <Flex marginBottom="spacingM" alignItems="center">
+        <Flex marginRight="spacingS">
+          <Tag tagType="muted">muted</Tag>
+          <Paragraph style={{ color: `${tokens.colorRedBase}` }}>
+            {' '}
+            - this tagType is deprecated, please use <strong>
+              secondary
+            </strong>{' '}
+            instead
+          </Paragraph>
+        </Flex>
+      </Flex>
+    </Flex>
+
     <Flex marginBottom="spacingS">
-      <SectionHeading element="h3">Tag types overview</SectionHeading>
+      <SectionHeading element="h3">Tag sizes overview</SectionHeading>
     </Flex>
     <Flex marginBottom="spacingM" alignItems="center">
       <Flex marginRight="spacingS">
-        <Tag tagType="primary">primary</Tag>
+        <Tag tagType="primary-filled">default</Tag>
       </Flex>
     </Flex>
     <Flex marginBottom="spacingM" alignItems="center">
       <Flex marginRight="spacingS">
-        <Tag tagType="primary-filled">primary filled</Tag>
-      </Flex>
-    </Flex>
-    <Flex marginBottom="spacingM" alignItems="center">
-      <Flex marginRight="spacingS">
-        <Tag tagType="positive">positive</Tag>
-      </Flex>
-    </Flex>
-    <Flex marginBottom="spacingM" alignItems="center">
-      <Flex marginRight="spacingS">
-        <Tag tagType="negative">negative</Tag>
-      </Flex>
-    </Flex>
-    <Flex marginBottom="spacingM" alignItems="center">
-      <Flex marginRight="spacingS">
-        <Tag tagType="warning">warning</Tag>
-      </Flex>
-    </Flex>
-    <Flex marginBottom="spacingM" alignItems="center">
-      <Flex marginRight="spacingS">
-        <Tag tagType="muted">muted</Tag>
-      </Flex>
-    </Flex>
-    <Flex marginBottom="spacingM" alignItems="center">
-      <Flex marginRight="spacingS">
-        <Tag tagType="secondary">secondary</Tag>
+        <Tag tagType="primary-filled" size="small">
+          small
+        </Tag>
       </Flex>
     </Flex>
   </>
