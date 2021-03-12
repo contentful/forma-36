9f0236cc991ea8fe2dbbfceb1a89005597f5ca67
Alex Suevalov <alex.suevalov@contentful.com>
2021-03-12 14:35:11 +0100

    fix: import typo

--- packages/forma-36-react-components/src/components/Flex/Flex.tsx
+++ packages/forma-36-react-components/src/components/Flex/Flex.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import tokens from '@contentful/forma-36-tokens';
+import tokens from '@contentful/f36-tokens';
 import cn from 'classnames';
 import { Primitive } from '@contentful/f36-core';
 import type { PolymorphicComponentProps } from '@contentful/f36-core';
