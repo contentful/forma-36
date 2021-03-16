7a2937b2d0537093112c19a1a45624026e0d5391
Alex Suevalov <alex.suevalov@contentful.com>
2021-03-16 08:07:27 +0100

    chore: finish jest setup

--- jest.config.js
+++ jest.config.js
@@ -158,7 +158,9 @@ module.exports = {
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx|mjs)$',
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
