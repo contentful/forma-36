60536236f0e1408d1bb901a61e17a9ebf1bda6b2
Alex Suevalov <alex.suevalov@contentful.com>
2021-03-23 22:28:08 +0100

    chore: update snapshots

--- packages/components/utils/src/TabFocusTrap/TabFocusTrap.tsx
+++ packages/components/utils/src/TabFocusTrap/TabFocusTrap.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { cx, css } from 'emotion';
 
 export interface TabFocusTrapProps {
   className?: string;
   children: React.ReactNode;
 }
 
+const styles = {
+  root: css({
+    display: 'inherit',
+    outline: 0,
+  }),
+};
+
 export function TabFocusTrap({
+  className,
   children,
   ...otherProps
 }: TabFocusTrapProps): React.ReactElement {
   return (
-    <span
-      tabIndex={-1}
-      css={{ display: 'inherit', outline: 0 }}
-      {...otherProps}
-    >
+    <span tabIndex={-1} className={cx(styles.root, className)} {...otherProps}>
       {children}
     </span>
   );
