3e814b395d6cbacd76053338f70e70697280006e
Thomas Kellermeier <thomas.kellermeier@contentful.com>
2021-05-14 18:17:31 +0200

    fix: correctly handle ref forwarding

--- packages/components/forms/src/checkbox-field/CheckboxField.tsx
+++ packages/components/forms/src/checkbox-field/CheckboxField.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { ControlledInputField } from '../controlled-input-field';
 import type { ControlledInputFieldProps } from '../controlled-input-field';
 
-export type CheckboxFieldProps = Omit<
-  ControlledInputFieldProps,
-  'inputType' | 'ref'
->;
+export type CheckboxFieldProps = Omit<ControlledInputFieldProps, 'inputType'>;
 
-export const _CheckboxField = ({
-  testId = 'cf-ui-checkbox-field',
-  ...otherProps
-}: CheckboxFieldProps) => {
+export const _CheckboxField = (
+  { testId = 'cf-ui-checkbox-field', ...otherProps }: CheckboxFieldProps,
+  ref: React.Ref<HTMLDivElement>,
+) => {
   return (
     <ControlledInputField
       testId={testId}
       {...otherProps}
+      ref={ref}
       inputType="checkbox"
     />
   );
