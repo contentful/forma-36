e737f84530e8ab8672a2fde6019678ee49565576
Nika Zawila <nika.zawila@contentful.com>
2021-05-20 10:02:15 +0200

    feat: extract EmptyState to a separate package

---
+++ packages/components/empty-state/src/EmptyState.styles.ts
@@ -0,0 +1,29 @@
+import { css } from 'emotion';
+import tokens from '@contentful/f36-tokens';
+
+export const styles = {
+  emptyState: css({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+  }),
+  element: css({
+    maxWidth: '32rem',
+  }),
+  container: css({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+  }),
+  illustrationContainer: css({
+    marginMottom: tokens.spacingM,
+  }),
+  illustration: css({
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }),
+};
