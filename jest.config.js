a8e784c0a708a6ff8f86b1e95e882b7333126c48
Kristoffer <denkristoffer@users.noreply.github.com>
2021-05-21 18:59:45 +0200

    v4 chore(deps): switch function specific lodash packages to lodash-es (#975)

--- jest.config.js
+++ jest.config.js
@@ -160,7 +160,7 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx|mjs)$',
+    '[/\\\\]node_modules[/\\\\](?!lodash-es).+\\.(js|jsx|ts|tsx|mjs)$',
   ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
