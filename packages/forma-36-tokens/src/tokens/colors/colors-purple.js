9ae3024219f156001e694a388bcb19aaf64aac63
burakukula <trycjapa@gmail.com>
2021-07-06 17:02:45 +0200

    fix: split files

--- packages/forma-36-tokens/src/tokens/colors/colors-purple.js
+++ packages/forma-36-tokens/src/tokens/colors/colors-purple.js
@@ -1,22 +1,5 @@
-const scalePurple = {
-  'purple-100': '#F7F2FF',
-  'purple-200': '#EDE3FF',
-  'purple-300': '#D1BBFF',
-  'purple-400': '#AE89FF',
-  'purple-500': '#8553E7',
-  'purple-600': '#6C3ECF',
-  'purple-700': '#5127B5',
-  'purple-800': '#3E16A4',
-  'purple-900': '#340E9C',
-};
-
-const colorsPurple = {
-  'color-purple-dark': scalePurple['purple-700'],
-  'color-purple-base': scalePurple['purple-600'],
-  'color-purple-mid': scalePurple['purple-500'],
-  'color-purple-light': scalePurple['purple-300'],
-  'color-purple-lightest': scalePurple['purple-100'],
-};
+const scalePurple = require('./scale-purple');
+const colorsPurple = require('./colors-purple-deprecated');
 
 module.exports = {
   ...colorsPurple,
--- packages/forma-36-tokens/src/tokens/colors/colors-yellow.js
+++ packages/forma-36-tokens/src/tokens/colors/colors-yellow.js
@@ -1,21 +1,4 @@
-const scaleYellow = {
-  'yellow-100': '#FEF9DF',
-  'yellow-200': '#FFF6CC',
-  'yellow-300': '#FFE993',
-  'yellow-400': '#FFD960',
-  'yellow-500': '#FFC835',
-  'yellow-600': '#EAAF09',
-  'yellow-700': '#B78300',
-  'yellow-800': '#956300',
-  'yellow-900': '#7F5200',
-};
-
-const colorsYellow = {
-  'color-yellow-dark': scaleYellow['yellow-800'],
-  'color-yellow-base': scaleYellow['yellow-600'],
-  'color-yellow-mid': scaleYellow['yellow-500'],
-  'color-yellow-light': scaleYellow['yellow-400'],
-  'color-yellow-lightest': scaleYellow['yellow-200'],
-};
+const scaleYellow = require('./scale-yellow');
+const colorsYellow = require('./colors-yellow-deprecated');
 
 module.exports = { ...scaleYellow, ...colorsYellow };
