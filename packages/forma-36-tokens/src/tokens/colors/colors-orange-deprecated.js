30ed4c2d94aa8984a6dc32923fa326e313adb183
burakukula <trycjapa@gmail.com>
2021-07-07 12:08:21 +0200

    fix: cleanup

--- packages/forma-36-tokens/src/tokens/colors/colors-orange-deprecated.js
+++ packages/forma-36-tokens/src/tokens/colors/colors-orange-deprecated.js
@@ -1,14 +1,4 @@
-const scaleOrange = {
-  'orange-100': '#FFF2E4',
-  'orange-200': '#FDE5C0',
-  'orange-300': '#FDB882',
-  'orange-400': '#F07F23',
-  'orange-500': '#CC4500',
-  'orange-600': '#B12D00',
-  'orange-700': '#892300',
-  'orange-800': '#731A00',
-  'orange-900': '#631C00',
-};
+const scaleOrange = require('./scale-orange');
 
 const colorsOrange = {
   'color-orange-dark': scaleOrange['orange-600'],
@@ -18,7 +8,4 @@ const colorsOrange = {
   'color-orange-lightest': scaleOrange['orange-100'],
 };
 
-module.exports = {
-  ...colorsOrange,
-  ...scaleOrange,
-};
+module.exports = colorsOrange;
--- packages/forma-36-tokens/src/tokens/colors/colors-red.js
+++ packages/forma-36-tokens/src/tokens/colors/colors-red.js
@@ -1,22 +1,5 @@
-const scaleRed = {
-  'red-100': '#FFF2F2',
-  'red-200': '#FFE0E0',
-  'red-300': '#FFB1B2',
-  'red-400': '#FF707D',
-  'red-500': '#DA294A',
-  'red-600': '#BD002A',
-  'red-700': '#990017',
-  'red-800': '#7F0010',
-  'red-900': '#72000E',
-};
-
-const colorsRed = {
-  'color-red-dark': scaleRed['red-700'],
-  'color-red-base': scaleRed['red-600'],
-  'color-red-mid': scaleRed['red-500'],
-  'color-red-light': scaleRed['red-300'],
-  'color-red-lightest': scaleRed['red-100'],
-};
+const scaleRed = require('./scale-red');
+const colorsRed = require('./colors-red-deprecated');
 
 module.exports = {
   ...colorsRed,
