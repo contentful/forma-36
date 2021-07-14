44aa42f30bb1edea9d6009b513a18b606594d85f
Renato Massao Yonamine <massao.yonamine@contentful.com>
2021-07-14 11:11:30 +0200

    feat(codemod): add initial cli for the forma-36-codemod package

---
+++ packages/forma-36-codemod/bin/cli.js
@@ -0,0 +1,191 @@
+const path = require('path');
+const chalk = require('chalk');
+const execa = require('execa');
+const isGitClean = require('is-git-clean');
+const inquirer = require('inquirer');
+const meow = require('meow');
+const globby = require('globby');
+const inquirerChoices = require('./inquirer-choices');
+
+const transformerDirectory = path.join(__dirname, '../', 'transforms');
+const jscodeshiftExecutable = require.resolve('.bin/jscodeshift');
+
+function checkGitStatus(force) {
+  let clean = false;
+  try {
+    clean = isGitClean.sync(process.cwd());
+  } catch (err) {
+    if (err && err.stderr && err.stderr.indexOf('Not a git repository') >= 0) {
+      if (!force) {
+        console.log(
+          chalk.yellow(
+            'Not a git repository, you may use the --force flag to override this check',
+          ),
+        );
+        process.exit(1);
+      }
+      clean = true;
+    }
+  }
+
+  if (!clean) {
+    console.log(
+      chalk.yellow(
+        'Before we continue, please stash or commit your git changes.',
+      ),
+    );
+    process.exit(1);
+  }
+}
+
+function runTransform({ files, flags, parser, transformer }) {
+  const transformerPath = path.join(transformerDirectory, `${transformer}.js`);
+
+  let args = [];
+
+  const { dry, print, explicitRequire } = flags;
+
+  if (dry) {
+    args.push('--dry');
+  }
+  if (print) {
+    args.push('--print');
+  }
+
+  if (explicitRequire === 'false') {
+    args.push('--explicit-require=false');
+  }
+
+  args.push('--verbose=2');
+
+  args.push('--ignore-pattern=**/node_modules/**');
+
+  args.push('--parser', parser);
+
+  if (parser === 'tsx') {
+    args.push('--extensions=tsx,ts,jsx,js');
+  } else {
+    args.push('--extensions=jsx,js');
+  }
+
+  args = args.concat(['--transform', transformerPath]);
+
+  if (flags.jscodeshift) {
+    args = args.concat(flags.jscodeshift);
+  }
+
+  args = args.concat(files);
+
+  console.log(`Executing command: jscodeshift ${args.join(' ')}`);
+
+  const result = execa.sync(jscodeshiftExecutable, args, {
+    stdio: 'inherit',
+    stripEof: false,
+  });
+
+  if (result.error) {
+    throw result.error;
+  }
+}
+
+function expandFilePathsIfNeeded(files) {
+  const shouldExpandFiles = files.some((file) => file.includes('*'));
+  return shouldExpandFiles ? globby.sync(files) : files;
+}
+
+function run() {
+  const cli = meow(
+    {
+      description: 'Codemods for updating Forma-36 codes',
+      help: `
+    Usage
+      $ npx @contentful/forma-36-codemod <transform> <path> <...options>
+        transform        Currently only color-tokens-to-new-tokens.
+        path             Files or directory to transform. Can be a glob like src/**.test.js
+    Options
+      --force            Bypass Git repository safety checks and forcibly run codemods
+      --dry              Dry run (no changes are made to files)
+      --print            Print transformed files to your terminal
+      --jscodeshift      (Advanced) Pass options directly to jscodeshift
+    `,
+    },
+    {
+      boolean: ['force', 'dry', 'print', 'help'],
+      string: ['_'],
+      alias: {
+        h: 'help',
+      },
+    },
+  );
+
+  if (!cli.flags.dry) {
+    checkGitStatus(cli.flags.force);
+  }
+
+  if (
+    cli.input[0] &&
+    !inquirerChoices.TRANSFORMS_CHOICES.find((x) => x.value === cli.input[0])
+  ) {
+    console.error('Invalid transform choice, pick one of:');
+    console.error(
+      inquirerChoices.TRANSFORMS_CHOICES.map((x) => '- ' + x.value).join('\n'),
+    );
+    process.exit(1);
+  }
+
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'files',
+        message: 'On which files or directory should the codemods be applied?',
+        when: !cli.input[1],
+        default: '.',
+        filter: (files) => files.trim(),
+      },
+      {
+        type: 'list',
+        name: 'parser',
+        message: 'Which dialect of JavaScript do you use?',
+        default: 'babel',
+        when: !cli.flags.parser,
+        pageSize: inquirerChoices.PARSER_CHOICES.length,
+        choices: inquirerChoices.PARSER_CHOICES,
+      },
+      {
+        type: 'list',
+        name: 'transformer',
+        message: 'Which transform would you like to apply?',
+        when: !cli.input[0],
+        pageSize: inquirerChoices.TRANSFORMS_CHOICES.length,
+        choices: inquirerChoices.TRANSFORMS_CHOICES,
+      },
+    ])
+    .then((answers) => {
+      const { files, transformer, parser } = answers;
+
+      const filesBeforeExpansion = cli.input[1] || files;
+      const filesExpanded = expandFilePathsIfNeeded([filesBeforeExpansion]);
+
+      const selectedTransformer = cli.input[0] || transformer;
+      const selectedParser = cli.flags.parser || parser;
+
+      if (!filesExpanded.length) {
+        console.log(
+          `No files found matching ${filesBeforeExpansion.join(' ')}`,
+        );
+        return null;
+      }
+
+      return runTransform({
+        files: filesExpanded,
+        flags: cli.flags,
+        parser: selectedParser,
+        transformer: selectedTransformer,
+      });
+    });
+}
+
+module.exports = {
+  run,
+};
---
+++ packages/forma-36-codemod/transforms/color-tokens-to-new-tokens.js
@@ -0,0 +1,84 @@
+const mapping = {
+  colorBlueLightest: 'blue100',
+  colorBlueLight: 'blue300',
+  colorBlueMid: 'blue500',
+  colorBlueBase: 'blue600',
+  colorBlueDark: 'blue700',
+  colorContrastDark: 'colorBlack',
+  colorContrastMid: 'gray900',
+  colorContrastLight: 'gray800',
+  colorCoralDark: 'red200',
+  colorCoralMid: 'red100',
+  colorElementDarkest: 'gray500',
+  colorElementDark: 'gray400',
+  colorElementMid: 'gray300',
+  colorElementLight: 'gray200',
+  colorElementLightest: 'gray100',
+  colorGreenDark: 'green700',
+  colorGreenBase: 'green600',
+  colorGreenMid: 'green500',
+  colorGreenLight: 'green300',
+  colorGreenLightest: 'green100',
+  colorIceDark: 'blue200',
+  colorIceMid: 'blue100',
+  colorMintDark: 'green200',
+  colorMintMid: 'green100',
+  colorOrangeDark: 'orange600',
+  colorOrangeBase: 'orange500',
+  colorOrangeMid: 'orange400',
+  colorOrangeLight: 'orange200',
+  colorOrangeLightest: 'orange100',
+  colorPeachDark: 'orange200',
+  colorPeachMid: 'orange100',
+  colorPurpleDark: 'purple700',
+  colorPurpleBase: 'purple600',
+  colorPurpleMid: 'purple500',
+  colorPurpleLight: 'purple300',
+  colorPurpleLightest: 'purple100',
+  colorRedDark: 'red700',
+  colorRedBase: 'red600',
+  colorRedMid: 'red500',
+  colorRedLight: 'red300',
+  colorRedLightest: 'red100',
+  colorTextDark: 'gray900',
+  colorTextBase: 'gray800',
+  colorTextMid: 'gray700',
+  colorTextLight: 'gray600',
+  colorTextLightest: 'gray500',
+  colorYellowDark: 'yellow800',
+  colorYellowBase: 'yellow600',
+  colorYellowMid: 'yellow500',
+  colorYellowLight: 'yellow400',
+  colorYellowLightest: 'yellow200',
+};
+
+const allReplaceable = Object.keys(mapping);
+
+module.exports = function (file, api) {
+  const j = api.jscodeshift;
+
+  let localImportName = 'tokens';
+
+  j(file.source)
+    .find(j.Identifier)
+    .filter(
+      (path) =>
+        path.parent.node.type === 'ImportDefaultSpecifier' &&
+        path.parent.parent.node.source.value === '@contentful/forma-36-tokens',
+    )
+    .forEach((path) => {
+      localImportName = path.parent.node.local.name;
+    });
+
+  return j(file.source)
+    .find(j.Identifier)
+    .filter(
+      (path) =>
+        allReplaceable.includes(path.value.name) &&
+        path.parent.node.object?.name === localImportName,
+    )
+    .forEach((path) => {
+      j(path).replaceWith(j.identifier(mapping[path.node.name]));
+    })
+    .toSource();
+};
