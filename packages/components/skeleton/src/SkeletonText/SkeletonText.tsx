03804d6472c0c9926f7db91f96a68079b6c17530
Alex Suevalov <alex.suevalov@contentful.com>
2021-08-03 10:54:12 +0200

    chore: enhance documentation

--- packages/components/skeleton/src/SkeletonText/SkeletonText.tsx
+++ packages/components/skeleton/src/SkeletonText/SkeletonText.tsx
@@ -3,11 +3,29 @@ import React, { useCallback } from 'react';
 type stringOrNumber = string | number;
 
 export interface SkeletonTextProps {
+  /**
+   * A number of skeleton likes
+   */
   numberOfLines?: number;
+  /**
+   * A distance between top of the container and the first line (in pixels)
+   */
   offsetTop?: stringOrNumber;
+  /**
+   * A distance between left of the container and the beginning of lines (in pixels)
+   */
   offsetLeft?: stringOrNumber;
+  /**
+   * A height of a one line (in pixels)
+   */
   lineHeight?: stringOrNumber;
+  /**
+   * Spacing between lines (in pixels)
+   */
   marginBottom?: stringOrNumber;
+  /**
+   * A width of a line
+   */
   width?: stringOrNumber;
 }
 
