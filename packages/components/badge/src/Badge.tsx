daa028aa20789b9cf87a0190303a6ab0ae274dde
Alex Suevalov <alex.suevalov@contentful.com>
2021-08-05 13:44:59 +0200

    chore: change Badge typings

--- packages/components/badge/src/Badge.tsx
+++ packages/components/badge/src/Badge.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { cx } from 'emotion';
 import { Box } from '@contentful/f36-core';
-import type { CommonProps } from '@contentful/f36-core';
+import type {
+  CommonProps,
+  PolymorphicComponentProps,
+} from '@contentful/f36-core';
 
 import type { BadgeSize, BadgeVariant } from './types';
 import { getBadgeStyles } from './getBadgeStyles';
 
-export interface BadgeProps extends CommonProps {
+export interface BadgeInternalProps extends CommonProps {
   /**
    * Sets the size of the component
    * @default default
@@ -21,28 +24,32 @@ export interface BadgeProps extends CommonProps {
   children: React.ReactNode;
 }
 
-const _Badge = (props: BadgeProps, ref: React.Ref<HTMLDivElement>) => {
-  const {
-    children,
-    variant = 'primary',
-    size = 'default',
-    testId = 'cf-ui-badge',
-    className,
-    ...otherProps
-  } = props;
+export type BadgeProps = PolymorphicComponentProps<'div', BadgeInternalProps>;
 
-  return (
-    <Box
-      as="div"
-      testId={testId}
-      display="inline-block"
-      className={cx(getBadgeStyles({ variant, size }), className)}
-      {...otherProps}
-      ref={ref}
-    >
-      {children}
-    </Box>
-  );
-};
+export const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(
+  (props, ref) => {
+    const {
+      children,
+      variant = 'primary',
+      size = 'default',
+      testId = 'cf-ui-badge',
+      className,
+      ...otherProps
+    } = props;
 
-export const Badge = React.forwardRef(_Badge);
+    return (
+      <Box
+        as="div"
+        testId={testId}
+        display="inline-block"
+        className={cx(getBadgeStyles({ variant, size }), className)}
+        {...otherProps}
+        ref={ref}
+      >
+        {children}
+      </Box>
+    );
+  },
+);
+
+Badge.displayName = 'Badge';
