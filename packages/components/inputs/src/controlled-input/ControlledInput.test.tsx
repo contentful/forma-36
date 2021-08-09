426f7b804c5d44d0cf65cd81ad1d400515376660
Renato Massao Yonamine <massao.yonamine@contentful.com>
2021-08-09 14:29:05 +0200

    test(inputs): refactor tests on inputs package to remove snapshots

--- packages/components/inputs/src/controlled-input/ControlledInput.test.tsx
+++ packages/components/inputs/src/controlled-input/ControlledInput.test.tsx
@@ -4,102 +4,107 @@ import { axe } from '@/scripts/test/axeHelper';
 
 import { ControlledInput } from './ControlledInput';
 
-it('renders the component with all required props', () => {
-  const { container } = render(
-    <ControlledInput id="ControlledInput" label="ControlledInput" />,
-  );
+describe('ControlledInput', function () {
+  it('renders the component with all required props', () => {
+    const { getByLabelText } = render(
+      <ControlledInput id="ControlledInput" label="ControlledInput" />,
+    );
 
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with an additional class name', () => {
-  const { container } = render(
-    <ControlledInput
-      className="my-extra-class"
-      id="ControlledInput"
-      label="ControlledInput"
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with required prop', () => {
-  const { container } = render(
-    <ControlledInput
-      className="my-extra-class"
-      id="ControlledInput"
-      label="ControlledInput"
-      isRequired
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with disabled prop', () => {
-  const { container } = render(
-    <ControlledInput
-      className="my-extra-class"
-      id="ControlledInput"
-      label="ControlledInput"
-      isDisabled
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with as checked', () => {
-  const { container } = render(
-    <ControlledInput
-      className="my-extra-class"
-      id="ControlledInput"
-      label="ControlledInput"
-      isChecked
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with a value', () => {
-  const { container } = render(
-    <ControlledInput
-      className="my-extra-class"
-      id="ControlledInput"
-      label="ControlledInput"
-      value="someValue"
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('has no a11y issues', async () => {
-  const { container } = render(
-    <ControlledInput id="ControlledInput" label="ControlledInput" />,
-  );
-  const results = await axe(container);
-
-  expect(results).toHaveNoViolations();
-});
-
-it('can blur when clicking escape', () => {
-  const onBlur = jest.fn();
-  const { getByRole } = render(
-    <ControlledInput
-      id="ControlledInput"
-      label="ControlledInput"
-      onBlur={onBlur}
-      canBlurOnEsc
-      type="checkbox"
-    />,
-  );
-  const input = getByRole('checkbox');
-  fireEvent.keyDown(input, {
-    code: 'Escape',
-    target: { blur: onBlur },
+    const input = getByLabelText('ControlledInput');
+    expect(input).toBeTruthy();
   });
-  expect(onBlur).toHaveBeenCalled();
+
+  it('renders the component with an additional class name', () => {
+    const { container } = render(
+      <ControlledInput
+        className="my-extra-class"
+        id="ControlledInput"
+        label="ControlledInput"
+      />,
+    );
+
+    expect(container.firstChild).toHaveClass('my-extra-class');
+  });
+
+  it('renders the component with required prop', () => {
+    const { getByLabelText } = render(
+      <ControlledInput
+        className="my-extra-class"
+        id="ControlledInput"
+        label="ControlledInput"
+        isRequired
+      />,
+    );
+
+    expect(getByLabelText('ControlledInput')).toBeRequired();
+  });
+
+  it('renders the component with disabled prop', () => {
+    const { getByLabelText } = render(
+      <ControlledInput
+        className="my-extra-class"
+        id="ControlledInput"
+        label="ControlledInput"
+        isDisabled
+      />,
+    );
+
+    expect(getByLabelText('ControlledInput')).toBeDisabled();
+  });
+
+  it('renders the component with as checked', () => {
+    const { getByLabelText } = render(
+      <ControlledInput
+        className="my-extra-class"
+        id="ControlledInput"
+        label="ControlledInput"
+        isChecked
+      />,
+    );
+
+    expect(getByLabelText('ControlledInput')).toBeChecked();
+  });
+
+  it('renders the component with a value', () => {
+    const { getByLabelText } = render(
+      <ControlledInput
+        className="my-extra-class"
+        id="ControlledInput"
+        label="ControlledInput"
+        value="someValue"
+      />,
+    );
+
+    expect(getByLabelText('ControlledInput').getAttribute('value')).toEqual(
+      'someValue',
+    );
+  });
+
+  it('can blur when clicking escape', () => {
+    const onBlur = jest.fn();
+    const { getByRole } = render(
+      <ControlledInput
+        id="ControlledInput"
+        label="ControlledInput"
+        onBlur={onBlur}
+        canBlurOnEsc
+        type="checkbox"
+      />,
+    );
+    const input = getByRole('checkbox');
+    fireEvent.keyDown(input, {
+      code: 'Escape',
+      target: { blur: onBlur },
+    });
+    expect(onBlur).toHaveBeenCalled();
+  });
+
+  it('has no a11y issues', async () => {
+    const { container } = render(
+      <ControlledInput id="ControlledInput" label="ControlledInput" />,
+    );
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
 });
--- packages/components/inputs/src/radio-button/RadioButton.test.tsx
+++ packages/components/inputs/src/radio-button/RadioButton.test.tsx
@@ -4,23 +4,25 @@ import { axe } from '@/scripts/test/axeHelper';
 
 import { RadioButton } from './RadioButton';
 
-it('renders the component', () => {
-  const { container } = render(<RadioButton label="radio-button" />);
+describe('RadioButton', function () {
+  it('renders the component', () => {
+    const { getByRole } = render(<RadioButton label="radio-button" />);
 
-  expect(container.firstChild).toMatchSnapshot();
-});
+    expect(getByRole('radio')).toBeTruthy();
+  });
 
-it('renders the component with an additional class name', () => {
-  const { container } = render(
-    <RadioButton label="radio-button" className="my-extra-class" />,
-  );
+  it('renders the component with an additional class name', () => {
+    const { container } = render(
+      <RadioButton label="radio-button" className="my-extra-class" />,
+    );
 
-  expect(container.firstChild).toMatchSnapshot();
-});
+    expect(container.firstChild).toHaveClass('my-extra-class');
+  });
 
-it('has no a11y issues', async () => {
-  const { container } = render(<RadioButton label="radio-button" />);
-  const results = await axe(container);
+  it('has no a11y issues', async () => {
+    const { container } = render(<RadioButton label="radio-button" />);
+    const results = await axe(container);
 
-  expect(results).toHaveNoViolations();
+    expect(results).toHaveNoViolations();
+  });
 });
