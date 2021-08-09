1e7ead35896548d73c6fe7ae7f7faa8ed34ef729
Renato Massao Yonamine <massao.yonamine@contentful.com>
2021-08-09 13:59:28 +0200

    test(forms): remove snapshots from forms package and refactor tests

--- packages/components/forms/src/Label/Label.test.tsx
+++ packages/components/forms/src/Label/Label.test.tsx
@@ -4,45 +4,58 @@ import { axe } from '@/scripts/test/axeHelper';
 
 import { Label } from '.';
 
-it('renders the component with all required props', () => {
-  const { container } = render(<Label htmlFor="someInput">Label Text</Label>);
+describe('Label', function () {
+  const labelText = 'Label Text';
+  it('renders the component with all required props', () => {
+    const { getByText } = render(
+      <Label htmlFor="someInput">{labelText}</Label>,
+    );
 
-  expect(container.firstChild).toMatchSnapshot();
-});
+    expect(getByText(labelText)).toBeTruthy();
+  });
 
-it('renders the component with an additional class name', () => {
-  const { container } = render(
-    <Label htmlFor="someInput" className="my-extra-class">
-      Label Text
-    </Label>,
-  );
+  it('renders the component with an additional class name', () => {
+    const { getByText } = render(
+      <Label htmlFor="someInput" className="my-extra-class">
+        {labelText}
+      </Label>,
+    );
 
-  expect(container.firstChild).toMatchSnapshot();
-});
+    const label = getByText(labelText);
+    expect(label).toHaveClass('my-extra-class');
+  });
 
-it('renders the component with an required flag', () => {
-  const { container } = render(
-    <Label htmlFor="someInput" required>
-      Label Text
-    </Label>,
-  );
+  it('renders the component with an required flag', () => {
+    const { getByText } = render(
+      <Label htmlFor="someInput" required>
+        {labelText}
+      </Label>,
+    );
 
-  expect(container.firstChild).toMatchSnapshot();
-});
+    const label = getByText(labelText);
+    expect(getByText(/required/)).toBeTruthy();
+    expect(label.textContent).toContain('required');
+  });
 
-it('renders the component with required text', () => {
-  const { container } = render(
-    <Label htmlFor="someInput" requiredText="pflichtfeld" required>
-      Label Text
-    </Label>,
-  );
+  it('renders the component with required text', () => {
+    const requiredText = 'pflichtfeld';
+    const { getByText } = render(
+      <Label htmlFor="someInput" requiredText={requiredText} required>
+        {labelText}
+      </Label>,
+    );
 
-  expect(container.firstChild).toMatchSnapshot();
-});
+    const label = getByText(labelText);
+    expect(label.textContent).toContain(requiredText);
+    expect(getByText(new RegExp(requiredText))).toBeTruthy();
+  });
 
-it('has no a11y issues', async () => {
-  const { container } = render(<Label htmlFor="someInput">Label Text</Label>);
-  const results = await axe(container);
+  it('has no a11y issues', async () => {
+    const { container } = render(
+      <Label htmlFor="someInput">{labelText}</Label>,
+    );
+    const results = await axe(container);
 
-  expect(results).toHaveNoViolations();
+    expect(results).toHaveNoViolations();
+  });
 });
