2828d0e58963a2c228aba57994a3ea5a1818ca8f
Renato Massao Yonamine <massao.yonamine@contentful.com>
2021-08-09 14:39:40 +0200

    test(note): refactor note package test to remove snapshots

--- packages/components/note/test/Note.test.tsx
+++ packages/components/note/test/Note.test.tsx
@@ -1,49 +1,53 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { Note } from '../src/Note';
 import { axe } from '@/scripts/test/axeHelper';
 
 describe('Note', function () {
+  const noteText =
+    'A piece of information that is relevant to the context the user is currently in.';
   it('renders the component', () => {
-    const { container } = render(
-      <Note>
-        A piece of information that is relevant to the context the user is
-        currently in.
-      </Note>,
-    );
+    const { container } = render(<Note>{noteText}</Note>);
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toBeTruthy();
   });
 
   it('renders the component with title', () => {
-    const { container } = render(
+    const { container, getByText } = render(
       <Note variant="positive" title="Positive title">
-        A piece of information that is relevant to the context the user is
-        currently in.
+        {noteText}
       </Note>,
     );
 
-    expect(container.firstChild).toMatchSnapshot();
+    const title = getByText('Positive title');
+    expect(title).toBeTruthy();
+    expect(container.firstChild).toContainElement(title);
   });
 
   it('renders the component with an additional class name', () => {
     const { container } = render(
-      <Note className="my-extra-class">
-        A piece of information that is relevant to the context the user is
-        currently in.
+      <Note className="my-extra-class">{noteText}</Note>,
+    );
+
+    expect(container.firstChild).toHaveClass('my-extra-class');
+  });
+
+  it('renders the component with a close button', () => {
+    const mockOnClose = jest.fn();
+    const { container, getByRole } = render(
+      <Note hasCloseButton onClose={mockOnClose}>
+        {noteText}
       </Note>,
     );
 
-    expect(container.firstChild).toMatchSnapshot();
+    const button = getByRole('button');
+    expect(container.firstChild).toContainElement(button);
+    fireEvent.click(button);
+    expect(mockOnClose).toHaveBeenCalled();
   });
 
   it(`has no a11y issues`, async () => {
-    const { container } = render(
-      <Note>
-        A piece of information that is relevant to the context the user is
-        currently in.
-      </Note>,
-    );
+    const { container } = render(<Note>{noteText}</Note>);
 
     const results = await axe(container);
 
