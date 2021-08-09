313da2637bab9771306f77793e73e1d1a694f83b
Renato Massao Yonamine <massao.yonamine@contentful.com>
2021-08-09 16:51:28 +0200

    test(tooltip): refactor tooltip package tests to remove snapshots

--- packages/components/tooltip/test/Tooltip.test.tsx
+++ packages/components/tooltip/test/Tooltip.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { axe } from '@/scripts/test/axeHelper';
 
@@ -7,98 +7,119 @@ import { Tooltip } from '../src/Tooltip';
 
 jest.mock('@contentful/f36-core', () => ({
   ...jest.requireActual('@contentful/f36-core'),
-  useId: () => {
-    return 'id';
-  },
+  useId: (id) => id,
 }));
 
-it('does not render the component if no mouseover event on child', () => {
-  const { container } = render(
-    <Tooltip content="Tooltip content">
-      <span>Hover me</span>
-    </Tooltip>,
-  );
+describe('Tooltip', () => {
+  it('does not render the component if no mouseover event on child', () => {
+    const { queryByText } = render(
+      <Tooltip content="Tooltip content">
+        <span>Hover me</span>
+      </Tooltip>,
+    );
 
-  expect(container.firstChild).toMatchSnapshot();
-});
+    expect(queryByText('Tooltip content')).toBeNull();
+  });
 
-it('renders the component', () => {
-  const { container } = render(
-    <Tooltip content="Tooltip content">
-      <span id="test">Hover me</span>
-    </Tooltip>,
-  );
+  it('renders the component', async () => {
+    const { getByText, getByRole } = render(
+      <Tooltip content="Tooltip content">
+        <span>Hover me</span>
+      </Tooltip>,
+    );
 
-  userEvent.hover(screen.getByText('Hover me'));
-  expect(container.firstChild).toMatchSnapshot();
-});
+    await act(async () => {
+      userEvent.hover(getByText('Hover me'));
+    });
+    expect(getByRole('tooltip').textContent).toEqual('Tooltip content');
+  });
 
-it('renders the component with an additional class name', () => {
-  const { container } = render(
-    <Tooltip content="Tooltip content" className="extra-class-name">
-      <span id="test">Hover me</span>
-    </Tooltip>,
-  );
+  it('renders the component with an additional class name', async () => {
+    const { getByText, getByRole } = render(
+      <Tooltip content="Tooltip content" className="extra-class-name">
+        <span>Hover me</span>
+      </Tooltip>,
+    );
 
-  userEvent.hover(screen.getByText('Hover me'));
-  expect(container.firstChild).toMatchSnapshot();
-});
+    await act(async () => {
+      userEvent.hover(getByText('Hover me'));
+    });
+    expect(getByRole('tooltip')).toHaveClass('extra-class-name');
+  });
 
-it('renders the component with a target wrapper classname', () => {
-  const { container } = render(
-    <Tooltip
-      content="Tooltip content"
-      targetWrapperClassName="target-wrapper-class-name"
-    >
-      <span id="test">Hover me</span>
-    </Tooltip>,
-  );
+  it('renders the component with a target wrapper classname', async () => {
+    const { container, getByText } = render(
+      <Tooltip
+        content="Tooltip content"
+        targetWrapperClassName="target-wrapper-class-name"
+      >
+        <span>Hover me</span>
+      </Tooltip>,
+    );
 
-  userEvent.hover(screen.getByText('Hover me'));
-  expect(container.firstChild).toMatchSnapshot();
-});
+    await act(async () => {
+      userEvent.hover(getByText('Hover me'));
+    });
+    expect(
+      container.querySelector('.target-wrapper-class-name').textContent,
+    ).toEqual('Hover me');
+  });
 
-it('renders the component with a place attribute', () => {
-  const { container } = render(
-    <Tooltip content="Tooltip content" placement="left">
-      <span id="test">Hover me</span>
-    </Tooltip>,
-  );
+  it('renders the component with a placement attribute', async () => {
+    const { getByText, getByRole } = render(
+      <Tooltip content="Tooltip content" placement="left">
+        <span>Hover me</span>
+      </Tooltip>,
+    );
 
-  userEvent.hover(screen.getByText('Hover me'));
-  expect(container.firstChild).toMatchSnapshot();
-});
+    await act(async () => {
+      userEvent.hover(getByText('Hover me'));
+    });
+    expect(getByRole('tooltip').getAttribute('data-popper-placement')).toEqual(
+      'left',
+    );
+  });
 
-it('renders the component with a id attribute', () => {
-  const { container } = render(
-    <Tooltip id="Tooltip" content="Tooltip content">
-      <span id="test">Hover me</span>
-    </Tooltip>,
-  );
+  it('renders the component with a id attribute', async () => {
+    const { getByRole, getByText } = render(
+      <Tooltip id="Tooltip" content="Tooltip content">
+        <span>Hover me</span>
+      </Tooltip>,
+    );
 
-  userEvent.hover(screen.getByText('Hover me'));
-  expect(container.firstChild).toMatchSnapshot();
-});
+    await act(async () => {
+      userEvent.hover(getByText('Hover me'));
+    });
+    expect(getByRole('tooltip').getAttribute('id')).toEqual('Tooltip');
+  });
 
-it('renders the component as span with a id attribute', () => {
-  const { container } = render(
-    <Tooltip as="span" content="Tooltip content">
-      <span id="test">Hover me</span>
-    </Tooltip>,
-  );
+  it('renders the component as span with a id attribute', async () => {
+    const { getByText, getByRole } = render(
+      <Tooltip as="span" id="Tooltip" content="Tooltip content">
+        <span>Hover me</span>
+      </Tooltip>,
+    );
 
-  userEvent.hover(screen.getByText('Hover me'));
-  expect(container.firstChild).toMatchSnapshot();
-});
+    await act(async () => {
+      userEvent.hover(getByText('Hover me'));
+    });
+    const tooltip = getByRole('tooltip');
+    expect(tooltip.getAttribute('id')).toEqual('Tooltip');
+    expect(tooltip.nodeName).toMatch(/span/i);
+  });
 
-it('has no a11y issues', async () => {
-  const { container } = render(
-    <Tooltip id="Tooltip" content="Tooltip content">
-      <span>Hover me</span>
-    </Tooltip>,
-  );
+  it('has no a11y issues', async () => {
+    const { container, getByText } = render(
+      <Tooltip id="Tooltip" content="Tooltip content">
+        <span>Hover me</span>
+      </Tooltip>,
+    );
+    await act(async () => {
+      userEvent.hover(getByText('Hover me'));
+    });
 
-  const results = await axe(container);
+    const results = await axe(container);
 
-  expect(results).toHaveNoViolations();
+    expect(results).toHaveNoViolations();
+  });
 });
