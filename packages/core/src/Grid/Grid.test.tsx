34917d3ba14d7506f1a7a8a70fb2d7413b936924
Renato Massao Yonamine <massao.yonamine@contentful.com>
2021-08-10 11:14:29 +0200

    test(grid): remove unused import

--- packages/core/src/Grid/Grid.test.tsx
+++ packages/core/src/Grid/Grid.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { axe } from '@/scripts/test/axeHelper';
 
 import { Grid } from './Grid';
