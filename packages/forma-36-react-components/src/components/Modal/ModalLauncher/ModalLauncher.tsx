21ea15db3150816ed726231bb64f7a3fc643a0ed
Renato Massao Yonamine <massao.yonamine@contentful.com>
2021-08-12 14:48:32 +0200

    feat(modallauncher): add closeAll method to ModalLauncher

--- packages/forma-36-react-components/src/components/Modal/ModalLauncher/ModalLauncher.tsx
+++ packages/forma-36-react-components/src/components/Modal/ModalLauncher/ModalLauncher.tsx
@@ -9,9 +9,46 @@ interface ModalLauncherComponentRendererProps<T = any> {
 }
 
 interface ModalLauncherOpenOptions {
+  /**
+   * Unique id to be used as identifier for the modal contianer
+   */
   modalId?: string;
-  // ms before removing the component from the tree, defaults to 25ms.
+  /**
+   * ms before removing the component from the tree
+   * @default 300
+   */
   delay?: number;
+}
+
+interface CloseModalData {
+  delay: number;
+  render: (args: ModalLauncherComponentRendererProps<any>) => void;
+  currentConfig: ModalLauncherComponentRendererProps<any>;
+}
+
+const getRoot = (rootElId: string): HTMLElement => {
+  // Reuse the container if we find it
+  let rootDom = document.getElementById(rootElId);
+  if (rootDom !== null) {
+    return rootDom;
+  }
+
+  // Otherwise create it
+  rootDom = document.createElement('div');
+  rootDom.setAttribute('id', rootElId);
+  document.body.appendChild(rootDom);
+  return rootDom;
+};
+
+let openModalsIds: Map<string, CloseModalData> = new Map();
+function closeAll() {
+  openModalsIds.forEach(async ({ render, currentConfig, delay }, rootElId) => {
+    const config = { ...currentConfig, isShown: false };
+    render(config);
+    await new Promise((resolveDelay) => setTimeout(resolveDelay, delay));
+    ReactDOM.unmountComponentAtNode(getRoot(rootElId));
+    openModalsIds.delete(rootElId);
+  });
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,25 +62,7 @@ function open<T = any>(
 
   // Allow components to specify if they wish to reuse the modal container
   const rootElId = `modals-root${options.modalId || Date.now()}`;
-  let rootDom: HTMLElement | null = null;
-
-  const getRoot = () => {
-    if (rootDom !== null) {
-      return rootDom;
-    }
-
-    // Reuse the container if we find it
-    rootDom = document.getElementById(rootElId);
-    if (rootDom !== null) {
-      return rootDom;
-    }
-
-    // Otherwise create it
-    rootDom = document.createElement('div');
-    rootDom.setAttribute('id', rootElId);
-    document.body.appendChild(rootDom);
-    return rootDom;
-  };
+  const rootDom = getRoot(rootElId);
 
   return new Promise((resolve) => {
     let currentConfig = { onClose, isShown: true };
@@ -52,7 +71,7 @@ function open<T = any>(
       onClose,
       isShown,
     }: ModalLauncherComponentRendererProps<T>) {
-      ReactDOM.render(componentRenderer({ onClose, isShown }), getRoot());
+      ReactDOM.render(componentRenderer({ onClose, isShown }), rootDom);
     }
 
     async function onClose(arg?: T) {
@@ -64,15 +83,22 @@ function open<T = any>(
       await new Promise((resolveDelay) =>
         setTimeout(resolveDelay, options.delay),
       );
-      ReactDOM.unmountComponentAtNode(getRoot());
-      getRoot().remove();
+      ReactDOM.unmountComponentAtNode(rootDom);
+      rootDom.remove();
+      openModalsIds.delete(rootElId);
       resolve(arg);
     }
 
     render(currentConfig);
+    openModalsIds.set(rootElId, {
+      render,
+      currentConfig,
+      delay: options.delay,
+    });
   });
 }
 
 export const ModalLauncher = {
   open,
+  closeAll,
 };
