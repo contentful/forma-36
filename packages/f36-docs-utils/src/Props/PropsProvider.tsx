f5a2394f60b8a7658198efc86c9945dca1ad36c4
Alex Suevalov <alex.suevalov@contentful.com>
2021-08-16 15:39:27 +0200

    feat: filter out all polymorphic props

--- packages/f36-docs-utils/src/Props/PropsProvider.tsx
+++ packages/f36-docs-utils/src/Props/PropsProvider.tsx
@@ -18,20 +18,29 @@ function useProps(props: { of: string }): PropDefinition[] {
   if (!componentMetadata) {
     return [];
   }
+
   const values = Object.values(componentMetadata.props);
+
   let core: PropDefinition[] = [];
   let component: PropDefinition[] = [];
-  values.map((value) => {
-    if (['ref', 'key'].includes(value.name)) {
-      // don't add this types
-    } else if (
-      ['as', 'children', 'className', 'style', 'testId'].includes(value.name)
-    ) {
-      core.push(value);
-    } else {
-      component.push(value);
-    }
-  });
+  values
+    .filter((value) => {
+      const hasPolymorphicParent =
+        Boolean(value.parent) &&
+        value.parent?.fileName.includes('@types/react/index.d.ts');
+      return !hasPolymorphicParent;
+    })
+    .map((value) => {
+      if (['ref', 'key'].includes(value.name)) {
+        // don't add this types
+      } else if (
+        ['as', 'children', 'className', 'style', 'testId'].includes(value.name)
+      ) {
+        core.push(value);
+      } else {
+        component.push(value);
+      }
+    });
 
   core = orderBy(core, ['required', 'name'], ['desc', 'asc']);
   component = orderBy(component, ['required', 'name'], ['desc', 'asc']);
