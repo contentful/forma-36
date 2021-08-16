2adebe3e92e532e5aa25256809a39cf4e6b3eb4d
Alex Suevalov <alex.suevalov@contentful.com>
2021-08-16 19:27:35 +0200

    chore: left some todos

--- packages/forma-36-codemod/transforms/v4-text-link.js
+++ packages/forma-36-codemod/transforms/v4-text-link.js
@@ -4,16 +4,30 @@ const { renameProperties } = require('../utils/renameProperties');
 module.exports = function (file, api) {
   const j = api.jscodeshift;
 
-  const localName = getComponentLocalName(j, file.source, {
+  let source = file.source;
+
+  const componentName = getComponentLocalName(j, source, {
     componentName: 'TextLink',
     importName: '@contentful/forma-36-react-components',
   });
 
-  return renameProperties(j, file.source, {
-    componentName: localName,
+  source = renameProperties(j, source, {
+    componentName,
     renameMap: {
       linkType: 'variant',
       disabled: 'isDisabled',
     },
   });
+
+  // todo: create a function that replaces icons with inlined icons
+
+  // todo: add as="a" if href is defined
+
+  // todo: rename iconPosition to alignIcon and update values / console.warn if update cannot be done
+
+  // todo: move text to children if it's specified
+
+  // todo: question: do we need to update imports somehow?
+
+  return source;
 };
