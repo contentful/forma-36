59497237b21e3ceaca77f52c755035ebb0a62f28
Alex Suevalov <alex.suevalov@contentful.com>
2021-08-18 17:33:23 +0200

    chore: remove some snapshots

--- packages/forma-36-react-components/src/components/Card/AssetCard/AssetCard.test.tsx
+++ packages/forma-36-react-components/src/components/Card/AssetCard/AssetCard.test.tsx
@@ -4,83 +4,6 @@ import { CalendarIcon } from '@contentful/f36-icons';
 import { axe } from '@/scripts/test/axeHelper';
 
 import { AssetCard } from './AssetCard';
-import { DropdownList, DropdownListItem } from '../../Dropdown';
-import { CardDragHandle } from '../CardDragHandle/CardDragHandle';
-
-jest.mock('@contentful/f36-core', () => ({
-  ...jest.requireActual('@contentful/f36-core'),
-  useId: () => {
-    return 'id';
-  },
-}));
-
-it('renders the component', () => {
-  const { container } = render(
-    <AssetCard
-      title="picture of a cat"
-      src="https://via.placeholder.com/200x300"
-    >
-      AssetCard
-    </AssetCard>,
-  );
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with an additional class name', () => {
-  const { container } = render(
-    <AssetCard
-      className="my-extra-class"
-      src="https://via.placeholder.com/200x300"
-      title="picture of a cat"
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component in loading state', () => {
-  const { container } = render(
-    <AssetCard
-      isLoading
-      src="https://via.placeholder.com/200x300"
-      title="picture of a cat"
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with status', () => {
-  const { container } = render(
-    <AssetCard
-      status="archived"
-      src="https://via.placeholder.com/200x300"
-      title="picture of a cat"
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with actions', () => {
-  Date.now = jest.fn(() => 123456);
-  Math.random = jest.fn(() => 500);
-
-  const { container } = render(
-    <AssetCard
-      dropdownListElements={
-        <DropdownList>
-          <DropdownListItem isTitle>Actions</DropdownListItem>
-          <DropdownListItem onClick={() => {}}>Edit</DropdownListItem>
-        </DropdownList>
-      }
-      src="https://via.placeholder.com/200x300"
-      title="picture of a cat"
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
 
 it('renders the component without actions', () => {
   const { container } = render(
@@ -92,45 +15,6 @@ it('renders the component without actions', () => {
   const dropdown = container.querySelector('[data-test-id="cf-ui-dropdown"]');
 
   expect(dropdown).not.toBeTruthy();
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with a drag handle', () => {
-  const { container } = render(
-    <AssetCard
-      className="my-extra-class"
-      src="https://via.placeholder.com/200x300"
-      title="picture of a cat"
-      withDragHandle
-    />,
-  );
-
-  expect(container).toMatchSnapshot();
-});
-
-it('renders the component with a custom drag handle', () => {
-  const { container } = render(
-    <AssetCard
-      className="my-extra-class"
-      src="https://via.placeholder.com/200x300"
-      title="picture of a cat"
-      cardDragHandleComponent={<CardDragHandle>Reorder card</CardDragHandle>}
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders a small variant of the component', () => {
-  const { container } = render(
-    <AssetCard
-      size="small"
-      src="https://via.placeholder.com/200x300"
-      title="picture of a cat"
-    />,
-  );
-
-  expect(container.firstChild).toMatchSnapshot();
 });
 
 it('has no a11y issues', async () => {
