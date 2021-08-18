3df535378653fdf7e831c8b235f894a9fdb72dfa
burakukula <trycjapa@gmail.com>
2021-08-18 12:02:09 +0200

    fix: add extended example into fixtures

--- packages/forma-36-codemod/transforms/__testfixtures__/v4-badge.input.js
+++ packages/forma-36-codemod/transforms/__testfixtures__/v4-badge.input.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { Tag } from "@contentful/forma-36-react-components";
+import { Tag, Heading, Button } from "@contentful/forma-36-react-components";
 
 <Tag tagType="primary">primary</Tag>;
 
 <Tag size="small">primary</Tag>;
 
 <Tag tagType="primary-filled" size="small">primary filled</Tag>;
+
+<Heading>heading</Heading>;
+
+<Button buttonType="primary">primary button</Button>;
--- packages/forma-36-codemod/transforms/__testfixtures__/v4-badge.output.js
+++ packages/forma-36-codemod/transforms/__testfixtures__/v4-badge.output.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Heading, Button } from "@contentful/forma-36-react-components";
 
 import { Badge } from "@contentful/f36-components";
 
@@ -7,3 +8,7 @@ import { Badge } from "@contentful/f36-components";
 <Badge size="small">primary</Badge>;
 
 <Badge variant="primary-filled" size="small">primary filled</Badge>;
+
+<Heading>heading</Heading>;
+
+<Button buttonType="primary">primary button</Button>;
