119225dfd2d09077f37122a72a7bcc8fe281a8e5
z0al <ah.tajelsir@gmail.com>
2021-08-24 10:02:51 +0200

    feat(EditorToolbarButton): custom Tooltip placement

--- packages/forma-36-react-components/src/components/EditorToolbar/EditorToolbarButton/EditorToolbarButton.tsx
+++ packages/forma-36-react-components/src/components/EditorToolbar/EditorToolbarButton/EditorToolbarButton.tsx
@@ -3,7 +3,7 @@ import cn from 'classnames';
 import type { MouseEventHandler } from 'react';
 
 import { IconButton } from '../../IconButton';
-import { Tooltip } from '../../Tooltip';
+import { Tooltip, TooltipPlace } from '../../Tooltip';
 import styles from './EditorToolbarButton.css';
 import type { IconButtonProps } from '../../IconButton';
 import type { IconType } from '../../Icon';
@@ -12,6 +12,7 @@ export interface EditorToolbarButtonProps {
   label: string;
   icon: IconType;
   tooltip?: string;
+  tooltipPlace?: TooltipPlace;
   iconButtonProps?: Partial<IconButtonProps>;
   isActive?: boolean;
   disabled?: boolean;
@@ -27,6 +28,7 @@ export function EditorToolbarButton({
   testId = 'cf-ui-editor-toolbar-button',
   icon,
   tooltip,
+  tooltipPlace,
   iconButtonProps,
   isActive = false,
   disabled = false,
@@ -40,7 +42,10 @@ export function EditorToolbarButton({
 
   return (
     <React.Fragment>
-      <Tooltip content={!disabled ? tooltip : undefined}>
+      <Tooltip
+        content={!disabled ? tooltip : undefined}
+        place={tooltipPlace || 'bottom'}
+      >
         <IconButton
           {...{ iconProps: { icon } }}
           testId={testId}
