21961f40689d07b9ccd0e41f19b9299a7114d035
Alex Suevalov <alex.suevalov@contentful.com>
2021-08-30 13:21:07 +0200

    refactor: make Accordion a compound component

--- packages/components/accordion/stories/Accordion.stories.tsx
+++ packages/components/accordion/stories/Accordion.stories.tsx
@@ -2,15 +2,14 @@ import React, { useState } from 'react';
 import { Flex } from '@contentful/f36-core';
 import { SectionHeading, Text } from '@contentful/f36-typography';
 import { Button } from '@contentful/f36-button';
-import { Accordion, AccordionProps } from '../src/Accordion';
-import { AccordionItem } from '../src/AccordionItem/AccordionItem';
+import { Accordion, AccordionProps } from '../src';
 
 export default {
   title: 'Components/Accordion',
   component: Accordion,
-  subcomponents: { AccordionItem },
+  subcomponents: { AccordionItem: Accordion.Item },
   parameters: {
-    propTypes: [Accordion['__docgenInfo'], AccordionItem['__docgenInfo']],
+    propTypes: [Accordion['__docgenInfo'], Accordion.Item['__docgenInfo']],
   },
   argTypes: {
     align: { control: { type: 'select', options: ['start', 'end'] } },
@@ -27,15 +26,15 @@ nisi ut aliquip ex ea commodo consequat.`;
 
 export const basic = ({ align, ...args }: AccordionProps) => (
   <Accordion align={align}>
-    <AccordionItem title={args['AccordionItem Title #1']}>
-      <Text as="p">{args['AccordionItem Content #1']}</Text>
-    </AccordionItem>
-    <AccordionItem title={args['AccordionItem Title #2']}>
-      <Text as="p">{args['AccordionItem Content #2']}</Text>
-    </AccordionItem>
-    <AccordionItem title={args['AccordionItem Title #3']}>
-      <Text as="p">{args['AccordionItem Content #3']}</Text>
-    </AccordionItem>
+    <Accordion.Item title={args['Accordion.Item Title #1']}>
+      <Text as="p">{args['Accordion.Item Content #1']}</Text>
+    </Accordion.Item>
+    <Accordion.Item title={args['Accordion.Item Title #2']}>
+      <Text as="p">{args['Accordion.Item Content #2']}</Text>
+    </Accordion.Item>
+    <Accordion.Item title={args['Accordion.Item Title #3']}>
+      <Text as="p">{args['Accordion.Item Content #3']}</Text>
+    </Accordion.Item>
   </Accordion>
 );
 
@@ -48,15 +47,15 @@ export const overview = ({ align, ...args }: AccordionProps) => (
 
       <Flex>
         <Accordion align="start">
-          <AccordionItem title={args['AccordionItem Title #1']}>
-            <Text as="p">{args['AccordionItem Content #1']}</Text>
-          </AccordionItem>
-          <AccordionItem title={args['AccordionItem Title #2']}>
-            <Text as="p">{args['AccordionItem Content #2']}</Text>
-          </AccordionItem>
-          <AccordionItem title={args['AccordionItem Title #3']}>
-            <Text as="p">{args['AccordionItem Content #3']}</Text>
-          </AccordionItem>
+          <Accordion.Item title={args['Accordion.Item Title #1']}>
+            <Text as="p">{args['Accordion.Item Content #1']}</Text>
+          </Accordion.Item>
+          <Accordion.Item title={args['Accordion.Item Title #2']}>
+            <Text as="p">{args['Accordion.Item Content #2']}</Text>
+          </Accordion.Item>
+          <Accordion.Item title={args['Accordion.Item Title #3']}>
+            <Text as="p">{args['Accordion.Item Content #3']}</Text>
+          </Accordion.Item>
         </Accordion>
       </Flex>
     </Flex>
@@ -67,15 +66,15 @@ export const overview = ({ align, ...args }: AccordionProps) => (
 
       <Flex>
         <Accordion align="end">
-          <AccordionItem title={args['AccordionItem Title #1']}>
-            <Text as="p">{args['AccordionItem Content #1']}</Text>
-          </AccordionItem>
-          <AccordionItem title={args['AccordionItem Title #2']}>
-            <Text as="p">{args['AccordionItem Content #2']}</Text>
-          </AccordionItem>
-          <AccordionItem title={args['AccordionItem Title #3']}>
-            <Text as="p">{args['AccordionItem Content #3']}</Text>
-          </AccordionItem>
+          <Accordion.Item title={args['Accordion.Item Title #1']}>
+            <Text as="p">{args['Accordion.Item Content #1']}</Text>
+          </Accordion.Item>
+          <Accordion.Item title={args['Accordion.Item Title #2']}>
+            <Text as="p">{args['Accordion.Item Content #2']}</Text>
+          </Accordion.Item>
+          <Accordion.Item title={args['Accordion.Item Title #3']}>
+            <Text as="p">{args['Accordion.Item Content #3']}</Text>
+          </Accordion.Item>
         </Accordion>
       </Flex>
     </Flex>
@@ -96,9 +95,9 @@ export const DynamicContent = ({ align, ...args }: AccordionProps) => {
       </Flex>
       <Flex>
         <Accordion align={align}>
-          <AccordionItem title={args['AccordionItem Title #1']}>
+          <Accordion.Item title={args['Accordion.Item Title #1']}>
             <Text as="p">{content}</Text>
-          </AccordionItem>
+          </Accordion.Item>
         </Accordion>
       </Flex>
     </Flex>
@@ -107,10 +106,10 @@ export const DynamicContent = ({ align, ...args }: AccordionProps) => {
 
 basic.args = {
   align: 'end',
-  'AccordionItem Title #1': 'Accordion I',
-  'AccordionItem Content #1': defaultText,
-  'AccordionItem Title #2': 'Accordion II',
-  'AccordionItem Content #2': defaultText,
-  'AccordionItem Title #3': 'Accordion III',
-  'AccordionItem Content #3': defaultText,
+  'Accordion.Item Title #1': 'Accordion I',
+  'Accordion.Item Content #1': defaultText,
+  'Accordion.Item Title #2': 'Accordion II',
+  'Accordion.Item Content #2': defaultText,
+  'Accordion.Item Title #3': 'Accordion III',
+  'Accordion.Item Content #3': defaultText,
 };
