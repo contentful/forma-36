2a2ebd101b523708b8d99c474e8a83fb8b750e76
Alex Suevalov <alex.suevalov@contentful.com>
2021-08-30 11:59:12 +0200

    chore: rename

--- packages/components/table/src/CompoundTable.tsx
+++ packages/components/table/src/CompoundTable.tsx
@@ -1,17 +1,17 @@
-import { Table as NakedTable } from './Table';
+import { Table as OriginalTable } from './Table';
 import { TableBody } from './TableBody';
 import { TableCell } from './TableCell';
 import { TableHead } from './TableHead';
 import { TableRow } from './TableRow';
 
-type CompoundTable = typeof NakedTable & {
+type CompoundTable = typeof OriginalTable & {
   Row: typeof TableRow;
   Head: typeof TableHead;
   Cell: typeof TableCell;
   Body: typeof TableBody;
 };
 
-export const Table = NakedTable as CompoundTable;
+export const Table = OriginalTable as CompoundTable;
 Table.Row = TableRow;
 Table.Head = TableHead;
 Table.Cell = TableCell;
