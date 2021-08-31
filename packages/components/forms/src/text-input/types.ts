69c05d32450472ceb25fe7f8a9624da520f23dc0
burakukula <trycjapa@gmail.com>
2021-08-31 17:07:07 +0200

    fix: import from base-input

--- packages/components/forms/src/text-input/types.ts
+++ packages/components/forms/src/text-input/types.ts
@@ -1,15 +1,6 @@
-import { BaseInputInternalProps } from '../base-input/types';
+import { BaseInputInternalProps } from '../base-input';
 
 export interface TextInputProps extends Omit<BaseInputInternalProps, 'as'> {
-  /**
-   * Controlls the character count for the input, works together with maxLength propery
-   * @default false
-   */
-  countCharacters?: boolean;
-  /**
-   * Pass max number of characters for the input, needed for countCharacters prop
-   */
-  maxLength?: number;
   /**
    * Displays the copy button after the input
    * @default false
