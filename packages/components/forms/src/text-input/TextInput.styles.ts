36e26265d136335cac9ae10b413abc0e0133010d
burakukula <trycjapa@gmail.com>
2021-08-31 16:23:03 +0200

    fix: fix double css in styles

--- packages/components/forms/src/text-input/TextInput.styles.ts
+++ packages/components/forms/src/text-input/TextInput.styles.ts
@@ -6,7 +6,7 @@ const getStyles = () => ({
     borderRadius: `${tokens.borderRadiusMedium} 0 0 ${tokens.borderRadiusMedium}`,
   }),
   copyButton: css({
-    button: css({
+    button: {
       borderLeft: 'none',
       height: '100%',
       borderRadius: `0 ${tokens.borderRadiusMedium} ${tokens.borderRadiusMedium} 0`,
@@ -17,23 +17,23 @@ const getStyles = () => ({
         boxShadow: tokens.glowPrimary,
         borderLeft: `1px solid ${tokens.blue600}`,
       },
-    }),
+    },
   }),
   invalid: css({
-    button: css({
+    button: {
       '&:focus': {
         borderColor: tokens.red600,
         boxShadow: tokens.glowNegative,
       },
-    }),
+    },
   }),
   disabled: css({
-    button: css({
+    button: {
       '&:focus': {
         borderColor: tokens.gray300,
         boxShadow: 'none',
       },
-    }),
+    },
   }),
 });
 
