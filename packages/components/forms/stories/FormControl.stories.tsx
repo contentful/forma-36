e7ba967526025f595584e1014f4ff08f135c8efb
Alex Suevalov <alex.suevalov@contentful.com>
2021-09-01 16:54:02 +0200

    chore: added simple FormaControl story

---
+++ packages/components/forms/stories/FormControl.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { FormControl, FormControlInternalProps, TextInput } from '../src';
+
+export default {
+  title: 'Form Elements/FormControl',
+  component: FormControl,
+  argTypes: {
+    className: { control: { disable: true } },
+    testId: { control: { disable: true } },
+  },
+};
+
+export const Basic = (args: FormControlInternalProps) => {
+  return (
+    <FormControl {...args}>
+      <FormControl.Label isRequired>Name</FormControl.Label>
+      <TextInput label="input" />
+      <FormControl.HelpText>Please enter your first name</FormControl.HelpText>
+    </FormControl>
+  );
+};
+
+Basic.args = {};
