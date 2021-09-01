1ad480692e91dc5cf1a889bea6438c75be07c672
Patrycja Radaczyńska <patrycja.radaczynska@contentful.com>
2021-09-01 13:11:11 +0200

    feat: v4 TextInput component (#1230)

--- packages/components/forms/src/index.ts
+++ packages/components/forms/src/index.ts
@@ -8,3 +8,4 @@ export * from './switch';
 export * from './radio-button';
 export * from './base-input';
 export * from './base-checkbox';
+export * from './text-input';
---
+++ packages/components/forms/src/text-input/TextInput.styles.ts
@@ -0,0 +1,36 @@
+import { css } from 'emotion';
+import tokens from '@contentful/f36-tokens';
+
+const getStyles = () => ({
+  inputWithCopyButton: css({
+    borderRadius: `${tokens.borderRadiusMedium} 0 0 ${tokens.borderRadiusMedium}`,
+  }),
+  copyButton: css({
+    '& button': {
+      borderLeft: 'none',
+      height: '100%',
+      borderRadius: `0 ${tokens.borderRadiusMedium} ${tokens.borderRadiusMedium} 0`,
+
+      '&:focus': {
+        zIndex: tokens.zIndexDefault,
+        borderColor: tokens.blue600,
+        boxShadow: tokens.glowPrimary,
+        borderLeft: `1px solid ${tokens.blue600}`,
+      },
+    },
+  }),
+  invalid: css({
+    '& button:focus': {
+      borderColor: tokens.red600,
+      boxShadow: tokens.glowNegative,
+    },
+  }),
+  disabled: css({
+    '& button:focus': {
+      borderColor: tokens.gray300,
+      boxShadow: 'none',
+    },
+  }),
+});
+
+export default getStyles;
