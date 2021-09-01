ba4678d74c33d1540dbb8feb25fca44bb8d088cb
burakukula <trycjapa@gmail.com>
2021-09-01 09:26:14 +0200

    fix: useCallback for handler

--- packages/components/forms/src/text-input/TextInput.tsx
+++ packages/components/forms/src/text-input/TextInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, useCallback } from 'react';
 import { BaseInput } from '../base-input';
 import { Flex } from '@contentful/f36-core';
 import { CopyButton } from '@contentful/f36-copybutton';
@@ -29,10 +29,13 @@ export const _TextInput = (
   // Store a copy of the value in state.
   // This is used by this component when the `countCharacters`
   // option is on
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValueState(e.target.value);
-    if (onChange) onChange(e);
-  };
+  const handleOnChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setValueState(e.target.value);
+      if (onChange) onChange(e);
+    },
+    [onChange],
+  );
 
   const copyButtonStyles = cx(styles.copyButton, {
     [styles.disabled]: Boolean(isDisabled),
