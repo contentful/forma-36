c80eb033294282a06081e64c31b5be2664b5affc
Alex Suevalov <alex.suevalov@contentful.com>
2021-09-03 16:57:38 +0200

    chore: remove default line-height

--- packages/core/src/GlobalStyles/GlobalStyles.tsx
+++ packages/core/src/GlobalStyles/GlobalStyles.tsx
@@ -21,7 +21,6 @@ export const GlobalStyles = ({
           min-height: 100%;
           font-size: 16px;
           font-family: ${tokens.fontStackPrimary};
-          line-height: ${tokens.lineHeightDefault};
           vertical-align: baseline;
         }
 
