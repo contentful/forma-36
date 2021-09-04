f810a194930222707a71eb217705ca766695188c
bgutsol <bohdan.hutsol@contentful.com>
2021-09-04 23:00:20 +0300

    feat(Menu): Menu stories

--- packages/components/menu/stories/Menu.stories.tsx
+++ packages/components/menu/stories/Menu.stories.tsx
@@ -17,6 +17,65 @@ export const Default: Story<MenuProps> = (args) => {
         <Button>Toggle</Button>
       </Menu.Trigger>
       <Menu.List>
+        {[...Array(5)].map((_, index) => (
+          <Menu.ListItem key={index}>Item {index}</Menu.ListItem>
+        ))}
+        {/* <Menu.ListSeparator />
+        <Menu.ListTitle >Some title</Menu.ListHeader> */}
+      </Menu.List>
+    </Menu>
+  );
+};
+
+export const WithDisabledItems: Story<MenuProps> = (args) => {
+  return (
+    <Menu {...args}>
+      <Menu.Trigger>
+        <Button>Toggle</Button>
+      </Menu.Trigger>
+      <Menu.List>
+        <Menu.ListItem>Item 1</Menu.ListItem>
+        <Menu.ListItem>Item 2</Menu.ListItem>
+        <Menu.ListItem>Item 3</Menu.ListItem>
+        <Menu.ListItem disabled>Disabled item</Menu.ListItem>
+        <Menu.ListItem>Item 4</Menu.ListItem>
+        <Menu.ListItem>Item 5</Menu.ListItem>
+        <Menu.ListItem disabled>Disabled item</Menu.ListItem>
+        <Menu.ListItem>Item 6</Menu.ListItem>
+      </Menu.List>
+    </Menu>
+  );
+};
+
+export const WithMaxHeight: Story<MenuProps> = (args) => {
+  return (
+    <Menu {...args}>
+      <Menu.Trigger>
+        <Button>Toggle</Button>
+      </Menu.Trigger>
+      <Menu.List
+        // You can pass a classname with maxHeight style or a style object itself
+        style={{ maxHeight: '100px' }}
+      >
+        {[...Array(10)].map((_, index) => (
+          <Menu.ListItem key={index}>Item {index}</Menu.ListItem>
+        ))}
+      </Menu.List>
+    </Menu>
+  );
+};
+
+export const WithStickyHeaderAndFooter: Story<MenuProps> = (args) => {
+  return (
+    <Menu {...args}>
+      <Menu.Trigger>
+        <Button>Toggle</Button>
+      </Menu.Trigger>
+      <Menu.List
+        headerItem={<Menu.ListItem>Item header</Menu.ListItem>}
+        footerItem={<Menu.ListItem>Item footer</Menu.ListItem>}
+        style={{ maxHeight: '200px' }}
+      >
         {[...Array(10)].map((_, index) => (
           <Menu.ListItem key={index}>Item {index}</Menu.ListItem>
         ))}
