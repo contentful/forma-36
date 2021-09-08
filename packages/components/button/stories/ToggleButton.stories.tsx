e4097e31dd13c9a325fa3765dd91112dfbd1d7ee
Alex Suevalov <alex.suevalov@contentful.com>
2021-09-08 15:14:22 +0200

    chore: simplify stories

--- packages/components/button/stories/ToggleButton.stories.tsx
+++ packages/components/button/stories/ToggleButton.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Meta } from '@storybook/react/types-6-0';
 import { SectionHeading } from '@contentful/f36-typography';
 import { action } from '@storybook/addon-actions';
-import { Box, Flex } from '@contentful/f36-core';
+import { Flex, Stack } from '@contentful/f36-core';
 import { Icon } from '@contentful/f36-icon';
 import * as icons from '@contentful/f36-icons';
 import { ButtonGroup } from '../src';
@@ -69,55 +69,47 @@ export const Overview = ({ icon, ...props }) => (
         Toggle variants
       </SectionHeading>
 
-      <Flex flexDirection="row" marginBottom="spacingM">
-        <Box marginRight="spacingXs">
-          <ToggleButton onToggle={props.onToggle}>Default</ToggleButton>
-        </Box>
-        <Box marginRight="spacingXs">
-          <ToggleButton isActive onToggle={props.onToggle}>
-            Active
-          </ToggleButton>
-        </Box>
-        <Box marginRight="spacingXs">
-          <ToggleButton isDisabled onToggle={props.onToggle}>
-            Disabled
-          </ToggleButton>
-        </Box>
-      </Flex>
+      <Stack marginBottom="spacingM" spacing="spacingXs">
+        <ToggleButton onToggle={props.onToggle}>Default</ToggleButton>
+
+        <ToggleButton isActive onToggle={props.onToggle}>
+          Active
+        </ToggleButton>
+
+        <ToggleButton isDisabled onToggle={props.onToggle}>
+          Disabled
+        </ToggleButton>
+      </Stack>
     </Flex>
     <Flex flexDirection="column" marginBottom="spacingL">
       <SectionHeading as="h3" marginBottom="spacingS">
         Toggle Button with icon
       </SectionHeading>
 
-      <Flex flexDirection="row" marginBottom="spacingM">
-        <Box marginRight="spacingXs">
-          <ToggleButton
-            onToggle={props.onToggle}
-            icon={icon && <Icon as={icons[icon]} />}
-          >
-            Default
-          </ToggleButton>
-        </Box>
-        <Box marginRight="spacingXs">
-          <ToggleButton
-            onToggle={props.onToggle}
-            isActive
-            icon={icon && <Icon as={icons[icon]} />}
-          >
-            Active
-          </ToggleButton>
-        </Box>
-        <Box marginRight="spacingXs">
-          <ToggleButton
-            onToggle={props.onToggle}
-            isDisabled
-            icon={icon && <Icon as={icons[icon]} />}
-          >
-            Disabled
-          </ToggleButton>
-        </Box>
-      </Flex>
+      <Stack marginBottom="spacingM" spacing="spacingXs">
+        <ToggleButton
+          onToggle={props.onToggle}
+          icon={icon && <Icon as={icons[icon]} />}
+        >
+          Default
+        </ToggleButton>
+
+        <ToggleButton
+          onToggle={props.onToggle}
+          isActive
+          icon={icon && <Icon as={icons[icon]} />}
+        >
+          Active
+        </ToggleButton>
+
+        <ToggleButton
+          onToggle={props.onToggle}
+          isDisabled
+          icon={icon && <Icon as={icons[icon]} />}
+        >
+          Disabled
+        </ToggleButton>
+      </Stack>
     </Flex>
   </>
 );
