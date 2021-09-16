f945fafe2edc1f0b8baecd300e399dc0b035058b
Tatiana Bessonova <tatiana.bessonova@contentful.com>
2021-09-16 14:24:36 +0200

    refactor(tooltip): add console.warn about content prop type

--- packages/forma-36-codemod/transforms/v4-tooltip.js
+++ packages/forma-36-codemod/transforms/v4-tooltip.js
@@ -1,4 +1,7 @@
 const { modifyPropsCodemod } = require('./common/modify-props-codemod');
+const { pipe } = require('./common/pipe');
+const { getComponentLocalName } = require('../utils');
+const { getFormaImport } = require('../utils/config');
 
 module.exports = modifyPropsCodemod({
   componentName: 'Tooltip',
@@ -7,3 +10,33 @@ module.exports = modifyPropsCodemod({
     containerElement: 'as',
   },
 });
+
+module.exports = pipe([
+  (file, api) => {
+    const j = api.jscodeshift;
+
+    let source = file.source;
+
+    const componentName = getComponentLocalName(j, source, {
+      componentName: 'Tooltip',
+      importName: getFormaImport(),
+    });
+
+    if (!componentName) {
+      return source;
+    }
+    // eslint-disable-next-line no-console
+    console.warn(
+      'If you are passing anything other than a string to the `content` prop of the `Tooltip`, please update it to a string.',
+    );
+
+    return source;
+  },
+  modifyPropsCodemod({
+    componentName: 'Tooltip',
+    renameMap: {
+      place: 'placement',
+      containerElement: 'as',
+    },
+  }),
+]);
