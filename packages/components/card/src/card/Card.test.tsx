9774df1eb8553a3739f70531eed2053b34f4894a
denkristoffer <denkristoffer@users.noreply.github.com>
2021-09-16 13:39:23 +0200

    build: update tests

--- packages/components/card/src/card/Card.test.tsx
+++ packages/components/card/src/card/Card.test.tsx
@@ -30,28 +30,13 @@ it('renders as an article by default', () => {
   expect(container.firstChild.nodeName).toBe('ARTICLE');
 });
 
-it('can be rendered as a div', () => {
-  const { container } = render(<Card as="div">Card</Card>);
-
-  expect(container.firstChild.nodeName).toBe('DIV');
-});
-
-it('renders as a link if passed a href prop', () => {
-  const { container } = render(<Card href="/">Card</Card>);
-  console.log(container.firstChild.nodeName);
-  expect(container.firstChild.nodeName).toBe('A');
-});
-
-it('renders as button if passed a onClick prop', () => {
-  const onClickFunc = jest.fn();
-  const { container } = render(<Card onClick={onClickFunc}>Card</Card>);
-
-  expect(container.firstChild.nodeName).toBe('BUTTON');
-});
-
 it('calls an onClick function', () => {
   const onClickFunc = jest.fn();
-  const { container } = render(<Card onClick={onClickFunc}>Card</Card>);
+  const { container } = render(
+    <Card as="button" onClick={onClickFunc}>
+      Card
+    </Card>,
+  );
   const element = container.firstChild as Element;
   userEvent.click(element);
 
