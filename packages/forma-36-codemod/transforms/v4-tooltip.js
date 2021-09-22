ca53b0b9f9c5e5ea3a8572d11038057459f21e8b
Tanya Bessonova <tatiana.bessonova@contentful.com>
2021-09-22 16:55:05 +0200

    feat(tooltip-v4): update tooltip-v4 codemod to add console error for wrong prop type (#1339)

--- packages/forma-36-codemod/transforms/v4-tooltip.js
+++ packages/forma-36-codemod/transforms/v4-tooltip.js
@@ -1,34 +1,21 @@
 const { modifyPropsCodemod } = require('./common/modify-props-codemod');
-const { pipe } = require('./common/pipe');
-const { getComponentLocalName } = require('../utils');
-const { getFormaImport } = require('../utils/config');
+const { hasProperty, getProperty } = require('../utils');
 
-module.exports = pipe([
-  (file, api) => {
-    const j = api.jscodeshift;
-
-    let source = file.source;
-
-    const componentName = getComponentLocalName(j, source, {
-      componentName: 'Tooltip',
-      importName: getFormaImport(),
-    });
-
-    if (!componentName) {
-      return source;
+module.exports = modifyPropsCodemod({
+  componentName: 'Tooltip',
+  beforeRename: (attributes) => {
+    if (
+      hasProperty(attributes, { propertyName: 'content' }) &&
+      typeof getProperty(attributes, { propertyName: 'content' } !== 'string')
+    ) {
+      console.error(
+        'Value of property "content" on Tooltip component should be a string.',
+      );
     }
-    // eslint-disable-next-line no-console
-    console.warn(
-      'If you are passing anything other than a string to the `content` prop of the `Tooltip`, please update it to a string.',
-    );
-
-    return source;
+    return attributes;
   },
-  modifyPropsCodemod({
-    componentName: 'Tooltip',
-    renameMap: {
-      place: 'placement',
-      containerElement: 'as',
-    },
-  }),
-]);
+  renameMap: {
+    place: 'placement',
+    containerElement: 'as',
+  },
+});
