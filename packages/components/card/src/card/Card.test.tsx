5fe8b2d40b49a2e0404367707423866a77492d54
denkristoffer <denkristoffer@users.noreply.github.com>
2021-09-22 14:24:15 +0200

    chore: remove snapshot tests

--- packages/components/card/src/card/Card.test.tsx
+++ packages/components/card/src/card/Card.test.tsx
@@ -4,18 +4,6 @@ import { axe } from '@/scripts/test/axeHelper';
 
 import { Card } from './Card';
 
-it('renders the component', () => {
-  const { container } = render(<Card>Card</Card>);
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-it('renders the component with an additional class name', () => {
-  const { container } = render(<Card className="my-extra-class">Card</Card>);
-
-  expect(container.firstChild).toMatchSnapshot();
-});
-
 it('has no a11y issues', async () => {
   const { container } = render(<Card>Card</Card>);
   const results = await axe(container);
@@ -27,10 +15,4 @@ it('renders as an article by default', () => {
   const { container } = render(<Card>Card</Card>);
 
   expect(container.firstChild.nodeName).toBe('ARTICLE');
-});
-
-it('can be selected', () => {
-  const output = render(<Card isSelected>Card</Card>);
-
-  expect(output).toMatchSnapshot();
 });
