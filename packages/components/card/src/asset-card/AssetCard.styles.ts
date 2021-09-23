161f2c3b2d3539a6a3309d2116a1e43e44469cb1
Kristoffer <denkristoffer@users.noreply.github.com>
2021-09-23 12:10:48 +0200

    feat: @contentful/f36-card (#1114)
    Co-authored-by: Gui Santos <guilherme.santos@contentful.com>
    Co-authored-by: Alex Suevalov <alex.suevalov@contentful.com>

---
+++ packages/components/card/src/asset-card/AssetCard.styles.ts
@@ -0,0 +1,62 @@
+import { css } from 'emotion';
+import type { ObjectInterpolation } from 'emotion';
+import tokens from '@contentful/f36-tokens';
+
+export const getAssetCardStyles = () => {
+  return {
+    asset: css({
+      height: '100%',
+    }),
+    contentBody: css({
+      padding: 0,
+    }),
+    header: css({
+      alignItems: 'center',
+      borderBottomColor: tokens.gray200,
+      borderBottomStyle: 'solid',
+      borderBottomWidth: 1,
+      borderTopLeftRadius: tokens.borderRadiusMedium,
+      borderTopRightRadius: tokens.borderRadiusMedium,
+      boxSizing: 'border-box',
+      color: tokens.gray600,
+      fontSize: tokens.fontSizeM,
+      fontWeight: tokens.fontWeightNormal,
+      gridColumn: 'content',
+      gridRow: 'header',
+      lineHeight: tokens.lineHeightM,
+      paddingBottom: tokens.spacingXs,
+      paddingLeft: tokens.spacingM,
+      paddingRight: tokens.spacingM,
+      paddingTop: tokens.spacingXs,
+    }),
+    headerItem: css({
+      marginLeft: tokens.spacingXs,
+    }),
+    headerWithActions: css({
+      paddingBottom: 0,
+      paddingRight: tokens.spacingXs,
+      paddingTop: 0,
+    }),
+    root: ({ isSelected, size }) => {
+      const styles: ObjectInterpolation<undefined> = {
+        borderRadius: tokens.borderRadiusMedium,
+        height:
+          size === 'small'
+            ? // 192px
+              `calc(12px * ${tokens.fontBaseDefault})`
+            : // 304px
+              `calc(19px * ${tokens.fontBaseDefault})`,
+        padding: 0,
+        textAlign: 'center',
+      };
+
+      if (isSelected) {
+        styles.backgroundColor = tokens.colorWhite;
+        styles.borderColor = tokens.blue500;
+        styles.boxShadow = `0 0 0 1px ${tokens.gray300}`;
+      }
+
+      return css(styles);
+    },
+  };
+};
---
+++ packages/components/card/src/card/Card.tsx
@@ -0,0 +1,85 @@
+import React, { forwardRef } from 'react';
+import { cx } from 'emotion';
+import type {
+  PolymorphicComponent,
+  PolymorphicProps,
+} from '@contentful/f36-core';
+import { Heading } from '@contentful/f36-typography';
+import { Flex } from '@contentful/f36-core';
+import { Icon } from '@contentful/f36-icon';
+
+import { BaseCard, DEFAULT_TAG } from '../base-card/BaseCard';
+import type { BaseCardInternalProps } from '../base-card/BaseCard';
+import { CardActions } from '../base-card/CardActions';
+import { getCardStyles } from './Card.styles';
+import { getBaseCardStyles } from '../base-card/BaseCard.styles';
+
+export type CardInternalProps = Omit<
+  BaseCardInternalProps,
+  'header' | 'ref' | 'type'
+> & {
+  /**
+   * Padding size to apply to the component
+   */
+  padding?: 'default' | 'large';
+};
+
+export type CardProps<
+  E extends React.ElementType = typeof DEFAULT_TAG
+> = PolymorphicProps<CardInternalProps, E>;
+
+function _Card<E extends React.ElementType = typeof DEFAULT_TAG>(
+  {
+    actions,
+    badge,
+    icon,
+    padding = 'default',
+    title,
+    ...otherProps
+  }: CardProps<E>,
+  forwardedRef: React.Ref<HTMLElement>,
+) {
+  const styles = getCardStyles();
+  const baseStyles = getBaseCardStyles();
+  const hasHeader = title || icon || badge || actions;
+  const header = (
+    <Flex
+      className={cx(
+        styles.header({ padding }),
+        actions && styles.headerWithActions({ padding }),
+      )}
+    >
+      {title && (
+        <Flex as="header" flexGrow={1}>
+          <Heading marginBottom="none">{title}</Heading>
+        </Flex>
+      )}
+
+      {icon && <Icon as={icon} className={baseStyles.headerItem} />}
+      {badge && (
+        <Flex alignItems="center" className={baseStyles.headerItem}>
+          {badge}
+        </Flex>
+      )}
+      {actions && <CardActions>{actions}</CardActions>}
+    </Flex>
+  );
+
+  return (
+    <BaseCard
+      contentBodyProps={
+        padding === 'large' && {
+          className: styles.contentWithLargePadding,
+        }
+      }
+      {...otherProps}
+      header={hasHeader ? header : null}
+      ref={forwardedRef}
+    />
+  );
+}
+
+export const Card: PolymorphicComponent<
+  CardInternalProps,
+  typeof DEFAULT_TAG
+> = forwardRef(_Card);
