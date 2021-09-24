0e8e8699c78047a8e05f32ee99083fe7388fd050
Moe Shaaban <mshaaban0@users.noreply.github.com>
2021-09-24 18:36:07 +0200

    feat: add useKeyboard hook to f36-utils (#1325)

--- packages/components/utils/src/index.ts
+++ packages/components/utils/src/index.ts
@@ -3,3 +3,5 @@ export type { PortalProps } from './Portal/Portal';
 export { TabFocusTrap } from './TabFocusTrap/TabFocusTrap';
 export type { TabFocusTrapProps } from './TabFocusTrap/TabFocusTrap';
 export { useArrowKeyNavigation } from './ArrowKeyNavigation/useArrowKeyNavigation';
+export { useKeyboard } from './useKeyboard/useKeyboard';
+export type { UseKeyboardProps } from './useKeyboard/useKeyboard';
---
+++ packages/components/utils/src/useKeyboard/useKeyboard.ts
@@ -0,0 +1,60 @@
+import { useEffect, useCallback, MutableRefObject } from 'react';
+
+export interface UseKeyboardProps {
+  /**
+   * @description object of key names and handlers defines which key to look for i.e. `ArrowUp`, `Escape`, `Shift`
+   * value is a callback function to be called when key matches
+   */
+  keys: {
+    [key: string]: (e: KeyboardEvent) => void;
+  };
+  /**
+   * @description defines the attached event type
+   * @default 'keydown'
+   */
+  event?: 'keyup' | 'keypress' | 'keydown';
+
+  /**
+   * @description React reference to attach the event to its current element
+   */
+  ref?: MutableRefObject<HTMLElement>;
+}
+
+/**
+ *
+ * @description hook to attach a handler keyboard event listener to `document` or `HTMLElements` with garbage collection
+ * @example
+ * useKeyboard({
+ *  event: 'keydown', // Optional, default is `keydown`
+ *  ref: yourReactRef, // Optional, by default event is attached to document
+ *  keys: {
+ *    ArrowUp: (e) => handleArrowUp(e),
+ *    Tab: (e) => handleTab(e)
+ *  }
+ * })
+ */
+export const useKeyboard = (props: UseKeyboardProps) => {
+  const { ref, keys, event = 'keydown' } = props;
+  let element: HTMLElement | Document = document;
+
+  const handleKeyEvent = useCallback(
+    (e) => {
+      const isKey = Object.prototype.hasOwnProperty.call(keys, e.key);
+      if (isKey) {
+        keys[e.key](e);
+      }
+    },
+    [keys],
+  );
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      element = ref.current;
+    }
+
+    element.addEventListener(event, handleKeyEvent);
+    return () => {
+      element.removeEventListener(event, handleKeyEvent);
+    };
+  }, [event, handleKeyEvent]);
+};
