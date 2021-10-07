4a2568d3f620773b09868e2cac9cfb95eef82363
burakukula <trycjapa@gmail.com>
2021-10-07 09:54:44 +0200

    fix: remove osano from Storybook

--- scripts/.storybook/manager.js
+++ scripts/.storybook/manager.js
@@ -1,6 +1,5 @@
 import { addons } from '@storybook/addons';
 import contentfulTheme from './contentful-theme';
-import '!style-loader!css-loader!./osano.css';
 
 addons.setConfig({
   theme: contentfulTheme,
