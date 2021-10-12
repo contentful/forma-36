fe4d3f78c087d635427f8b1066313c07cca60517
bgutsol <bohdan.hutsol@contentful.com>
2021-10-12 13:41:09 +0200

    docs(menu): adjust menu and popover docs

--- packages/components/popover/stories/Popover.stories.tsx
+++ packages/components/popover/stories/Popover.stories.tsx
@@ -6,6 +6,9 @@ import type { PopoverProps } from '../src/.';
 import { Button } from '@contentful/f36-button';
 import { Paragraph } from '@contentful/f36-typography';
 import { Box } from '@contentful/f36-core';
+import { Stack } from '@contentful/f36-core';
+import { Switch } from '@contentful/f36-forms';
+import FocusLock from 'react-focus-lock';
 
 export default {
   component: Popover,
@@ -28,6 +31,37 @@ export const Basic: Story<PopoverProps> = (args) => {
           <Paragraph>This is the content.</Paragraph>
           <Button>Some action</Button>
         </Box>
+      </Popover.Content>
+    </Popover>
+  );
+};
+
+export const WithFocusLock: Story<PopoverProps> = (args) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Popover
+      {...args}
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      closeOnBlur={false}
+    >
+      <Popover.Trigger>
+        <Button onClick={() => setIsOpen(!isOpen)}>Toggle</Button>
+      </Popover.Trigger>
+      <Popover.Content>
+        <FocusLock>
+          <Stack
+            padding="spacingM"
+            margin="none"
+            spacing="spacingS"
+            flexDirection="column"
+          >
+            <Switch>Option 1</Switch>
+            <Switch>Option 2</Switch>
+            <Switch>Option 3</Switch>
+          </Stack>
+        </FocusLock>
       </Popover.Content>
     </Popover>
   );
