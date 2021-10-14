dbc79b7b3da3fd3f386e0dd602fa9dd57808ddf9
Alex Suevalov <alex.suevalov@contentful.com>
2021-10-14 10:33:23 +0200

    fix: test configuration

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,35 @@
-module.exports = {
-  plugins: [
-    [
-      'babel-plugin-emotion',
-      {
-        // sourceMap is on by default but source maps are dead code eliminated in production
-        sourceMap: true,
-        autoLabel: process.env.NODE_ENV !== 'production',
-        labelFormat: '[local]',
-        cssPropOptimization: true,
-      },
-    ],
-  ],
+const emotionPlugin = [
+  'babel-plugin-emotion',
+  {
+    // sourceMap is on by default but source maps are dead code eliminated in production
+    sourceMap: true,
+    autoLabel: process.env.NODE_ENV !== 'production',
+    labelFormat: '[local]',
+    cssPropOptimization: true,
+  },
+];
+
+module.exports = (api) => {
+  const isTest = api.env('test');
+  if (isTest) {
+    return {
+      presets: [
+        [
+          '@babel/preset-react',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@parcel/babel-preset-env',
+        ['@babel/preset-typescript', { allExtensions: true, isTSX: true }],
+      ],
+      plugins: [emotionPlugin],
+    };
+  } else {
+    return {
+      plugins: [emotionPlugin],
+    };
+  }
 };
