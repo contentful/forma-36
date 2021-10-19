bf130c9e7fbd91a0b8d73839c17c8711a1de3785
Renato Massao Yonamine <massao.yonamine@contentful.com>
2021-10-19 17:28:46 +0200

    refactor(notification): update typing on CTA of notification

--- packages/components/notification/src/types.ts
+++ packages/components/notification/src/types.ts
@@ -2,7 +2,7 @@ import { TextLinkProps } from '@contentful/f36-text-link';
 
 export interface NotificationCta {
   label: string;
-  textLinkProps: Partial<TextLinkProps<'button'>>;
+  textLinkProps: Partial<TextLinkProps>;
 }
 
 export type NotificationVariant = 'positive' | 'negative' | 'warning';
