6dc41a12423210c745f8cc06c46ca14a416046de
Alexander Suevalov <alex.suevalov@contentful.com>
2021-10-21 10:00:06 +0200

    fix: small Modal adjustments (#1432)

--- packages/components/modal/src/ModalHeader/ModalHeader.styles.ts
+++ packages/components/modal/src/ModalHeader/ModalHeader.styles.ts
@@ -4,9 +4,20 @@ import { css } from 'emotion';
 export function getModalHeaderStyles() {
   return {
     root: css({
+      position: 'relative',
       padding: `${tokens.spacingM} ${tokens.spacingM} ${tokens.spacingM} ${tokens.spacingL}`,
       borderRadius: `${tokens.borderRadiusMedium} ${tokens.borderRadiusMedium} 0 0`,
       borderBottom: `1px solid ${tokens.gray300}`,
+    }),
+    buttonContainer: css({
+      position: 'relative',
+      width: tokens.spacing2Xl,
+      height: tokens.spacingL,
+      button: {
+        position: 'absolute',
+        top: `calc(-1 * ${tokens.spacing2Xs})`,
+        right: 0,
+      },
     }),
   };
 }
