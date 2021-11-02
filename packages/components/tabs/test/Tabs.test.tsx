a4ae9aa312f9b972fb59ef6ece1b0034515aae27
Alex Suevalov <alex.suevalov@contentful.com>
2021-11-02 16:09:14 +0100

    chore: remove redundant test

--- packages/components/tabs/test/Tabs.test.tsx
+++ packages/components/tabs/test/Tabs.test.tsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { axe } from '@/scripts/test/axeHelper';
 
 import { Tabs } from '../src';
 
 describe('Tabs', () => {
-  it('should trigger the onSelect when clicked', () => {
-    const mockOnSelect = jest.fn();
-    const { getByText } = render(
-      <div>
-        <Tabs defaultTab="first">
-          <Tabs.List>
-            <Tabs.Tab panelId="first">First</Tabs.Tab>
-            <Tabs.Tab panelId="second" onSelect={mockOnSelect}>
-              Second
-            </Tabs.Tab>
-            <Tabs.Tab panelId="third">Third</Tabs.Tab>
-          </Tabs.List>
-          <Tabs.Panel id="first">content first panel</Tabs.Panel>
-          <Tabs.Panel id="second">content second panel</Tabs.Panel>
-          <Tabs.Panel id="third">content third panel</Tabs.Panel>
-        </Tabs>
-      </div>,
-    );
-
-    const secondTab = getByText('Second');
-    fireEvent.click(secondTab);
-
-    expect(mockOnSelect).toHaveBeenCalledTimes(1);
-  });
-
   it('has no a11y issues', async () => {
     const { container } = render(
       <div>
