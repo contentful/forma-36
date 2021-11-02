aa0e5ce9895ec2f5e1c65825439ff24a431a64c1
Gui Santos <guilherme.goncalvessantos@gmail.com>
2021-11-02 18:23:01 +0100

    docs: add basic contribution guide structure

--- packages/forma-36-website/gatsby-config.js
+++ packages/forma-36-website/gatsby-config.js
@@ -73,6 +73,10 @@ module.exports = {
             link: '/guidelines/accessibility/',
           },
           {
+            name: 'Contributing to Forma 36',
+            link: '/guidelines/contributing/',
+          },
+          {
             name: 'Copy',
             link: '',
             menuLinks: [
