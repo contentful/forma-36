d7f5588d5c0810c09677e91d3751f90f9cc07e7e
burakukula <trycjapa@gmail.com>
2021-11-12 12:58:02 +0100

    fix: set nav and migration guide in the alpabetical order

--- packages/forma-36-website/gatsby-config.js
+++ packages/forma-36-website/gatsby-config.js
@@ -113,58 +113,6 @@ module.exports = {
         link: '',
         menuLinks: [
           {
-            name: 'Layout Components',
-            link: '',
-            menuLinks: [
-              {
-                name: 'Box',
-                link: '/components/box/',
-              },
-              {
-                name: 'Flex',
-                link: '/components/flex/',
-              },
-              {
-                name: 'Stack',
-                link: '/components/stack/',
-              },
-              {
-                name: 'Grid',
-                link: '/components/grid/',
-              },
-            ],
-          },
-          {
-            name: 'Typography Components',
-            link: '',
-            menuLinks: [
-              {
-                name: 'Text',
-                link: '/components/text/',
-              },
-              {
-                name: 'Paragraph',
-                link: '/components/paragraph/',
-              },
-              {
-                name: 'Heading',
-                link: '/components/heading/',
-              },
-              {
-                name: 'SectionHeading',
-                link: '/components/section-heading/',
-              },
-              {
-                name: 'Subheading',
-                link: '/components/subheading/',
-              },
-              {
-                name: 'DisplayText',
-                link: '/components/display-text/',
-              },
-            ],
-          },
-          {
             name: 'Accordion',
             link: '/components/accordion/',
           },
@@ -274,6 +222,28 @@ module.exports = {
             link: '/components/icon-button/',
           },
           {
+            name: 'Layout Components',
+            link: '',
+            menuLinks: [
+              {
+                name: 'Box',
+                link: '/components/box/',
+              },
+              {
+                name: 'Flex',
+                link: '/components/flex/',
+              },
+              {
+                name: 'Stack',
+                link: '/components/stack/',
+              },
+              {
+                name: 'Grid',
+                link: '/components/grid/',
+              },
+            ],
+          },
+          {
             name: 'List',
             link: '/components/list/',
           },
@@ -360,6 +330,36 @@ module.exports = {
           {
             name: 'Tooltip',
             link: '/components/tooltip/',
+          },
+          {
+            name: 'Typography Components',
+            link: '',
+            menuLinks: [
+              {
+                name: 'Text',
+                link: '/components/text/',
+              },
+              {
+                name: 'Paragraph',
+                link: '/components/paragraph/',
+              },
+              {
+                name: 'Heading',
+                link: '/components/heading/',
+              },
+              {
+                name: 'SectionHeading',
+                link: '/components/section-heading/',
+              },
+              {
+                name: 'Subheading',
+                link: '/components/subheading/',
+              },
+              {
+                name: 'DisplayText',
+                link: '/components/display-text/',
+              },
+            ],
           },
           {
             name: 'Workbench',
