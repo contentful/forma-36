0e77ffa0d99923466080494c6b87ccf0d0d48c56
Patrycja Radaczyńska <patrycja.radaczynska@contentful.com>
2021-11-23 14:17:41 +0100

    fix: add link to v3 in the top nav (#1556)

--- packages/forma-36-website/src/components/Header.js
+++ packages/forma-36-website/src/components/Header.js
@@ -117,6 +117,14 @@ const Header = () => (
               Give us feedback
             </a>
           </li>
+          <li className={styles.navListItem}>
+            <a
+              className={styles.navListLink}
+              href="https://v3.f36.contentful.com/"
+            >
+              Forma 36 v3
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
