1bd3ea10c0513b2cb6fe2f30b75a59f9891abec5
Gui Santos <guilherme.goncalvessantos@gmail.com>
2021-11-26 15:58:00 +0100

    fix: Props list padding

--- packages/f36-docs-utils/src/Props/Props.styles.ts
+++ packages/f36-docs-utils/src/Props/Props.styles.ts
@@ -2,7 +2,7 @@ import { css } from 'emotion';
 import tokens from '@contentful/f36-tokens';
 
 export const getPropsStyles = () => ({
-  list: css({ listStyleType: 'none' }),
+  list: css({ listStyleType: 'none', padding: 0 }),
   listItem: css({
     padding: `${tokens.spacingM} 0`,
     borderBottom: `1px solid ${tokens.gray200}`,
