f5656d08871e17b99dd8908d4ce82f435120c9f0
Tatiana Bessonova <tatiana.bessonova@contentful.com>
2021-11-30 20:39:26 +0100

    refactor(usearrowkeynaviigation): move useArrowKeyNavigation hook to "Menu" folder

--- packages/components/utils/src/index.ts
+++ packages/components/utils/src/index.ts
@@ -1,6 +1,5 @@
 export { Portal } from './Portal/Portal';
 export type { PortalProps } from './Portal/Portal';
-export { useArrowKeyNavigation } from './ArrowKeyNavigation/useArrowKeyNavigation';
 export { useKeyboard } from './useKeyboard/useKeyboard';
 export type { UseKeyboardProps } from './useKeyboard/useKeyboard';
 export { getStringMatch } from './getStringMatch/getStringMatch';
