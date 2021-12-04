8b4dc50893bc73f44f815b7792b0af0ebdb46866
bgutsol <bohdan.hutsol@contentful.com>
2021-12-04 14:46:44 +0100

    chore: replace Menu examples with paths to files

---
+++ packages/components/menu/examples/ControlledMenu.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Menu, IconButton } from '@contentful/f36-components';
+import { MenuIcon } from '@contentful/f36-icons';
+
+export default function ControlledMenu() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <Menu
+      isOpen={isOpen}
+      onOpen={() => setIsOpen(true)}
+      onClose={() => setIsOpen(false)}
+    >
+      <Menu.Trigger>
+        <IconButton
+          variant="secondary"
+          icon={<MenuIcon />}
+          aria-label="toggle menu"
+        />
+      </Menu.Trigger>
+      <Menu.List>
+        <Menu.Item>Create an entry</Menu.Item>
+        <Menu.Item>Remove an entry</Menu.Item>
+        <Menu.Item>Embed existing entry</Menu.Item>
+      </Menu.List>
+    </Menu>
+  );
+}
