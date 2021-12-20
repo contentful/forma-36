7dede7ec27d196587ebb5c1262336cbc82955973
Alex Suevalov <alex.suevalov@contentful.com>
2021-12-20 15:28:53 +0100

    fix: proper inline styles for next.js

---
+++ packages/website/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { extractCritical } from 'emotion-server';
+
+export default class MyDocument extends Document<{
+  ids: string[];
+  css: string;
+}> {
+  static getInitialProps({ renderPage }) {
+    const page = renderPage();
+    const styles = extractCritical(page.html);
+    return { ...page, ...styles };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <style
+            data-emotion-css={this.props.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: this.props.css }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
