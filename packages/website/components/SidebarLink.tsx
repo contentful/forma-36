32f2a0e92a2d6f7193710bce1f1416ac6fe6dce6
Alex Suevalov <alex.suevalov@contentful.com>
2021-12-20 12:26:05 +0100

    feat: split into more sections and implement second level navigation

--- packages/website/components/SidebarLink.tsx
+++ packages/website/components/SidebarLink.tsx
@@ -2,26 +2,30 @@ import React from 'react';
 import { css, cx } from 'emotion';
 import Link from 'next/link';
 import tokens from '@contentful/f36-tokens';
-import { List } from '@contentful/f36-components';
+import { List, Flex } from '@contentful/f36-components';
+import { ChevronDownIcon } from '@contentful/f36-icons';
 
-const getSectionTitleStyles = (isActive = false, isTitle = false) => {
+export const getSectionTitleStyles = (isActive = false, indent = 1) => {
   return {
     sidebarItem: css({
       padding: `${tokens.spacingXs} ${tokens.spacingM}`,
-      ...(!isTitle && { paddingLeft: tokens.spacingXl }),
+      paddingLeft: `calc(${indent} * ${tokens.spacingM})`,
     }),
     link: css({
       display: 'block',
+    }),
+    clickable: css({
       textDecoration: 'none',
+      cursor: 'pointer',
       color: isActive ? tokens.colorWhite : tokens.gray900,
       backgroundColor: isActive ? tokens.colorPrimary : 'transparent',
       transition: `background-color ${tokens.transitionDurationDefault} ${tokens.transitionEasingDefault}`,
       '&:hover': {
         backgroundColor: isActive ? tokens.colorPrimary : tokens.gray200,
       },
-      '& h2': {
-        color: isActive ? tokens.colorWhite : tokens.gray900,
-      },
+    }),
+    closedIcon: css({
+      transform: 'rotate(-90deg)',
     }),
   };
 };
@@ -30,22 +34,53 @@ interface SidebarLinkProps {
   children: string;
   href: string;
   isActive?: boolean;
-  isTitle?: boolean;
+  indent?: number;
+}
+
+export function SidebarSectionButton(props: {
+  children: string;
+  isOpen: boolean;
+  onClick?: React.MouseEventHandler;
+}) {
+  const titleStyles = getSectionTitleStyles(false);
+  return (
+    <List.Item>
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        className={cx([titleStyles.clickable, titleStyles.sidebarItem])}
+        role="button"
+        onClick={props.onClick}
+      >
+        <span>{props.children}</span>
+        <ChevronDownIcon
+          variant="muted"
+          className={!props.isOpen ? titleStyles.closedIcon : ''}
+        />
+      </Flex>
+    </List.Item>
+  );
 }
 
 export function SidebarLink({
   children,
   href,
   isActive = false,
-  isTitle = false,
+  indent = 1,
 }: SidebarLinkProps) {
-  const titleStyles = getSectionTitleStyles(isActive, isTitle);
+  const titleStyles = getSectionTitleStyles(isActive, indent);
 
   return (
     <List.Item>
       <Link href={href} passHref>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a className={cx([titleStyles.link, titleStyles.sidebarItem])}>
+        <a
+          className={cx([
+            titleStyles.link,
+            titleStyles.clickable,
+            titleStyles.sidebarItem,
+          ])}
+        >
           {children}
         </a>
       </Link>
