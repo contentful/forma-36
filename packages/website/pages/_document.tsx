963a37f5a30814dd289909ceb713f84c8f4ae201
burakukula <trycjapa@gmail.com>
2021-12-22 12:57:33 +0100

    feat: add Segment

--- packages/website/pages/_document.tsx
+++ packages/website/pages/_document.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { extractCritical } from 'emotion-server';
+import * as snippet from '@segment/snippet';
 
+const { ANALYTICS_WRITE_KEY, NODE_ENV } = process.env;
 export default class MyDocument extends Document<{
   ids: string[];
   css: string;
 }> {
+  renderSnipet() {
+    const opts = {
+      apiKey: ANALYTICS_WRITE_KEY,
+      page: true,
+    };
+
+    if (NODE_ENV === 'development') {
+      return snippet.max(opts);
+    }
+
+    return snippet.min(opts);
+  }
+
   static getInitialProps({ renderPage }) {
     const page = renderPage();
     const styles = extractCritical(page.html);
@@ -20,6 +35,7 @@ export default class MyDocument extends Document<{
             data-emotion-css={this.props.ids.join(' ')}
             dangerouslySetInnerHTML={{ __html: this.props.css }}
           />
+          <script dangerouslySetInnerHTML={{ __html: this.renderSnipet() }} />
         </Head>
         <body>
           <Main />
