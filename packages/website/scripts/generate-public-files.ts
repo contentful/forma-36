6a90e184db0c6132dd23cfe573f6388fa4061d45
Alex Suevalov <alex.suevalov@contentful.com>
2021-12-23 10:46:27 +0100

    feat: generate robots.txt

--- packages/website/scripts/generate-public-files.ts
+++ packages/website/scripts/generate-public-files.ts
@@ -5,17 +5,18 @@ import { getMdxPaths } from '../utils/content';
 
 dotenv.config();
 
-async function generate() {
-  // todo: replace with f36.contentful.com when we release v4
-  const baseUrl = 'https://v4-forma-36.vercel.app';
+// todo: replace with f36.contentful.com when we release v4
+const deploymentUrl =
+  process.env.VERCEL_URL || 'https://v4-forma-36.vercel.app';
 
+async function generateSitemap() {
   const staticSlugs = [''];
   const mdxSlugs = (await getMdxPaths())
     .map((item) => item.params.slug)
     .map((slugs) => slugs.join('/'));
 
   const fullUrls = [...staticSlugs, ...mdxSlugs].map((staticPagePath) => {
-    return `${baseUrl}/${staticPagePath}`;
+    return `${deploymentUrl}/${staticPagePath}`;
   });
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -43,8 +44,26 @@ async function generate() {
   writeFileSync('public/sitemap.xml', formatted);
 }
 
+async function generateRobots() {
+  // eslint-disable-next-line no-sync
+  writeFileSync(
+    'public/robots.txt',
+    `# *
+  User-agent: *
+  Allow: /
+
+  # Host
+  Host: ${deploymentUrl}
+
+  # Sitemaps
+  Sitemap: ${deploymentUrl}/sitemap.xml
+  `,
+  );
+}
+
 async function run() {
-  await generate();
+  await generateSitemap();
+  await generateRobots();
 }
 
 run();
