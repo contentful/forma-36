7c6c9670e6b285935aca9c5d6983f158340db534
Alex Suevalov <alex.suevalov@contentful.com>
2021-12-28 17:07:33 +0100

    chore: adjust ts problem

--- packages/website/pages/playground.tsx
+++ packages/website/pages/playground.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 
 const PlaygroundComponentWithNoSSR = dynamic(
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore ignore a complaint about dynamic import
   () => import('../components/Playground/Playground'),
   {
     ssr: false,
