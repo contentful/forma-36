5db4a86963ec60e0d21c91baa1ba1c3197104371
Alex Suevalov <alex.suevalov@contentful.com>
2021-12-29 12:22:37 +0100

    chore: revert GlobalStyles change

--- packages/website/components/GlobalStyles.tsx
+++ packages/website/components/GlobalStyles.tsx
@@ -28,7 +28,7 @@ export const GlobalStyles = () => {
         body {
           color: ${tokens.gray800};
           font-family: ${tokens.fontStackPrimary};
-          font-size: ${tokens.fontSizeL} !important;
+          font-size: ${tokens.fontSizeM};
           line-height: ${tokens.lineHeightM};
           min-width: 1280px;
         }
