b37c4c7ea1b8edd9455745656939506ca55b8387
burakukula <trycjapa@gmail.com>
2021-12-30 13:21:38 +0100

    fix: meh

--- packages/website/utils/mdx-utils.ts
+++ packages/website/utils/mdx-utils.ts
@@ -3,11 +3,12 @@ import slugger from 'github-slugger';
 //see https://github.com/hashicorp/next-mdx-remote/issues/53#issuecomment-725906664
 export function getTableOfContents(mdxContent: string) {
   const regexp = new RegExp(/^(### |## )(.*)\n/, 'gm');
-  // @ts-ignore
+
   const headings = [...mdxContent.matchAll(regexp)];
   let tableOfContents = [];
 
   if (headings.length) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     tableOfContents = headings.map((heading) => {
       const headingText = heading[2].trim();
