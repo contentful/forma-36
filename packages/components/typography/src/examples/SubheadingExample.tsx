c5953c20e217ba39f1f9ddec31134ae95fa853e0
bgutsol <bohdan.hutsol@contentful.com>
2022-01-04 17:53:56 +0200

    docs: Subheading

---
+++ packages/components/typography/src/examples/SubheadingExample.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Subheading } from '@contentful/f36-components';
+
+export default function SubheadingExample() {
+  return <Subheading>I love Forma 36 design system</Subheading>;
+}
