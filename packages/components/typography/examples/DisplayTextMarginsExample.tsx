9a1289d7f1b46ff8554bc3999cd78b7f26808700
Alexander Suevalov <alex.suevalov@contentful.com>
2022-01-05 13:15:58 +0100

    feat: display text documentation (#1691)

---
+++ packages/components/typography/examples/DisplayTextMarginsExample.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Flex, DisplayText } from '@contentful/f36-components';
+
+export default function DisplayTextMarginsExample() {
+  return (
+    <Flex flexDirection="column">
+      <DisplayText size="huge" marginBottom="none">
+        Welcome!
+      </DisplayText>
+      <DisplayText size="large" marginBottom="spacing4Xl">
+        Pages
+      </DisplayText>
+      <DisplayText marginLeft="spacingM">Select space</DisplayText>
+    </Flex>
+  );
+}
