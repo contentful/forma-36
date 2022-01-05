166cce4b89c7426db36f5337003542053e30da3d
Alexander Suevalov <alex.suevalov@contentful.com>
2022-01-05 16:59:18 +0100

    fix: bring the previous version of Algolia back (#1705)
    * fix: bring the previous version of Algolia back
    
    * fix: ignore ts complaint
    
    * fix: width

--- packages/website/components/DocSearch.tsx
+++ packages/website/components/DocSearch.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from 'react';
 import { css } from 'emotion';
-// @ts-expect-error ignore missing type declarations
-import docsearch from '@docsearch/js';
 import tokens from '@contentful/f36-tokens';
-
-const DOCSEARCH_API_KEY = process.env.DOCSEARCH_API_KEY || '';
-const DOCSEARCH_APP_ID = process.env.DOCSEARCH_APP_ID || '';
+import { SearchIcon } from '@contentful/f36-icons';
+import { TextInput } from '@contentful/f36-forms';
 
 const styles = {
   container: css`
@@ -14,30 +11,43 @@ const styles = {
     width: 100%;
     padding: ${tokens.spacingM} ${tokens.spacingM} 0;
     margin-bottom: ${tokens.spacingM};
-
-    button {
-      margin-left: 0;
-      width: 280px;
+    & .algolia-autocomplete {
+      width: 100%;
     }
   `,
 };
 
 export const DocSearch = () => {
   useEffect(() => {
-    try {
-      docsearch({
-        // The key is added here only give access to searching the public content of the website https://docsearch.algolia.com/docs/what-is-docsearch
-        // You can even check Forma 36's configuration in DocSearch's repo https://github.com/algolia/docsearch-configs/blob/master/configs/contentful_forma-36.json
-        appId: DOCSEARCH_APP_ID,
-        apiKey: DOCSEARCH_API_KEY,
-        indexName: 'forma-36',
-        container: '#search-container',
-        debug: false,
-        placeholder: 'Search the docs',
-      });
-    } catch (e) {
-      console.error('Failed to initialize Algolia search', e);
-    }
+    if (typeof window === 'undefined') return;
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore ignore a complaint about dynamic import
+    import('docsearch.js').then((docsearch) => {
+      try {
+        docsearch.default({
+          // The key is added here only give access to searching the public content of the website https://docsearch.algolia.com/docs/what-is-docsearch
+          // You can even check Forma 36's configuration in DocSearch's repo https://github.com/algolia/docsearch-configs/blob/master/configs/contentful_forma-36.json
+          appId: process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID || '',
+          apiKey: process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY || '',
+          indexName: 'forma-36',
+          inputSelector: '#search',
+        });
+      } catch (e) {
+        console.error('Failed to initialize Algolia search', e);
+      }
+    });
   }, []);
-  return <div className={styles.container} id="search-container" />;
+  return (
+    <div className={styles.container}>
+      <TextInput
+        id="search"
+        icon={<SearchIcon variant="muted" />}
+        name="search"
+        type="text"
+        placeholder="Search the docs"
+        defaultValue=""
+      />
+    </div>
+  );
 };
