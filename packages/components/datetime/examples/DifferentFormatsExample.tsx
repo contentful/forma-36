b475954cc9f7351471426449c8bec522d95a6a01
Andi Pätzold <andreas.paetzold@contentful.com>
2022-01-11 15:37:05 +0100

    feat: add `fullWithSeconds` datetime format (#1720)
    * feat: add full-with-seconds datetime format
    
    * feat: rename full-with-seconds to fullWithSeconds
    
    * docs: add example

--- packages/components/datetime/examples/DifferentFormatsExample.tsx
+++ packages/components/datetime/examples/DifferentFormatsExample.tsx
@@ -4,12 +4,14 @@ import { Stack, Text, formatDateAndTime } from '@contentful/f36-components';
 export default function DifferentFormatsExample() {
   return (
     <Stack flexDirection="column">
+      {/* With `fullWithSeconds` format */}
+      <Text>{formatDateAndTime('2021-08-17T15:45:34', 'fullWithSeconds')}</Text>
       {/* With `day` format */}
-      <Text>{formatDateAndTime('2021-08-17T15:45', 'day')}</Text>
+      <Text>{formatDateAndTime('2021-08-17T15:45:34', 'day')}</Text>
       {/* With `time` format */}
-      <Text>{formatDateAndTime('2021-08-17T15:45', 'time')}</Text>
+      <Text>{formatDateAndTime('2021-08-17T15:45:34', 'time')}</Text>
       {/* With `weekday` format */}
-      <Text>{formatDateAndTime('2021-08-17T15:45', 'weekday')}</Text>
+      <Text>{formatDateAndTime('2021-08-17T15:45:34', 'weekday')}</Text>
     </Stack>
   );
 }
