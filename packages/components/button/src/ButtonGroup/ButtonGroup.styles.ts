5fab018605aae8978525359610a43d371a86a20b
Renato Massao Yonamine <massao.yonamine@contentful.com>
2022-01-13 15:13:27 +0100

    fix(togglebutton): fix border when hovering grouped toggle button

--- packages/components/button/src/ButtonGroup/ButtonGroup.styles.ts
+++ packages/components/button/src/ButtonGroup/ButtonGroup.styles.ts
@@ -19,7 +19,7 @@ const getGroupContentStyle = ({ withDivider }: GetStyleArguments) => {
       borderTopRightRadius: `${tokens.borderRadiusMedium} !important`,
       marginRight: 0,
     },
-    '&:focus': {
+    '&:focus, &:hover': {
       zIndex: tokens.zIndexDefault + 1,
     },
     ...dividerStyle,
