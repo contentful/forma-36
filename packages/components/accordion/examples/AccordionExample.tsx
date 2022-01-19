a1bb62a09009b3af80548ad2a8c7fa8fe0d31506
burakukula <trycjapa@gmail.com>
2022-01-19 10:16:31 +0100

    fix: fix examples

--- packages/components/accordion/examples/AccordionExample.tsx
+++ packages/components/accordion/examples/AccordionExample.tsx
@@ -11,6 +11,13 @@ export default function IconButtonExample() {
           paying with a credit card or wire transfer.
         </Text>
       </Accordion.Item>
+      <Accordion.Item title="What spaces can I use Compose + Launch on?">
+        <Text>
+          Once you have purchased Compose + Launch, they can be installed on all
+          of your spaces, or only the ones you want to have them on. There is no
+          extra cost to install Compose + Launch on multiple spaces.
+        </Text>
+      </Accordion.Item>
     </Accordion>
   );
 }
--- packages/components/accordion/examples/AccordionWithOtherComponentExample.tsx
+++ packages/components/accordion/examples/AccordionWithOtherComponentExample.tsx
@@ -5,6 +5,7 @@ import {
   SectionHeading,
   Stack,
   Button,
+  List,
 } from '@contentful/f36-components';
 
 export default function IconButtonExample() {
@@ -27,6 +28,63 @@ export default function IconButtonExample() {
             Accordion’s button
           </Button>
         </Stack>
+      </Accordion.Item>
+      <Accordion.Item
+        title={
+          <SectionHeading marginBottom="none">
+            What can I do in Compose + Launch?
+          </SectionHeading>
+        }
+      >
+        <Text marginBottom="spacingS">
+          Compose lets editors easily create and manage web content in the
+          context of “pages.”
+        </Text>
+        <List>
+          <List.Item>
+            Easy page creation. With a clearer view of the content you’re
+            creating
+          </List.Item>
+          <List.Item>
+            Integrated SEO settings. Manage SEO titles and description, or hide
+            content from search engines
+          </List.Item>
+          <List.Item>
+            Work with structured content. Use existing content models to build
+            new page
+          </List.Item>
+          <List.Item>
+            Page localization. Easier to manage pages across multiple markets
+            and teams
+          </List.Item>
+          <List.Item>
+            Page localization. Easier to manage pages across multiple markets
+            and teams
+          </List.Item>
+        </List>
+
+        <Text marginBottom="spacingS">
+          Launch gives planners visibility and confidence in the orchestration
+          of their content releases.
+        </Text>
+        <List>
+          <List.Item>
+            Build releases. Published and scheduled groups of content at the
+            same time
+          </List.Item>
+          <List.Item>
+            Release calendar. Gives users a clear view of all upcoming published
+            and unpublished events
+          </List.Item>
+          <List.Item>
+            Flexible scheduling. Move scheduled releases forward or back in
+            seconds
+          </List.Item>
+          <List.Item>
+            Workflow states. Flag content to team members who need to see it
+            before publication
+          </List.Item>
+        </List>
       </Accordion.Item>
     </Accordion>
   );
