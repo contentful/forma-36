490e3de0e2e9cb9a2a50a2eb422ca7ea042c65e1
Tatiana Bessonova <tatiana.bessonova@contentful.com>
2022-01-20 13:39:25 +0300

    docs(card): fix ts linter

--- packages/components/card/examples/Card/WithLinkAndTargetExample.tsx
+++ packages/components/card/examples/Card/WithLinkAndTargetExample.tsx
@@ -3,7 +3,7 @@ import { Card, Text } from '@contentful/f36-components';
 
 export default function WithLinkAndTargetExample() {
   return (
-    <Card href="https://f36.contentful.com" target="_blank">
+    <Card as="a" href="https://f36.contentful.com" target="_blank">
       <Text>
         Forma 36 is an open-source design system by Contentful created with the
         intent to reduce the overhead of creating UI by providing tools and
