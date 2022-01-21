e3757df954ea692b52d90af13d182c3dd4d895e8
burakukula <trycjapa@gmail.com>
2022-01-21 09:55:55 +0100

    fix: add version switch

--- packages/forma-36-website/src/components/Header.js
+++ packages/forma-36-website/src/components/Header.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import tokens from '@contentful/forma-36-tokens';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
 // eslint-disable-next-line
 import pkg from '../../../forma-36-react-components/package.json';
+import {
+  Dropdown,
+  DropdownList,
+  DropdownListItem,
+  Button,
+  TextLink,
+} from '@contentful/forma-36-react-components';
 
 const styles = {
   header: css`
@@ -20,11 +27,12 @@ const styles = {
     align-items: center;
     text-decoration: none;
     color: #fff;
+    margin-right: ${tokens.spacingM};
   `,
   logoText: css`
     font-weight: ${tokens.fontWeightDemiBold};
     font-size: ${tokens.fontSizeXl};
-    margin-left: ${tokens.spacingL};
+    margin-left: ${tokens.spacingS};
   `,
   versionText: css`
     font-weight: ${tokens.fontWeightDemiBold};
@@ -74,56 +82,78 @@ const Logo = () => (
   </svg>
 );
 
-const Header = () => (
-  <header css={styles.header}>
-    <Link to="/" css={styles.logoLink}>
-      <Logo />
-      <div css={styles.logoText}>Forma 36</div>
-    </Link>
-    <div css={styles.versionText}>v{pkg.version}</div>
+const Header = () => {
+  const [isOpen, setOpen] = useState(false);
 
-    <div css={styles.searchNavContainer}>
-      <nav css={styles.nav}>
-        <ul css={styles.navList}>
-          <li css={styles.navListItem}>
-            <a
-              css={styles.navListLink}
-              href="https://github.com/contentful/forma-36"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </li>
-          <li css={styles.navListItem}>
-            <a
-              css={styles.navListLink}
-              href="https://contentful.design/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Blog
-            </a>
-          </li>
-          <li css={styles.navListItem}>
-            <a
-              css={styles.navListLink}
-              href="https://www.contentful.com/developers/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Join the community
-            </a>
-          </li>
-          <li css={styles.navListItem}>
-            <a css={styles.navListLink} href="https://f36.contentful.com/">
-              Forma 36 v4
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
+  return (
+    <header css={styles.header}>
+      <Link to="/" css={styles.logoLink}>
+        <Logo />
+        <div css={styles.logoText}>Forma 36</div>
+      </Link>
+      <Dropdown
+        isOpen={isOpen}
+        onClose={() => setOpen(false)}
+        toggleElement={
+          <Button
+            size="small"
+            buttonType="muted"
+            indicateDropdown
+            onClick={() => setOpen(!isOpen)}
+          >
+            v{pkg.version}
+          </Button>
+        }
+      >
+        <DropdownList>
+          <DropdownListItem>
+            <TextLink href="https://v3.f36.contentful.com/">
+              v{pkg.version}
+            </TextLink>
+          </DropdownListItem>
+          <DropdownListItem>
+            <TextLink href="https://f36.contentful.com/">v4</TextLink>
+          </DropdownListItem>
+        </DropdownList>
+      </Dropdown>
+      <div css={styles.searchNavContainer}>
+        <nav css={styles.nav}>
+          <ul css={styles.navList}>
+            <li css={styles.navListItem}>
+              <a
+                css={styles.navListLink}
+                href="https://github.com/contentful/forma-36"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li css={styles.navListItem}>
+              <a
+                css={styles.navListLink}
+                href="https://contentful.design/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Blog
+              </a>
+            </li>
+            <li css={styles.navListItem}>
+              <a
+                css={styles.navListLink}
+                href="https://www.contentful.com/developers/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Join the community
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
