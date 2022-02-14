0b200eb7d815647873889df31368de9a9de94274
Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
2022-02-14 14:12:14 +0100

    feat(pagination): add Pagination component (#1845)
    * feat(pagination): add folder structure for pagination component
    
    * feat(pagination): migrate pagination component from experience-package (#1850)
    
    * feat(pagination): migrate pagination component from experience-package and remove lodash dependency
    
    * fix: fix typo on package.json dependency
    
    * feat(pagination): add props descriptions and pass otherProps down
    
    * feat(pagination): add content to example file on Pagination component
    
    * feat(pagination): Add Pagination documentation and add Pagination to the f36-components (#1854)
    
    * feat(website): add New badge on sidebar and remove lodash dependency
    
    * feat(website): add alpha and deprecated note based on component status
    
    * feat(pagination): add a11y properties and documentation
    
    * feat(components): add Pagination to f36-components package
    
    * refactor: bump yarn.lock
    
    * feat: update version of pagination to correct one
    
    * refactor(website): improve showNote validation
    
    * fix(pagination): make pagination example work on playground
    
    * fix(website): pass Pagination into scope of ComponentSource
    
    * feat(pagination): add private true so Pagination package is not automatically published
    
    * feat(pagination): remove unnecessary export

---
+++ packages/components/pagination/src/Pagination.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+
+import { Button } from '@contentful/f36-button';
+import { CommonProps, Stack } from '@contentful/f36-core';
+import { ChevronLeftIcon, ChevronRightIcon } from '@contentful/f36-icons';
+
+export interface PaginationProps extends CommonProps {
+  /**
+   * Sets which page is active on the Pagination
+   */
+  activePage: number;
+  /**
+   * Sets how many items are displayed per page
+   */
+  itemsPerPage: number;
+  /**
+   * Total amount of items the pagination is applied to.
+   */
+  totalItems: number;
+  /**
+   * Handler function called when user navigates to another page on the pagination.
+   */
+  onPageChange: (page: number) => void;
+}
+
+function getPagesRange(
+  page: number,
+  total: number,
+  neighboursCount = 2,
+): number[] {
+  const PAGINATION_RANGE = neighboursCount * 2;
+
+  if (total <= PAGINATION_RANGE) {
+    // Total amount of pages are less than the possible pagination range
+    return [...Array(total).keys()];
+  }
+
+  const baseRange = [...Array(PAGINATION_RANGE + 1).keys()];
+
+  if (page <= neighboursCount) {
+    // Active page is at the start of the pagination page count
+    return baseRange;
+  }
+  if (page > total - neighboursCount) {
+    // Active page is at the end of the pagination page count
+    return baseRange.map((i) => i + total - PAGINATION_RANGE - 1);
+  }
+  // Active page is in the middle of the pagination count
+  return baseRange.map((i) => i + page - neighboursCount - 1);
+}
+
+function _Pagination(props: PaginationProps, ref: React.Ref<HTMLDivElement>) {
+  const {
+    className,
+    onPageChange,
+    testId = 'cf-ui-pagination',
+    activePage: propsActivePage,
+    totalItems,
+    itemsPerPage,
+    ...otherProps
+  } = props;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const activePage = Math.max(Math.min(propsActivePage, totalPages), 1);
+  const hasOnlyOnePage = totalPages === 1;
+  const activePageIsAtPaginationStart = activePage === 1;
+  const activePageIsAtPaginationEnd = activePage === totalPages;
+
+  return (
+    <Stack
+      as="nav"
+      role="navigation"
+      aria-label="Pagination Navigation"
+      className={className}
+      testId={testId}
+      spacing="spacingS"
+      ref={ref}
+      {...otherProps}
+    >
+      <Button
+        aria-label="To previous page"
+        size="small"
+        startIcon={<ChevronLeftIcon />}
+        variant="transparent"
+        isDisabled={hasOnlyOnePage || activePageIsAtPaginationStart}
+        onClick={() => onPageChange(activePage - 1)}
+        testId="cf-ui-pagination-previous"
+      >
+        Previous
+      </Button>
+      <Stack spacing="spacing2Xs">
+        {getPagesRange(activePage, totalPages).map((pageIndex) => {
+          const page = pageIndex + 1;
+          return (
+            <Button
+              aria-label={`To Page ${page}`}
+              aria-current={page === activePage ? 'true' : undefined}
+              onClick={() => onPageChange(page)}
+              size="small"
+              variant={page === activePage ? 'secondary' : 'transparent'}
+              testId={page === activePage ? 'active' : `inactive-${page}`}
+              key={pageIndex}
+            >
+              {page}
+            </Button>
+          );
+        })}
+      </Stack>
+      <Button
+        variant="transparent"
+        size="small"
+        endIcon={<ChevronRightIcon />}
+        isDisabled={hasOnlyOnePage || activePageIsAtPaginationEnd}
+        onClick={() => onPageChange(activePage + 1)}
+        aria-label="To next page"
+        testId="cf-ui-pagination-next"
+      >
+        Next
+      </Button>
+    </Stack>
+  );
+}
+
+export const Pagination = React.forwardRef(_Pagination);
