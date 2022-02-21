12dd1ee0c87e7a0bc4cb2234ba3c92686672cf80
Renato Massao Yonamine <massao.yonamine@contentful.com>
2022-02-21 13:37:33 +0100

    feat(pagination): update pagination visual and functionalities

--- packages/components/pagination/src/utils.tsx
+++ packages/components/pagination/src/utils.tsx
@@ -1,13 +1,24 @@
-export function getExpectedItemsOfActivePage(
-  activePage: number,
-  totalItems: number,
-  itemsPerPage: number,
-): number {
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-  if (activePage === totalPages) {
-    return totalItems % itemsPerPage || itemsPerPage;
+export function getRangeText({
+  activePage,
+  itemsPerPage,
+  totalItems = 0,
+  pageLength,
+  isLastPage = false,
+}: {
+  activePage: number;
+  itemsPerPage: number;
+  totalItems?: number;
+  pageLength?: number;
+  isLastPage?: boolean;
+}): string {
+  const total = totalItems ? `of ${totalItems} items` : '';
+  const firstItem = activePage * itemsPerPage + 1;
+  if (isLastPage && pageLength) {
+    return [`${firstItem} - ${firstItem - 1 + pageLength}`, total].join(' ');
   }
-
-  return itemsPerPage;
+  if (totalItems) {
+    const lastItem = Math.min(firstItem - 1 + itemsPerPage, totalItems);
+    return [`${firstItem} - ${lastItem}`, total].join(' ');
+  }
+  return [`${firstItem} - ${firstItem - 1 + itemsPerPage}`, total].join(' ');
 }
