88c24f10d4e298179ac27f4b18040c20aa905ae8
Renato Massao Yonamine <massao.yonamine@contentful.com>
2022-02-23 15:05:34 +0100

    docs(pagination): add more examples and update documentation

--- packages/components/pagination/examples/PaginationWithTotalExample.tsx
+++ packages/components/pagination/examples/PaginationWithTotalExample.tsx
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import { Pagination } from '@contentful/f36-pagination';
 
-export default function PaginationExample() {
+export default function PaginationWithTotalExample() {
   const [page, setPage] = useState(0);
-  // isLastPage needs to be set to hide Next Button
-  const isLastPage = page > 3;
 
   return (
     <Pagination
       activePage={page}
       onPageChange={setPage}
-      itemsPerPage={10}
-      isLastPage={isLastPage}
+      itemsPerPage={20}
+      totalItems={113}
     />
   );
 }
