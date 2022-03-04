f820e4f9ec4eb43a68861d677438557347bd3443
Gui Santos <guilherme.goncalvessantos@gmail.com>
2022-03-04 11:58:35 +0100

    fix: width of grid examples containers

--- packages/core/src/Grid/examples/ContentAlignmentGridExample.tsx
+++ packages/core/src/Grid/examples/ContentAlignmentGridExample.tsx
@@ -4,7 +4,11 @@ import tokens from '@contentful/f36-tokens';
 
 export default function ContentAlignmentGridExample() {
   return (
-    <Grid columns="250px 250px" justifyContent="space-between">
+    <Grid
+      style={{ width: '100%' }}
+      columns="250px 250px"
+      justifyContent="space-between"
+    >
       <Grid.Item style={{ backgroundColor: tokens.gray700, height: '100px' }} />
       <Grid.Item style={{ backgroundColor: tokens.gray700, height: '100px' }} />
     </Grid>
