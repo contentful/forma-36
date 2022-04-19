bcc26fb7f95614587aa6ee6682311ef3a835e938
Meike Pollakowski <meike.pollakowski@gmail.com>
2022-04-19 16:57:30 +0200

    feat(icons): Add TriangleOutline and rrowBackward icons

---
+++ packages/components/icons/src/TriangleOutline.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { generateIcon } from '@contentful/f36-icon';
+
+export const TriangleOutline = /*#__PURE__*/ generateIcon({
+  name: 'TriangleOutline',
+  path: (
+    <>
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 0L18 15.5455H0L9 0ZM2.83819 13.9091L9 3.26596L15.1618 13.9091H2.83819Z"
+      />
+    </>
+  ),
+});
