1f99d90c7ca08fcf702d32768a857cb8bbb7e22c
Kristoffer <denkristoffer@users.noreply.github.com>
2022-04-20 10:29:46 +0200

    fix(modal): don't scale modal with size="zen" (#1994)
    Co-authored-by: Renato Massao Yonamine <1071799+massao@users.noreply.github.com>

---
+++ .changeset/long-onions-behave.md
@@ -0,0 +1,5 @@
+---
+"@contentful/f36-modal": patch
+---
+
+fix(modal): fix scaling issues for modals with size "zen"
--- packages/components/modal/src/types.ts
+++ packages/components/modal/src/types.ts
@@ -1,10 +1,3 @@
-export type ModalSizeType =
-  | 'small'
-  | 'medium'
-  | 'large'
-  | 'fullWidth'
-  | 'zen'
-  | string
-  | number;
+export type ModalSizeType = 'small' | 'medium' | 'large' | 'fullWidth' | 'zen';
 
 export type ModalPositionType = 'center' | 'top';
