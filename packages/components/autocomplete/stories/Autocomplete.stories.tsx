607004737aa3c16ae1594be55cf02dfe60d206b9
Kathrin <kathrin.holzmann@contentful.com>
2022-05-17 15:46:26 +0200

    docs: don t remove args from story

--- packages/components/autocomplete/stories/Autocomplete.stories.tsx
+++ packages/components/autocomplete/stories/Autocomplete.stories.tsx
@@ -147,8 +147,8 @@ UsingObjectsAsItems.args = {
   placeholder: 'Search your favorite fruit',
 };
 
-export const UsingGroupedItems = () => {
-  const [selectedItem, setSelectedItem] = useState<Produce>();
+export const UsingGroupedItems = (args: AutocompleteProps<Produce>) => {
+  const [selectedItem, setSelectedItem] = useState<Produce>({} as Produce);
   const [filteredItems, setFilteredItems] = useState(groceryList);
 
   const handleInputValueChange = (value: string) => {
@@ -176,6 +176,8 @@ export const UsingGroupedItems = () => {
     >
       {/* It’s not necessary to pass "Fruit" (type of one item)  */}
       <Autocomplete<Produce>
+        {...args}
+        selectedItem={selectedItem}
         items={filteredItems}
         isGrouped
         renderItem={(item) => item.name}
