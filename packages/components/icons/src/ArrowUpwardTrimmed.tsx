83bd86af474d917cce5df89bb25adea90ce45e54
Daniel Vasylenko <Spring3@users.noreply.github.com>
2022-06-09 11:41:49 +0200

    Update packages/components/icons/src/ArrowUpwardTrimmed.tsx
    Co-authored-by: Kristoffer <denkristoffer@users.noreply.github.com>

--- packages/components/icons/src/ArrowUpwardTrimmed.tsx
+++ packages/components/icons/src/ArrowUpwardTrimmed.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { generateIcon } from '@contentful/f36-icon';
 
-export const ArrowUpTrimmed = /*#__PURE__*/ generateIcon({
+export const ArrowUpwardTrimmed = /*#__PURE__*/ generateIcon({
   name: 'ArrowUpwardTrimmed',
   path: (
     <Fragment>
