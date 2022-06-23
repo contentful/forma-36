ec91d9bc2fca62068347d8add53e2ff339395736
Tanya Bessonova <tatiana.bessonova@contentful.com>
2022-06-23 10:50:57 +0200

    test(tabs): add tests as an example for testing Tabs component (#2103)

--- packages/components/tabs/src/Tabs.test.tsx
+++ packages/components/tabs/src/Tabs.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { axe } from '@/scripts/test/axeHelper';
 
 import { Tabs } from '.';
@@ -21,5 +21,62 @@ describe('Tabs', () => {
       </div>,
     );
     expect(await axe(container)).toHaveNoViolations();
+  });
+
+  it('switches tab panels on tab click', async () => {
+    render(
+      <div>
+        <Tabs defaultTab="first">
+          <Tabs.List>
+            <Tabs.Tab panelId="first">First</Tabs.Tab>
+            <Tabs.Tab panelId="second">Second</Tabs.Tab>
+            <Tabs.Tab panelId="third">Third</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel id="first">content first panel</Tabs.Panel>
+          <Tabs.Panel id="second">content second panel</Tabs.Panel>
+          <Tabs.Panel id="third">content third panel</Tabs.Panel>
+        </Tabs>
+      </div>,
+    );
+
+    fireEvent.mouseDown(screen.getAllByRole('tab')[1]);
+    expect(screen.getAllByRole('tab')[1]).toHaveAttribute(
+      'aria-selected',
+      'true',
+    );
+    expect(screen.getAllByRole('tab')[0]).toHaveAttribute(
+      'aria-selected',
+      'false',
+    );
+    expect(screen.getAllByRole('tab')[2]).toHaveAttribute(
+      'aria-selected',
+      'false',
+    );
+    expect(screen.getByRole('tabpanel')).toHaveTextContent(
+      'content second panel',
+    );
+  });
+
+  it('fires `onTabChange` events', async () => {
+    const mockOnTabChange = jest.fn();
+    render(
+      <div>
+        <Tabs onTabChange={mockOnTabChange} defaultTab="first">
+          <Tabs.List>
+            <Tabs.Tab panelId="first">First</Tabs.Tab>
+            <Tabs.Tab panelId="second">Second</Tabs.Tab>
+            <Tabs.Tab panelId="third">Third</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel id="first">content first panel</Tabs.Panel>
+          <Tabs.Panel id="second">content second panel</Tabs.Panel>
+          <Tabs.Panel id="third">content third panel</Tabs.Panel>
+        </Tabs>
+      </div>,
+    );
+
+    fireEvent.mouseDown(screen.getAllByRole('tab')[1]);
+    expect(mockOnTabChange).toHaveBeenCalledWith('second');
+    fireEvent.mouseDown(screen.getAllByRole('tab')[2]);
+    expect(mockOnTabChange).toHaveBeenCalledWith('third');
   });
 });
