8169dd462cb9ca69f0d12c70837ca1851fceac24
Bohdan Hutsol <bohdan.hutsol@contentful.com>
2022-08-04 16:19:23 +0300

    feat: calendar reexport custom components, provide docs example (#2151)
    * feat: calendar reexport custom components, provice docs example
    
    * chore: bump datepicker pkg version

--- packages/components/datepicker/stories/Calendar.stories.tsx
+++ packages/components/datepicker/stories/Calendar.stories.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import type { Meta, Story } from '@storybook/react/types-6-0';
 
-import { Calendar } from '../src';
+import { Calendar, DayContent, DayContentProps } from '../src';
 import type { CalendarProps } from '../src';
+import { css } from 'emotion';
+import tokens from '@contentful/f36-tokens';
 
 const testDate = new Date('2022-04-15');
 
@@ -52,6 +54,57 @@ export const WithMultipleMonths: Story<CalendarProps> = (args) => {
       selected={selectedDay}
       onSelect={setSelectedDay}
       defaultMonth={selectedDay}
+    />
+  );
+};
+
+const CustomDayContent = ({
+  date,
+  displayMonth,
+  activeModifiers,
+}: DayContentProps) => {
+  const styles = {
+    scheduleIndicator: css({
+      width: tokens.spacingS,
+      height: tokens.spacing2Xs,
+      borderRadius: tokens.borderRadiusSmall,
+      backgroundColor: tokens.green400,
+      position: 'absolute',
+      bottom: '11%',
+      left: '50%',
+      transform: 'translate(-50%, -11%)',
+    }),
+  };
+
+  return (
+    <>
+      <DayContent
+        date={date}
+        displayMonth={displayMonth}
+        activeModifiers={activeModifiers}
+      />
+      {activeModifiers?.scheduled && (
+        <span
+          className={styles.scheduleIndicator}
+          data-test-id={`schedule-indicator`}
+        />
+      )}
+    </>
+  );
+};
+
+export const WithCustomDayContent: Story<CalendarProps> = (args) => {
+  const [selectedDay, setSelectedDay] = useState<Date>(testDate);
+
+  return (
+    <Calendar
+      {...args}
+      mode="single"
+      selected={selectedDay}
+      onSelect={setSelectedDay}
+      defaultMonth={selectedDay}
+      components={{ DayContent: CustomDayContent }}
+      modifiers={{ scheduled: new Date('2022-04-20') }}
     />
   );
 };
