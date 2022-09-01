cfe572c4b824689b287bd102dbb500addd22c4d5
denkristoffer <denkristoffer@users.noreply.github.com>
2022-09-01 16:25:31 +0200

    build: tsup and esbuild

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entryPoints: ['src/index.ts'],
+  external: ['react', 'react-dom', '@contentful/f36-*'],
+  format: ['cjs', 'esm'],
+  minify: true,
+  platform: 'browser',
+  sourcemap: true,
+  target: 'es2016',
+  treeshake: true,
+});
