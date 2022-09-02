92902220d11da7b6f473e175e6090bcb0e827174
Kristoffer <denkristoffer@users.noreply.github.com>
2022-09-02 08:58:39 +0000

    Update .size-limit.js

--- .size-limit.js
+++ .size-limit.js
@@ -1,25 +1,25 @@
 module.exports = [
   {
     ignore: ['react', 'react-dom'],
-    limit: '2.2 s',
+    limit: '3.2 s',
     name: 'f36-components',
     path: 'packages/forma-36-react-components/dist/f36-components.js',
   },
   {
     ignore: ['react', 'react-dom'],
-    limit: '2.2 s',
+    limit: '3.2 s',
     name: 'Modern',
     path: 'packages/forma-36-react-components/dist/f36-components.modern.js',
   },
   {
     ignore: ['react', 'react-dom'],
-    limit: '2.2 s',
+    limit: '3.2 s',
     name: 'Module',
     path: 'packages/forma-36-react-components/dist/f36-components.module.js',
   },
   {
     ignore: ['react', 'react-dom'],
-    limit: '2.2 s',
+    limit: '3.2 s',
     name: 'UMD',
     path: 'packages/forma-36-react-components/dist/f36-components.umd.js',
   },
