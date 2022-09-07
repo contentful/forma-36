493d00b4ca1fb468f4c74d04e6de8a57e40bd149
bgutsol <bohdan.hutsol@contentful.com>
2022-09-07 14:07:24 +0200

    fix: change redirect and status code

--- packages/website/pages/api/exit-preview.ts
+++ packages/website/pages/api/exit-preview.ts
@@ -1,8 +1,10 @@
-export default async function exit(_, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function exit(_: NextApiRequest, res: NextApiResponse) {
   // Exit the current user from "Preview Mode". This function accepts no args.
   res.clearPreviewData();
 
   // Redirect the user back to the index page.
-  res.writeHead(307, { Location: '/' });
+  res.redirect('/');
   res.end();
 }
--- packages/website/pages/api/preview.ts
+++ packages/website/pages/api/preview.ts
@@ -1,6 +1,10 @@
 import { getSingleArticleBySlug } from '../../lib/api';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function preview(req, res) {
+export default async function preview(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   const { secret, slug } = req.query;
 
   if (secret !== process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN || !slug) {
@@ -12,7 +16,7 @@ export default async function preview(req, res) {
 
   // If the slug doesn't exist prevent preview mode from being enabled
   if (!post) {
-    return res.status(401).json({ message: 'Invalid slug' });
+    return res.status(404).json({ message: 'Invalid slug' });
   }
 
   // Enable Preview Mode by setting the cookies
@@ -20,14 +24,6 @@ export default async function preview(req, res) {
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  // res.writeHead(307, { Location: `/${post.slug}` })
-  const url = `/${post.slug}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.write(
-    `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${url}" />
-    <script>window.location.href = '${url}'</script>
-    </head>
-    </html>`,
-  );
+  res.redirect(post.slug);
   res.end();
 }
