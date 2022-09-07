8542f4dd514f9f91c2f17961e114b1de28fb4dda
denkristoffer <denkristoffer@users.noreply.github.com>
2022-09-07 17:39:34 +0200

    build: make size-limit test all ESM imports

--- .size-limit.js
+++ .size-limit.js
@@ -3,12 +3,13 @@ module.exports = [
     ignore: ['react', 'react-dom'],
     limit: '3.2 s',
     name: 'CommonJS',
-    path: 'packages/forma-36-react-components/dist/index.js',
+    path: './packages/forma-36-react-components/dist/index.js',
   },
   {
     ignore: ['react', 'react-dom'],
+    import: '*',
     limit: '3.2 s',
     name: 'Module',
-    path: 'packages/forma-36-react-components/dist/index.mjs',
+    path: './packages/forma-36-react-components/dist/index.mjs',
   },
 ];
