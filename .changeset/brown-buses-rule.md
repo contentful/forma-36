cac7dabe97ab370e0c5d971f2a5416e9780c0d38
Kristoffer <denkristoffer@users.noreply.github.com>
2022-09-12 15:49:00 +0000

    build: fix size-limit target (#2218)
    * build: fix size-limit target
    
    * Create brown-buses-rule.md

---
+++ .changeset/brown-buses-rule.md
@@ -0,0 +1,7 @@
+---
+'@contentful/f36-datepicker': patch
+'@contentful/f36-typography': patch
+---
+
+- fix(text): truncated styles
+- fix(datepicker): update react-day-picker
--- .size-limit.js
+++ .size-limit.js
@@ -10,6 +10,6 @@ module.exports = [
     import: '*',
     limit: '3.2 s',
     name: 'Module',
-    path: './packages/forma-36-react-components/dist/esm/index.js',
+    path: './packages/forma-36-react-components/dist/index.esm.js',
   },
 ];
