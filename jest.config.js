890635191b8119d134552bd2057554799896850a
denkristoffer <denkristoffer@users.noreply.github.com>
2022-09-14 18:17:06 +0200

    chore(deps): remove jest-emotion

--- jest.config.js
+++ jest.config.js
@@ -119,9 +119,6 @@ module.exports = {
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['<rootDir>/scripts/test/setupTests.js'],
 
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-  snapshotSerializers: ['jest-emotion'],
-
   // The test environment that will be used for testing
   testEnvironment: 'jsdom',
 
