14866087a7aa6649e1810c8a613401ce91614a73
Wake1st <joeltwakefield@gmail.com>
2022-09-13 21:35:57 -0600

    fix(badge): lint fix - remove unused imports
    test files for the Badge and EntityStatusBadge contained unused imports - they have been removed,
    and they no longer trigger the previous lint errors.

--- packages/components/badge/src/EntityStatusBadge/EntityStatusBadge.test.tsx
+++ packages/components/badge/src/EntityStatusBadge/EntityStatusBadge.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { PreviewIcon } from '@contentful/f36-icons';
 import { axe } from '@/scripts/test/axeHelper';
 import { EntityStatusBadge } from './EntityStatusBadge';
 
