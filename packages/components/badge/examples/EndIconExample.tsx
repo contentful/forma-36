f917116b0b15691354c53119ab0fa6bf8fbf3662
Wake1st <joeltwakefield@gmail.com>
2022-09-13 21:10:30 -0600

    docs(badge): documentation updates for the Start and End Icon usage of the Badge
    The REAMME file now includes examples of how to utilize the startIcon and endIcon attributes.
    
    #2125

---
+++ packages/components/badge/examples/EndIconExample.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Badge } from '../src';
+import { Icon } from '@contentful/f36-icon';
+
+export default function EndIconExample() {
+  return <Badge endIcon={<Icon />}>End Icon</Badge>;
+}
---
+++ packages/components/badge/examples/StartIconExample.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Badge } from '../src';
+import { Icon } from '@contentful/f36-icon';
+
+export default function StartIconExample() {
+  return <Badge startIcon={<Icon />}>Start Icon</Badge>;
+}
