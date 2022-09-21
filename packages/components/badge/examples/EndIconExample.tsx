27be43183adbae6e21deca392db5de942a451d64
denkristoffer <denkristoffer@users.noreply.github.com>
2022-09-21 16:17:24 +0200

    docs(badge): collect icon examples in one

--- packages/components/badge/examples/EndIconExample.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Badge } from '../src';
-import { Icon } from '@contentful/f36-icon';
-
-export default function EndIconExample() {
-  return <Badge endIcon={<Icon />}>End Icon</Badge>;
-}
--- packages/components/badge/examples/StartIconExample.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Badge } from '../src';
-import { Icon } from '@contentful/f36-icon';
-
-export default function StartIconExample() {
-  return <Badge startIcon={<Icon />}>Start Icon</Badge>;
-}
