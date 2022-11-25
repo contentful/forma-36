0f4a47278cbd222293d585941bf98af0e60c43a5
denkristoffer <denkristoffer@users.noreply.github.com>
2022-11-25 09:36:58 +0100

    test(text-link): remove style tests

--- packages/components/text-link/src/TextLink.test.tsx
+++ packages/components/text-link/src/TextLink.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ArrowDownIcon } from '@contentful/f36-icons';
-import tokens from '@contentful/f36-tokens';
 import { axe } from '@/scripts/test/axeHelper';
 import { TextLink } from './TextLink';
 
@@ -19,66 +18,6 @@ describe('TextLink', function () {
     const anchor = screen.getByRole('link');
     expect(anchor).toBeTruthy();
     expect(anchor.getAttribute('href')).toBe('#');
-  });
-
-  it('renders as a "primary" link type', () => {
-    const { container } = render(
-      <TextLink variant="primary">Text Link</TextLink>,
-    );
-
-    expect(container.firstChild).toHaveStyle({
-      color: tokens.blue600,
-    });
-  });
-
-  it('renders as a "positive" link type', () => {
-    const { container } = render(
-      <TextLink variant="positive">Text Link</TextLink>,
-    );
-
-    expect(container.firstChild).toHaveStyle({
-      color: tokens.green600,
-    });
-  });
-
-  it('renders as a "negative" link type', () => {
-    const { container } = render(
-      <TextLink variant="negative">Text Link</TextLink>,
-    );
-
-    expect(container.firstChild).toHaveStyle({
-      color: tokens.red600,
-    });
-  });
-
-  it('renders as a "secondary" link type', () => {
-    const { container } = render(
-      <TextLink variant="secondary">Text Link</TextLink>,
-    );
-
-    expect(container.firstChild).toHaveStyle({
-      color: tokens.gray600,
-    });
-  });
-
-  it('renders as a "muted" link type', () => {
-    const { container } = render(
-      <TextLink variant="muted">Text Link</TextLink>,
-    );
-
-    expect(container.firstChild).toHaveStyle({
-      color: tokens.gray400,
-    });
-  });
-
-  it('renders as a "white" link type', () => {
-    const { container } = render(
-      <TextLink variant="white">Text Link</TextLink>,
-    );
-
-    expect(container.firstChild).toHaveStyle({
-      color: tokens.colorWhite,
-    });
   });
 
   it('calls an onClick function', async () => {
