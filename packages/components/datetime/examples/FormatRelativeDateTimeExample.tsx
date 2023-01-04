3a119becb8ef57558faee81bb9a0195fc524a8b1
Kristoffer <denkristoffer@users.noreply.github.com>
2023-01-04 12:36:35 +0000

    docs(format-relative-date-time): add docs and example (#2364)

---
+++ packages/components/datetime/examples/FormatRelativeDateTimeExample.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Stack,
+  Text,
+  formatRelativeDateTime,
+} from '@contentful/f36-components';
+
+export default function FormatRelativeDateTimeExample() {
+  return (
+    <Stack flexDirection="column">
+      {/* passing a JS Date */}
+      <Text>{formatRelativeDateTime('2021-08-17')}</Text>
+      {/* passing a base date to compare to */}
+      <Text>{formatRelativeDateTime('2021-08-17', '2021-08-18')}</Text>
+    </Stack>
+  );
+}
