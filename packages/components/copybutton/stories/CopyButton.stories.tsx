0c9fd61a9cbc1162972acbaff866c9a2fd55705d
Kristoffer <denkristoffer@users.noreply.github.com>
2023-01-19 20:38:44 +0100

    Restore packages/components/copybutton/stories/CopyButton.stories.tsx

--- packages/components/copybutton/stories/CopyButton.stories.tsx
+++ packages/components/copybutton/stories/CopyButton.stories.tsx
@@ -3,7 +3,6 @@ import type { Meta, Story } from '@storybook/react/types-6-0';
 import { action } from '@storybook/addon-actions';
 
 import { CopyButton, type CopyButtonProps } from '../src/CopyButton';
-import { Flex } from '@contentful/f36-core';
 
 export default {
   component: CopyButton,
@@ -14,21 +13,11 @@ export default {
 } as Meta;
 
 export const Default: Story<CopyButtonProps> = (args) => {
-  const asyncValue = async (): Promise<string> =>
-    new Promise((resolve) =>
-      setTimeout(resolve, 3000, 'A value that you will have to wait for'),
-    );
-
-  return (
-    <Flex flexDirection="column">
-      <CopyButton value="Lorem Ipsum" {...args} />
-
-      <CopyButton value={asyncValue} {...args} />
-    </Flex>
-  );
+  return <CopyButton {...args}>CopyButton</CopyButton>;
 };
 
 Default.args = {
+  value: 'Lorem Ipsum',
   tooltipCopiedText: 'Copied!',
   tooltipText: 'Copy to clipboard',
   onCopy: action('onCopy'),
