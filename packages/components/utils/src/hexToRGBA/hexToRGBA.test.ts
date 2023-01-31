8339ff28d3d49ce65a69e86fb439d4924efbd29a
Moe Shaaban <info@mshaaban.com>
2023-01-31 14:17:43 +0100

    fix: tests

--- packages/components/utils/src/hexToRGBA/hexToRGBA.test.ts
+++ packages/components/utils/src/hexToRGBA/hexToRGBA.test.ts
@@ -8,7 +8,7 @@ describe('hexToRGBA', () => {
   });
 
   it('should return rgba color from tokens', () => {
-    expect(hexToRGBA(tokens.colorBlack)).toBe('rgba(0, 0, 0, 1)');
-    expect(hexToRGBA(tokens.colorBlack, 0.5)).toBe('rgba(0, 0, 0, 0.5)');
+    expect(hexToRGBA(tokens.colorBlack)).toBe('rgba(12, 20, 28, 1)');
+    expect(hexToRGBA(tokens.colorBlack, 0.5)).toBe('rgba(12, 20, 28, 0.5)');
   });
 });
