2c76497e3b493f099bfaae05db01e58472c3b910
bgutsol <bohdan.hutsol@contentful.com>
2023-03-07 13:46:42 +0100

    style: adjust TableCell styling

--- packages/components/table/src/TableCell/TableCell.styles.ts
+++ packages/components/table/src/TableCell/TableCell.styles.ts
@@ -10,15 +10,14 @@ export const getTableCellStyles = ({
   isTableHead?: boolean;
 } & Pick<TableCellInternalProps, 'sorting' | 'align'>) => ({
   container: css({
-    backgroundColor: isTableHead ? tokens.gray100 : undefined,
     borderBottom: `1px solid ${tokens.gray200}`,
+    padding: `${tokens.spacingS} ${tokens.spacingXs}`,
+    verticalAlign: 'top',
+    textAlign: align,
     color: sorting ? tokens.gray900 : tokens.gray700,
     fontFamily: tokens.fontStackPrimary,
-    fontSize: tokens.fontSizeM,
     fontWeight: isTableHead ? tokens.fontWeightMedium : tokens.fontWeightNormal,
-    lineHeight: tokens.lineHeightL,
-    padding: `${tokens.spacingS} ${tokens.spacingM}`,
-    textAlign: align,
-    verticalAlign: 'top',
+    fontSize: isTableHead ? tokens.fontSizeS : tokens.fontSizeM,
+    lineHeight: isTableHead ? tokens.lineHeightS : tokens.lineHeightM,
   }),
 });
