d099144a50ba2098051e9f90157e62c066ea5808
Kathrin <kathrin.holzmann@contentful.com>
2023-04-21 11:18:13 +0200

    docs: add example with table to storybook

--- packages/components/empty-state/stories/MissingContent.stories.tsx
+++ packages/components/empty-state/stories/MissingContent.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Flex } from '@contentful/f36-core';
+import { Table } from '@contentful/f36-components';
 import { MissingContent } from '../src';
 
 export default {
@@ -24,6 +25,32 @@ export const Basic = () => {
       <Flex flexDirection="row">
         Missing: <MissingContent testId="missing-entry-1234" />
       </Flex>
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            <Table.Cell>Filename</Table.Cell>
+            <Table.Cell>Dimensions</Table.Cell>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Pony.jpg</Table.Cell>
+            <Table.Cell>160px x 300px</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Cat.jpg</Table.Cell>
+            <Table.Cell>
+              <MissingContent label="No Dimensions available" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Dog.jpg</Table.Cell>
+            <Table.Cell>
+              <MissingContent label="No Dimensions available" />
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
     </div>
   );
 };
