ac54708738d54455d962cfe6d7a58c99d8ddaf8d
denkristoffer <denkristoffer@users.noreply.github.com>
2023-05-24 13:25:51 +0200

    docs: address feedback

---
+++ packages/components/badge/examples/EntityStatusBadgePublishedExample.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { EntityStatusBadge } from '@contentful/f36-components';
+
+export default function EntityStatusBadgePublishedExample() {
+  return <EntityStatusBadge entityStatus="published" />;
+}
