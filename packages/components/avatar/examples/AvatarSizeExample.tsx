1affe1d51507d11b101b7df99550e500835eb246
Miguel Crespo <miguel.crespo6@gmail.com>
2023-07-04 16:05:41 +0200

    fix: remove all usage of enums

--- packages/components/avatar/examples/AvatarSizeExample.tsx
+++ packages/components/avatar/examples/AvatarSizeExample.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Avatar, Size } from '@contentful/f36-avatar';
+import { Avatar } from '@contentful/f36-avatar';
 import { Stack } from '@contentful/f36-core';
 
 export default function AvatarVariantsExample() {
@@ -7,22 +7,22 @@ export default function AvatarVariantsExample() {
     <Stack>
       <Avatar
         src="https://images.ctfassets.net/iq4lnigp6fgt/2EEEk92Kiz6KxREsjBLPAN/810d5a21650d91abad12e95da4cd3beb/2021-06_Everyone_is_Welcome_here_1_.png?fit=fill&f=top_left&w=100&h=100"
-        size={Size.Tiny}
+        size="tiny"
       />
 
       <Avatar
         src="https://images.ctfassets.net/iq4lnigp6fgt/2EEEk92Kiz6KxREsjBLPAN/810d5a21650d91abad12e95da4cd3beb/2021-06_Everyone_is_Welcome_here_1_.png?fit=fill&f=top_left&w=100&h=100"
-        size={Size.Small}
+        size="small"
       />
 
       <Avatar
         src="https://images.ctfassets.net/iq4lnigp6fgt/2EEEk92Kiz6KxREsjBLPAN/810d5a21650d91abad12e95da4cd3beb/2021-06_Everyone_is_Welcome_here_1_.png?fit=fill&f=top_left&w=100&h=100"
-        size={Size.Medium}
+        size="medium"
       />
 
       <Avatar
         src="https://images.ctfassets.net/iq4lnigp6fgt/2EEEk92Kiz6KxREsjBLPAN/810d5a21650d91abad12e95da4cd3beb/2021-06_Everyone_is_Welcome_here_1_.png?fit=fill&f=top_left&w=100&h=100"
-        size={Size.Large}
+        size="large"
       />
     </Stack>
   );
