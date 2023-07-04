075884fbc7b631360ee7d89f7930230f2dc93787
Miguel Crespo <miguel.crespo6@gmail.com>
2023-07-04 15:38:37 +0200

    docs(Avatar): add examples for icons, isActive, isPrimary properties

---
+++ packages/components/avatar/examples/AvatarIsPrimaryExamples.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Avatar } from '@contentful/f36-avatar';
+import { Stack } from '@contentful/f36-core';
+
+export default function AvatarPrimaryExample() {
+  return (
+    <Stack>
+      <Avatar
+        src="https://images.ctfassets.net/iq4lnigp6fgt/2EEEk92Kiz6KxREsjBLPAN/810d5a21650d91abad12e95da4cd3beb/2021-06_Everyone_is_Welcome_here_1_.png?fit=fill&f=top_left&w=100&h=100"
+        size="medium"
+      />
+
+      <Avatar
+        src="https://images.ctfassets.net/iq4lnigp6fgt/2EEEk92Kiz6KxREsjBLPAN/810d5a21650d91abad12e95da4cd3beb/2021-06_Everyone_is_Welcome_here_1_.png?fit=fill&f=top_left&w=100&h=100"
+        size="medium"
+        isPrimary
+      />
+    </Stack>
+  );
+}
