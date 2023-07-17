be5364fd931d61b9af33d20e991d812aef06fb2d
Kathrin <kathrin.holzmann@contentful.com>
2023-07-17 15:47:15 +0200

    feat: allow programmatic trigger of border color

--- packages/components/avatar/src/Avatar/Avatar.tsx
+++ packages/components/avatar/src/Avatar/Avatar.tsx
@@ -3,6 +3,7 @@ import { cx } from 'emotion';
 import { type CommonProps } from '@contentful/f36-core';
 import { Image, type ImageProps } from '@contentful/f36-image';
 import { convertSizeToPixels, getAvatarStyles } from './Avatar.styles';
+import type { ColorVariant } from './utils';
 
 export type Size = 'tiny' | 'small' | 'medium' | 'large';
 
@@ -14,9 +15,9 @@ export interface AvatarProps extends CommonProps {
   size?: Size;
   src?: ImageProps['src'];
   variant?: Variant;
-  showColorBorder?: boolean;
+  colorVariant?: ColorVariant;
   isPrimary?: boolean;
-  isActive?: boolean;
+  isInactive?: boolean;
   icon?: React.ReactElement;
 }
 
@@ -29,25 +30,23 @@ function _Avatar(
     src,
     testId = 'cf-ui-avatar',
     variant = 'user',
-    showColorBorder = false,
-    isPrimary = false,
-    isActive = true,
+    colorVariant,
+    isInactive = false,
     icon = null,
   }: AvatarProps,
   forwardedRef: React.Ref<HTMLDivElement>,
 ) {
   // Only render the fallback when `src` is undefined or an empty string
   const isFallback = Boolean(!isLoading && !src);
-  const styles = getAvatarStyles({ isFallback, size, variant });
+  const styles = getAvatarStyles({ isFallback, size, variant, colorVariant });
   const sizePixels = convertSizeToPixels(size);
 
   return (
     <div
       className={cx(styles.root, className, {
-        [styles.rootColorBorder]: showColorBorder,
-        [styles.isPrimaryAvatar]: isPrimary,
         [styles.imageContainer]: icon !== null,
-        [styles.isInactive]: !isActive,
+        [styles.isInactive]: isInactive,
+        [styles.colorBorder]: !!colorVariant,
       })}
       data-test-id={testId}
       ref={forwardedRef}
---
+++ packages/components/avatar/src/Avatar/utils.ts
@@ -0,0 +1,18 @@
+export type ColorVariant =
+  | 'primary'
+  | 'green'
+  | 'red'
+  | 'orange'
+  | 'yellow'
+  | 'purple'
+  | 'gray';
+
+export const avatarColorVariant = {
+  primary: 'blue400',
+  green: 'green400',
+  red: 'red400',
+  orange: 'orange400',
+  yellow: 'yellow500',
+  purple: 'purple400',
+  gray: 'gray400',
+};
