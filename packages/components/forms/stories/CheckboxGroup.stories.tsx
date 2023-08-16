3913d52e60e415c40fe1ddc0e5d933993dd194ab
Kathrin <kathrin.holzmann@contentful.com>
2023-08-16 16:25:53 +0200

    docs: update accessibility guidelines

--- packages/components/forms/stories/CheckboxGroup.stories.tsx
+++ packages/components/forms/stories/CheckboxGroup.stories.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { action } from '@storybook/addon-actions';
 import { Checkbox, CheckboxGroupProps } from '../src';
+import { FormControl } from '@contentful/f36-forms';
+import { Paragraph } from '@contentful/f36-typography';
 
 export default {
   title: 'Form Elements/Checkbox/CheckboxGroup',
@@ -61,5 +63,50 @@ export const Uncontrolled = (args: CheckboxGroupProps) => {
 };
 
 Uncontrolled.args = {
+  defaultValue: ['apples', 'kiwis'],
+};
+
+export const WithFormControl = (args: CheckboxGroupProps) => {
+  const [groupState, setGroupState] = useState(args.value || args.defaultValue);
+  const handleOnChange = (e) => {
+    e.persist();
+    const value = e.target.value;
+    setGroupState((prevState) =>
+      prevState.includes(value)
+        ? prevState.filter((v) => v !== value)
+        : [...prevState, value],
+    );
+    action('onChange')(e);
+  };
+
+  const { value } = args;
+  useEffect(() => {
+    Array.isArray(value) && setGroupState(value);
+  }, [value]);
+
+  return (
+    <FormControl>
+      <FormControl.Label as="legend" marginBottom="spacing2Xs">
+        Fruits
+      </FormControl.Label>
+      <Paragraph>Choose your favorit fruit</Paragraph>
+      <Checkbox.Group
+        {...args}
+        value={groupState}
+        name="Fruit"
+        onChange={handleOnChange}
+      >
+        <Checkbox value="apples">Apples</Checkbox>
+        <Checkbox value="pears">Pears</Checkbox>
+        <Checkbox value="peaches">Peaches</Checkbox>
+        <Checkbox value="mangos">Mangos</Checkbox>
+        <Checkbox value="kiwis">Kiwis</Checkbox>
+        <Checkbox value="bananas">Bananas</Checkbox>
+      </Checkbox.Group>
+    </FormControl>
+  );
+};
+
+WithFormControl.args = {
   defaultValue: ['apples', 'kiwis'],
 };
