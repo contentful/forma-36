cf80edb58371c38e2885b2b31f370f7642db67ce
Miguel Crespo <miguel.crespo6@gmail.com>
2023-09-15 17:07:05 +0200

    fix: make types match

--- packages/components/avatar/src/Avatar/Avatar.tsx
+++ packages/components/avatar/src/Avatar/Avatar.tsx
@@ -20,7 +20,7 @@ export interface AvatarProps extends CommonProps {
   /**
    * A tooltipProps attribute used to conditionally render the tooltip around root element
    */
-  tooltipProps?: Omit<TooltipProps, 'children'>;
+  tooltipProps?: TooltipProps;
   variant?: Variant;
   colorVariant?: ColorVariant;
   icon?: React.ReactElement;
