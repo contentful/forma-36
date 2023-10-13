6fa2fc52c7f352db8444010f21de17ec1d9e1cca
Max Cheremisin <max.cheremisin@contentful.com>
2023-10-13 11:40:09 +0200

    chore(Button): update stories

--- packages/components/button/stories/ButtonGroup.stories.tsx
+++ packages/components/button/stories/ButtonGroup.stories.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import type { Meta, Story } from '@storybook/react/types-6-0';
-import { ChevronDownIcon, PlusIcon } from '@contentful/f36-icons';
+import { ChevronDownIcon } from '@contentful/f36-icons';
 import { SectionHeading } from '@contentful/f36-typography';
 import { action } from '@storybook/addon-actions';
 import { Box, Flex } from '@contentful/f36-core';
 import { ButtonGroup, Button, IconButton, type ButtonGroupProps } from '../src';
 import tokens from '@contentful/f36-tokens';
+import { ButtonVariant } from '../src/types';
+import { ButtonGroupVariants } from '../src/ButtonGroup/types';
 
 export default {
   component: ButtonGroup,
   parameters: {
     propTypes: ButtonGroup['__docgenInfo'],
   },
-  title: 'Components/Button/ButtonGroup',
+  title: 'Components/Button components/ButtonGroup',
   argTypes: {
     variant: {
       control: {
@@ -67,82 +69,74 @@ spaced.args = {
 export const overview: Story<ButtonGroupProps> = () => {
   const onClick = action('click');
 
+  const buttonVariants: Record<ButtonVariant, true> = {
+    primary: true,
+    secondary: true,
+    positive: true,
+    negative: true,
+    transparent: true,
+  };
+
+  const buttonGroupVariants: Record<
+    Exclude<ButtonGroupVariants, 'collapsed'>,
+    true
+  > = {
+    merged: true,
+    spaced: true,
+  };
+
   return (
-    <>
-      <Flex flexDirection="column" marginBottom="spacingL">
-        <SectionHeading as="h3" marginBottom="spacingS">
-          Button Group Collapse
-        </SectionHeading>
+    <Flex flexDirection="column" marginBottom="spacingL">
+      {Object.keys(buttonGroupVariants).map((key) => {
+        const groupVariant = key as Exclude<ButtonGroupVariants, 'collapsed'>;
 
-        <Flex flexDirection="column" marginBottom="spacingM">
-          <Box marginBottom="spacingS">
-            <ButtonGroup>
-              <Button onClick={onClick} variant="secondary">
-                Button
-              </Button>
-              <Button onClick={onClick} variant="secondary">
-                Button
-              </Button>
-              <Button onClick={onClick} variant="secondary">
-                Button
-              </Button>
-              <IconButton
-                onClick={onClick}
-                variant="secondary"
-                icon={<ChevronDownIcon variant="secondary" />}
-                aria-label="Open dropdown"
-              />
-            </ButtonGroup>
-          </Box>
-          <Box marginBottom="spacingS">
-            <ButtonGroup withDivider>
-              <Button onClick={onClick} variant="positive">
-                Button
-              </Button>
-              <IconButton
-                onClick={onClick}
-                variant="positive"
-                icon={<ChevronDownIcon variant="white" />}
-                aria-label="Open dropdown"
-              />
-            </ButtonGroup>
-          </Box>
-          <Box marginBottom="spacingS">
-            <ButtonGroup withDivider>
-              <Button
-                onClick={onClick}
-                variant="primary"
-                startIcon={<PlusIcon />}
-              >
-                Button
-              </Button>
-              <IconButton
-                onClick={onClick}
-                variant="primary"
-                icon={<ChevronDownIcon variant="white" />}
-                aria-label="Open dropdown"
-              />
-            </ButtonGroup>
-          </Box>
-        </Flex>
+        return (
+          <>
+            {Object.keys(buttonVariants).map((key) => {
+              const buttonVariant = key as ButtonVariant;
 
-        <SectionHeading as="h3" marginBottom="spacingS">
-          Button Group Spaced
-        </SectionHeading>
+              return (
+                <>
+                  <SectionHeading as="h3" marginBottom="spacingS">
+                    Button Group {groupVariant} {buttonVariant}
+                  </SectionHeading>
 
-        <Flex flexDirection="column" marginBottom="spacingM">
-          <Box marginBottom="spacingS">
-            <ButtonGroup variant="spaced">
-              <Button onClick={onClick} variant="primary">
-                Button
-              </Button>
-              <Button onClick={onClick} variant="secondary">
-                Button
-              </Button>
-            </ButtonGroup>
-          </Box>
-        </Flex>
-      </Flex>
-    </>
+                  <Flex flexDirection="column" marginBottom="spacingM">
+                    <Box marginBottom="spacingS">
+                      <ButtonGroup
+                        {...(groupVariant === 'spaced'
+                          ? { variant: groupVariant }
+                          : {
+                              variant: groupVariant,
+                              withDivider:
+                                buttonVariant === 'positive' ||
+                                buttonVariant === 'primary',
+                            })}
+                      >
+                        <Button onClick={onClick} variant={buttonVariant}>
+                          Button
+                        </Button>
+                        <Button onClick={onClick} variant={buttonVariant}>
+                          Button
+                        </Button>
+                        <Button onClick={onClick} variant={buttonVariant}>
+                          Button
+                        </Button>
+                        <IconButton
+                          onClick={onClick}
+                          variant={buttonVariant}
+                          icon={<ChevronDownIcon variant="secondary" />}
+                          aria-label="Open dropdown"
+                        />
+                      </ButtonGroup>
+                    </Box>
+                  </Flex>
+                </>
+              );
+            })}
+          </>
+        );
+      })}
+    </Flex>
   );
 };
