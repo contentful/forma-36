bf38336069d2d931efb4e1766f1254f9c7f0918d
Rémy Lenoir <remy.lenoir@contentful.com>
2023-10-16 16:55:52 +0200

    docs: update function name

--- packages/components/datepicker/examples/Datepicker/DefaultOpen.tsx
+++ packages/components/datepicker/examples/Datepicker/DefaultOpen.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Datepicker } from '@contentful/f36-components';
 
-export default function BasicExample() {
+export default function DefaultOpenExample() {
   const [selectedDay, setSelectedDay] = useState(new Date());
 
   return (
