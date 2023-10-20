3b0414fb4f4923837e2e97c4253710733c1decfd
Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>
2023-10-20 11:56:46 +0200

    docs(Datepicker): update documentation + stories (#2592)

--- packages/components/datepicker/examples/Datepicker/WithMinMaxDate.tsx
+++ packages/components/datepicker/examples/Datepicker/WithMinMaxDate.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { Datepicker } from '@contentful/f36-components';
 
-export default function DefaultOpenExample() {
+export default function WithMinMaxDateExample() {
   const [selectedDay, setSelectedDay] = useState(new Date());
+  const fromDate = new Date();
+  const twoYearsFromNow = new Date(
+    new Date(fromDate).setFullYear(fromDate.getFullYear() + 2),
+  );
 
   return (
     <Datepicker
-      defaultIsOpen
       selected={selectedDay}
       onSelect={setSelectedDay}
+      fromDate={fromDate}
+      toDate={twoYearsFromNow}
     />
   );
 }
--- packages/components/datepicker/examples/Datepicker/WithMultipleMonths.tsx
+++ packages/components/datepicker/examples/Datepicker/WithMultipleMonths.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Datepicker } from '@contentful/f36-components';
 
-export default function DefaultOpenExample() {
+export default function WithMultipleMonthsExample() {
   const [selectedDay, setSelectedDay] = useState(new Date());
 
   return (
     <Datepicker
-      defaultIsOpen
       selected={selectedDay}
       onSelect={setSelectedDay}
+      numberOfMonths={2}
     />
   );
 }
