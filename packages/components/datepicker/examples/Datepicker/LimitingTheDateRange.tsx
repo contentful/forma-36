f360713073dc94853056c3eab46dc6df46c03b49
Rémy Lenoir <remy.lenoir@contentful.com>
2023-11-23 14:15:52 +0100

    docs: align `Calendar` and `Datepicker`

--- packages/components/datepicker/examples/Datepicker/LimitingTheDateRange.tsx
+++ packages/components/datepicker/examples/Datepicker/LimitingTheDateRange.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Datepicker } from '@contentful/f36-components';
 
-export default function WithMinMaxDateExample() {
+export default function LimitingTheDateRangeExample() {
   const [selectedDay, setSelectedDay] = useState(new Date());
   const fromDate = new Date();
   const twoYearsFromNow = new Date(
@@ -10,10 +10,10 @@ export default function WithMinMaxDateExample() {
 
   return (
     <Datepicker
-      selected={selectedDay}
-      onSelect={setSelectedDay}
       fromDate={fromDate}
       toDate={twoYearsFromNow}
+      selected={selectedDay}
+      onSelect={setSelectedDay}
     />
   );
 }
