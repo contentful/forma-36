a76c2f853c760c25141887956041feb557f1996f
Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
2024-01-02 10:15:07 +0100

    chore: update registry to github packages [CFCX-857] (#2639)
    * chore: update circleci and packages to have new registry info
    
    * chore: add github registry to dependabot
    
    * chore: update prerelease script
    
    * chore: add vault get secrets to build steps on ci
    
    * chore: fix identation on config.yml file
    
    * chore: add vault to context of build job
    
    * chore: add permissions to github action size-limit
    
    * chore: add npmrc file to action size-limit
    
    * refactor: fix typo
    
    Co-authored-by: Andi Pätzold <andreas.paetzold@contentful.com>
    
    ---------
    
    Co-authored-by: Andi Pätzold <andreas.paetzold@contentful.com>

--- scripts/prerelease.mjs
+++ scripts/prerelease.mjs
@@ -1,86 +1,144 @@
-import fs from 'fs/promises'
-import path from 'path'
-import inquirer from 'inquirer'
-import semver from 'semver'
-import { getPackages } from '@manypkg/get-packages'
-import { exec } from 'child_process'
+import fs from 'fs/promises';
+import path from 'path';
+import inquirer from 'inquirer';
+import semver from 'semver';
+import { getPackages } from '@manypkg/get-packages';
+import { exec } from 'child_process';
+import chalk from 'chalk';
 
-const cwd = process.cwd()
+const cwd = process.cwd();
 
 async function updatePackageJson(pkgJsonPath, version) {
-  const pkgRaw = await fs.readFile(pkgJsonPath, { encoding: 'utf-8' })
-  const stringified = pkgRaw.replace(/("version".*?) (".*?")/i, `$1 "${version}"`)
-  await fs.writeFile(pkgJsonPath, stringified)
+  const pkgRaw = await fs.readFile(pkgJsonPath, { encoding: 'utf-8' });
+  const stringified = pkgRaw.replace(
+    /("version".*?) (".*?")/i,
+    `$1 "${version}"`,
+  );
+  await fs.writeFile(pkgJsonPath, stringified);
 }
 
 async function ignorePackage(pkgName) {
-  const changesetConfigPath = path.resolve(cwd, '.changeset/config.json')
-  const rawConfig = await fs.readFile(changesetConfigPath, { encoding: 'utf-8' })
-  const jsonConfig = JSON.parse(rawConfig)
-  const ignorePkgs = jsonConfig.ignore || []
-  if(!ignorePkgs.includes(pkgName)) {
-    jsonConfig.ignore = [pkgName, ...ignorePkgs]
-    const stringified = JSON.stringify(jsonConfig, null, 2)
-    await fs.writeFile(changesetConfigPath, stringified, { encoding: 'utf-8' })
+  const changesetConfigPath = path.resolve(cwd, '.changeset/config.json');
+  const rawConfig = await fs.readFile(changesetConfigPath, {
+    encoding: 'utf-8',
+  });
+  const jsonConfig = JSON.parse(rawConfig);
+  const ignorePkgs = jsonConfig.ignore || [];
+  if (!ignorePkgs.includes(pkgName)) {
+    jsonConfig.ignore = [pkgName, ...ignorePkgs];
+    const stringified = JSON.stringify(jsonConfig, null, 2);
+    await fs.writeFile(changesetConfigPath, stringified, { encoding: 'utf-8' });
   }
 }
 
 async function main() {
-  const { packages } = await getPackages(cwd)
-  const choices = packages.map(({ packageJson }) => ({
-    name: `${packageJson.name} (${packageJson.version})`,
-    value: packageJson.name
-  })).concat(new inquirer.Separator())
+  const { packages } = await getPackages(cwd);
+  const choices = packages
+    .map(({ packageJson }) => ({
+      name: `${packageJson.name} (${packageJson.version})`,
+      value: packageJson.name,
+    }))
+    .concat(new inquirer.Separator());
 
-  const { pkgName } = await inquirer.prompt([{
-    pageSize: 12,
-    name: 'pkgName',
-    message: 'Which package to make a pre-release?',
-    type: 'list',
-    choices
-  }])
+  const { pkgName } = await inquirer.prompt([
+    {
+      pageSize: 12,
+      name: 'pkgName',
+      message: 'Which package to make a pre-release?',
+      type: 'list',
+      choices,
+    },
+  ]);
 
-  const { packageJson, dir } = packages.find(({ packageJson }) => packageJson.name === pkgName)
-  const { version, name } = packageJson
-  const prereleaseTag = semver.prerelease(version)?.[0]
+  const { packageJson, dir } = packages.find(
+    ({ packageJson }) => packageJson.name === pkgName,
+  );
+  const {
+    version,
+    name,
+    publishConfig: { registry },
+  } = packageJson;
+  const prereleaseTag = semver.prerelease(version)?.[0];
 
-  const { tag, publish } = await inquirer.prompt([{
-    name: 'tag',
-    message: 'Which tag should be used for the pre-release?',
-    type: 'list',
-    choices: [{
-      name: 'alpha',
-    }, {
-      name: 'beta'
-    }],
-    default: prereleaseTag
-  }, {
-    name: 'publish',
-    message: 'Should the package be published?',
-    type: 'confirm'
-  }])
+  const { tag, publish, dryRun } = await inquirer.prompt([
+    {
+      name: 'dryRun',
+      message: 'Run as dry-run, i.e. no changes will be applied?',
+      type: 'confirm',
+    },
+    {
+      name: 'tag',
+      message: 'Which tag should be used for the pre-release?',
+      type: 'list',
+      choices: [
+        {
+          name: 'alpha',
+        },
+        {
+          name: 'beta',
+        },
+      ],
+      default: prereleaseTag,
+    },
+    {
+      name: 'publish',
+      message: 'Should the package be published?',
+      type: 'confirm',
+    },
+  ]);
 
-  const increase = prereleaseTag === tag ? 'prerelease' : 'preminor'
-  const newVersion = semver.inc(version, increase, tag)
-  await updatePackageJson(path.resolve(dir, 'package.json'), newVersion)
+  const increase = prereleaseTag === tag ? 'prerelease' : 'preminor';
+  const newVersion = semver.inc(version, increase, tag);
+  const url =
+    registry === 'https://npm.pkg.github.com/'
+      ? `https://github.com/contentful/forma-36/pkgs/npm/${name.replace(
+          '@contentful/',
+          '',
+        )}`
+      : [registry, name].join('/');
 
-  // Avoid chageset publishing it, by adding the package to the ignore list
-  await ignorePackage(name)
-
-  if(publish) {
-    // TODO: remove dry-run, kept here for testing before merging
-    await exec(`npm publish ${dir} --tag ${tag}`, (error, stdout, stderr) => {
-      if(!error) {
-        console.log(stdout)
-        console.log(`${name}@${newVersion} published: \nhttps://www.npmjs.com/package/${name}\n`)
-      } else {
-        console.error(error)
-        console.error(stderr)
-      }
-    })
+  if (!dryRun) {
+    await updatePackageJson(path.resolve(dir, 'package.json'), newVersion);
+    // Avoid changeset publishing it, by adding the package to the ignore list
+    await ignorePackage(name);
   } else {
-    console.log(`Version for ${name} updated on package.json.`)
+    console.log(chalk.bold('Dry-run mode'));
+    console.log(
+      `Version for ${chalk.bold(name)} will be updated to ${chalk.bold(
+        newVersion,
+      )}.`,
+    );
+    console.log(
+      `Package ${chalk.bold(name)} will be added to the changeset ignore list.`,
+    );
+    console.log(
+      `Package ${chalk.bold(name)} will be published to registry ${chalk.bold(
+        registry,
+      )} with tag ${chalk.bold(tag)}.`,
+    );
+  }
+
+  if (publish) {
+    const dryRunFlag = dryRun ? '--dry-run' : '';
+    console.log('');
+    console.log(chalk.bold('Npm publish output'));
+    await exec(
+      `npm publish ${dir} --tag ${tag} ${dryRunFlag}`,
+      (error, stdout, stderr) => {
+        if (!error) {
+          console.log(stdout);
+          console.log(
+            `${name}@${newVersion} published: \n${chalk.bold(url)}\n`,
+          );
+        } else {
+          console.error(error);
+          console.error(stderr);
+        }
+      },
+    );
+  } else {
+    console.log(`Version for ${name} updated on package.json.`);
   }
 }
 
-main()
+main();
