16a8d4c96ff6520238845b0fba4d0b930a9436a3
Stephan Leece <stephan.leece@outlook.com>
2024-01-10 13:37:10 +0100

    docs(radio.checkbox): show Density support directly in Radio and Checkbox stories

--- packages/components/density-container/stories/DensityContainer.stories.tsx
+++ packages/components/density-container/stories/DensityContainer.stories.tsx
@@ -8,8 +8,6 @@ import {
   Flex,
   Heading,
   Text,
-  Checkbox,
-  Radio,
 } from '@contentful/f36-components';
 import { PlusIcon } from '@contentful/f36-icons';
 import { type Density } from '@contentful/f36-utils';
@@ -74,26 +72,6 @@ const Components = [
         size: 'small',
         icon: <PlusIcon />,
         'aria-label': 'Medium',
-      },
-    ],
-  },
-  {
-    name: 'Checkbox',
-    component: Checkbox,
-    props: [
-      {
-        children: 'Checkbox Label',
-        helpText: 'Some help text',
-      },
-    ],
-  },
-  {
-    name: 'Radio',
-    component: Radio,
-    props: [
-      {
-        children: 'Radio Label',
-        helpText: 'Some help text',
       },
     ],
   },
