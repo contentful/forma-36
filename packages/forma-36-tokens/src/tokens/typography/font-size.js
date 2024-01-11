7b6c63768ebb9e8e4d1fbddb69df58247a9a30c8
Rémy Lenoir <remy.lenoir@contentful.com>
2024-01-11 15:18:29 +0100

    feat(tokens): add high-density values

--- packages/forma-36-tokens/src/tokens/typography/font-size.js
+++ packages/forma-36-tokens/src/tokens/typography/font-size.js
@@ -3,10 +3,13 @@ const fontSize = {
   'font-size-3xl': '2.25rem',
   'font-size-2xl': '1.75rem',
   'font-size-xl': '1.25rem',
+  'font-size-xl-high': '1.125rem',
   'font-size-l': '1rem',
+  'font-size-l-high': '0.875rem',
   'font-size-m': '0.875rem',
+  'font-size-m-high': '0.75rem',
   'font-size-s': '0.75rem',
-  'font-size-xs': '0.625rem',
+  'font-size-s-high': '0.625rem',
 };
 
 module.exports = fontSize;
