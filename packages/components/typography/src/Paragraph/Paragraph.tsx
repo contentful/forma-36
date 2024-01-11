255cea1d79e60af42a6f45407b7975547e6d9f4c
Rémy Lenoir <remy.lenoir@contentful.com>
2024-01-11 16:19:11 +0100

    feat(paragraph): add high-density support

--- packages/components/typography/src/Paragraph/Paragraph.tsx
+++ packages/components/typography/src/Paragraph/Paragraph.tsx
@@ -6,6 +6,7 @@ import type {
   ExpandProps,
 } from '@contentful/f36-core';
 import { Text } from '../Text';
+import { useDensity } from '@contentful/f36-utils';
 
 export type ParagraphInternalProps = CommonProps &
   MarginProps & {
@@ -20,10 +21,13 @@ export const Paragraph = React.forwardRef<
   HTMLParagraphElement,
   ExpandProps<ParagraphProps>
 >(({ children, testId = 'cf-ui-paragraph', ...otherProps }, ref) => {
+  const density = useDensity();
+
   return (
     <Text
       as="p"
       testId={testId}
+      fontSize={density === 'high' ? 'fontSizeMHigh' : 'fontSizeM'}
       marginBottom="spacingM"
       lineHeight="lineHeightM"
       {...otherProps}
--- packages/components/typography/stories/Paragraph.stories.tsx
+++ packages/components/typography/stories/Paragraph.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import { Flex } from '@contentful/f36-core';
+import { Heading } from '@contentful/f36-typography';
+import type { Density } from '@contentful/f36-utils';
 import { Paragraph, ParagraphProps } from '../src/Paragraph/Paragraph';
+import { DensityContainer } from '../../density-container';
 
 export default {
   title: 'Typography/Paragraph',
@@ -17,4 +20,36 @@ export const Basic = (props: ParagraphProps) => <Paragraph {...props} />;
 
 Basic.args = {
   children: 'Paragraph',
+};
+
+export const WithDensitySupport = (props: ParagraphProps) => {
+  const Densities = [
+    {
+      name: 'Low density',
+      density: 'low',
+    },
+    {
+      name: 'High density',
+      density: 'high',
+    },
+  ];
+
+  return (
+    <Flex flexDirection="column" gap="spacingS">
+      {Densities.map((density) => {
+        return (
+          <Flex key={density.name} flexDirection="column">
+            <Heading>{density.name}</Heading>
+            <DensityContainer density={density.density as Density}>
+              <Paragraph {...props} />
+            </DensityContainer>
+          </Flex>
+        );
+      })}
+    </Flex>
+  );
+};
+
+WithDensitySupport.args = {
+  children: 'The brown fox jumps over the lazy dog',
 };
