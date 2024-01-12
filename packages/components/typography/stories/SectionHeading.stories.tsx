476812b4bf090b2cecbe5f4ac55a54b7be046122
Rémy Lenoir <remy.lenoir@contentful.com>
2024-01-12 12:51:48 +0100

    chore(section-heading): update story

--- packages/components/typography/stories/SectionHeading.stories.tsx
+++ packages/components/typography/stories/SectionHeading.stories.tsx
@@ -40,10 +40,14 @@ export const WithDensitySupport = (props: SectionHeadingProps<'h3'>) => {
   ];
 
   return (
-    <Flex flexDirection="column" gap="spacingS">
+    <Flex gap="spacing2Xl">
       {Densities.map((density) => {
         return (
-          <Flex key={density.name} flexDirection="column">
+          <Flex
+            key={density.name}
+            flexDirection="column"
+            style={{ width: '230px' }}
+          >
             <Heading>{density.name}</Heading>
             <DensityContainer density={density.density as Density}>
               <SectionHeading {...props} />
@@ -56,5 +60,5 @@ export const WithDensitySupport = (props: SectionHeadingProps<'h3'>) => {
 };
 
 WithDensitySupport.args = {
-  children: 'The brown fox jumps over the lazy dog',
+  children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 };
