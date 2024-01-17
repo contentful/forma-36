02beece5c3bdc061568e7b4168e451adf734e6aa
Rémy Lenoir <remy.lenoir@contentful.com>
2024-01-17 17:59:15 +0100

    chore: remove `@contentful/f36-density-container` package

--- packages/components/density-container/examples/DensitySupport.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Box } from '@contentful/f36-components';
-import { useDensity } from '@contentful/f36-utils';
-
-export default function YourComponent() {
-  const density = useDensity();
-
-  return <Box>{density === 'high' ? 'High density' : 'Low density'}</Box>;
-}
