e0d16aed3b41b1cdee326f5ecac22f54a744a6fb
Kathrin <kathrin.holzmann@contentful.com>
2024-02-15 13:16:47 +0100

    feat: extend SelectAllOption props

--- packages/components/multiselect/src/SelectAllOption.tsx
+++ packages/components/multiselect/src/SelectAllOption.tsx
@@ -6,10 +6,12 @@ import { cx } from 'emotion';
 export interface SelectAllOptionProps
   extends Omit<MultiselectOptionProps, 'value' | 'itemId' | 'label'> {
   label?: string;
+  itemId?: string;
 }
 
 export const SelectAllOption = ({
   label,
+  itemId = 'SelectAll',
   onSelectItem,
   isChecked = false,
   className,
@@ -21,7 +23,7 @@ export const SelectAllOption = ({
     <MultiselectOption
       value="all"
       label={displayLabel}
-      itemId="SelectAll"
+      itemId={itemId}
       onSelectItem={onSelectItem}
       isChecked={isChecked}
       className={cx(styles.selectAll, className)}
