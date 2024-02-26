e7b38b15cb857f6a9f04f6d93e48b42d6ba40a54
Thomas Kellermeier <Chaoste@users.noreply.github.com>
2024-02-26 10:37:45 +0100

    fix: drop transform to UTC when creting machine readable datetime [SPA-1849] (#2680)
    * test: add test for using non-UTC timezone in datetime utils
    
    * fix: dont transform dates to UTC timezone
    
    ---------
    
    Co-authored-by: Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>

--- packages/components/datetime/src/utils/formatDateTimeUtils.test.ts
+++ packages/components/datetime/src/utils/formatDateTimeUtils.test.ts
@@ -1,6 +1,8 @@
 import dayjs, { extend } from 'dayjs';
 import utcPlugin from 'dayjs/plugin/utc.js';
+import timezonePlugin from 'dayjs/plugin/timezone.js';
 extend(utcPlugin);
+extend(timezonePlugin);
 
 import { formatDateAndTime, formatMachineReadableDateTime } from '.';
 
@@ -65,26 +67,36 @@ describe('formatMachineReadableDateTime', () => {
   it('returns a string with "YYYY-MM-DDTHH:mm:ss.SSS[Z]" format', () => {
     const expected = formatMachineReadableDateTime(today);
 
-    expect(expected).toBe(
-      dayjs(today).utc().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'),
-    );
+    expect(expected).toBe(dayjs(today).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'));
   });
 
   it('returns a string with "YYYY-MM-DD" format when "day" is passed', () => {
     const expected = formatMachineReadableDateTime(today, 'day');
 
-    expect(expected).toBe(dayjs(today).utc().format('YYYY-MM-DD'));
+    expect(expected).toBe(dayjs(today).format('YYYY-MM-DD'));
   });
 
   it('returns a string with "MM-DD" format when "weekday" is passed', () => {
     const expected = formatMachineReadableDateTime(today, 'weekday');
 
-    expect(expected).toBe(dayjs(today).utc().format('MM-DD'));
+    expect(expected).toBe(dayjs(today).format('MM-DD'));
   });
 
   it('returns a string with "HH:mm:ss.SSS" format when "time" is passed', () => {
     const expected = formatMachineReadableDateTime(today, 'time');
 
-    expect(expected).toBe(dayjs(today).utc().format('HH:mm:ss.SSS'));
+    expect(expected).toBe(dayjs(today).format('HH:mm:ss.SSS'));
+  });
+
+  describe('when using a non-UTC timezone', () => {
+    it('returns the day for the current timezone', () => {
+      const time = '2024-02-19T01:30:00.000+01:00';
+      // We test with the LA timezone (-7h/-8h) which means that for the current
+      // client this appears to have happened on the previous day.
+      // In UTC time and in the original used timezone, it would appear as the 19th.
+      const expected = '2024-02-18';
+      const actual = formatMachineReadableDateTime(time, 'day');
+      expect(actual).toBe(expected);
+    });
   });
 });
