05fb7123d19341c26d3f5fee09367b8bbcfbc7ca
Kathrin <kathrin.holzmann@contentful.com>
2024-04-10 16:41:16 +0200

    chore: update deprecation information

--- packages/components/icon/src/generateIcon.tsx
+++ packages/components/icon/src/generateIcon.tsx
@@ -22,7 +22,7 @@ type GenerateIconParameters = {
   props?: GeneratedIconProps;
   /**
    * Whether or not to trim the icon width, i.e. set `width` to `auto`
-   * @deprecated IconsV5 will no longer support the trimmed option.
+   * @deprecated Trimmed icons will be removed in a future major release. Please try to adapt to the untrimmed icon variant.
    */
   trimmed?: IconProps['trimmed'];
   /**
