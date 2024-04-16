e1cbed88c3fc0921426250257fd388d3fb17199a
Rémy Lenoir <remy.lenoir@contentful.com>
2024-04-16 16:14:29 +0200

    refactor: export individual icons modules

---
+++ packages/components/icons/src/MinusIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon';
+import { Minus } from '@phosphor-icons/react';
+
+export const MinusIcon = generateForma36Icon(Minus);
---
+++ packages/components/icons/src/TextHThreeIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon';
+import { TextHThree } from '@phosphor-icons/react';
+
+export const TextHThreeIcon = generateForma36Icon(TextHThree);
