ffcecd8f88f4b5a9292ac17b94024f18ef60ff06
Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
2024-04-25 10:45:59 +0200

    feat:(codemod) add v5-icons codemod (#2716)
    * feat: add v5 option to codemod that install v5-icons-alpha
    
    * feat: add v5/icons codemod
    
    * refactor: remove v4 and color tokens transformers
    
    * refactor: update utils folder
    
    * refactor: update utils and add jsdocs to functions
    
    * refactor: update readme and remove v4 support
    
    * refactor: run git check on provided path if present
    
    * chore: update help text on codemods
    
    * refactor: update new icon names
    
    * fix: update mapping (#2728)
    
    Co-authored-by: Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kristoffer <denkristoffer@users.noreply.github.com>

---
+++ packages/forma-36-codemod/transforms/__testfixtures__/v5/icons.output.js
@@ -0,0 +1,7 @@
+import { CaretDownIcon, CaretUpIcon, XIcon, TrashSimpleIcon, InfoFillIcon } from "@contentful/f36-icons-alpha";
+
+<XIcon size="small" />;
+<CaretUpIcon size="small" />;
+<CaretDownIcon size="small" />;
+<TrashSimpleIcon size="small" />;
+<InfoFillIcon />;
---
+++ packages/forma-36-codemod/transforms/v5/icons.js
@@ -0,0 +1,153 @@
+const {
+  getComponentLocalName,
+  changeComponentName,
+  changeImport,
+} = require('../../utils');
+const { shouldSkipUpdateImport, getImport } = require('../../utils/config');
+
+// V4 icon name : V5 icon name
+const iconsMap = {
+  Archive: 'FileArchive',
+  ArrowBackward: 'ArrowLeft',
+  ArrowDown: 'CaretDown',
+  ArrowDownward: 'ArrowDown',
+  ArrowForward: 'ArrowRight',
+  ArrowUp: 'CaretUp',
+  ArrowUpward: 'ArrowUp',
+  Asset: 'ImageSquare',
+  Audio: 'FileAudio',
+  Calendar: 'CalendarBlank',
+  ChatBubble: 'Chat',
+  CheckCircle: 'CheckCircle',
+  ChevronDown: 'CaretDown',
+  ChevronLeft: 'CaretLeft',
+  ChevronRight: 'CaretRight',
+  ChevronUp: 'CaretUp',
+  Clock: 'Clock',
+  Close: 'X',
+  CloudUpload: 'CloudArrowUp',
+  Code: 'CodeSimple',
+  CodeIllustration: 'BracketsCurly',
+  Copy: 'CopySimple',
+  Cycle: 'Repeat',
+  Delete: 'TrashSimple',
+  Diamond: 'SketchLogo',
+  Done: 'Check',
+  DoubleArrow: 'CaretUpDown',
+  Download: 'DownloadSimple',
+  Drag: 'DotsSixVertical',
+  Edit: 'PencilSimple',
+  EmbeddedEntryBlock: 'EmbeddedBlock',
+  EmbeddedEntryInline: 'EmbeddedLine',
+  Entry: 'Entry',
+  Environment: 'Environment',
+  EnvironmentAlias: 'EnvironmentAlias',
+  ErrorCircle: 'WarningOctagon',
+  ErrorCircleOutline: 'WarningOctagon',
+  ExternalLink: 'ArrowSquareOut',
+  FaceHappy: 'Smiley',
+  Filter: 'FunnelSimple',
+  Folder: 'FolderSimple',
+  FolderCreate: 'FolderSimplePlus',
+  FolderOpen: 'FolderOpen',
+  FormatBold: 'TextBold',
+  FormatItalic: 'TextItalic',
+  FormatUnderlined: 'TextUnderline',
+  Gift: 'Gift',
+  Heading: 'TextH',
+  HeadingOne: 'TextHOne',
+  HeadingTwo: 'TextHTwo',
+  HelpCircle: 'Info',
+  HelpCircleInverted: 'Info',
+  HorizontalRule: 'Minus',
+  Image: 'ImageSquare',
+  InfoCircle: 'Info',
+  Language: 'Globe',
+  Link: 'LinkSimpleHorizontal',
+  LinkAlternate: 'LinkSimpleHorizontal',
+  ListBulleted: 'ListBullets',
+  ListNumbered: 'ListNumbers',
+  Lock: 'LockSimple',
+  Logout: 'SignOut',
+  Markup: 'FileCode',
+  Menu: 'List',
+  Minus: 'Minus',
+  MoreHorizontal: 'DotsThree',
+  Page: 'File',
+  Pdf: 'FilePdf',
+  Person: 'User',
+  Plaintext: 'FileText',
+  Plus: 'Plus',
+  PlusCircle: 'PlusCircle',
+  Presentation: 'Presentation',
+  Preview: 'Eye',
+  PreviewOff: 'EyeSlash',
+  Puzzle: 'PuzzlePiece',
+  Quote: 'Quotes',
+  Receipt: 'Receipt',
+  References: 'References',
+  Release: 'Release',
+  Richtext: 'RichText',
+  Search: 'MagnifyingGlass',
+  Settings: 'GearSix',
+  ShoppingCart: 'ShoppingCartSimple',
+  Sort: 'CaretUpDown',
+  SortAscending: 'SortAscending',
+  SortDescending: 'SortDescending',
+  Spreadsheet: 'Table',
+  Star: 'Star',
+  Subscript: 'TextSubscript',
+  Superscript: 'TextSuperscript',
+  Tab: 'Tabs',
+  Table: 'Table',
+  Tags: 'Tag',
+  Text: 'TextT',
+  ThumbDown: 'ThumbsDown',
+  ThumbUp: 'ThumbsUp',
+  Toggle: 'ArrowsLeftRight',
+  TriangleOutline: 'Triangle',
+  Users: 'Users',
+  Video: 'FileVideo',
+  Warning: 'Warning',
+  Workflows: 'Workflows',
+};
+
+module.exports = function (file, api) {
+  const j = api.jscodeshift;
+
+  let source = file.source;
+
+  const importName = getImport('f36-icons');
+
+  const components = Object.keys(iconsMap)
+    .map((v4IconName) => {
+      return {
+        localName: getComponentLocalName(j, source, {
+          componentName: `${v4IconName}Icon`,
+          importName,
+        }),
+        v4IconName,
+      };
+    })
+    .filter(({ localName }) => !!localName);
+
+  components.forEach(({ localName, v4IconName }) => {
+    const newComponentName = `${iconsMap[v4IconName]}Icon`;
+
+    source = changeComponentName(j, source, {
+      componentName: localName,
+      outputComponentName: newComponentName,
+    });
+
+    if (!shouldSkipUpdateImport()) {
+      source = changeImport(j, source, {
+        componentName: localName,
+        from: importName,
+        to: '@contentful/f36-icons-alpha',
+        outputComponentName: newComponentName,
+      });
+    }
+  });
+
+  return source;
+};
