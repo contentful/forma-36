2c85c729b3cf2cc0197a29d3c3e22e6d3cbdfd50
Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
2024-04-25 10:45:59 +0200

    feat:(codemod) add v5-icons codemod (#2716)
    * feat: add v5 option to codemod that install v5-icons-alpha
    
    * feat: add v5/icons codemod
    
    * refactor: remove v4 and color tokens transformers
    
    * refactor: update utils folder
    
    * refactor: update utils and add jsdocs to functions
    
    * refactor: update readme and remove v4 support
    
    * refactor: run git check on provided path if present
    
    * chore: update help text on codemods
    
    * refactor: update new icon names
    
    * fix: update mapping (#2728)
    
    Co-authored-by: Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kristoffer <denkristoffer@users.noreply.github.com>

--- packages/forma-36-codemod/transforms/v4-tooltip.js
+++
@@ -1,9 +0,0 @@
-const { modifyPropsCodemod } = require('./common/modify-props-codemod');
-
-module.exports = modifyPropsCodemod({
-  componentName: 'Tooltip',
-  renameMap: {
-    place: 'placement',
-    containerElement: 'as',
-  },
-});
