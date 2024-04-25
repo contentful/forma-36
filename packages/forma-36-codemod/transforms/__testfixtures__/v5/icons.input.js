46afac8c85277a11c8f0a8dceea59f7917a492c8
Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
2024-04-25 10:45:59 +0200

    feat:(codemod) add v5-icons codemod (#2716)
    * feat: add v5 option to codemod that install v5-icons-alpha
    
    * feat: add v5/icons codemod
    
    * refactor: remove v4 and color tokens transformers
    
    * refactor: update utils folder
    
    * refactor: update utils and add jsdocs to functions
    
    * refactor: update readme and remove v4 support
    
    * refactor: run git check on provided path if present
    
    * chore: update help text on codemods
    
    * refactor: update new icon names
    
    * fix: update mapping (#2728)
    
    Co-authored-by: Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kristoffer <denkristoffer@users.noreply.github.com>

---
+++ packages/forma-36-codemod/transforms/__testfixtures__/v5/icons.input.js
@@ -0,0 +1,13 @@
+import {
+  CloseIcon,
+  ChevronUpIcon,
+  ChevronDownIcon,
+  DeleteIcon,
+  InfoCircleIcon,
+} from '@contentful/f36-icons';
+
+<CloseIcon size="small" />;
+<ChevronUpIcon size="small" />;
+<ChevronDownIcon size="small" />;
+<DeleteIcon size="small" />;
+<InfoCircleIcon />;
