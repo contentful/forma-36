74581d770533751870d978697f014bb6157859f5
Kathrin <kathrin.holzmann@contentful.com>
2024-04-25 09:19:28 +0200

    style: apply light color schema for bottom navbar

--- packages/components/navbar/stories/Navbar.stories.tsx
+++ packages/components/navbar/stories/Navbar.stories.tsx
@@ -53,7 +53,7 @@ const Account = ({
 
 const MainItems = () => (
   <>
-    <Navbar.Item title="Home" isActive />
+    <Navbar.Item title="Home" icon={<FaceHappyIcon />} isActive />
     <Navbar.Item title="Content model" />
     <Navbar.Item title="Content" icon={<EntryIcon />} />
     <Navbar.Item title="Media" icon={<AssetIcon />} />
