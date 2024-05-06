7c102136dfb86e8fddfa0b0b9224f733d9bc7740
Stephan Leece <stephan.leece@outlook.com>
2024-05-06 17:07:55 +0200

    feat(layout): begin updating component to new page cut design

--- packages/components/layout/src/LayoutBody.styles.ts
+++ packages/components/layout/src/LayoutBody.styles.ts
@@ -1,8 +1,10 @@
 import { css } from 'emotion';
+import tokens from '@contentful/f36-tokens';
 
 export const getLayoutBodyStyles = () => ({
   root: css({
     flexGrow: 1,
     minWidth: 0,
+    padding: `${tokens.spacingL} ${tokens.spacingL} 0`,
   }),
 });
