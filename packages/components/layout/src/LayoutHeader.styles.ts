f5934472861e7452ffbbd572b5d0708f002216f8
Stephan Leece <stephan.leece@outlook.com>
2024-05-06 17:07:55 +0200

    feat(layout): begin updating component to new page cut design

--- packages/components/layout/src/LayoutHeader.styles.ts
+++ packages/components/layout/src/LayoutHeader.styles.ts
@@ -8,6 +8,7 @@ export const getLayoutHeaderStyles = (
 ) => ({
   root: css({
     borderBottom: `1px solid ${tokens.gray200}`,
+    padding: `0 ${tokens.spacingL}`,
     width: '100%',
     justifyContent: 'center',
   }),
