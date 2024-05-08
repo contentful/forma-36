e2ed177178ea3fc811c74e876fbc04d5cc7f00e1
Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>
2024-05-08 17:12:26 +0200

    build: switch to dual package emitting (#2746)

--- packages/components/icons/src/vendor/phosphor/ThumbsUpIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/ThumbsUpIcon.tsx
@@ -1,4 +1,4 @@
 import { generateForma36Icon } from '@contentful/f36-icon-alpha';
-import { ThumbsUp } from '@phosphor-icons/react/ThumbsUp';
+import { ThumbsUp } from '@phosphor-icons/react';
 
 export const ThumbsUpIcon = generateForma36Icon(ThumbsUp);
