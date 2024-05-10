6d28a76622279854282e45aeb14b59439533b579
Stephan Leece <stephan.leece@outlook.com>
2024-05-10 17:58:40 +0200

    feat(layout): use tokens in component margin"

--- packages/components/layout/src/Layout.styles.ts
+++ packages/components/layout/src/Layout.styles.ts
@@ -16,7 +16,7 @@ export const getLayoutMaxWidthStyles = (variant: LayoutProps['variant']) => {
     boxShadow:
       '0px 6px 16px -2px rgba(25, 37, 50, 0.1), 0px 3px 6px -3px rgba(25, 37, 50, 0.15)',
     borderRadius: `10px 10px 0 0`,
-    margin: '12px 16px 0',
+    margin: `${tokens.spacingS} ${tokens.spacingM} 0`,
   });
 };
 
