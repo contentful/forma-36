7578b41a2cab3241084964d31194d6de81e12ca7
Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
2024-05-28 13:47:41 +0200

    refactor: apply avatar changes to light navbar (#2770)

--- packages/components/navbar/src/NavbarAccount/NavbarAccount.tsx
+++ packages/components/navbar/src/NavbarAccount/NavbarAccount.tsx
@@ -7,12 +7,14 @@ import {
   type ExpandProps,
 } from '@contentful/f36-core';
 import { NavbarMenu } from '../NavbarMenu/NavbarMenu';
+import { Avatar } from '@contentful/f36-avatar';
 import { Flex } from '@contentful/f36-core';
 
 type NavbarAccountOwnProps = CommonProps & {
   children: React.ReactNode;
   username: string;
-  avatar: string;
+  avatar?: string;
+  initials?: string;
   hasNotification?: boolean;
   /**
    * @default 'warning'
@@ -34,6 +36,7 @@ function _NavbarAccount(
     className,
     testId = 'cf-ui-navbar-account-trigger',
     avatar,
+    initials,
     username,
     hasNotification,
     notificationVariant = 'warning',
@@ -52,10 +55,11 @@ function _NavbarAccount(
           testId={testId}
           alignItems="center"
         >
-          <img
+          <Avatar
             src={avatar}
-            alt={`Avatar for user ${username}`}
-            className={styles.avatar}
+            initials={initials}
+            size="small"
+            variant="user"
           />
           {hasNotification ? (
             <span className={styles.notificationIcon(notificationVariant)} />
