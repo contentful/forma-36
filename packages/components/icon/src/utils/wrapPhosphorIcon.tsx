58a6ba298591ef72cefc09dc72ba3e219e86772f
Renato Massao Yonamine <massao.yonamine@contentful.com>
2024-05-28 15:35:18 +0200

    refactor: set default color to currentColor

--- packages/components/icon/src/utils/wrapPhosphorIcon.tsx
+++ packages/components/icon/src/utils/wrapPhosphorIcon.tsx
@@ -4,7 +4,6 @@ import type {
   IconWeight as PhosphorIconWeight,
   IconProps as PhosphorIconProps,
 } from '@phosphor-icons/react';
-import tokens from '@contentful/f36-tokens';
 import { Icon, sizes } from '../Icon.js';
 import type { GeneratedIconProps } from './generateIconComponent.js';
 
@@ -13,8 +12,7 @@ type IconWeight = Extract<PhosphorIconWeight, 'duotone'>;
 // Unfortunately we have to pass props directly to the Phosphor icon
 export function wrapPhosphorIcon(PhosphorIcon: PhosphorIcon) {
   const Component = ({
-    isActive = false,
-    color = isActive ? tokens.blue500 : tokens.gray900,
+    color = 'currentColor',
     size = 'medium',
     ...props
   }: GeneratedIconProps & { weight?: IconWeight }) => {
