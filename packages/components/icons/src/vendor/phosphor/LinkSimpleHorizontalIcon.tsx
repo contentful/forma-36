3aea6626b51ef4793e85596a55835eba652ba9d4
Rémy Lenoir <remy.lenoir@contentful.com>
2024-05-29 16:26:04 +0200

    feat: update link icon + add link-break icon

--- packages/components/icons/src/vendor/phosphor/LinkSimpleHorizontalIcon.tsx
+++
@@ -1,5 +0,0 @@
-import { generateForma36Icon } from '@contentful/f36-icon-alpha';
-import { LinkSimpleHorizontal } from '@phosphor-icons/react';
-
-export const LinkSimpleHorizontalIcon =
-  generateForma36Icon(LinkSimpleHorizontal);
