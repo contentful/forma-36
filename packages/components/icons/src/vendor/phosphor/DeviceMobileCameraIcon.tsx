a3b694562f75e12ff699ccd0103125c8a65172e1
Rémy Lenoir <remy.lenoir@contentful.com>
2024-06-10 16:14:57 +0200

    feat: add missing icons

---
+++ packages/components/icons/src/vendor/phosphor/DeviceMobileCameraIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon-alpha';
+import { DeviceMobileCamera } from '@phosphor-icons/react';
+
+export const DeviceMobileCameraIcon = generateForma36Icon(DeviceMobileCamera);
