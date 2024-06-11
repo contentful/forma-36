5ef94f55aa748a32f40e153a22bc847c844678f3
Lillian Bitner <lillian.bitner@contentful.com>
2024-06-11 09:44:31 -0600

    chore: add lagging commit for unsaved name change

--- packages/components/progress-stepper/src/index.ts
+++ packages/components/progress-stepper/src/index.ts
@@ -1,4 +1,4 @@
 export { ProgressStepper } from './CompoundProgressStepper';
-export type { StepperProps } from './ProgressStepper';
+export type { ProgressStepperProps } from './ProgressStepper';
 export { Step } from './Step';
 export type { StepProps } from './Step';
