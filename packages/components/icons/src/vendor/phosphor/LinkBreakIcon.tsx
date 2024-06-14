f4c912644ae971ee424e40c98d562fa07a1a7506
Rémy Lenoir <remy.lenoir@contentful.com>
2024-06-14 17:13:35 +0200

    fix: prevent prop leaking into Phosphor

--- packages/components/icons/src/vendor/phosphor/LinkBreakIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/LinkBreakIcon.tsx
@@ -1,4 +1,4 @@
-import { generateForma36Icon } from '@contentful/f36-icon-alpha';
+import { generateForma36Icon } from '@contentful/f36-icon';
 import { LinkBreak } from '@phosphor-icons/react';
 
 export const LinkBreakIcon = generateForma36Icon(LinkBreak);
--- packages/components/icons/src/vendor/phosphor/TreeStructureIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/TreeStructureIcon.tsx
@@ -1,4 +1,4 @@
-import { generateForma36Icon } from '@contentful/f36-icon-alpha';
+import { generateForma36Icon } from '@contentful/f36-icon';
 import { TreeStructure } from '@phosphor-icons/react';
 
 export const TreeStructureIcon = generateForma36Icon(TreeStructure);
