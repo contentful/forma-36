e5f5990fd6412586fe95f610c7e9137fcee2576b
Kathrin <kathrin.holzmann@contentful.com>
2024-06-17 12:13:35 +0200

    fix: reset import package name

--- packages/components/icons/src/vendor/phosphor/FileAudioIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/FileAudioIcon.tsx
@@ -1,4 +1,4 @@
-import { generateForma36Icon } from '@contentful/f36-icon';
+import { generateForma36Icon } from '@contentful/f36-icon-alpha';
 import { FileAudio } from '@phosphor-icons/react';
 
 export const FileAudioIcon = generateForma36Icon(FileAudio);
--- packages/components/icons/src/vendor/phosphor/XIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/XIcon.tsx
@@ -1,4 +1,4 @@
-import { generateForma36Icon } from '@contentful/f36-icon';
+import { generateForma36Icon } from '@contentful/f36-icon-alpha';
 import { X } from '@phosphor-icons/react';
 
 export const XIcon = generateForma36Icon(X);
