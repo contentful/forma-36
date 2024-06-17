7de866840170d00b6ce6ae7c1b939b3cc04dbad8
Kathrin <kathrin.holzmann@contentful.com>
2024-06-17 12:13:35 +0200

    fix: reset import package name

--- packages/components/icons/src/vendor/phosphor/GlobeIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/GlobeIcon.tsx
@@ -1,4 +1,4 @@
-import { generateForma36Icon } from '@contentful/f36-icon';
+import { generateForma36Icon } from '@contentful/f36-icon-alpha';
 import { Globe } from '@phosphor-icons/react';
 
 export const GlobeIcon = generateForma36Icon(Globe);
--- packages/components/icons/src/vendor/phosphor/TextBIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/TextBIcon.tsx
@@ -1,4 +1,4 @@
-import { generateForma36Icon } from '@contentful/f36-icon';
+import { generateForma36Icon } from '@contentful/f36-icon-alpha';
 import { TextB } from '@phosphor-icons/react';
 
 export const TextBIcon = generateForma36Icon(TextB);
