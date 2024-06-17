4b56ec8d498e675da9fdc54d882b3bb60b2afcac
Kathrin <kathrin.holzmann@contentful.com>
2024-06-17 12:13:35 +0200

    fix: reset import package name

--- packages/components/icons/src/vendor/phosphor/PaperPlaneTiltIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/PaperPlaneTiltIcon.tsx
@@ -1,4 +1,4 @@
-import { generateForma36Icon } from '@contentful/f36-icon';
+import { generateForma36Icon } from '@contentful/f36-icon-alpha';
 import { PaperPlaneTilt } from '@phosphor-icons/react';
 
 export const PaperPlaneTiltIcon = generateForma36Icon(PaperPlaneTilt);
