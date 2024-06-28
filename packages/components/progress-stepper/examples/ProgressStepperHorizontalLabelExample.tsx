60faea546a66f955fcb5f12c6a2d0a64303b4525
Lisa White <lisa.white@contentful.com>
2024-06-28 13:29:04 -0600

    fix: refactor interactive example and update steps to be zero-indexed

--- packages/components/progress-stepper/examples/ProgressStepperHorizontalLabelExample.tsx
+++ packages/components/progress-stepper/examples/ProgressStepperHorizontalLabelExample.tsx
@@ -3,7 +3,7 @@ import { ProgressStepper } from '@contentful/f36-progress-stepper';
 
 export default function ProgressStepperHorizontalLabelExample() {
   return (
-    <ProgressStepper activeStep={3}>
+    <ProgressStepper activeStep={2}>
       <ProgressStepper.Step variant="complete" labelText="Label 1" />
       <ProgressStepper.Step variant="complete" labelText="Label 2" />
       <ProgressStepper.Step variant="active" labelText="Label 3" />
--- packages/components/progress-stepper/examples/ProgressStepperNumberExample.tsx
+++ packages/components/progress-stepper/examples/ProgressStepperNumberExample.tsx
@@ -3,7 +3,7 @@ import { ProgressStepper } from '@contentful/f36-progress-stepper';
 
 export default function ProgressStepperNumberExample() {
   return (
-    <ProgressStepper activeStep={1} stepStyle="number">
+    <ProgressStepper activeStep={0} stepStyle="number">
       <ProgressStepper.Step variant="active" labelText="Active" />
       <ProgressStepper.Step variant="complete" labelText="Complete" />
       <ProgressStepper.Step labelText="Incomplete" />
