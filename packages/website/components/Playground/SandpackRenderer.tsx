6e08b8d6f3f7b20f0b72a1a23c25aa08f178c607
denkristoffer <denkristoffer@users.noreply.github.com>
2024-06-28 16:55:53 +0200

    chore: layout deps

--- packages/website/components/Playground/SandpackRenderer.tsx
+++ packages/website/components/Playground/SandpackRenderer.tsx
@@ -95,7 +95,7 @@ export function SandpackRenderer({
           'react-dom': '^17.0.0',
           'react-scripts': '^4.0.0',
           '@contentful/f36-components': '^4.0.0',
-          '@contentful/f36-layout': '^4.0.0-alpha.0', // Remove when added to f36-components
+          '@contentful/f36-layout': '4.0.0-alpha.1', // Remove when added to f36-components
           '@contentful/f36-multiselect': '^4.0.0', // Remove when added to f36-components
           '@contentful/f36-navlist': '^4.1.0-alpha.0', // Remove when added to f36-components
           '@contentful/f36-tokens': '^4.0.0',
