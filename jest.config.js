8d4ceb6e8cefe2cb11d1c98d94b0111c8aa63e5c
Rémy Lenoir <remy.lenoir@contentful.com>
2024-07-17 16:21:54 +0200

    test: ignore dist path

--- jest.config.js
+++ jest.config.js
@@ -75,7 +75,7 @@ module.exports = {
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: ['/dist/'],
 
   // Activates notifications for test results
   // notify: false,
