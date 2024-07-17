293b9fd559c9604e8fcfc73fb289d459d152796e
Rémy Lenoir <remy.lenoir@contentful.com>
2024-07-17 16:25:27 +0200

    test: map ESM import extension

--- jest.config.js
+++ jest.config.js
@@ -71,6 +71,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/scripts/test/__mocks__/fileMock.js',
+    '(.+)\\.js': '$1',
     '@/scripts/(.*)': '<rootDir>/scripts/$1',
   },
 
