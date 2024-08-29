fb0650fb894b260f3680e0359be968025b3a2a5f
Stephan Leece <stephan.leece@outlook.com>
2024-08-29 13:21:44 +0200

    feat(layout): use vh instead on % to avoid page jumping

--- packages/components/layout/src/Layout.styles.ts
+++ packages/components/layout/src/Layout.styles.ts
@@ -34,7 +34,7 @@ export const getLayoutStyles = ({
 }) => ({
   root: css({
     width: '100%',
-    height: '100%',
+    height: '100vh',
   }),
   mainContainer: css(getLayoutMaxWidthStyles({ variant, withBoxShadow }), {
     width: '100%',
