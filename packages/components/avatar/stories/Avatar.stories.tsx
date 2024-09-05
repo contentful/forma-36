0f88f9e5aa6ba4232f4866fa058e478091f56fd8
Andi Pätzold <andreas.paetzold@contentful.com>
2024-09-05 16:48:40 +0200

    docs: render all border colors on one screen

--- packages/components/avatar/stories/Avatar.stories.tsx
+++ packages/components/avatar/stories/Avatar.stories.tsx
@@ -3,7 +3,12 @@ import type { Meta, Story } from '@storybook/react/types-6-0';
 import { Flex } from '@contentful/f36-core';
 import { SectionHeading } from '@contentful/f36-typography';
 
-import { Avatar, type AvatarProps } from '../src/Avatar';
+import {
+  Avatar,
+  avatarColorMap,
+  type AvatarProps,
+  type ColorVariant,
+} from '../src/Avatar';
 import { CheckCircleIcon } from '@contentful/f36-icons/src';
 
 export default {
@@ -12,11 +17,66 @@ export default {
 } as Meta;
 
 export const Overview: Story<AvatarProps> = (args) => {
-  const { src, ...argsNoSrc } = args;
   return (
     <>
       <SectionHeading as="h3" marginBottom="spacingS">
-        Avatar variants & sizes
+        Icon
+      </SectionHeading>
+
+      <Flex
+        alignItems="center"
+        flexDirection="row"
+        gap="spacingS"
+        marginBottom="spacingM"
+      >
+        <Avatar
+          {...args}
+          size="tiny"
+          icon={<CheckCircleIcon variant="positive" />}
+        />
+        <Avatar
+          {...args}
+          size="small"
+          icon={<CheckCircleIcon variant="positive" />}
+        />
+        <Avatar
+          {...args}
+          size="medium"
+          icon={<CheckCircleIcon variant="positive" />}
+        />
+        <Avatar
+          {...args}
+          size="large"
+          icon={<CheckCircleIcon variant="positive" />}
+        />
+        <Avatar
+          {...args}
+          size="large"
+          variant="app"
+          icon={<CheckCircleIcon variant="positive" />}
+        />
+        <Avatar
+          {...args}
+          size="medium"
+          variant="app"
+          icon={<CheckCircleIcon variant="positive" />}
+        />
+        <Avatar
+          {...args}
+          size="small"
+          variant="app"
+          icon={<CheckCircleIcon variant="positive" />}
+        />
+        <Avatar
+          {...args}
+          size="tiny"
+          variant="app"
+          icon={<CheckCircleIcon variant="positive" />}
+        />
+      </Flex>
+
+      <SectionHeading as="h3" marginBottom="spacingS">
+        Loading
       </SectionHeading>
 
       <Flex
@@ -36,91 +96,9 @@ export const Overview: Story<AvatarProps> = (args) => {
         <Avatar size="tiny" variant="app" />
       </Flex>
 
-      <Flex
-        alignItems="center"
-        flexDirection="row"
-        gap="spacingS"
-        marginBottom="spacingM"
-      >
-        <Avatar {...argsNoSrc} size="tiny" variant="user" />
-        <Avatar {...argsNoSrc} size="small" variant="user" />
-        <Avatar {...argsNoSrc} size="medium" variant="user" />
-        <Avatar {...argsNoSrc} size="large" variant="app" />
-        <Avatar {...argsNoSrc} size="large" variant="user" />
-        <Avatar {...argsNoSrc} size="medium" variant="app" />
-        <Avatar {...argsNoSrc} size="small" variant="app" />
-        <Avatar {...argsNoSrc} size="tiny" variant="app" />
-      </Flex>
-
-      <Flex
-        alignItems="center"
-        flexDirection="row"
-        gap="spacingS"
-        marginBottom="spacingM"
-      >
-        <Avatar {...args} size="tiny" />
-        <Avatar {...args} size="small" />
-        <Avatar {...args} size="medium" />
-        <Avatar {...args} size="large" />
-        <Avatar {...args} size="large" variant="app" />
-        <Avatar {...args} size="medium" variant="app" />
-        <Avatar {...args} size="small" variant="app" />
-        <Avatar {...args} size="tiny" variant="app" />
-      </Flex>
-
-      <Flex
-        alignItems="center"
-        flexDirection="row"
-        gap="spacingS"
-        marginBottom="spacingM"
-      >
-        <Avatar
-          {...args}
-          size="tiny"
-          icon={<CheckCircleIcon variant="positive" />}
-        />
-        <Avatar
-          {...args}
-          size="small"
-          icon={<CheckCircleIcon variant="positive" />}
-        />
-        <Avatar
-          {...args}
-          size="medium"
-          icon={<CheckCircleIcon variant="positive" />}
-        />
-        <Avatar
-          {...args}
-          size="large"
-          icon={<CheckCircleIcon variant="positive" />}
-        />
-        <Avatar
-          {...args}
-          size="large"
-          variant="app"
-          icon={<CheckCircleIcon variant="positive" />}
-        />
-        <Avatar
-          {...args}
-          size="medium"
-          variant="app"
-          icon={<CheckCircleIcon variant="positive" />}
-        />
-        <Avatar
-          {...args}
-          size="small"
-          variant="app"
-          icon={<CheckCircleIcon variant="positive" />}
-        />
-        <Avatar
-          {...args}
-          size="tiny"
-          variant="app"
-          icon={<CheckCircleIcon variant="positive" />}
-        />
-      </Flex>
-
-      <SectionHeading as="h4">Indicator properties</SectionHeading>
+      <SectionHeading as="h3" marginBottom="spacingS">
+        Indicator properties
+      </SectionHeading>
       <Flex
         alignItems="center"
         flexDirection="row"
@@ -144,7 +122,9 @@ export const Overview: Story<AvatarProps> = (args) => {
         <Avatar {...args} size="large" variant="app" colorVariant="olive" />
       </Flex>
 
-      <SectionHeading as="h4">Tooltip properties</SectionHeading>
+      <SectionHeading as="h3" marginBottom="spacingS">
+        Tooltip properties
+      </SectionHeading>
       <Flex
         alignItems="center"
         flexDirection="row"
@@ -167,6 +147,65 @@ export const Overview: Story<AvatarProps> = (args) => {
 };
 
 Overview.args = {
+  initials: 'BB',
+  src: 'https://images.ctfassets.net/iq4lnigp6fgt/2EEEk92Kiz6KxREsjBLPAN/810d5a21650d91abad12e95da4cd3beb/2021-06_Everyone_is_Welcome_here_1_.png?fit=fill&f=top_left&w=100&h=100',
+};
+
+export const BorderColors: Story<AvatarProps> = (args) => {
+  const { src, ...argsNoSrc } = args;
+  return (
+    <>
+      {Object.keys(avatarColorMap).map((color: ColorVariant) => (
+        <>
+          <SectionHeading as="h3" marginBottom="spacingS">
+            {color}
+          </SectionHeading>
+          <Flex
+            alignItems="center"
+            flexDirection="row"
+            gap="spacingS"
+            marginBottom="spacingM"
+          >
+            {/* prettier-ignore */}
+            <Avatar {...argsNoSrc} colorVariant={color} size="tiny" variant="user" />
+            {/* prettier-ignore */}
+            <Avatar {...argsNoSrc} colorVariant={color} size="small" variant="user" />
+            {/* prettier-ignore */}
+            <Avatar {...argsNoSrc} colorVariant={color} size="medium" variant="user" />
+            {/* prettier-ignore */}
+            <Avatar {...argsNoSrc} colorVariant={color} size="large" variant="app" />
+            {/* prettier-ignore */}
+            <Avatar {...argsNoSrc} colorVariant={color} size="large" variant="user" />
+            {/* prettier-ignore */}
+            <Avatar {...argsNoSrc} colorVariant={color} size="medium" variant="app" />
+            {/* prettier-ignore */}
+            <Avatar {...argsNoSrc} colorVariant={color} size="small" variant="app" />
+            {/* prettier-ignore */}
+            <Avatar {...argsNoSrc}  colorVariant={color}size="tiny" variant="app" />
+            {/* prettier-ignore */}
+            <Avatar {...args} colorVariant={color} size="tiny" />
+            {/* prettier-ignore */}
+            <Avatar {...args} colorVariant={color} size="small" />
+            {/* prettier-ignore */}
+            <Avatar {...args} colorVariant={color} size="medium" />
+            {/* prettier-ignore */}
+            <Avatar {...args} colorVariant={color} size="large" />
+            {/* prettier-ignore */}
+            <Avatar {...args} colorVariant={color} size="large" variant="app" />
+            {/* prettier-ignore */}
+            <Avatar {...args} colorVariant={color} size="medium" variant="app" />
+            {/* prettier-ignore */}
+            <Avatar {...args} colorVariant={color} size="small" variant="app" />
+            {/* prettier-ignore */}
+            <Avatar {...args} colorVariant={color} size="tiny" variant="app" />
+          </Flex>
+        </>
+      ))}
+    </>
+  );
+};
+
+BorderColors.args = {
   initials: 'BB',
   src: 'https://images.ctfassets.net/iq4lnigp6fgt/2EEEk92Kiz6KxREsjBLPAN/810d5a21650d91abad12e95da4cd3beb/2021-06_Everyone_is_Welcome_here_1_.png?fit=fill&f=top_left&w=100&h=100',
 };
