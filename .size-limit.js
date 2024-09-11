5319edcea55992b5a01c49c24d1072f90dde0592
Kristoffer <denkristoffer@users.noreply.github.com>
2024-09-11 09:24:22 +0200

    chore: update size-limit [] (#2870)

--- .size-limit.js
+++ .size-limit.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     ignore: ['react', 'react-dom'],
-    limit: '3.6 s',
+    limit: '4 s',
     name: 'CommonJS',
     path: './packages/forma-36-react-components/dist/index.js',
   },
   {
     ignore: ['react', 'react-dom'],
     import: '*',
-    limit: '3.6 s',
+    limit: '4 s',
     name: 'Module',
     path: './packages/forma-36-react-components/dist/index.module.js',
   },
