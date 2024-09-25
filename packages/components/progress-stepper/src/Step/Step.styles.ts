9eaac4052d386a5d3aa56ae409b5d16215ed8902
Lillian Bitner <lillian.bitner@contentful.com>
2024-09-25 09:20:49 -0600

    fix: resolve height and width issues of step and adjust left most line

--- packages/components/progress-stepper/src/Step/Step.styles.ts
+++ packages/components/progress-stepper/src/Step/Step.styles.ts
@@ -10,12 +10,13 @@ export const getStyles = () => {
         gridTemplateRows: '40px 1fr',
         placeItems: 'center',
         textAlign: 'center',
-        minWidth: '4rem',
+        // this is from original code of Daisy UI but I believe is not ideal
+        // minWidth: '4rem',
 
-        '&::before': {
+        ':not(:first-child):before': {
           gridColumnStart: 1,
           gridRowStart: 1,
-          height: '0.5rem',
+          height: '2px',
           width: '100%',
           content: '""',
           marginInlineStart: '-100%',
@@ -23,19 +24,19 @@ export const getStyles = () => {
         },
 
         '&::after': {
-          // for numbered steps
-          // content: 'counter(step)',
-          // counterIncrement: 'step',
+          //for numbered steps
+          content: 'counter(step)',
+          counterIncrement: 'step',
 
           // for icon steps
-          content: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='%23000000' viewBox='0 0 256 256'%3E%3Cpath d='M232,104a56.06,56.06,0,0,0-56-56H136a24,24,0,0,1,24-24,8,8,0,0,0,0-16,40,40,0,0,0-40,40H80a56.06,56.06,0,0,0-56,56,16,16,0,0,0,8,13.83V128c0,35.53,33.12,62.12,59.74,83.49C103.66,221.07,120,234.18,120,240a8,8,0,0,0,16,0c0-5.82,16.34-18.93,28.26-28.51C190.88,190.12,224,163.53,224,128V117.83A16,16,0,0,0,232,104ZM80,64h96a40.06,40.06,0,0,1,40,40H40A40,40,0,0,1,80,64Zm74.25,135c-10.62,8.52-20,16-26.25,23.37-6.25-7.32-15.63-14.85-26.25-23.37C77.8,179.79,48,155.86,48,128v-8H208v8C208,155.86,178.2,179.79,154.25,199Z'%3E%3C/path%3E%3C/svg%3E")`,
+          // content: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='%23000000' viewBox='0 0 256 256'%3E%3Cpath d='M232,104a56.06,56.06,0,0,0-56-56H136a24,24,0,0,1,24-24,8,8,0,0,0,0-16,40,40,0,0,0-40,40H80a56.06,56.06,0,0,0-56,56,16,16,0,0,0,8,13.83V128c0,35.53,33.12,62.12,59.74,83.49C103.66,221.07,120,234.18,120,240a8,8,0,0,0,16,0c0-5.82,16.34-18.93,28.26-28.51C190.88,190.12,224,163.53,224,128V117.83A16,16,0,0,0,232,104ZM80,64h96a40.06,40.06,0,0,1,40,40H40A40,40,0,0,1,80,64Zm74.25,135c-10.62,8.52-20,16-26.25,23.37-6.25-7.32-15.63-14.85-26.25-23.37C77.8,179.79,48,155.86,48,128v-8H208v8C208,155.86,178.2,179.79,154.25,199Z'%3E%3C/path%3E%3C/svg%3E")`,
           zIndex: 1,
           position: 'relative',
           gridColumnStart: 1,
           gridRowStart: 1,
           display: 'grid',
-          height: '2rem',
-          width: '2rem',
+          height: '24px',
+          width: '24px',
           placeItems: 'center',
           placeSelf: 'center',
           borderRadius: '9999px',
