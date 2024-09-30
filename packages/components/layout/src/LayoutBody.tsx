79811627dab24c60d58d1fd9e93a3114bf5fcf75
Kathrin Holzmann <Lelith@users.noreply.github.com>
2024-09-30 16:45:08 +0200

    fix: avoid necessity for overwriting height and scrolling behavior  [CFISO-1861] (#2859)
    * docs: show different layout variations
    
    * docs: improve storybook examples
    
    * docs: extend examples
    
    * refactor: layout component html structure and styling
    
    * refactor: use grid instead of flexbox for column arrangement
    
    * refactor: change scroll container for narrow variant
    
    * refactor: only scroll content, never header
    
    * chore: optimize code for sidebar
    
    * chore: import sizes from dependent component packages
    
    * fix: adjust size for border
    
    * style: adjust outer spacing for smaller screens
    
    style: adjust outer spacing for smaller screens
    
    * chore: fix ci build run
    
    * Update packages/components/layout/src/Layout.styles.ts
    
    Co-authored-by: Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>
    
    * Update packages/components/layout/src/Layout.styles.ts
    
    Co-authored-by: Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>
    
    * Update packages/components/layout/src/Layout.styles.ts
    
    Co-authored-by: Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>
    
    * feat: forward overwriting styles to main container
    
    * docs: add comment to code
    
    * fix: reset package.json
    
    * chore: update package-lock
    
    * feat: make offsetTop a overwritable property
    fix: story import
    
    * chore: bump to reflect current published version
    
    * refactor: define default top offset to navbar height
    
    * feat: introduce sidebar variants
    
    ---------
    
    Co-authored-by: Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>
    Co-authored-by: Rémy Lenoir <remy.lenoir@contentful.com>

--- packages/components/layout/src/LayoutBody.tsx
+++ packages/components/layout/src/LayoutBody.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef, type HTMLAttributes, type Ref } from 'react';
 import { cx } from 'emotion';
-import { type CommonProps, Box, Flex } from '@contentful/f36-core';
-import { getLayoutBodyStyles } from './LayoutBody.styles';
+import { type CommonProps, Box } from '@contentful/f36-core';
+import { getLayoutBodyStyles } from './Layout.styles';
 import { useLayoutContext } from './LayoutContext';
 
 export type LayoutBodyProps = {
@@ -16,21 +16,25 @@ const _LayoutBody = (props: LayoutBodyProps, ref: Ref<HTMLDivElement>) => {
     testId = 'cf-layout-body',
     ...otherProps
   } = props;
-  const { variant } = useLayoutContext();
-  const styles = getLayoutBodyStyles(variant);
+  const { variant, withHeader, offsetTop } = useLayoutContext();
+  const styles = getLayoutBodyStyles(withHeader, offsetTop);
 
   return (
-    <Flex className={styles.root} justifyContent="center">
-      <Box
-        {...otherProps}
-        as="div"
-        ref={ref}
-        className={cx(styles.mainContainer, className)}
-        testId={testId}
-      >
-        {children}
-      </Box>
-    </Flex>
+    <Box
+      {...otherProps}
+      as="div"
+      ref={ref}
+      className={cx(styles.layoutBodyContainer, className)}
+      testId={testId}
+    >
+      {variant === 'narrow' ? (
+        <Box as="div" className={styles.layoutBodyInner}>
+          {children}
+        </Box>
+      ) : (
+        children
+      )}
+    </Box>
   );
 };
 
