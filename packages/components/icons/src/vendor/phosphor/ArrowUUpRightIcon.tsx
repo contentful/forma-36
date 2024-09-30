574cf09f273d6570efcea4cb90da62aa8452ab24
Bohdan Hutsol <bohdan.hutsol@contentful.com>
2024-09-30 18:19:03 +0200

    feat: add `ArrowUUpLeftIcon` and `ArrowUUpRightIcon` icons (#2895)
    * feat: add new ArrowUUpLeftIcon and ArrowUUpRightIcon icons
    
    * chore: bump f36-icons package

---
+++ packages/components/icons/src/vendor/phosphor/ArrowUUpRightIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon-alpha';
+import { ArrowUUpRight } from '@phosphor-icons/react';
+
+export const ArrowUUpRightIcon = generateForma36Icon(ArrowUUpRight);
