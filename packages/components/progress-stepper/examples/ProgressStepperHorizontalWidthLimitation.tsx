a38356225cce8a80b3c8ca29035c38b6803844b7
Lisa White <lisa.white@contentful.com>
2024-10-29 15:53:15 -0600

    docs(progress-stepper): add horizontal width limitation example

---
+++ packages/components/progress-stepper/examples/ProgressStepperHorizontalWidthLimitation.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ProgressStepper } from '@contentful/f36-progress-stepper';
+import { Box, Header } from '@contentful/f36-components';
+import tokens from '@contentful/f36-tokens';
+import { css } from 'emotion';
+
+export default function ProgressStepperHorizontalWidthLimitation() {
+  const styles = {
+    root: css({
+      border: `1px solid ${tokens.gray300}`,
+    }),
+    stepperContainer: css({
+      padding: `${tokens.spacingS} 0`,
+    }),
+    headerContainer: css({
+      borderTop: `1px solid ${tokens.gray300}`,
+    }),
+  };
+
+  return (
+    <Box className={styles.root}>
+      <Box className={styles.stepperContainer}>
+        <ProgressStepper
+          activeStep={2}
+          ariaLabel="Horizontal progress stepper width limitation example"
+        >
+          <ProgressStepper.Step state="complete" labelText="Label 1" />
+          <ProgressStepper.Step state="complete" labelText="Label 2" />
+          <ProgressStepper.Step state="active" labelText="Label 3" />
+          <ProgressStepper.Step labelText="Label 4" />
+          <ProgressStepper.Step labelText="Label 5" />
+        </ProgressStepper>
+      </Box>
+      <Box className={styles.headerContainer}>
+        <Header title="Header within the same parent container" />
+      </Box>
+    </Box>
+  );
+}
