730372f84d6fc06455c3d33d75c5620ff0d37b84
Kathrin Holzmann <lelith@gmx.de>
2025-06-11 16:51:43 +0200

    feat: update navbar utility styles to not use emotion serialize

--- packages/components/navbar/src/utils.styles.ts
+++ packages/components/navbar/src/utils.styles.ts
@@ -1,5 +1,5 @@
 import tokens from '@contentful/f36-tokens';
-import type { CSSObject } from '@emotion/serialize';
+import { css } from 'emotion';
 
 type screens = 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
 type mediaqueries = Record<screens, string>;
@@ -12,31 +12,31 @@ export const mqs: mediaqueries = {
   xlarge: '@media (min-width: 1920px)',
 };
 
-export const getGlowOnFocusStyles = (
-  shadow: string = tokens.glowPrimary,
-): CSSObject => ({
-  '&:focus': {
-    boxShadow: shadow,
-  },
-  '&:focus:not(:focus-visible)': {
-    boxShadow: 'unset',
-  },
-  '&:focus-visible': {
-    boxShadow: shadow,
-  },
-});
+export const getGlowOnFocusStyles = (shadow: string = tokens.glowPrimary) =>
+  css({
+    '&:focus': {
+      boxShadow: shadow,
+    },
+    '&:focus:not(:focus-visible)': {
+      boxShadow: 'unset',
+    },
+    '&:focus-visible': {
+      boxShadow: shadow,
+    },
+  });
 
-export const increaseHitArea = (minSize = '44px'): CSSObject => ({
-  overflow: 'visible',
-  position: 'relative',
-  '&:after': {
-    minHeight: minSize,
-    minWidth: minSize,
-    position: 'absolute',
-    width: '100%',
-    content: '""',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-  },
-});
+export const increaseHitArea = (minSize = '44px') =>
+  css({
+    overflow: 'visible',
+    position: 'relative',
+    '&:after': {
+      minHeight: minSize,
+      minWidth: minSize,
+      position: 'absolute',
+      width: '100%',
+      content: '""',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  });
