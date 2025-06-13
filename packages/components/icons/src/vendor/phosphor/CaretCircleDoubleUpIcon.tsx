9b99640ed6a59cb4240cadd80138a5a8662cf90e
cemreyuksel <cemre.yuksel@contentful.com>
2025-06-13 17:56:45 +0200

    feat: add new icons from phosphoricons lib

---
+++ packages/components/icons/src/vendor/phosphor/CaretCircleDoubleUpIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon-alpha';
+import { CaretCircleDoubleUp } from '@phosphor-icons/react';
+
+export const CaretCircleDoubleUpIcon = generateForma36Icon(CaretCircleDoubleUp);
