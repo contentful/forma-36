731063c5a74cc6e9d4f1c3d41f4ee2cec4466ab1
cemreyuksel <cemre.yuksel@contentful.com>
2025-06-17 11:55:02 +0200

    refactor:  keep the name and casing following Phosphor

--- packages/components/icons/src/vendor/phosphor/LightbulbIcon.tsx
+++ packages/components/icons/src/vendor/phosphor/LightbulbIcon.tsx
@@ -1,4 +1,4 @@
 import { generateForma36Icon } from '@contentful/f36-icon-alpha';
 import { Lightbulb } from '@phosphor-icons/react';
 
-export const LightBulbIcon = generateForma36Icon(Lightbulb);
+export const LightbulbIcon = generateForma36Icon(Lightbulb);
