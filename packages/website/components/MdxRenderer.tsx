ac12b89d0b9f51b23b20775bed746b60cb6edaf0
Nkoyo Ating <atingenkay@gmail.com>
2025-07-03 09:33:07 +0200

    chore: remove unused package

--- packages/website/components/MdxRenderer.tsx
+++ packages/website/components/MdxRenderer.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 
 import * as f36Components from '@contentful/f36-components';
-import * as f36Icons from '@contentful/f36-icons';
 import NextLink from 'next/link';
 import { MdxComponents } from '../mdx-components';
 import { ComponentSource } from './LiveEditor/ComponentSource';
@@ -51,7 +50,6 @@ const components = {
   td: (props) => <Table.Cell {...props} />,
   // eslint-disable-next-line jsx-a11y/alt-text
   img: (props) => <img style={{ width: '100%' }} {...props} />,
-  ...f36Icons,
   ...f36Components,
   ...MdxComponents,
 };
