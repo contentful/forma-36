e350fbb6b6a8da6a39e1d24e21983a7e0d4f7420
Kathrin Holzmann <lelith@gmx.de>
2025-07-03 10:32:15 +0200

    feat: integrate navbar package for documentation website

--- packages/website/components/MdxRenderer.tsx
+++ packages/website/components/MdxRenderer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 
 import * as f36Components from '@contentful/f36-components';
+import * as f36Navbar from '@contentful/f36-navbar';
 import NextLink from 'next/link';
 import { MdxComponents } from '../mdx-components';
 import { ComponentSource } from './LiveEditor/ComponentSource';
@@ -51,6 +52,7 @@ const components = {
   // eslint-disable-next-line jsx-a11y/alt-text
   img: (props) => <img style={{ width: '100%' }} {...props} />,
   ...f36Components,
+  ...f36Navbar,
   ...MdxComponents,
 };
 /* eslint-enable @next/next/no-img-element */
