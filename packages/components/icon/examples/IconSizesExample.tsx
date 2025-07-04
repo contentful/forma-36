5f4f67dc6551dd6ac04ea7efcd9eef4cc2d089d4
Rémy Lenoir <remy.lenoir@contentful.com>
2025-07-04 12:15:16 +0200

    docs: adjust examples

--- packages/components/icon/examples/IconSizesExample.tsx
+++ packages/components/icon/examples/IconSizesExample.tsx
@@ -5,13 +5,13 @@ import { Stack, Flex, Text } from '@contentful/f36-components';
 export default function IconSizesExample() {
   return (
     <Stack>
-      <Flex alignItems="center">
+      <Flex alignItems="center" gap="spacingS">
         <CalendarBlankIcon size="tiny" /> <Text>Tiny</Text>
       </Flex>
-      <Flex alignItems="center">
+      <Flex alignItems="center" gap="spacingS">
         <CalendarBlankIcon size="small" /> <Text>Small</Text>
       </Flex>
-      <Flex alignItems="center">
+      <Flex alignItems="center" gap="spacingS">
         <CalendarBlankIcon size="medium" /> <Text>Medium</Text>
       </Flex>
     </Stack>
