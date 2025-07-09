3e841fcb1bd21b483486a904a644332e05d9a8e2
Andrei Tigai <andrey.tigay@gmail.com>
2025-07-09 16:15:50 +0200

    docs: add UsageCountEntitlements example

---
+++ packages/components/usagecount/examples/UsageCountEntitlementsExample.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { UsageCount } from '@contentful/f36-usage-count';
+
+export default function UsageCountExample() {
+  return (
+    <UsageCount
+      variant="entitlement"
+      value={150}
+      valueUnit="GB"
+      quota={200}
+    ></UsageCount>
+  );
+}
