067dec776c823c45c7e92b5cf406d5452d371047
Yuri Mazursky <colomolo@users.noreply.github.com>
2025-07-18 15:23:59 +0100

    feat: add placeholder examples

---
+++ packages/components/datepicker/examples/Datepicker/Placeholder.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { Datepicker } from '@contentful/f36-components';
+
+export default function PlaceholderExample() {
+  const [selectedDay, setSelectedDay] = useState(undefined);
+
+  return <Datepicker selected={selectedDay} onSelect={setSelectedDay} placeholder="Select a date" />;
+}
