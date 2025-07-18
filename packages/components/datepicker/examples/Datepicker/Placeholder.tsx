31ccae6d5ab27615314316c2b9f543bab54eb667
Yuri Mazursky <colomolo@users.noreply.github.com>
2025-07-18 15:36:27 +0100

    chore: fix lint errors

--- packages/components/datepicker/examples/Datepicker/Placeholder.tsx
+++ packages/components/datepicker/examples/Datepicker/Placeholder.tsx
@@ -4,5 +4,11 @@ import { Datepicker } from '@contentful/f36-components';
 export default function PlaceholderExample() {
   const [selectedDay, setSelectedDay] = useState(undefined);
 
-  return <Datepicker selected={selectedDay} onSelect={setSelectedDay} placeholder="Select a date" />;
+  return (
+    <Datepicker
+      selected={selectedDay}
+      onSelect={setSelectedDay}
+      placeholder="Select a date"
+    />
+  );
 }
