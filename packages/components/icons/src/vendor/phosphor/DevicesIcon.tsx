8afc09f59e4b73f76394eb43b7d7b426ee22575d
Kathrin Holzmann <lelith@gmx.de>
2025-08-21 16:49:11 +0200

    feat: add phosphor icons to the set
    AlignBottom
    
    AlignCenterHorizontalIcon
    
    AlignLeftIcon
    
    AlignRightIcon
    
    ArrowArcLeftIcon
    
    ArrowArcRightIcon
    
    ArrowClockwiseIcon
    
    ArrowLineDownIcon
    
    ArrowLineRightIcon
    
    ArrowLineUpIcon
    
    ArrowsOutIcon
    
    ArrowsOutCardinalIcon
    
    ArrowUDownLeftIcon
    
    ArrowUDownRightIcon
    
    ArticleIcon
    
    BuildingIcon
    
    CardsIcon
    
    ChartLineIcon
    
    CheckerboardIcon
    
    ColumnsIcon
    
    ColumnsPlusRightIcon
    
    CubeIcon
    
    CurrencyEuroIcon
    
    DevicesIcon
    
    FileArchiveIcon
    
    FileImageIcon
    
    FingerprintIcon
    
    GaugeIcon
    
    GithubLogoIcon
    
    GridFourIcon
    
    HeartStraightIcon
    
    HouseIcon
    
    ImagesSquareIcon
    
    KeyholeIcon
    
    KeyReturnIcon
    
    MonitorPlayIcon
    
    PlaceholderIcon
    
    RectangleDashedIcon
    
    RowsIcon
    
    RowsPlusBottomIcon
    
    SlideshowIcon
    
    SubtitlesIcon
    
    TextAlignCenterIcon
    
    TextAlignLeftIcon
    
    TextAlignRightIcon
    
    TextAUnderlineIcon
    
    TextboxIcon
    
    TextColumnsIcon
    
    TextIndentIcon
    
    TextOutdentIcon
    
    TextStrikethroughIcon
    
    TreeViewIcon
    
    UserFocusIcon
    
    UsersThreeIcon
    
    VectorThreeIcon
    
    XSquareIcon
    
    NumberSquare from Zero to Nine
    
    feat: add phosphor icons to the set

---
+++ packages/components/icons/src/vendor/phosphor/DevicesIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon';
+import { DevicesIcon as Devices } from '@phosphor-icons/react';
+
+export const DevicesIcon = generateForma36Icon(Devices);
