105e48440127643ec9f9ed522f596d6f24247940
Kathrin Holzmann <lelith@gmx.de>
2025-09-02 12:50:50 +0200

    chore: remove usage of tooltip

--- packages/components/forms/stories/InputGroup.stories.tsx
+++ packages/components/forms/stories/InputGroup.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IconButton, Button } from '@contentful/f36-button';
 import { LockSimpleIcon } from '@contentful/f36-icons';
 import { SectionHeading } from '@contentful/f36-typography';
-import { Tooltip } from '@contentful/f36-tooltip';
+//import { Tooltip } from '@contentful/f36-tooltip';
 import { Flex } from '@contentful/f36-core';
 import { TextInput, InputGroupProps } from '../src';
 import { CopyButton } from '@contentful/f36-copybutton';
@@ -176,13 +176,20 @@ export const Overview = () => {
             id="TextInput4"
             defaultValue="Some value"
           />
-          <Tooltip content="Tooltip text">
+
+          <IconButton
+            variant="secondary"
+            icon={<LockSimpleIcon />}
+            aria-label="Lock"
+          />
+
+          {/* <Tooltip content="Tooltip text">
             <IconButton
               variant="secondary"
               icon={<LockSimpleIcon />}
               aria-label="Lock"
             />
-          </Tooltip>
+          </Tooltip> */}
         </TextInput.Group>
       </Flex>
       <SectionHeading as="h3" marginBottom="spacingS">
