c691df839a711e0d029bd4eae69bb0507b3ba861
Kathrin Holzmann <lelith@gmx.de>
2025-09-02 10:48:04 +0200

    chore: delete all components based on popper for now
    react/popper is no longer developed, reactpopper is no longer developed

--- packages/components/datepicker/examples/Datepicker/Basic.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import { Datepicker } from '@contentful/f36-components';
-
-export default function BasicExample() {
-  const [selectedDay, setSelectedDay] = useState(new Date());
-
-  return <Datepicker selected={selectedDay} onSelect={setSelectedDay} />;
-}
--- packages/components/popover/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Popover } from './CompoundPopover';
-export type { PopoverProps } from './Popover';
-export { PopoverTrigger } from './PopoverTrigger/PopoverTrigger';
-export type { PopoverTriggerProps } from './PopoverTrigger/PopoverTrigger';
-export { PopoverContent } from './PopoverContent/PopoverContent';
-export type { PopoverContentProps } from './PopoverContent/PopoverContent';
