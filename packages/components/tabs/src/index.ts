957f07e2c97ceb56be6b9e2e0f6e75dd3a66ed81
Kathrin Holzmann <lelith@gmx.de>
2025-09-02 10:54:58 +0200

    chore: remove tabs for now
    depdency radix/react-tabs

--- packages/components/tabs/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { Tabs } from './CompoundTabs';
-export { Tab } from './Tab';
-export { TabList } from './TabList';
-export { TabPanel } from './TabPanel';
-export type { TabsProps } from './Tabs';
-export type { TabProps } from './Tab';
-export type { TabListProps } from './TabList';
-export type { TabPanelProps } from './TabPanel';
