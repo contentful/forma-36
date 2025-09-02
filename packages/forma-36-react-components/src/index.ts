eba025e6e27c95839a551bbfcfa4a3aeed560c4b
Kathrin Holzmann <lelith@gmx.de>
2025-09-02 16:22:05 +0200

    chore: remove breaking tooltip references for now

--- packages/forma-36-react-components/src/index.ts
+++ packages/forma-36-react-components/src/index.ts
@@ -1,10 +1,7 @@
 export * from '@contentful/f36-accordion';
 export * from '@contentful/f36-asset';
-
-
 export * from '@contentful/f36-badge';
 export * from '@contentful/f36-button';
-
 export * from '@contentful/f36-collapse';
 export * from '@contentful/f36-copybutton';
 export * from '@contentful/f36-core';
@@ -19,21 +16,15 @@ export * from '@contentful/f36-header';
 export * from '@contentful/f36-image';
 export * from '@contentful/f36-layout';
 export * from '@contentful/f36-list';
-
-export * from '@contentful/f36-multiselect';
 export * from '@contentful/f36-navlist';
 export * from '@contentful/f36-note';
 export * from '@contentful/f36-notification';
 export * from '@contentful/f36-pagination';
-export * from '@contentful/f36-pill';
-
 export * from '@contentful/f36-progress-stepper';
 export * from '@contentful/f36-skeleton';
 export * from '@contentful/f36-spinner';
 export * from '@contentful/f36-table';
-
 export * from '@contentful/f36-text-link';
-
 export * from '@contentful/f36-typography';
 export * from '@contentful/f36-usage-count';
 export * from '@contentful/f36-usage-card';
