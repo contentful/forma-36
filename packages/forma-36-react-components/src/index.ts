8f8776eb1b3171257a2f6e18449d8dcc29943a3c
Kathrin Holzmann <lelith@gmx.de>
2025-09-02 13:06:32 +0200

    chore: remove incompatible packages for now

--- packages/forma-36-react-components/src/index.ts
+++ packages/forma-36-react-components/src/index.ts
@@ -1,17 +1,17 @@
 export * from '@contentful/f36-accordion';
 export * from '@contentful/f36-asset';
-export * from '@contentful/f36-autocomplete';
-export * from '@contentful/f36-avatar';
+
+
 export * from '@contentful/f36-badge';
 export * from '@contentful/f36-button';
-export * from '@contentful/f36-card';
+
 export * from '@contentful/f36-collapse';
 export * from '@contentful/f36-copybutton';
 export * from '@contentful/f36-core';
 export * from '@contentful/f36-datetime';
-export * from '@contentful/f36-datepicker';
+
 export * from '@contentful/f36-drag-handle';
-export * from '@contentful/f36-entity-list';
+
 export * from '@contentful/f36-empty-state';
 export * from '@contentful/f36-forms';
 export * from '@contentful/f36-icon';
@@ -19,22 +19,21 @@ export * from '@contentful/f36-header';
 export * from '@contentful/f36-image';
 export * from '@contentful/f36-layout';
 export * from '@contentful/f36-list';
-export * from '@contentful/f36-menu';
-export * from '@contentful/f36-modal';
+
 export * from '@contentful/f36-multiselect';
 export * from '@contentful/f36-navlist';
 export * from '@contentful/f36-note';
 export * from '@contentful/f36-notification';
 export * from '@contentful/f36-pagination';
 export * from '@contentful/f36-pill';
-export * from '@contentful/f36-popover';
+
 export * from '@contentful/f36-progress-stepper';
 export * from '@contentful/f36-skeleton';
 export * from '@contentful/f36-spinner';
 export * from '@contentful/f36-table';
-export * from '@contentful/f36-tabs';
+
 export * from '@contentful/f36-text-link';
-export * from '@contentful/f36-tooltip';
+
 export * from '@contentful/f36-typography';
 export * from '@contentful/f36-usage-count';
 export * from '@contentful/f36-usage-card';
