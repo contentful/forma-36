647c04fe480e6a99340c8e08e590ec8fee6d6d2d
Kathrin Holzmann <lelith@gmx.de>
2025-09-05 09:25:48 +0200

    chore: remove notification for now

--- packages/components/notification/src/NotificationItem/index.ts
+++
@@ -1,4 +0,0 @@
-export { NotificationItem } from './NotificationItem';
-export type { NotificationItemProps } from './NotificationItem';
-export { NotificationItemContainer } from './NotificationItemContainer';
-export type { NotificationItemContainerProps } from './NotificationItemContainer';
