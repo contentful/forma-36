f61477e95579b44166230b18d9b7eb112ba2eb73
Kathrin Holzmann <lelith@gmx.de>
2025-09-10 12:54:17 +0200

    test: add a11y test

--- packages/components/drag-handle/src/DragHandle.test.tsx
+++ packages/components/drag-handle/src/DragHandle.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
 
 import { DragHandle } from '.';
 
@@ -8,5 +9,11 @@ describe('DragHandle', function () {
     const tree = render(<DragHandle label="Reorder entry" />);
 
     expect(tree).toBeTruthy();
+  });
+  it('has no a11y issues', async () => {
+    const { container } = render(<DragHandle label="drag me" />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
   });
 });
