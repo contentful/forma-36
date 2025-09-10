cb05618915b61e1ed6f74b380c76ca8d3b6cfe18
Kathrin Holzmann <lelith@gmx.de>
2025-09-10 13:02:22 +0200

    chore: update button import in test

--- packages/components/header/src/Segmentation.test.tsx
+++ packages/components/header/src/Segmentation.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Segmentation } from './Segmentation';
-import { Button } from '@contentful/f36-components';
+import { Button } from '@contentful/f36-button';
 
 describe('Segmentation', function () {
   it('renders text', () => {
