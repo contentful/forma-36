81139807275b5c1cc6b713503858e9248f29b234
Kathrin Holzmann <lelith@gmx.de>
2025-09-11 15:37:19 +0200

    chore: remove vitest setup
    move this for later

--- scripts/.storybook/main.ts
+++ scripts/.storybook/main.ts
@@ -1,29 +1,25 @@
-import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { StorybookConfig } from '@storybook/nextjs-vite';
 
-import { join, dirname } from "path"
+import { join, dirname } from 'path';
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
-  "stories": [
-  './docs/**/*.stories.mdx',
-  '../../packages/**/*.stories.@(ts|md)x',
-  ],
-  "addons": [
+  stories: ['./docs/**/*.stories.mdx', '../../packages/**/*.stories.@(ts|md)x'],
+  addons: [
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-docs'),
     getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest")
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
-  "framework": {
-    "name": getAbsolutePath("@storybook/nextjs-vite"),
-    "options": {}
-  }
+  framework: {
+    name: getAbsolutePath('@storybook/nextjs-vite'),
+    options: {},
+  },
 };
 export default config;
