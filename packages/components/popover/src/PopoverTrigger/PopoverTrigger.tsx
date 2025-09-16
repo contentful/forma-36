b9b937705c855b79185b817723666f91993141b3
Kathrin Holzmann <lelith@gmx.de>
2025-09-16 07:29:43 +0200

    feat: WIP replace popper with floating ui

--- packages/components/popover/src/PopoverTrigger/PopoverTrigger.tsx
+++ packages/components/popover/src/PopoverTrigger/PopoverTrigger.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import { useMergeRefs } from '@floating-ui/react';
 import { usePopoverContext } from '../PopoverContext';
 
 export interface PopoverTriggerProps {
-  children: React.ReactNode;
+  children: React.ReactElement;
 }
 
 /**
  * PopoverTrigger opens the popover. It must be an interactive element.
  */
-export const PopoverTrigger = (props: PopoverTriggerProps) => {
-  const child = React.Children.only(props.children) as any;
-  const { getTriggerProps } = usePopoverContext();
+export const PopoverTrigger = React.forwardRef<any, PopoverTriggerProps>(
+  function PopoverTrigger({ children, ...otherProps }, propRef) {
+    const context = usePopoverContext();
+    const childrenRef = (children as any).ref;
+    const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
 
-  return React.cloneElement(child, {
-    ...getTriggerProps(child.props, child.ref),
-    'aria-haspopup': child.props['aria-haspopup'] ?? 'dialog',
-  });
-};
+    if (React.isValidElement(children)) {
+      const childProps = (children as React.ReactElement).props as Record<
+        string,
+        any
+      >;
+      return React.cloneElement(
+        children,
+        context.getReferenceProps({
+          ...childProps,
+          ...otherProps,
+          ref,
+          'aria-haspopup': childProps.props['aria-haspopup'] ?? 'dialog',
+          'aria-expanded': context.isOpen ? true : false,
+        }),
+      );
+    }
+    return null;
+  },
+);
