c776fc1d6a01437e81395dd226c176a3ea26defb
Kathrin Holzmann <lelith@gmx.de>
2025-09-16 09:11:14 +0200

    chore: change server from next to vite

--- scripts/.storybook/vitest.setup.ts
+++ scripts/.storybook/vitest.setup.ts
@@ -1,5 +1,5 @@
-import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
-import { setProjectAnnotations } from '@storybook/nextjs-vite';
+import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview';
+import { setProjectAnnotations } from '@storybook/react-vite';
 import * as projectAnnotations from './preview';
 
 // This is an important step to apply the right configuration when testing your stories.
