ddf3eecb96bc7e30ee6d39a4ddea6c433689137d
Kathrin Holzmann <lelith@gmx.de>
2025-09-16 12:26:20 +0200

    feat: setup contentful theme

---
+++ scripts/.storybook/contentful-theme.ts
@@ -0,0 +1,38 @@
+import { create } from 'storybook/theming';
+import tokens from '@contentful/f36-tokens';
+
+export default create({
+  base: 'light',
+
+  // Branding information
+  brandTitle: 'Forma 36',
+  brandUrl: '/',
+  brandImage: './public/forma-logo.png',
+
+  // Color palette
+  colorPrimary: tokens.colorPrimary,
+
+  // UI
+  appBg: tokens.gray100,
+  appContentBg: tokens.colorWhite,
+  appBorderColor: tokens.gray200,
+  appBorderRadius: 4,
+
+  // Fonts
+  fontBase: tokens.fontStackPrimary,
+  fontCode: tokens.fontStackMonospace,
+
+  // Text colors
+  textColor: tokens.colorTextDark,
+  textInverseColor: tokens.colorWhite,
+
+  // Toolbar default and active colors
+  barTextColor: tokens.gray500,
+  barSelectedColor: tokens.colorPrimary,
+  barBg: tokens.colorWhite,
+  // Form colors
+  inputBg: tokens.colorWhite,
+  inputBorder: tokens.gray300,
+  inputTextColor: tokens.gray900,
+  inputBorderRadius: 2,
+});
