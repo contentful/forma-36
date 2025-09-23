137ae49cef5fc24f2271ff17d5daeff825ac1320
Kathrin Holzmann <lelith@gmx.de>
2025-09-23 09:21:43 +0200

    test: update tests

--- packages/components/forms/src/Checkbox/Checkbox.test.tsx
+++ packages/components/forms/src/Checkbox/Checkbox.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { axe } from 'jest-axe';
 
 import { Checkbox } from './Checkbox';
@@ -13,6 +14,21 @@ describe('Checkbox', function () {
     const checkbox = getByLabelText('label text');
     expect(checkbox).toBeInTheDocument();
     expect(checkbox.id).toBe('checkbox');
+  });
+
+  it('calls the onchange handler', async () => {
+    const handleChange = jest.fn();
+    const user = userEvent.setup();
+
+    const { getByLabelText } = render(
+      <Checkbox id="checkbox" onChange={handleChange}>
+        label text
+      </Checkbox>,
+    );
+
+    await user.click(getByLabelText('label text'));
+
+    expect(handleChange).toHaveBeenCalled();
   });
 
   it('has no a11y issues', async () => {
