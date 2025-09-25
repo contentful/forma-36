17195daeb05f69ce96f8a784301bfec7e2756ed0
Kathrin Holzmann <lelith@gmx.de>
2025-09-25 15:29:02 +0200

    feat: make menu work with new popover
    test: improve popover test
    
    test: reorganize menu test
    
    feat: work in progress,
    
    feat: refactor WIP
    
    feat: refactor Menu to be based on floating-ui

--- packages/components/menu/src/MenuContext.ts
+++ packages/components/menu/src/MenuContext.ts
@@ -1,37 +1,17 @@
-import React, { ComponentPropsWithRef } from 'react';
-import { MenuProps } from '.';
+import React from 'react';
+import { useMenu } from './useMenu';
 
-export type MenuContextType = {
-  isOpen: boolean;
-  menuId: string;
-  focusMenuItem: (item: HTMLElement) => void;
-  getTriggerProps: (
-    _props: ComponentPropsWithRef<'button'>,
-    _ref: React.Ref<HTMLButtonElement>,
-  ) => ComponentPropsWithRef<'button'>;
-  getMenuListProps: (
-    _props: ComponentPropsWithRef<'div'>,
-    _ref: React.Ref<HTMLDivElement>,
-  ) => ComponentPropsWithRef<'div'>;
-  getMenuItemProps: (
-    _props: ComponentPropsWithRef<'button'>,
-  ) => ComponentPropsWithRef<'button'>;
-  propsToPropagateToSubmenus: Pick<
-    MenuProps,
-    'closeOnBlur' | 'closeOnEsc' | 'closeOnSelect'
-  >;
-};
+export const MenuContext = React.createContext<ReturnType<
+  typeof useMenu
+> | null>(null);
 
-const MenuContext = React.createContext<MenuContextType | undefined>(undefined);
-
-export const useMenuContext = () => {
-  const context = React.useContext(MenuContext);
-
-  if (context === undefined) {
-    throw new Error('useMenuContext must be used within a MenuContextProvider');
+export function useMenuContext() {
+  const ctx = React.useContext(MenuContext);
+  if (!ctx) {
+    throw new Error(
+      'useMenuContext must be used within <MenuContext.Provider>',
+    );
   }
-
-  return context;
-};
-
+  return ctx;
+}
 export const MenuContextProvider = MenuContext.Provider;
