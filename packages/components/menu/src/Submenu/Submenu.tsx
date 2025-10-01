c89d0547d2652fed84f1eb68721b4f4b536763f2
Kathrin Holzmann <lelith@gmx.de>
2025-10-01 09:03:56 +0200

    feat: add deprecation warning for Submenu

---
+++ packages/components/menu/src/Submenu/Submenu.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Menu, type MenuProps } from '../Menu';
+
+/**
+ * @deprecated Submenu is deprecated. Use Menu component instead.
+ * This component will be removed in a future version.
+ *
+ * Migration guide:
+ * Replace `<Submenu>` with `<Menu>` - the API is identical.
+ */
+export const Submenu = (props: MenuProps) => {
+  if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line no-console
+    console.warn(
+      '[Submenu] This component is deprecated. Use Menu component instead. ' +
+        'Replace <Submenu> with <Menu> - the API is identical.',
+    );
+  }
+
+  return <Menu {...props} />;
+};
+
+Submenu.displayName = 'Submenu';
