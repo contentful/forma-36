51e16f5590825f994e78cc160a99f91db3bf5290
Kathrin Holzmann <lelith@gmx.de>
2025-10-01 12:09:48 +0200

    feat: enable individual sub menu placement

--- packages/components/menu/src/MenuComponent.tsx
+++ packages/components/menu/src/MenuComponent.tsx
@@ -8,8 +8,8 @@ import type { MenuProps } from './Menu';
 export const MenuComponent = React.forwardRef<
   HTMLDivElement,
   React.PropsWithChildren<MenuProps>
->(({ children, isOpen = false, defaultIsOpen = false, ...otherProps }) => {
-  const menuContext = useMenu({ isOpen, defaultIsOpen, ...otherProps });
+>(({ children, ...otherProps }) => {
+  const menuContext = useMenu({ ...otherProps });
   return (
     <MenuContextProvider value={menuContext}>
       <FloatingNode id={menuContext.nodeId}>{children}</FloatingNode>
