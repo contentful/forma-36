8ac06c1175b1ebd25702692a894a8b6271c04150
Kathrin Holzmann <lelith@gmx.de>
2025-10-10 16:14:46 +0200

    feat: cover SubmenuProps as well in test

--- packages/forma-36-codemod/transforms/__testfixtures__/v6/menu.output.tsx
+++ packages/forma-36-codemod/transforms/__testfixtures__/v6/menu.output.tsx
@@ -1,39 +1,43 @@
 import React from "react";
-import { Menu } from "@contentful/f36-menu";
+import { Menu, SubmenuTrigger, type MenuProps } from "@contentful/f36-menu";
 
 function Example (){
-  return(
-      <Menu>
-        <Menu.Trigger>
-          <button> Toggle </button>
-        </Menu.Trigger>
-        <Menu.List>
-          <Menu.Item>MenuItem 1</Menu.Item>
-          <Menu>
-            <Menu.SubmenuTrigger>Submenu Trigger</Menu.SubmenuTrigger>
-            <Menu.List>
-              <Menu.Item>Sub item 1</Menu.Item>
-              <Menu>
-                <Menu.SubmenuTrigger>Sub-Submenu</Menu.SubmenuTrigger>
-                <Menu.List>
-                  <Menu.Item>Sub item 1</Menu.Item>
-                  <Menu.Item>Sub item 2</Menu.Item>
-                </Menu.List>
-              </Menu>
-              <Menu.Item>Sub item 3</Menu.Item>
-            </Menu.List>
-          </Menu>
-          <Menu.Item>MenuItem 2</Menu.Item>
-          <Menu>
-            <Menu.SubmenuTrigger>SubMenu 2</Menu.SubmenuTrigger>
-            <Menu.List>
-              <Menu.Item>Sub item 1</Menu.Item>
-              <Menu.Item>Sub item 2</Menu.Item>
-              <Menu.Item>Sub item 3</Menu.Item>
-            </Menu.List>
-          </Menu>
-        </Menu.List>
-      </Menu>
+  const submenuProps: MenuProps = {
+    placement: 'left',
+  }
+
+  return (
+    <Menu>
+      <Menu.Trigger>
+        <button> Toggle </button>
+      </Menu.Trigger>
+      <Menu.List>
+        <Menu.Item>MenuItem 1</Menu.Item>
+        <Menu {...submenuProps}>
+          <Menu.SubmenuTrigger>Submenu Trigger</Menu.SubmenuTrigger>
+          <Menu.List>
+            <Menu.Item>Sub item 1</Menu.Item>
+            <Menu>
+              <Menu.SubmenuTrigger>Sub-Submenu</Menu.SubmenuTrigger>
+              <Menu.List>
+                <Menu.Item>Sub item 1</Menu.Item>
+                <Menu.Item>Sub item 2</Menu.Item>
+              </Menu.List>
+            </Menu>
+            <Menu.Item>Sub item 3</Menu.Item>
+          </Menu.List>
+        </Menu>
+        <Menu.Item>MenuItem 2</Menu.Item>
+        <Menu>
+          <SubmenuTrigger>SubMenu 2</SubmenuTrigger>
+          <Menu.List>
+            <Menu.Item>Sub item 1</Menu.Item>
+            <Menu.Item>Sub item 2</Menu.Item>
+            <Menu.Item>Sub item 3</Menu.Item>
+          </Menu.List>
+        </Menu>
+      </Menu.List>
+    </Menu>
   );
 }
 
