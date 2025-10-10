2092d8792c39ef723883bba155d8321340bf0088
Kathrin Holzmann <lelith@gmx.de>
2025-10-10 16:16:04 +0200

    feat: add codemod for migrating deprecated Submenu with Menu

--- packages/forma-36-codemod/transforms/__tests__/v6.test.js
+++ packages/forma-36-codemod/transforms/__tests__/v6.test.js
@@ -2,7 +2,7 @@
 const defineTest = require('jscodeshift/dist/testUtils').defineTest;
 
 describe('v6 codemods', () => {
-  const tests = ['v6/skeleton', 'v6/popover'];
+  const tests = ['v6/skeleton', 'v6/popover', 'v6/menu'];
   beforeEach(() => {
     // Silence warnings we show on the transforms for cleaner tests
     // Comment this out if you want to check the warning messages
