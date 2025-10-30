e7abfa0442b2bd282726f9039648862cd7a4e523
damann <67584870+damann@users.noreply.github.com>
2025-10-30 11:48:31 +0100

    Update border-radius-semantic.js

--- packages/forma-36-tokens/src/tokens/border-radius/border-radius-semantic.js
+++ packages/forma-36-tokens/src/tokens/border-radius/border-radius-semantic.js
@@ -1,5 +1,13 @@
-const borderRadius = {
-  'name': 'value',
+const shapeControlBorderRadiusSmall = require('./colors-orange');
+const colorsRed = require('./colors-red');
+const colorsGreen = require('./colors-green');
+const colorsBlue = require('./colors-blue');
+
+const colorsSemantic = {
+  'color-primary': colorsBlue['blue-600'],
+  'color-positive': colorsGreen['green-600'],
+  'color-negative': colorsRed['red-600'],
+  'color-warning': colorsOrange['orange-500'],
 };
 
-module.exports = borderRadiusSemantic;
+module.exports = colorsSemantic;
