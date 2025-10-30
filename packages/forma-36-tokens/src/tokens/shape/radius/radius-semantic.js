ba2bf3cc34e46d81da950947f0bdcc38750f009c
damann <67584870+damann@users.noreply.github.com>
2025-10-30 12:29:32 +0100

    Update and rename packages/forma-36-tokens/src/tokens/radius/shape-radius-semantic.js to packages/forma-36-tokens/src/tokens/shape/radius/radius-semantic.js

---
+++ packages/forma-36-tokens/src/tokens/shape/radius/radius-semantic.js
@@ -0,0 +1,10 @@
+const scaleRadius = require('./scale-radius');
+
+
+const radiusSemantic = {
+  'shape-radius-control-small': scale-radius['radius-s'],
+  'shape-radius-control-medium': scale-radius['radius-m'],
+  'shape-radius-control-full': scale-radius['radius-full'],
+};
+
+module.exports = radiusSemantic;
