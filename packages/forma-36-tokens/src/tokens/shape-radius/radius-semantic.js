803247810f63f0eea1d270fd5560b79883754a2e
damann <67584870+damann@users.noreply.github.com>
2025-10-30 14:22:31 +0100

    Update radius-semantic.js

--- packages/forma-36-tokens/src/tokens/shape-radius/radius-semantic.js
+++ packages/forma-36-tokens/src/tokens/shape-radius/radius-semantic.js
@@ -5,7 +5,8 @@ const radiusSemantic = {
   'shape-radius-control-small': scale-radius['radius-s'],
   'shape-radius-control-medium': scale-radius['radius-m'],
   'shape-radius-control-full': scale-radius['radius-full'],
-   'shape-radius-container-small': scale-radius['radius-l'],
+  'shape-radius-container-extrasmall': scale-radius['radius-m'],
+  'shape-radius-container-small': scale-radius['radius-l'],
   'shape-radius-control-medium': scale-radius['radius-xl'],
   'shape-radius-control-full': scale-radius['radius-full'],
 };
