f8687152826d25a006c4c8b25d6221c1179b2408
damann <67584870+damann@users.noreply.github.com>
2025-10-30 11:58:39 +0100

    Update border-radius.js

--- packages/forma-36-tokens/src/tokens/border-radius/border-radius.js
+++ packages/forma-36-tokens/src/tokens/border-radius/border-radius.js
@@ -1,8 +1,8 @@
-const borderRadius = {
+const scaleRadius = {
   'border-radius-s': '4px',
   'border-radius-m': '8px',
   'border-radius-l': '12px',
   'border-radius-xl': '16px',
 };
 
-module.exports = borderRadius;
+module.exports = scaleRadius;
