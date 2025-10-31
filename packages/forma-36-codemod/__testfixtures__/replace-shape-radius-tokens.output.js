517e802b0667079d94b5fe688d85ec862a6a8679
damann <67584870+damann@users.noreply.github.com>
2025-10-31 13:53:03 +0100

    Update replace-shape-radius-tokens.output.js

--- packages/forma-36-codemod/__testfixtures__/replace-shape-radius-tokens.output.js
+++ packages/forma-36-codemod/__testfixtures__/replace-shape-radius-tokens.output.js
@@ -1,4 +1,4 @@
-import { radius100, radius200, radius300 } from '@contentful/f36-tokens';
+import { radius100, radius200} from '@contentful/f36-tokens';
 
 const small = radius100;
 const medium = radius200;
