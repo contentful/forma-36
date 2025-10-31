d78abef7defc556e7e99ac5bdfaee17d1c853337
damann <67584870+damann@users.noreply.github.com>
2025-10-31 09:37:59 +0100

    Update radius-semantic.js

--- packages/forma-36-tokens/src/tokens/shape-radius/radius-semantic.js
+++ packages/forma-36-tokens/src/tokens/shape-radius/radius-semantic.js
@@ -2,15 +2,15 @@ const scaleRadius = require('./scale-radius');
 
 const radiusSemantic = {
   // Control radii
-  'shape-radius-control-small': scaleRadius['radius-100'],   // 4px
-  'shape-radius-control-medium': scaleRadius['radius-200'],  // 8px
-  'shape-radius-control-pill': scaleRadius['radius-full'],   // 999px
+  'shape-radius-control-small': scaleRadius['radius-100'],
+  'shape-radius-control-medium': scaleRadius['radius-200'],
+  'shape-radius-control-pill': scaleRadius['radius-full'],
 
   // Container radii
-  'shape-radius-container-tight': scaleRadius['radius-200'],  // 8px
-  'shape-radius-container-small': scaleRadius['radius-300'],  // 12px
-  'shape-radius-container-medium': scaleRadius['radius-400'], // 16px
-  'shape-radius-container-full': scaleRadius['radius-full'],  // 999px
+  'shape-radius-container-tight': scaleRadius['radius-200'],
+  'shape-radius-container-small': scaleRadius['radius-300'],
+  'shape-radius-container-medium': scaleRadius['radius-400'],
+  'shape-radius-container-full': scaleRadius['radius-full'],
 };
 
 module.exports = radiusSemantic;
