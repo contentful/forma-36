f0ac832be603710512a106d62670acd77ad5f2d9
damann <67584870+damann@users.noreply.github.com>
2025-10-31 10:25:13 +0100

    Update replace-radius-tokens.test.ts

--- packages/forma-36-codemod/jest/replace-radius-tokens.test.ts
+++ packages/forma-36-codemod/jest/replace-radius-tokens.test.ts
@@ -2,14 +2,11 @@ const defineTest = require('jscodeshift/dist/testUtils').defineTest;
 
 /**
  * Runs the test fixture for the replace-shape-radius-tokens transform.
- * 
- * This will look for matching files under:
- * __testfixtures__/replace-shape-radius-tokens/basic.input.js
- * __testfixtures__/replace-shape-radius-tokens/basic.output.js
+ * This assumes a flat test fixture structure under __testfixtures__/.
  */
 defineTest(
   __dirname,
   'replace-shape-radius-tokens',
   null,
-  'replace-shape-radius-tokens/basic',
+  'replace-shape-radius-tokens',
 );
