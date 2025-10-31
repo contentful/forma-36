ca24d4d3cfd462d5dff474f391efc41c6c4a10aa
damann <67584870+damann@users.noreply.github.com>
2025-10-31 10:25:47 +0100

    Update replace-shape-radius-tokens.input.js

--- packages/forma-36-codemod/__testfixtures__/replace-shape-radius-tokens.input.js
+++ packages/forma-36-codemod/__testfixtures__/replace-shape-radius-tokens.input.js
@@ -1,1 +1,5 @@
+import { borderRadiusSmall, borderRadiusMedium, borderRadiusLarge } from '@contentful/f36-tokens';
 
+const small = borderRadiusSmall;
+const medium = borderRadiusMedium;
+const large = borderRadiusLarge;
