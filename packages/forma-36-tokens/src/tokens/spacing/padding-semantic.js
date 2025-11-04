4429ab4f87d4b6f5e231f049c5d3d80bc2f98172
damann <67584870+damann@users.noreply.github.com>
2025-11-04 09:57:58 +0100

    Update padding-semantic.js

--- packages/forma-36-tokens/src/tokens/spacing/padding-semantic.js
+++ packages/forma-36-tokens/src/tokens/spacing/padding-semantic.js
@@ -6,6 +6,15 @@ const paddingSemantic = {
 
   // Container paddings
   'space-container-padding-horizontal-tight': scaleSpace['space-100'],
+  'space-container-padding-horizontal-small': scaleSpace['space-200'],
+  'space-container-padding-horizontal-medium': scaleSpace['space-300'],
+  'space-container-padding-horizontal-large': scaleSpace['space-400'],
+  'space-container-padding-horizontal-xlarge': scaleSpace['space-500'],
+  'space-container-padding-horizontal-xxlarge': scaleSpace['space-600'],
+  'space-container-padding-vertical-tight': scaleSpace['space-100'],
+  'space-container-padding-vertical-small': scaleSpace['space-200'],
+  'space-container-padding-vertical-medium': scaleSpace['space-300'],
+  'space-container-padding-vertical-large': scaleSpace['space-400'],
 };
 
 module.exports = paddingSemantic;
