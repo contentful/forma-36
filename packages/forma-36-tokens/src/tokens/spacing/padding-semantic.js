6247fe1e311585c15c3d063b0885591038d6d662
damann <67584870+damann@users.noreply.github.com>
2025-11-04 09:50:37 +0100

    Update and rename padding.js to padding-semantic.js

--- packages/forma-36-tokens/src/tokens/spacing/padding-semantic.js
+++ packages/forma-36-tokens/src/tokens/spacing/padding-semantic.js
@@ -2,10 +2,10 @@ const scaleSpace = require('./space-scale');
 
 const paddingSemantic = {
   // Control paddings
-  'space-padding-horizontal-control-small': scaleSpace['space-100'],
+ 
 
   // Container paddings
-  'space-container-padding-horizontal-tight': scaleSpace['space-400'],
+  'space-container-padding-horizontal-tight': scaleSpace['space-100'],
 };
 
 module.exports = paddingSemantic;
