0ffa2a19bdc54ed2350ea0789eb88990f40d4fcb
damann <67584870+damann@users.noreply.github.com>
2025-11-04 08:19:51 +0100

    Update padding.js

--- packages/forma-36-tokens/src/tokens/spacing/padding.js
+++ packages/forma-36-tokens/src/tokens/spacing/padding.js
@@ -3,14 +3,9 @@ const scaleSpacing = require('./spacing-scale');
 const paddingSemantic = {
   // Control paddings
   'shape-radius-control-small': scaleRadius['radius-100'],
-  'shape-radius-control-medium': scaleRadius['radius-200'],
-  'shape-radius-control-pill': scaleRadius['radius-full'],
 
   // Container paddings
-  'shape-radius-container-tight': scaleRadius['radius-200'],
-  'shape-radius-container-small': scaleRadius['radius-300'],
-  'shape-radius-container-medium': scaleRadius['radius-400'],
-  'shape-radius-container-full': scaleRadius['radius-full'],
+  'spacing-padding-horizontal-container-tight': scaleSpacing['spacing-400'],
 };
 
 module.exports = paddingSemantic;
