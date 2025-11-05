5ea2ca7f767d1c8881eb0ef5929f6959f5b2e1b8
Nkoyo Ating <atingenkay@gmail.com>
2025-11-05 13:05:38 +0100

    feat: update website package

---
+++ packages/website/components/DocSearch.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+
+import { TextInput } from '@contentful/f36-components';
+import { MagnifyingGlassIcon } from '@contentful/f36-icons';
+
+export const DocSearch = () => {
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore ignore a complaint about dynamic import
+    import('docsearch.js').then((docsearch) => {
+      try {
+        docsearch.default({
+          // The key is added here only give access to searching the public content of the website https://docsearch.algolia.com/docs/what-is-docsearch
+          // You can even check Forma 36's configuration in DocSearch's repo https://github.com/algolia/docsearch-configs/blob/main/configs/contentful_forma-36.json
+          appId: process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID || '',
+          apiKey: process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY || '',
+          indexName: process.env.NEXT_PUBLIC_DOCSEARCH_INDEX_NAME || '',
+          inputSelector: '#search',
+          handleSelected: (_input, _event, suggestion) => {
+            if (process.env.NODE_ENV !== 'production') {
+              const path = suggestion.url.replace(/.*\.com/, '');
+              window.location.assign(`${window.location.origin}${path}`);
+            } else {
+              window.location.assign(suggestion.url);
+            }
+          },
+        });
+      } catch (e) {
+        // eslint-disable-next-line no-console -- Allow this error
+        console.error('Failed to initialize Algolia search', e);
+      }
+    });
+  }, []);
+
+  return (
+    <TextInput
+      id="search"
+      icon={<MagnifyingGlassIcon />}
+      name="search"
+      type="text"
+      placeholder="Search the docs"
+    />
+  );
+};
---
+++ packages/website/components/MdxRenderer.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote';
+
+import * as f36Components from '@contentful/f36-components';
+import * as f36Icons from '@contentful/f36-icons';
+
+import NextLink from 'next/link';
+import { MdxComponents } from '../mdx-components';
+import { ComponentSource } from './LiveEditor/ComponentSource';
+import { StaticSource } from './LiveEditor/StaticSource';
+import tokens from '@contentful/f36-tokens';
+import { CustomHeading2 } from './CustomHeading2';
+
+const {
+  DisplayText,
+  Subheading,
+  Paragraph,
+  Text,
+  TextLink,
+  List,
+  Table,
+  Heading,
+  Stack,
+  Note,
+} = f36Components;
+
+const components = {
+  h1: (props) => <DisplayText as="h1" {...props} />,
+  // to cover a specific case with "Props (API reference)"
+  h2: CustomHeading2,
+  h3: (props) => <Subheading as="h3" marginTop="spacingXl" {...props} />,
+  h4: (props) => <Subheading as="h4" {...props} />,
+  h5: (props) => <Subheading as="h5" {...props} />,
+  h6: (props) => <Subheading as="h6" {...props} />,
+  Heading,
+  TextLink,
+  Note,
+  p: (props) => <Paragraph {...props} />,
+  strong: (props) => <Text fontWeight="fontWeightDemiBold" {...props} />,
+  a: (props) => {
+    if (props.href && props.href.startsWith('..')) {
+      return (
+        <NextLink href={props.href}>
+          <TextLink {...props} />
+        </NextLink>
+      );
+    }
+    return <TextLink {...props} />;
+  },
+  Stack,
+  ul: (props) => <List style={{ marginBottom: tokens.spacingM }} {...props} />,
+  li: (props) => <List.Item {...props} />,
+  code: (props) => {
+    // Inline code will never be inside <pre>
+    if (props.parentName !== 'pre') {
+      return (
+        <code
+          style={{
+            fontFamily: tokens.fontStackMonospace,
+            backgroundColor: tokens.gray100,
+            color: tokens.gray800,
+            padding: `${tokens.spacing2Xs} ${tokens.spacingXs}`,
+            borderRadius: tokens.borderRadiusSmall,
+            fontSize: tokens.fontSizeS,
+          }}
+          {...props}
+        />
+      );
+    }
+
+    // If it's inside <pre>, MDX will render via `pre` mapping below
+    return <code {...props} />;
+  },
+  pre: (props) => {
+    const codeElement = props.children;
+    const {
+      className = '',
+      static: isStatic,
+      file,
+      children,
+    } = codeElement.props;
+
+    const code = Array.isArray(children) ? children.join('') : children;
+    const language = className.replace('language-', '');
+
+    if (isStatic) {
+      return <StaticSource code={code} language={language} />;
+    }
+
+    return <ComponentSource code={code} file={file} />;
+  },
+  table: (props) => <Table {...props} />,
+  thead: (props) => <Table.Head {...props} />,
+  tbody: (props) => <Table.Body {...props} />,
+  tr: (props) => <Table.Row {...props} />,
+  th: (props) => <Table.Cell style={{ textAlign: 'left' }} {...props} />,
+  td: (props) => <Table.Cell {...props} />,
+  // eslint-disable-next-line jsx-a11y/alt-text
+  img: (props) => <img style={{ width: '100%' }} {...props} />,
+  ...f36Icons,
+
+  ...MdxComponents,
+};
+
+export function MdxRenderer(props: {
+  source: MDXRemoteSerializeResult<Record<string, unknown>>;
+}) {
+  return <MDXRemote {...props.source} components={components} />;
+}
