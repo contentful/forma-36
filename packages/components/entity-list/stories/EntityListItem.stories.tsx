3b5bd2d4ea8a12b2d6c8a76183996e300509402f
Rebecca König <rebecca.koenig@contentful.com>
2025-11-06 18:17:06 +0100

    chore: read entity list item thumbnail

--- packages/components/entity-list/stories/EntityListItem.stories.tsx
+++ packages/components/entity-list/stories/EntityListItem.stories.tsx
@@ -33,6 +33,8 @@ Basic.args = {
   withThumbnail: true,
   entityType: 'Experience',
   status: 'published',
+  thumbnailUrl:
+    'https://images.ctfassets.net/iq4lnigp6fgt/72KhxI84kw1SE9gP8gDp7R/c5fa24bdc295a318018aea0ca46e2de8/forma-36-storybook-asset.png?fit=fill&f=top_left&w=400&h=400',
 };
 
 export const WithDragHandle = (args) => (
