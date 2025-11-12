e6c38d58db90bc0443dc0644fb9495f519474881
Nkoyo Ating <atingenkay@gmail.com>
2025-11-12 09:20:30 +0100

    feat: update sandpack styles and website dependency

--- packages/website/next-env.d.ts
+++ packages/website/next-env.d.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference path="./.next/types/routes.d.ts" />
