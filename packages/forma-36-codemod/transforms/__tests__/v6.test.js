4712b6ad1d219e3b55b11598c0ae1cae78582119
Nkoyo Ating <atingenkay@gmail.com>
2025-11-12 14:19:55 +0100

    chore: update test script

--- packages/forma-36-codemod/transforms/__tests__/v6.test.js
+++ packages/forma-36-codemod/transforms/__tests__/v6.test.js
@@ -2,12 +2,18 @@
 const defineTest = require('jscodeshift/dist/testUtils').defineTest;
 
 describe('v6 codemods', () => {
-  const tests = ['v6/skeleton', 'v6/popover', 'v6/menu'];
+  const tsxTests = ['v6/popover', 'v6/menu'];
+  const tsTests = ['v6/skeleton'];
   beforeEach(() => {
     // Silence warnings we show on the transforms for cleaner tests
     // Comment this out if you want to check the warning messages
     console.warn = jest.fn();
   });
 
-  tests.forEach((test) => defineTest(__dirname, test, null, `${test}`));
+  tsxTests.forEach((test) =>
+    defineTest(__dirname, test, null, `${test}`, { parser: 'tsx' }),
+  );
+  tsTests.forEach((test) =>
+    defineTest(__dirname, test, null, `${test}`, { parser: 'ts' }),
+  );
 });
