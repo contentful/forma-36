fc51875bef2989fa7bbba8d504a129a114f9f58b
Kathrin Holzmann <lelith@gmx.de>
2025-11-14 08:54:50 +0100

    feat: narrow types for generated icons

--- packages/components/icon/src/utils/generateForma36Icon.tsx
+++ packages/components/icon/src/utils/generateForma36Icon.tsx
@@ -3,21 +3,24 @@ import type { Icon } from '@phosphor-icons/react';
 import { IconVariant } from '../types.js';
 import { generateComponentWithVariants } from './generateComponentWithVariants.js';
 import { wrapPhosphorIcon } from './wrapPhosphorIcon.js';
-import { type GeneratedIconProps } from './generateIconComponent.js';
+import type { GeneratedIconProps } from './generateIconComponent';
+import type { GeneratedIconComponent } from './generateComponentWithVariants';
 
 /**
  * Helper function to generate a Forma 36 icon component from a Phosphor icon component
  */
-export function generateForma36Icon(PhosphorIcon: Icon) {
-  const Icon = wrapPhosphorIcon(PhosphorIcon);
+export function generateForma36Icon(
+  PhosphorIcon: Icon,
+): GeneratedIconComponent {
+  const Wrapped = wrapPhosphorIcon(PhosphorIcon);
 
   return generateComponentWithVariants({
     variants: {
-      [IconVariant.Active]: ({ isActive, ...props }: GeneratedIconProps) => (
-        <Icon {...props} weight="duotone" />
+      [IconVariant.Active]: (props: GeneratedIconProps) => (
+        <Wrapped {...props} weight="duotone" />
       ),
-      [IconVariant.Default]: ({ isActive, ...props }: GeneratedIconProps) => (
-        <Icon {...props} />
+      [IconVariant.Default]: (props: GeneratedIconProps) => (
+        <Wrapped {...props} />
       ),
     },
   });
