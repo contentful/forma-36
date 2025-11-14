2d5f10632946b67f52313672a8502967b59e0dad
Kathrin Holzmann <lelith@gmx.de>
2025-11-14 10:41:37 +0100

    chore: change utilities to not use double quotes when changing imports

--- packages/forma-36-codemod/transforms/v5/icons.js
+++ packages/forma-36-codemod/transforms/v5/icons.js
@@ -262,7 +262,7 @@ const updateToV5Icons = function (file, api) {
           });
 
           const { result } = addImport(j, source, [
-            j.template.statement([`import tokens from "${tokensImportName}"`]),
+            j.template.statement([`import tokens from '${tokensImportName}'`]),
           ]);
           addTokensImport = true;
 
@@ -407,7 +407,7 @@ const updateToV5Icons = function (file, api) {
 
   if (addTokensImport) {
     source = addImport(j, source, [
-      j.template.statement([`import tokens from "${tokensImportName}"`]),
+      j.template.statement([`import tokens from '${tokensImportName}'`]),
     ]).source;
   }
   if (iconProps?.localName) {
