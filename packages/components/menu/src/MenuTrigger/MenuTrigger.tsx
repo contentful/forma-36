184c4e7d9684967d7e6d2d566b916214f69cfc5e
Kathrin Holzmann <lelith@gmx.de>
2025-11-17 16:10:00 +0100

    feat: simplify MenuTrigger typing

--- packages/components/menu/src/MenuTrigger/MenuTrigger.tsx
+++ packages/components/menu/src/MenuTrigger/MenuTrigger.tsx
@@ -2,58 +2,55 @@ import React from 'react';
 import { useMergeRefs } from '@floating-ui/react';
 import { useMenuContext } from '../MenuContext';
 
-export interface MenuTriggerProps<E extends HTMLElement = HTMLElement> {
-  children: React.ReactElement<
-    Record<string, unknown> & {
-      ref?: React.Ref<E>;
-    },
-    React.ElementType
-  >;
+export interface MenuTriggerProps {
+  children: React.ReactElement;
+  onFocus?: React.FocusEventHandler<HTMLElement>;
 }
+export const MenuTrigger = React.forwardRef<HTMLElement, MenuTriggerProps>(
+  function MenuTrigger({ children, ...otherProps }, forwardedRef) {
+    const menu = useMenuContext();
+    const {
+      refs,
+      item,
+      isNested,
+      isOpen,
+      hasFocusInside,
+      setHasFocusInside,
+      parent,
+    } = menu;
 
-export const MenuTrigger = React.forwardRef<
-  HTMLElement,
-  MenuTriggerProps & { onFocus?: React.FocusEventHandler<HTMLElement> }
->(function MenuTrigger({ children, ...otherProps }, forwardedRef) {
-  const menu = useMenuContext();
-  const {
-    refs,
-    item,
-    isNested,
-    isOpen,
-    hasFocusInside,
-    setHasFocusInside,
-    parent,
-  } = menu;
+    // Existing ref on the child (if any) so we can merge it.
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const childRef: React.Ref<HTMLElement> | undefined = (children as any).ref;
 
-  // Existing ref on the child (if any) so we can merge it.
-  const childRef: React.Ref<HTMLElement> | undefined = (
-    children as unknown as { ref?: React.Ref<HTMLElement> }
-  ).ref;
+    const parentActiveIndex = parent?.activeIndex;
+    const getItemProps = parent ? parent.getItemProps : menu.getItemProps;
 
-  const parentActiveIndex = parent?.activeIndex;
-  const getItemProps = parent ? parent.getItemProps : menu.getItemProps;
+    const triggerProps = {
+      ref: useMergeRefs([refs.setReference, item.ref, forwardedRef, childRef]),
+      tabIndex: !isNested
+        ? undefined
+        : parentActiveIndex === item.index
+          ? 0
+          : -1,
+      role: isNested ? 'menuitem' : undefined,
+      'data-open': isOpen ? '' : undefined,
+      'data-nested': isNested ? '' : undefined,
+      'data-focus-inside': hasFocusInside ? '' : undefined,
+      ...menu.getReferenceProps(
+        getItemProps({
+          ...otherProps,
+          onFocus(event: React.FocusEvent<HTMLElement>) {
+            (
+              otherProps as { onFocus?: React.FocusEventHandler<HTMLElement> }
+            ).onFocus?.(event);
+            setHasFocusInside(false);
+            if (parent) parent.setHasFocusInside(true);
+          },
+        }),
+      ),
+    };
 
-  const triggerProps = {
-    ref: useMergeRefs([refs.setReference, item.ref, forwardedRef, childRef]),
-    tabIndex: !isNested ? undefined : parentActiveIndex === item.index ? 0 : -1,
-    role: isNested ? 'menuitem' : undefined,
-    'data-open': isOpen ? '' : undefined,
-    'data-nested': isNested ? '' : undefined,
-    'data-focus-inside': hasFocusInside ? '' : undefined,
-    ...menu.getReferenceProps(
-      getItemProps({
-        ...otherProps,
-        onFocus(event: React.FocusEvent<HTMLElement>) {
-          (
-            otherProps as { onFocus?: React.FocusEventHandler<HTMLElement> }
-          ).onFocus?.(event);
-          setHasFocusInside(false);
-          if (parent) parent.setHasFocusInside(true);
-        },
-      }),
-    ),
-  };
-
-  return React.cloneElement(children, triggerProps);
-});
+    return React.cloneElement(children, triggerProps);
+  },
+);
