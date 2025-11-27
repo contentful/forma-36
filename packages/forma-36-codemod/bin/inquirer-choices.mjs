30173308d0d9a4d4805298a1088de0142535c2e8
Kathrin Holzmann <lelith@gmx.de>
2025-11-27 10:23:47 +0100

    chore: Extend description of codmods

--- packages/forma-36-codemod/bin/inquirer-choices.mjs
+++ packages/forma-36-codemod/bin/inquirer-choices.mjs
@@ -11,17 +11,17 @@ export const PARSER_CHOICES = [
 
 export const SETUP_CHOICES = [
   {
-    name: 'v6-popover: Update changed placement and offset properties of Popover, Menu and Tooltip',
+    name: 'v6-popper: Update changed placement and offset properties of Popover, Menu and Tooltip after migration from react-popper to floating-ui',
     short: 'Update popover',
     value: 'v6/popover',
   },
   {
-    name: 'v6-menu: Update usage on codebase.',
+    name: 'v6-menu: Update usage on codebase. This will replace the deprecated Submenu compound component with Menu.',
     short: 'Update menu',
     value: 'v6/menu',
   },
   {
-    name: 'v6-skeleton: Update usage on codebase.',
+    name: 'v6-skeleton: Update usage on codebase. It will remove deprecated PropTypes for SkeletonDisplayTextProps and SkeletonBodyTextProps and replace them with SkeletonTextProps',
     short: 'Update skeleton',
     value: 'v6/skeleton',
   },
