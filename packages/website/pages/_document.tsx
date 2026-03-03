9ded791c1868bb1bbe284b37d9c6c657055c8fb6
Kristoffer <denkristoffer@users.noreply.github.com>
2026-03-03 10:15:47 +0100

    chore(website): fix flash of unstyled content (#3358)

--- packages/website/pages/_document.tsx
+++ packages/website/pages/_document.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  type DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import * as snippet from '@segment/snippet';
 import { renderStatic } from '../utils/renderer';
 
 const { NEXT_PUBLIC_SEGMENT_API_KEY, NODE_ENV } = process.env;
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const page = await ctx.renderPage();
-    const { css, ids } = await renderStatic(page.html);
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
+    const { css, ids } = await renderStatic(initialProps.html);
+
     return {
       ...initialProps,
       styles: (
