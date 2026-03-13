14d6a6f5896901324651a4615c1359e7bdd24b37
Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>
2026-03-13 13:45:38 +0100

    chore(docs): display supported `as` types in the props table (#3365)

--- packages/f36-docs-utils/src/PropsTable/PropertyType/PropertyType.tsx
+++ packages/f36-docs-utils/src/PropsTable/PropertyType/PropertyType.tsx
@@ -23,9 +23,19 @@ interface PropertyTypeProps {
 }
 
 export function PropertyType({ type, name }: PropertyTypeProps) {
-  if (name === 'as') {
+  if (name === 'as' && type.value) {
+    const filteredValue = type.value.filter(
+      (item) => !item.value.includes('&'),
+    );
+    const sortedValue = filteredValue.sort((a, b) =>
+      a.value.localeCompare(b.value),
+    );
     return (
-      <PropertyValue value="HTML Tag or React Component (e.g. div, span, etc)" />
+      <>
+        {sortedValue.map(({ value }, idx) => {
+          return <PropertyValue key={idx} value={value} />;
+        })}
+      </>
     );
   }
 
