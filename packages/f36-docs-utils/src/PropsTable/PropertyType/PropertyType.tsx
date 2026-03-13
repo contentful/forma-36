bf648f63ca5007e7e8ca554561917981e161566b
Rémy Lenoir <remy.lenoir@contentful.com>
2026-03-13 13:13:54 +0100

    chore(docs): formatting

--- packages/f36-docs-utils/src/PropsTable/PropertyType/PropertyType.tsx
+++ packages/f36-docs-utils/src/PropsTable/PropertyType/PropertyType.tsx
@@ -24,8 +24,12 @@ interface PropertyTypeProps {
 
 export function PropertyType({ type, name }: PropertyTypeProps) {
   if (name === 'as') {
-    const filteredValue = type.value.filter((item) => !item.value.includes('&'));
-    const sortedValue = filteredValue.sort((a, b) => a.value.localeCompare(b.value));
+    const filteredValue = type.value.filter(
+      (item) => !item.value.includes('&'),
+    );
+    const sortedValue = filteredValue.sort((a, b) =>
+      a.value.localeCompare(b.value),
+    );
 
     return (
       <>
