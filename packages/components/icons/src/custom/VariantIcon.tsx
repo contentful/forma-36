dc9c81d9f4c770529852f8105bad11e66df80778
maroun-baydoun <maroun@ninetailed.com>
2026-04-24 16:55:25 +0200

    fix(icons): add .js extension to VariantIcon import

--- packages/components/icons/src/custom/VariantIcon.tsx
+++ packages/components/icons/src/custom/VariantIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { css, cx } from '@emotion/css';
 import type { GeneratedIconProps } from '@contentful/f36-icon';
-import { GitBranchIcon } from '../vendor/phosphor/GitBranchIcon';
+import { GitBranchIcon } from '../vendor/phosphor/GitBranchIcon.js';
 
 const rotatedClassName = css({
   transform: 'rotate(90deg)',
