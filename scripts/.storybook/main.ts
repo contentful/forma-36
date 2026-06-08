75127de3199e39ec9404bf2091c4182e958c87c4
Kathrin Holzmann <lelith@gmx.de>
2026-06-08 16:54:20 +0200

    chore: upgrade to storybook 10

--- scripts/.storybook/main.ts
+++ scripts/.storybook/main.ts
@@ -1,6 +1,10 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { createRequire } from 'node:module';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 import { join, dirname } from 'path';
+
+const require = createRequire(import.meta.url);
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -41,9 +45,6 @@ const config: StorybookConfig = {
         'packages/components/**/examples/',
       ],
     },
-  },
-  core: {
-    builder: getAbsolutePath('@storybook/builder-vite'),
   },
 };
 export default config;
