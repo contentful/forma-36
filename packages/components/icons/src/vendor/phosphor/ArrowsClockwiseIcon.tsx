ed1e486ff9ca5b6bc9ccbcfe4b09af191e4f4b63
Yves Rijckaert <yves.rijckaert@contentful.com>
2026-08-06 10:34:15 +0200

    feat(icons): add ArrowsClockwiseIcon [TOL-4331] (#3604)

---
+++ packages/components/icons/src/vendor/phosphor/ArrowsClockwiseIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon';
+import { ArrowsClockwiseIcon as ArrowsClockwise } from '@phosphor-icons/react';
+
+export const ArrowsClockwiseIcon = generateForma36Icon(ArrowsClockwise);
