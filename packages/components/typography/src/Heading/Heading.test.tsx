179570747d0bcbd87540ffe1558f564e3d88cd31
denkristoffer <denkristoffer@users.noreply.github.com>
2026-08-11 14:47:56 +0200

    chore: migrate typography package to Vitest

--- packages/components/typography/src/Heading/Heading.test.tsx
+++ packages/components/typography/src/Heading/Heading.test.tsx
@@ -1,12 +1,11 @@
+import { it } from 'vitest';
 import React from 'react';
 import { render } from '@testing-library/react';
-import { axe } from 'jest-axe';
+import { expectNoA11yViolations } from '@/scripts/test/expectNoA11yViolations';
 
 import { Heading } from './Heading';
 
 it('has no a11y issues', async () => {
   const { container } = render(<Heading>Heading</Heading>);
-  const results = await axe(container);
-
-  expect(results).toHaveNoViolations();
+  await expectNoA11yViolations(container);
 });
