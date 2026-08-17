7f953888963c2eaa18ad3d6eca3a2fbe46ce8d18
Khaalid <khaalidsubaan@gmail.com>
2026-08-17 15:37:56 +0200

    feat(f36-icons): add ScalesEvenIcon, SlidersHorizontalIcon, GraphIcon [NT-3981]

---
+++ packages/components/icons/src/vendor/phosphor/SlidersHorizontalIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon';
+import { SlidersHorizontalIcon as SlidersHorizontal } from '@phosphor-icons/react';
+
+export const SlidersHorizontalIcon = generateForma36Icon(SlidersHorizontal);
