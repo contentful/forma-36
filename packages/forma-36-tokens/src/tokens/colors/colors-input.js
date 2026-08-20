e229e1d3e91c2dae7fc11b5cbf9f49561c9ae6ad
MAhsenArif <ahsenarif92@gmail.com>
2026-08-20 10:05:39 +0200

    [AHOY-5112] Update TextInput value text color token from gray700 to gray900 (#3664)
    * feat(tokens): add semantic input color tokens and update TextInput/Select
    
    Introduce color-text-input-value (gray900) and color-text-input-defaultvalue (gray500)
    as semantic tokens. Update BaseInput and Select styles to use them, replacing the
    direct gray700/gray500 references. Cascades to Textarea, Autocomplete, and MultiSelect
    via BaseInput/TextInput.
    
    * refactor(tokens): rename color-text-input-defaultvalue to color-text-input-placeholder
    
    * chore: add changeset for AHOY-5112

---
+++ packages/forma-36-tokens/src/tokens/colors/colors-input.js
@@ -0,0 +1,8 @@
+const colorsGray = require('./colors-gray');
+
+const colorsInput = {
+  'color-text-input-value': colorsGray['gray-900'],
+  'color-text-input-placeholder': colorsGray['gray-500'],
+};
+
+module.exports = colorsInput;
