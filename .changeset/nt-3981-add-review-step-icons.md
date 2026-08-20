ba11c76c13f11d8c69f80973465dfa5167d2dc8f
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2026-08-20 14:51:21 +0200

    feat(f36-icons): add ScalesEvenIcon, SlidersHorizontalIcon, GraphIcon [NT-3981] (#3663)
    * feat(f36-icons): add ScalesEvenIcon, SlidersHorizontalIcon, GraphIcon [NT-3981]
    
    * update:readmen changeset

---
+++ .changeset/nt-3981-add-review-step-icons.md
@@ -0,0 +1,5 @@
+---
+'@contentful/f36-icons': minor
+---
+
+Add SlidersHorizontalIcon and GraphIcon from Phosphor and add custom ScalesEvenIcon with Default/Active variants (NT-3981)
---
+++ packages/components/icons/src/vendor/phosphor/GraphIcon.tsx
@@ -0,0 +1,4 @@
+import { generateForma36Icon } from '@contentful/f36-icon';
+import { GraphIcon as Graph } from '@phosphor-icons/react';
+
+export const GraphIcon = generateForma36Icon(Graph);
