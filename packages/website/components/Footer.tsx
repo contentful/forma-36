e3275820c0287e41bd31795964f13ef46ff2d674
Rémy Lenoir <103024358+cf-remylenoir@users.noreply.github.com>
2026-09-01 22:10:09 +0200

    docs(website): update footer logo (#3666)

--- packages/website/components/Footer.tsx
+++ packages/website/components/Footer.tsx
@@ -5,7 +5,7 @@ import { css, cx } from '@emotion/css';
 import tokens from '@contentful/f36-tokens';
 import { TextLink, Flex, type TextLinkProps } from '@contentful/f36-components';
 
-import contentfulLogoSVG from '../resources/icons/contentful-logo.svg';
+import contentfulLogoSVG from '../resources/icons/contentful-by-salesforce-logo.svg';
 import { getGridStyles } from '../utils/getGridStyles';
 import { ArrowSquareOutIcon } from '@contentful/f36-icons';
 
@@ -82,8 +82,8 @@ export function Footer() {
         rel="noopener noreferrer"
       >
         <Image
-          width={120}
-          height={25}
+          width={140}
+          height={36}
           src={contentfulLogoSVG}
           alt="Contentful’s logo"
         />
