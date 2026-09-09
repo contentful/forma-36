2499c10527672273d43c7121aaad4973fcfabe79
Rebecca König <rebecca.koenig@contentful.com>
2026-09-09 09:02:49 +0200

    chore: update tsdown config file name

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,7 @@ export default [
       '*.css.d.ts',
       'public/**',
       '.cache/**',
-      '**/tsdown.config.ts',
+      '**/tsdown.config.mts',
       '**/__testfixtures__',
       '**/.next/**',
       '**/.turbo/**',
